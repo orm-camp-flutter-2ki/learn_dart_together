b4cefe4886c9c7bcf3adb2aef839a11db9ae8d8e
hwangtaewook <hwangxodnr@nate.com>
2024-03-07 12:29:17 +0900

    클레릭의 기본 속성을 정의했습니다.

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -1,1 +1,17 @@
-class Cleric {}
+class Cleric {
+  String name;
+  int hp = 50;
+
+  int maxHp = 50;
+
+  int mp = 10;
+
+  int maxMp = 10;
+
+  Cleric(this.name, this.hp, this.maxHp, this.mp, this.maxMp);
+
+  void a() {
+    print('a');
+  }
+}
+//  int hp >50 ? hp =maxHp : hp;
