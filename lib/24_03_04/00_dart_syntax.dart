e1ddd53d78e31838323b093e8cc991e2db59aa70
hyojung.sung <hyo8967@naver.com>
2024-03-07 15:06:26 +0900

    3번

--- lib/24_03_04/00_dart_syntax.dart
+++ lib/24_03_04/00_dart_syntax.dart
@@ -1,3 +1,20 @@
-void main() {
-  print('hello world');
+void main() {Cleric().selfAid();}
+
+class Cleric {
+  String? name;
+  int hp = 50;
+  final int maxHp = 50;
+  int mp = 10;
+  final int maxMp = 10;
+
+
+  void selfAid() {
+    if (hp < 50) {
+      mp -= 5;
+      hp == maxHp;
+
+    }
+    print(hp);
+    print(mp);
+  }
 }
