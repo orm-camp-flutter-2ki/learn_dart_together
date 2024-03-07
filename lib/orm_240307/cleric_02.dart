b11484c8104a03623fd3f50467b0f02a13b06008
BAUu <yimej07@gmail.com>
2024-03-07 15:27:03 +0900

    fix: max hp, max mp 상수로 변환

--- lib/orm_240307/cleric_02.dart
+++ lib/orm_240307/cleric_02.dart
@@ -2,8 +2,8 @@ class Cleric {
   String name;
   int hp;
   int mp;
-  int maxhp=50;
-  int maxmp=10;
+  final int maxhp=50;
+  final int maxmp=10;
   Cleric(this.name, this.hp, this.mp);
 
   void selfAid(){
