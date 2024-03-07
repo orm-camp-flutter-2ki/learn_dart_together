b3ceae92059f158221d8eb0f3b9e79c6799a0e0c
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-07 14:25:29 +0900

    feat: 클레릭의 기본 속성을 정의했습니다.

--- lib/0307/00_cleric.dart
+++ lib/0307/00_cleric.dart
@@ -1,3 +1,12 @@
 class Cleric { // class 생성
+  String name; // field
+  int hp;
+  int mp;
+  final int maxHp = 50; // field, 상수
+  final int maxMp = 10;
+
+  // 생성자 : 인스턴스를 만드는 것
+  // this. : 나의.name, 나의.hp
+  Cleric(this.name, this.hp, this.mp);
 
 }
