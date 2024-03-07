ede1fa383f9a1a0dc9d6440cfa4ef7db854cb661
Stranger Yang <ccamdol@gmail.com>
2024-03-07 12:43:46 +0900

    feat: Cleric 속성 추가
    - 속성 5새 추가 완료

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -1,3 +1,10 @@
 class Cleric(){
+  String name();
+  int hp;
+  int mp;
+  int maxhp;
+  int maxmp;
 
+  final int maxhp = 50;
+  final int maxmp = 10;
 }
--- lib/240307/hero.dart
+++ lib/240307/hero.dart
@@ -1,8 +1,15 @@
-class Hero(){
+class Hero {
   String name;
   int hp;
 
+  Hero(this.name, this.hp);
+
   void attack(){}
   void run(){}
-  void sleep(){}
+  void sleep(){
+    hp = 100;
+    print(hp);
+    print('$name 은 잠을 자고 회복했다');
+    print(hp);
+  }
 }
