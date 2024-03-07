cc7a2b8a5c0dc203cbd3c8a64845afba4ed7a99c
icicle_4 <dwayne.han4@gmail.com>
2024-03-07 14:57:38 +0900

    연습문제4. main함수 분리, pray() 추가

--- lib/24_03_04/240307/cleric.dart
+++ lib/24_03_04/240307/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   /**
    * 커밋 메시지
@@ -5,20 +7,38 @@ class Cleric {
    * 클레릭의 능력을 구현했습니다.
    * 클레릭을 문자열로 표현하는 기능을 추가했습니다.
    */
-  String name = '메딕';
-  int level = 10;
+  String name;
   int hp;
   int mp;
   final int maxHp = 50;
   final int maxMp = 10;
 
-  String suffix;
+  int mpRecover = 0;
 
-  Cleric(this.hp, this.mp, this.suffix);
+  Cleric(this.name, this.hp, this.mp);
 
-  void selfAid(){
-    mp -= 5;
+  void selfAid() {
+    if (mp >= 5) {
+      mp -= 5;
+    }else{
+      print('mp가 부족합니다. \nselfAid가 발동되지 않았습니다.');
+    }
     hp = 50;
   }
 
+  int pray(int seconds) {    
+
+    int ran = Random().nextInt(3);
+    int mpRecover = 0;
+    //print('ran: $ran');
+    mpRecover += seconds+ran;    
+    //print('mpRecover: $mpRecover');
+    
+    if((mp+mpRecover) >= 10){
+      mp = 10;
+    }else{
+      mp += mpRecover;
+    }
+    return mpRecover;
+  }
 }
