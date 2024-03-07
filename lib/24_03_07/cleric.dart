5d4a8ecdbee12c3645808ffc16ee0f6dbac57cfa
JEON-Sungsu <anagma@naver.com>
2024-03-07 15:36:31 +0900

    refactor: pray method refactoring
    - else 구문 삭제 및 리팩토링

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -1,12 +1,23 @@
 import 'dart:math';
 
 void main() {
-  Cleric itsMe = Cleric('Howard');
+  Cleric cleric  = Cleric('홍길동');
 
-  itsMe.selfAid();
-  itsMe.decreaseStatus(5, 5);
-  print('MP회복량: ${itsMe.pray(second: 3)}');
-  print('MP회복량: ${itsMe.pray(second: 5)}');
+  print('===== 클래릭 생성 =====');
+  print('${cleric.hp}, ${cleric.mp}');
+
+  for (int i = 0; i < 3; i++) {
+    cleric.selfAid();
+    print('===== selfAid() 발동 $i =====');
+    print('${cleric.hp}, ${cleric.mp}');
+  }
+
+  for (int i = 0; i < 3; i++) {
+    print('===== pray() 발동 $i =====');
+    int recoveryMp = cleric.pray(second: 5);
+    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
+  }
+
 }
 
 class Cleric {
@@ -19,37 +30,47 @@ class Cleric {
   Cleric(this.name);
 
   void selfAid() {
-    print('셀프힐 시전');
-    if (this.hp < this.maxHp && this.mp >= 5) {
+    if (this.mp < 5) {
+      print('MP가 부족합니다.');
+    } else if (this.hp == this.maxHp) {
+      print('이미 체력이 가득 찼습니다.');
+    } else {
       this.hp = this.maxHp;
       this.mp -= 5;
       print('HP가 모두 회복되었습니다.');
-    } else if (this.mp < 5){
-      print('MP가 부족합니다.');
-    } else {
-      print('이미 체력이 가득 찼습니다.');
     }
   }
 
   int pray({int second = 1}) {
-    print('$second 초간 기도 시작');
-    int randomPoint = Random().nextInt(3);
-    int chargedMp = second + randomPoint;
+    int chargedMp = second + Random().nextInt(3);
 
     if (this.mp + chargedMp <= this.maxMp) {
       this.mp = this.mp + chargedMp;
+
       return chargedMp;
-    } else {
-      int currentMp = this.mp;
-      this.mp = this.maxMp;
-      return this.maxMp - currentMp;
     }
+
+    chargedMp = this.maxMp - this.mp;
+    this.mp = this.maxMp;
+
+    return chargedMp;
+
   }
 
   //테스트용 메서드
-  void decreaseStatus(int hp, int mp) {
-    this.hp -= hp;
-    this.mp -= mp;
+  void decreaseStatus({int hp = 1, int mp = 1}) {
+    if (this.hp > hp) {
+      this.hp -= hp;
+    }
+
+    if (this.mp > mp) {
+      this.mp -= mp;
+    }
+
     print('HP $hp 감소, MP $mp 감소');
+  }
+
+  void showCurrentStatus() {
+    print('현재체마 HP: ${this.hp}, MP: ${this.mp}');
   }
 }
