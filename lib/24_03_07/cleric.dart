37c734d821991a54650fb12e045e65521ba754b5
Iris.Park <hellopark821@gmail.com>
2024-03-07 12:32:12 +0900

    feat: selfAid 메소드 추가

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -13,4 +13,16 @@ Class Cleric(){
   final int maxMp =10;
 
   Cleric(this.name, this.hp, this.maxHp, this.mp, this.maxMp)
+
+  void selfAid(){
+    mp -= 5;
+    hp = maxHp;
+  }
 }
+
+/*
+성직자는 “셀프 에이드" 라는 마법을 사용할 수 있고, MP를 5소비하는 것으로 자신의 HP 를 최대 HP 까지 회복할 수 있다.
+연습 1-2 에 선언한 Cleric 클래스에 “selfAid()” 메소드를 추가 하시오.
+또한, 이 메소드는 인수가 없고, 리턴 값도 없다.
+*/
+
