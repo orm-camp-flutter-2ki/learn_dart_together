603ed97cdbd5d06893b999b6e2d8d56d1c8a215c
chojja7188 <chojja7188@gmail.com>
2024-03-07 12:33:32 +0900

    feat: Cleric 클래스 메소드 추가
    - Cleric 클래스에 pray 메소드 추가

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   String name; // 이름
   int hp; // HP
@@ -10,5 +12,15 @@ class Cleric {
   void selfAid() {
     mp -= 5;
     hp = maximumHp;
+  }
+
+  int pray(int second) {
+    int healedMp = Random().nextInt(2) + second;
+
+    if (healedMp > maximumMp - mp) {
+      healedMp = maximumMp - mp;
+    }
+
+    return healedMp;
   }
 }
