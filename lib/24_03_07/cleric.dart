6d445bc2ff32540debf21effae786d674323b1aa
JEON-Sungsu <anagma@naver.com>
2024-03-07 12:50:16 +0900

    feat: pray method add
    - 실제 회복량 수식 리팩필요할듯

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -1,7 +1,13 @@
 import 'dart:math';
 
 void main() {
+  Cleric itsMe = Cleric('Howard');
 
+  itsMe.selfAid();
+  print('현재 상태 - HP: ${itsMe.hp}, MP: ${itsMe.mp}');
+  print('${itsMe.pray(second: 3)}만큼 MP를 회복했습니다.');
+  print('현재 상태 - HP: ${itsMe.hp}, MP: ${itsMe.mp}');
+  print('${itsMe.pray(second: 3)}만큼 MP를 회복했습니다.');
 }
 
 class Cleric {
@@ -14,10 +20,26 @@ class Cleric {
   Cleric(this.name);
 
   void selfAid() {
-    if (this.hp < this.maxHp) {
+    if (this.hp < this.maxHp && this.mp > 2) {
       this.hp = this.maxHp;
+      this.mp -= 2;
     } else {
-      print('이미 체력이 가득 찼습니다.')
+      print('이미 체력이 가득 찼습니다.');
+    }
+  }
+
+  int pray({int second = 1}) {
+    var random = Random();
+    int randomPoint = random.nextInt(2);
+    int chargedMp = second + randomPoint;
+
+    if (this.mp + chargedMp <= this.maxMp) {
+      this.mp = this.mp + chargedMp;
+      return chargedMp;
+    } else {
+      int currentMp = this.mp;
+      this.mp = this.maxMp;
+      return this.maxMp - currentMp;
     }
   }
 }
