c38082fdf6814864dfaf536a0fd86fd229a2b133
hoogom88 <ziziwhdgus@naver.com>
2024-03-07 12:53:53 +0900

    Feat. 클래릭 HP, MP 관련 메서드 추가

--- lib/24_03_07/class_hw.dart
+++ lib/24_03_07/class_hw.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   static const int maxHp = 50;
   static const int maxMp = 10;
@@ -7,5 +9,31 @@ class Cleric {
   int mp = maxMp;
 
   Cleric(this.name);
+
+  bool changeMp(int value) {
+    int origin = mp;
+
+    if (mp + value < 0) {
+      print(" MP가 부족합니다.");
+      return false;
+    }
+
+    mp = min(maxMp, mp + value);
+    print(" MP $origin => $mp");
+    return true;
+  }
+
+  bool changeHp(int value) {
+    int origin = hp;
+
+    if (hp + value < 0) {
+      print("사망하였습니다.");
+      return false;
+    }
+
+    hp = min(maxHp, hp + value);
+    print(" HP $origin => $hp");
+    return true;
+  }
 
 }
