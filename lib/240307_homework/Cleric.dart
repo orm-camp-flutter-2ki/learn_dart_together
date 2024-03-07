cfadf57b3613bbf30f8aa06f586c2b30154032fa
Yejin.Shin <mingmi_27@naver.com>
2024-03-07 16:29:30 +0900

    feat: add Cleric class

--- lib/240307_homework/Cleric.dart
+++ lib/240307_homework/Cleric.dart
@@ -7,9 +7,27 @@
  */
 
 import 'dart:math';
-void main(){
-  print(Random().nextInt(3) + 5);
+
+void main() {
+  Cleric cleric = Cleric('홍길동', 50, 10);
+
+  print('===== 클래릭 생성 =====');
+  print('${cleric.hp}, ${cleric.mp}');
+
+  for (int i = 0; i < 3; i++) {
+    print('===== selfAid() 발동 $i =====');
+    cleric.selfAid();
+    print('${cleric.hp}, ${cleric.mp}');
+  }
+
+  for (int i = 0; i < 3; i++) {
+    print('===== pray() 발동 $i =====');
+    int recoveryMp = cleric.pray(5);
+    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
+  }
 }
+
+
 class Cleric {
   String name;
   int hp = 50;
@@ -19,15 +37,39 @@ class Cleric {
 
   Cleric(this.name, this.hp, this.mp);
 
-  void selfAid(){
-    mp - 5;
-    hp = hpMax;
-  }
-  void pray(int sec){
-    if(mp == mpMax){} else {
-      var intValue = Random().nextInt(3) + sec; // Value is >= 0 and < 10.
-
+  void selfAid() {
+    if (mp < 5) {
+      print('마나가 없습니다.스킬을 쓸 수 없습니다.');
+    } else {
+      mp -= 5;
+      hp = hpMax;
     }
+  }
 
+  int pray(int sec) {
+    int healedMP = 0;
+    //sec 만큼 반복
+    for (int i = 1; i <= sec; i++) {
+      //mp가 최대가 아닐때
+      if (mp < mpMax) {
+        int randomRecovery = Random().nextInt(3);
+        if ((randomRecovery + mp) >= mpMax) {
+          int mpOver = (randomRecovery + mp)- mpMax;
+          healedMP += randomRecovery - mpOver;
+          mp += randomRecovery - mpOver;
+          break;
+        } else {
+          healedMP += randomRecovery;
+          //추가
+          mp += randomRecovery;
+        }
+      }
+      //mp가 최대일때
+      else {
+        print('마나가 다 찼습니다.');
+        break;
+      }
+    }//for
+    return healedMP;
   }
 }
