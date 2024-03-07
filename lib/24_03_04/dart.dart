8ca13c16c458749f69fd38a43bd7620bafe86ff0
hyojung.sung <hyo8967@naver.com>
2024-03-07 14:48:11 +0900

    성직자는 “셀프 에이드" 라는 마법을 사용할 수 있고, MP를 5소비하는 것으로 자신의 HP 를 최대 HP 까지 회복할 수 있다.

--- lib/24_03_04/dart.dart
+++ lib/24_03_04/dart.dart
@@ -5,7 +5,12 @@ class Cleric {
   int mp = 10;
   final int maxMp = 10;
 
+  Cleric(this.name, this.hp);
 
-  // 연습 1에서 작성한 내용이 없는 Cleric 클래스에 “이름", “HP”, “최대 HP”, “MP”, “최대 MP”를 속성으로 추가 하시오.
-  // 또한 HP와 최대 HP는 정수로 초기치 50, MP와 최대 MP는 정수로 초기치 10으로 하고,
-  // 최대 HP와 최대 MP는 상수 필드로 선언 하시오.
+  void selfAid() {
+    if (hp < 50) {
+      mp -= 5;
+      hp = maxHp;
+    }
+  }
+}
