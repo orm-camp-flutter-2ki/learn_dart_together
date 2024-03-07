0eb8e6a5e44e8dff21e25c03e6513b2f2902c529
YunZi.Song <yoonji5809@gmail.com>
2024-03-07 15:51:37 +0900

    fix: selfAid, pray 함수 수정

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -17,6 +17,7 @@ class Cleric {
   // 셀프에이드 마법. mp를 5 소비하고 hp를 최대로 회복한다.
   // 인수도 리턴값도 없음.
   void selfAid() {
+    if (mp < 0) return;
     mp -= 5;
     hp = maxHp;
     if (mp < 0) {
@@ -24,17 +25,17 @@ class Cleric {
     }
   }
 
-  // 기도하기. 자신의 mp를 회복한다. 회복량은 기도한 시간에 랜덤하게 0~2포인트의 보정을 한 양.(3초 기도시 0~5).
+  // 기도하기. 자신의 mp를 회복한다. 회복량은 기도한 시간에 랜덤하게 0~2포인트의 보정을 한 양.(3초 기도시 3~5).
   // 최대 mp보다 더 회복하는 것은 불가능.
   // 인수에 기도할 시간(초) 지정, 리턴값은 실제 회복된 mp 양을 반환
   int pray(int second) {
     int prevMp = mp;
-    int plusPoint= second * Random().nextInt(3); // 0,1,2 랜덤
+    int plusPoint = Random().nextInt(3); // 0,1,2 랜덤
     int healPoint = second + plusPoint;
     mp += healPoint;
-    if (mp > maxMp) {
+    if (mp >= maxMp) {
       mp = maxMp;
     }
-    return mp-prevMp;
+    return mp - prevMp;
   }
 }
