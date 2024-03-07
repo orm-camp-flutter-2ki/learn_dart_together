8b683a8c098cd1ca5d58991f0a86b910d37b73da
hee462 <a01080809887@gmail.com>
2024-03-07 15:00:21 +0900

    test:  main() test

--- lib/230307/cleric.dart
+++ lib/230307/cleric.dart
@@ -1,5 +1,22 @@
 import 'dart:math';
+void main() {
+  Cleric cleric = Cleric('홍길동', 50, 10);
 
+  print('===== 클래릭 생성 =====');
+  print('${cleric.hp}, ${cleric.mp}');
+
+  for (int i = 0; i < 3; i++) {
+    cleric.selfAid();
+    print('===== selfAid() 발동 $i =====');
+    print('${cleric.hp}, ${cleric.mp}');
+  }
+
+  for (int i = 0; i < 3; i++) {
+    print('===== pray() 발동 $i =====');
+    int recoveryMp = cleric.pray(5);
+    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
+  }
+}
 
 
 class Cleric {
