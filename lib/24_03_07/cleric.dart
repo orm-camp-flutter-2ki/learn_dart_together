c1fb8c78ecf59d93c687ded87aef6ed75ad87117
JEON-Sungsu <anagma@naver.com>
2024-03-07 14:21:58 +0900

    fix: mp consume count modify
    - selfAid시 마력소모값 수정
    - test용 체력, 마력 감소 메서드 추가
    - 메서드별 동작알림 프린트 추가

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -4,10 +4,9 @@ void main() {
   Cleric itsMe = Cleric('Howard');
 
   itsMe.selfAid();
-  print('현재 상태 - HP: ${itsMe.hp}, MP: ${itsMe.mp}');
-  print('${itsMe.pray(second: 3)}만큼 MP를 회복했습니다.');
-  print('현재 상태 - HP: ${itsMe.hp}, MP: ${itsMe.mp}');
-  print('${itsMe.pray(second: 3)}만큼 MP를 회복했습니다.');
+  itsMe.decreaseStatus(5, 5);
+  print('MP회복량: ${itsMe.pray(second: 3)}');
+  print('MP회복량: ${itsMe.pray(second: 5)}');
 }
 
 class Cleric {
@@ -20,17 +19,21 @@ class Cleric {
   Cleric(this.name);
 
   void selfAid() {
-    if (this.hp < this.maxHp && this.mp > 2) {
+    print('셀프힐 시전');
+    if (this.hp < this.maxHp && this.mp >= 5) {
       this.hp = this.maxHp;
-      this.mp -= 2;
+      this.mp -= 5;
+      print('HP가 모두 회복되었습니다.');
+    } else if (this.mp < 5){
+      print('MP가 부족합니다.');
     } else {
       print('이미 체력이 가득 찼습니다.');
     }
   }
 
   int pray({int second = 1}) {
-    var random = Random();
-    int randomPoint = random.nextInt(2);
+    print('$second 초간 기도 시작');
+    int randomPoint = Random().nextInt(3);
     int chargedMp = second + randomPoint;
 
     if (this.mp + chargedMp <= this.maxMp) {
@@ -41,5 +44,12 @@ class Cleric {
       this.mp = this.maxMp;
       return this.maxMp - currentMp;
     }
+  }
+
+  //테스트용 메서드
+  void decreaseStatus(int hp, int mp) {
+    this.hp -= hp;
+    this.mp -= mp;
+    print('HP $hp 감소, MP $mp 감소');
   }
 }
