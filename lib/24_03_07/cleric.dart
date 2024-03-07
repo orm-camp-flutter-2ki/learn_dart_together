01c668a2faa6d3eee43eda13e3e012ed1d8c9093
David Kim <mycodingtool9@gmail.com>
2024-03-07 17:02:46 +0900

    연습문제 4 feat : pray 함수 추가했습니다. - main 함수에 print문 보강했습니다.

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -1,37 +1,58 @@
 import 'dart:math';
 
-void main() {
-  Cleric().pray(3);
-}
-
 class Cleric {
-  String? name;
-
-  int hp = 50;
-  int mp = 10;
+  String name;
+  int hp;
+  int mp;
 
   final int maxHp = 50;
   final int maxMp = 10;
 
+  Cleric(this.name, this.hp, this.mp);
+
   void selfAid() {
-    if (hp < 50) {
+    if (hp > 5) {
       mp -= 5;
-      hp == maxHp;
+      hp = maxHp;
     }
+    print('현재 HP : $hp , 현재 MP : $mp');
+
   }
 
   int pray(int praySec) {
-    int healAmount;
+    int healAmount = 0;
     int mpWithHealAmount;
-    int randomPoint = Random().nextInt(2);
+    int randomPoint = Random().nextInt(3);
 
     healAmount = randomPoint + praySec;
 
     mpWithHealAmount = mp + healAmount;
 
-    if(mpWithHealAmount < maxMp){
+    if (mpWithHealAmount < maxMp) {
       mp += healAmount;
     }
     return healAmount;
   }
+}
+
+void main() {
+  Cleric cleric = Cleric('홍길동', 50, 10);
+
+  print('======= 크레릭 생성 =======');
+  print('초기 HP : ${cleric.hp} , 초기 MP : ${cleric.mp}');
+  print('');
+
+
+  for(int i = 0; i<2; i++) {
+    print('======= selfAid() $i번 발동  =======');
+    // print('현재 HP는 :${cleric.selfAid()}');
+    cleric.selfAid();
+  }
+  print('');
+
+  for(int i = 0; i<3; i++) {
+    print('======= pray() $i번 발동  =======');
+    print('현재 HP : ${cleric.hp} , 현재 MP : ${cleric.mp} , 회복량 : ${cleric.pray(3)}');
+  }
+
 }
