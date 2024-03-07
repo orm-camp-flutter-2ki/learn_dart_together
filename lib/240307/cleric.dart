803ecce1f480d0beb83cecce068609115d23bccb
jin song <songjin3914@gmail.com>
2024-03-07 14:52:44 +0900

    Feat: 클레릭 클래스 함수 추가 및 클래릭 함수 테스트 완료
    - pray 함수 작성완료 및 테스트 완료

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   String name;
   int hp;
@@ -5,15 +7,50 @@ class Cleric {
   final int maxHp = 50;
   final int maxMp = 10;
 
-  Cleric( this.name,this.hp , this.mp);
+  Cleric( this.name ,this.hp , this.mp);
 
   void selfAid(){
     if(mp<5){
       print("mp가 부족합니다");
     }else{
-      mp = mp - 5
-      hp = maxHp
+      mp = mp - 5;
+      hp = maxHp;
     }
   }
 
+  int pray(int second){
+   int heal = second + Random().nextInt(3);
+
+   int healPoint = 0;
+   if(mp + heal > 10){
+     healPoint = 10 - mp;
+     mp = 10;
+   }else{
+     mp = mp + heal;
+     healPoint = heal;
+   }
+   return healPoint;
+  }
+
 }
+
+
+void main() {
+  Cleric cleric = Cleric('홍길동', 50, 10);
+
+  print('===== 클래릭 생성 =====');
+  print('${cleric.hp}, ${cleric.mp}');
+
+  for (int i = 0; i < 3; i++) {
+    cleric.selfAid();
+    print('===== selfAid() 발동 $i =====');
+    print('${cleric.hp}, ${cleric.mp}');
+  }
+
+  for (int i = 0; i < 3; i++) {
+    print('===== pray() 발동 $i =====');
+    int recoveryMp = cleric.pray(5);
+    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
+  }
+}
+
