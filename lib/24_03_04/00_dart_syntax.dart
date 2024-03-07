8f584d39a2ef72dab91dc6335eca4ba13ffc5081
kaggle68 <kaggle68@gmail.com>
2024-03-07 16:47:31 +0900

    클래스의 기본 속성 정의

--- lib/24_03_04/00_dart_syntax.dart
+++ lib/24_03_04/00_dart_syntax.dart
@@ -1,1 +1,15 @@
-class Cleric {}
+class Cleric {
+  String name;
+
+  int HP;
+  int MP;
+
+  final int maxHP = 50;
+  final int maxMP = 10;
+
+  Cleric(
+    this.name,
+    this.HP,
+    this.MP,
+  );
+}
