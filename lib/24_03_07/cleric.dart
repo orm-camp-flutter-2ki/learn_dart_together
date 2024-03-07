70868b3c3c8280c74fffea0dda6dcffa5df0fdef
David Kim <mycodingtool9@gmail.com>
2024-03-07 14:30:09 +0900

    feat: Cleric 클래스에 selfAid함수 추가 - mp를 5소비하면 hp를 최대로 회복하는 메서드 구현했습니다.

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -1,8 +1,20 @@
-class Cleric {
- String? name;
- int hp = 50;
- final int maxHp = 50;
- int mp = 10;
- final int maxMp = 10;
+void main() {
+  Cleric().selfAid();
+}
 
+class Cleric {
+  String? name;
+
+  int hp = 50;
+  int mp = 10;
+
+  final int maxHp = 50;
+  final int maxMp = 10;
+
+  void selfAid() {
+    if (hp < 50) {
+      mp -= 5;
+      hp == maxHp;
+    }
+  }
 }
