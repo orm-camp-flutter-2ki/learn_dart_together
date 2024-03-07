ad9c056d14af5ec3954f02323a00f09146118dc9
YunZi.Song <yoonji5809@gmail.com>
2024-03-07 12:45:32 +0900

    feat: cleric 클래스에 selfAid 함수 추가

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -3,12 +3,19 @@
 /// hp,최대 hp는 초기치 정수 50
 /// mp,최대 mp는 초기치 정수 10
 /// 최대 hp, 최대 mp는 상수
-class Cleric{
+class Cleric {
   String name;
-  int hp=50;
-  int mp =10;
-  static const int maxHp=50;
-  static const int maxMp=10;
+  int hp = 50;
+  int mp = 10;
+  static const int maxHp = 50;
+  static const int maxMp = 10;
 
-  Cleric(this.name,this.hp,this.mp);
+  Cleric(this.name, this.hp, this.mp);
+
+  // 셀프에이드 마법. mp를 5 소비하고 hp를 최대로 회복한다.
+  // 인수도 리턴값도 없음.
+  selfAid() {
+    mp -= 5;
+    hp = maxHp;
+  }
 }
