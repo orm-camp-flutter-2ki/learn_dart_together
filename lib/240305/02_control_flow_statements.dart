52579bb70a8e3ce1001b5a9c0720f6b82a7b6915
KimDonghyeok <qoophon@gmail.com>
2024-03-07 12:10:27 +0900

    chore: Dart 기본 문법 학습
    - 00_hello_world.dart 파일 생성 및 작성
    - 01_variables.dart 파일 생성 및 작성
    - 02_control_flow_statements.dart 파일 생성 및 작성
    - 03_functions.dart 파일 생성 및 작성

---
+++ lib/240305/02_control_flow_statements.dart
@@ -0,0 +1,23 @@
+void main() {
+  var year = 1977;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+  // control flow statements
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (final object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
+}
