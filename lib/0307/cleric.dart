d58c5a3a48c0d3ba407a9542be717ab68f97f060
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-07 19:34:38 +0900

    feat: add pray method to cleric class

--- lib/0307/cleric.dart
+++ lib/0307/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
 
   late String name;
@@ -10,11 +12,14 @@ class Cleric {
     this.name = name;
     this.hp = this.maxHp;
     this.mp = this.maxMp;
-    print('===== 클래릭 생성 =====');
+
+    print('===== 클레릭 "${this.name}" 생성 =====');
     print('초기 HP: ${this.hp}, 초기 MP: ${this.mp}');
   }
 
   void selfAid(int cnt) {
+    print('===== selfAid() 발동 $cnt =====');
+
     if (this.mp < 5) {
       print('HP 회복에 필요한 MP 5가 없습니다.');
       return;
@@ -26,7 +31,20 @@ class Cleric {
     this.mp -= 5;
     this.hp = this.maxHp;
 
-    print('===== selfAid() 발동 $cnt =====');
     print('${this.hp}, ${this.mp}');
+  }
+
+  void pray(int second, int cnt) {
+    print('===== pray() 발동 $cnt =====');
+
+    if (this.mp == this.maxMp) {
+      print('MP가 이미 최댓값입니다.');
+      return;
+    }
+
+    int recovery = second + Random().nextInt(3);
+    this.mp = min(this.mp + recovery, this.maxMp);
+
+    print('${this.hp}, ${this.mp}, 회복량: $recovery');
   }
 }
