3f4b0d6a88a7b655b509e5451360ffcf363c1b03
BAUu <yimej07@gmail.com>
2024-03-07 15:18:15 +0900

    feat: 클레릭 클레스 추가
    클레릭의 기본 속성을 정의했습니다.
    클레릭의 능력을 구현하였습니다.
    클레릭의 hp, mp 상태를 터미널로 확인 할수 있게 문자열 구성하였습니다.

--- lib/orm_240307/cleric.dart
+++ lib/orm_240307/cleric.dart
@@ -1,9 +1,29 @@
+import 'dart:math';
+
 class Cleric{
   String name;
   int hp;
   int mp;
-  final maxhp;
-  final maxmp;
-  Cleric(this.name, this.hp, this.mp, this.maxhp, this.maxmp);
+  int maxhp =50;
+  int maxmp =10;
+  Cleric(this.name, this.hp, this.mp);
+
+  void selfAid(){
+    mp -= 5;
+    hp = maxhp;
+  }
+
+  int pray(a){
+    int sec = a;
+    int recovery = sec + Random().nextInt(3);
+    int b= -(mp-maxmp);
+    mp += recovery ;
+    if(mp >= maxmp){
+      recovery = b;
+      mp = maxmp;
+    }
+    return recovery;
+  }
+
 
 }
---
+++ lib/orm_240307/main.dart
@@ -0,0 +1,23 @@
+import 'cleric.dart';
+
+void main() {
+  Cleric cleric = Cleric('홍길동', 50, 10);
+
+  print('===== 클래릭 생성 =====');
+  print('${cleric.hp}, ${cleric.mp}');
+
+  for (int i = 0; i < 3; i++) {
+    cleric.selfAid();
+    print('===== selfAid() 발동 ${i+1} =====');
+    print('${cleric.hp}, ${cleric.mp}');
+    if(cleric.mp == 0){
+      break;
+    }
+  }
+
+  for (int i = 0; i < 3; i++) {
+    print('===== pray() 발동 ${i+1} =====');
+    int recoveryMp = cleric.pray(5);
+    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
+  }
+}
