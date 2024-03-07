5c9262300ecb806bca2d41df0fa6d89ee3ab69e6
leepl <yplunap25@gmail.com>
2024-03-07 13:54:00 +0900

    feat: 클레릭 클래스 추가
    -클레릭의 기본 속성을 정의했습니다.
    -클레릭의 selfAid 능력을 구현했습니다.

--- lib/24_03_07/00_practice01.dart
+++ lib/24_03_07/00_practice01.dart
@@ -1,1 +1,43 @@
-class Cleric {}
+void main() {
+  Cleric cleric = Cleric('홍길동', 100, 10);
+
+  print('===== 클래릭 생성 =====');
+  print('${cleric.hp}, ${cleric.mp}');
+
+  for (int i = 0; i < 3; i++) {
+    cleric.selfAid();
+    print('===== selfAid() 발동 $i =====');
+    print('${cleric.hp}, ${cleric.mp}');
+  }
+
+  // for (int i = 0; i < 3; i++) {
+  //   print('===== pray() 발동 $i =====');
+  //   int recoveryMp = cleric.pray(5);
+  //   print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
+  // }
+}
+
+class Cleric {
+  String name;
+
+  int hp;
+  final int maxHp = 50;
+  int mp;
+  final int maxMp = 10;
+
+  Cleric(this.name, this.hp, this.mp);
+
+  void selfAid() {
+    if (mp == 10) {
+      mp = maxMp - 5;
+      hp = maxHp;
+    } else if (5 <= mp && mp < 10) {
+      mp -= 5;
+      hp = maxHp;
+    } else if (mp < 5) {
+      print('MP가 부족하여 사용할 수 없습니다.');
+    }
+  }
+
+  void pray() {}
+}
