c0d2cb44f5404f4e54712549610fa2f4107b0b70
icicle_4 <dwayne.han4@gmail.com>
2024-03-07 12:05:57 +0900

    연습문제2. 최대 Hp와 최대 Mp등 필드를 추가했습니다.

---
+++ lib/24_03_04/240307/cleric.dart
@@ -0,0 +1,18 @@
+class Cleric {
+  /**
+   * 커밋 메시지
+   * 클레릭의 기본 속성을 정의했습니다.
+   * 클레릭의 능력을 구현했습니다.
+   * 클레릭을 문자열로 표현하는 기능을 추가했습니다.
+   */
+  String name = '메딕';
+  int level = 10;
+  int hp;
+  final int maxHp = 50;
+  final int maxMp = 10;
+
+  String suffix;
+
+  Cleric(this.hp, this.suffix);
+
+}
