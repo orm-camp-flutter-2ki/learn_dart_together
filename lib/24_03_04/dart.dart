d1bc460b850018e7b618953e2e1adc12143f05d6
hyojung.sung <hyo8967@naver.com>
2024-03-07 14:18:12 +0900

    클레릭 클래스 추가
    - 클레릭의 기본 속성을 정의했습니다.
    - 클레릭의 능력을 구현했습니다.
    - 클레릭을 문자열로 표현하는 기능을 추가했습니다.

--- lib/24_03_04/dart.dart
+++ lib/24_03_04/dart.dart
@@ -1,3 +1,11 @@
-void main() {
-  class Cleric  }
-}
+class Cleric {
+  String name;
+  int hp = 50;
+  final int maxHp = 50;
+  int mp = 10;
+  final int maxMp = 10;
+
+
+  // 연습 1에서 작성한 내용이 없는 Cleric 클래스에 “이름", “HP”, “최대 HP”, “MP”, “최대 MP”를 속성으로 추가 하시오.
+  // 또한 HP와 최대 HP는 정수로 초기치 50, MP와 최대 MP는 정수로 초기치 10으로 하고,
+  // 최대 HP와 최대 MP는 상수 필드로 선언 하시오.
