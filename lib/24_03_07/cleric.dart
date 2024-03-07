4958a03c0237e233438c006b77ea73ef744596d6
SeongJongHo <whdgh9595@gmail.com>
2024-03-07 13:55:47 +0900

    [MODIFY] Cleric class pray 메소드 작성

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric{
   final int _maxHp = 50;
   final int _maxMp = 10;
@@ -12,5 +14,16 @@ class Cleric{
       _mp -= 5;
       _hp = _maxHp;
     }
+  }
+
+  int pray(int second){
+    int mpAmount = (_getRandomHealAmount(2) + 1) * second;
+
+    return _maxMp < _mp + mpAmount? _maxMp - _mp: mpAmount;
+  }
+
+  int _getRandomHealAmount(int limit){
+
+    return Random().nextInt(limit);
   }
 }
