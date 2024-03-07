acf31f8eb966c9888b4363f557db58dfd90d21e2
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-07 15:24:47 +0900

    fear: add selfAid method to cleric class

--- lib/0307/cleric.dart
+++ lib/0307/cleric.dart
@@ -10,5 +10,23 @@ class Cleric {
     this.name = name;
     this.hp = this.maxHp;
     this.mp = this.maxMp;
+    print('===== 클래릭 생성 =====');
+    print('초기 HP: ${this.hp}, 초기 MP: ${this.mp}');
+  }
+
+  void selfAid(int cnt) {
+    if (this.mp < 5) {
+      print('HP 회복에 필요한 MP 5가 없습니다.');
+      return;
+    } else if (this.hp == this.maxHp) {
+      print('HP가 이미 최댓값입니다.');
+      return;
+    }
+
+    this.mp -= 5;
+    this.hp = this.maxHp;
+
+    print('===== selfAid() 발동 $cnt =====');
+    print('${this.hp}, ${this.mp}');
   }
 }
