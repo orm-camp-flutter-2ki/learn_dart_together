4398f4550ddc667d3c8e86e01d28a51640d0fe6c
KimDonghyeok <qoophon@gmail.com>
2024-03-07 14:19:10 +0900

    feat: Cleric 클래스 필드, 생성자 추가
    - maxHP, maxMP, name, hp, mp 필드 추가
    - 생성자 추가

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -1,3 +1,12 @@
 import 'dart:math';
 
-class Cleric {}
+class Cleric {
+  final int maxHp = 50;
+  final int maxMp = 10;
+
+  String name;
+  int hp = 50;
+  int mp = 10;
+
+  Cleric(this.name, this.hp, this.mp);
+}
