085ac7ffa72e99bda12d862682af9c4276ac8e28
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-07 14:28:24 +0900

    feat: 클레릭의 selfAid 능력을 구현했습니다.

--- lib/0307/00_cleric.dart
+++ lib/0307/00_cleric.dart
@@ -9,4 +9,12 @@ class Cleric { // class 생성
   // this. : 나의.name, 나의.hp
   Cleric(this.name, this.hp, this.mp);
 
+  // selfAid 동작 작성
+  // mp 5를 소비하여 자신의 hp를 maxHp까지 회복하는 동작
+  void selfAid() {
+    int aidHeal = 5;
+    mp -= aidHeal;
+    hp = maxHp;
+  }
+
 }
