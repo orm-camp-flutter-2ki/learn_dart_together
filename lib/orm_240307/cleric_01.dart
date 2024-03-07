f24be116a33c9565937a202716f8d0c4027c0c4f
BAUu <yimej07@gmail.com>
2024-03-07 15:22:58 +0900

    feat: 클레릭 클레스 추가
    클레릭의 기본 속성을 정의했습니다.
    클레릭의 능력을 구현하였습니다.
    클레릭의 hp, mp 상태를 터미널로 확인 할수 있게 문자열 구성하였습니다.

---
+++ lib/orm_240307/cleric_01.dart
@@ -0,0 +1,9 @@
+class Cleric {
+  String name;
+  int hp;
+  int mp;
+  int maxhp=50;
+  int maxmp=10;
+  Cleric(this.name, this.hp, this.mp);
+
+}
---
+++ lib/orm_240307/cleric_03.dart
@@ -0,0 +1,13 @@
+class Cleric {
+  String name;
+  int hp;
+  int mp;
+  int maxhp=50;
+  int maxmp=10;
+  Cleric(this.name, this.hp, this.mp);
+
+  void selfAid(){
+    mp -= 5;
+    hp = maxhp;
+  }
+}
