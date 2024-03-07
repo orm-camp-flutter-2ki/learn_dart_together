f46756ee75263ddbca675a3619f29f23f26f52d1
SeongJongHo <whdgh9595@gmail.com>
2024-03-07 15:17:59 +0900

    [MODIFY] 컨벤션 수정 및 print문 추가

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-class Cleric{
+class Cleric {
   final int _maxHp = 50;
   final int _maxMp = 10;
   int _hp = 50;
@@ -9,22 +9,24 @@ class Cleric{
 
   Cleric(this._name);
 
-
-  void selfAid(){
-    if(_mp >=5) {
+  void selfAid() {
+    if (_mp >= 5) {
       _mp -= 5;
       _hp = _maxHp;
-    }
+      print('$_name의 hp가 회복 되었습니다. 잔여 mp: $_mp, hp: $_hp');
+    } else {}
   }
 
-  int pray(int second){
-    int mpAmount = _getRandomHealAmount(3) + second;
+  int pray(int second) {
+    int randomAmount = _getRandomHealAmount(3);
+    int mpAmount = randomAmount + second;
+    int result = _maxMp < _mp + mpAmount ? _maxMp - _mp : mpAmount;
+    print('$_name의 mp가 총$result만큼 회복되었습니다. 잔여 mp: $_mp, mp회복량: $result, random회복량: $randomAmount');
 
-    return _maxMp < _mp + mpAmount? _maxMp - _mp: mpAmount;
+    return result;
   }
 
-  int _getRandomHealAmount(int limit){
-
+  int _getRandomHealAmount(int limit) {
     return Random().nextInt(limit);
   }
 }
