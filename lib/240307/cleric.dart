fcbf82fc68ad26c44b647a5a7a6c810c757408f8
KimDonghyeok <qoophon@gmail.com>
2024-03-07 14:11:27 +0900

    feat: Cleric 클래스 추가
    - commit 분할 하여 재작성

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -1,37 +1,3 @@
 import 'dart:math';
 
-class Cleric {
-  final int maxHp = 50;
-  final int maxMp = 10;
-
-  String name;
-  int hp = 50;
-  int mp = 10;
-
-  Cleric(this.name);
-
-  void selfAid() {
-    mp -= 5;
-    hp = maxHp;
-
-    print('$name (은)는 자가치유를 시전합니다.');
-    showHp();
-  }
-
-  int pray(int time) {
-    int randomMpAmount = Random().nextInt(2);
-    
-    print('$name (은)는 기도를 합니다.');
-    showMp();
-
-    return time + randomMpAmount;
-  }
-
-  void showHp() {
-    print('[현재 $name 클레릭의 체력은 $hp]');
-  }
-
-  void showMp() {
-    print('[현재 $name 클레릭의 마나는 $mp]');
-  }
-}
+class Cleric {}
