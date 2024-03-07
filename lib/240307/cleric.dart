e43841a3c642c85b9ce8d49d195a05f1c49d2f6f
jungeun272 <hanjungeun272@gmail.com>
2024-03-07 14:16:04 +0900

    - 기본 속성 정의

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -1,3 +1,13 @@
 class Cleric {
+  String name;
+  int hp;
+  int mp;
+  final int maxhp = 50;
+  final int maxmp = 10; //상수로 선언해야 하는거니 숫자가 들어가야 함
+
+  Cleric(this.name, this.hp, this.mp); //매개변수(함수나 메서드가 호출될 때 전달되는 값)로 전달된 값을 클래스의 멤버 변수에 할당하기 위해 this 키워드를 사용
 
 }
+
+
+
