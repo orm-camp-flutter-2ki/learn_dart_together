d27273401e4c67bfb2b007d803b21c637f27ac3a
jungeun272 <hanjungeun272@gmail.com>
2024-03-07 14:50:26 +0900

    - 셀프에이드 속성 추가
    ###궁금한 점
    void main() {
      Cleric cleric = Cleric('성직자', 50, 10);
    
      print('===== 클래릭 생성 =====');
      print('${cleric.hp}, ${cleric.mp}');
    
      for (int i = 0; i < 3; i++) {
        cleric.selfAid();
        print('===== selfAid() 발동 $i =====');
        print('${cleric.hp}, ${cleric.mp}');
      }
    
      for (int i = 0; i < 3; i++) {
        print('===== pray() 발동 $i =====');
        int recoveryMp = cleric.pray(5);
        print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
      }
      을 돌렸을 때, 값이
      ===== 클래릭 생성 =====
      50, 10
      ===== selfAid() 발동 0 =====
      50, 5
      ===== selfAid() 발동 1 =====
      50, 0
      mp가 부족합니다.
      ===== selfAid() 발동 2 =====
      50, 0
     이렇게 나오는데 왜 마지막에는 mp가 부족합니다라는 문구가 나오지 않는지 궁금합니다.

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -5,9 +5,28 @@ class Cleric {
   final int maxhp = 50;
   final int maxmp = 10; //상수로 선언해야 하는거니 숫자가 들어가야 함
 
-  Cleric(this.name, this.hp, this.mp); //매개변수(함수나 메서드가 호출될 때 전달되는 값)로 전달된 값을 클래스의 멤버 변수에 할당하기 위해 this 키워드를 사용
+  Cleric(this.name, this.hp, this.mp);
+  //매개변수(함수나 메서드가 호출될 때 전달되는 값)로 전달된 값을 클래스의 멤버 변수에 할당하기 위해 this 키워드를 사용
+  void selfAid() {
+    if (mp == maxmp) {
+      mp = maxmp - 5;
+      hp = maxhp;
+    } else if (5 <= mp && mp < 10) {
+      mp = mp - 5;
+      hp = maxhp;
+    } else if (mp < 5) {
+      print('mp가 부족합니다.');
+    }
+  }
+
+  void pray() {
+  }
+
 
 }
+
+
+
 
 
 
