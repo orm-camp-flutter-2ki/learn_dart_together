c0cc3244b8c0a13049b336ffeb402ccb0c2defa6
hwangtaewook <hwangxodnr@nate.com>
2024-03-07 12:40:04 +0900

    클레릭의 능력을 구현했습니다.

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -10,8 +10,21 @@ class Cleric {
 
   Cleric(this.name, this.hp, this.maxHp, this.mp, this.maxMp);
 
-  void a() {
-    print('a');
+  void slefAid() {
+    if (mp >= 5) {
+      mp -= 5;
+      hp = maxHp;
+      print('마나 에이드 사용. \nhp가 $hp이 되었습니다.\nmp가 $mp이 되었습니다');
+    } else {
+      print('마나가 부족합니다');
+    }
   }
 }
-//  int hp >50 ? hp =maxHp : hp;
+
+void main() {
+  Cleric cleric = Cleric('클래릭', 50, 50, 10, 10);
+
+  cleric.slefAid();
+  cleric.slefAid();
+  cleric.slefAid();
+}
