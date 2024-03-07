6e70121d14030ad1a1f13d14f07028f6ef973fe5
hwangtaewook <hwangxodnr@nate.com>
2024-03-07 11:55:45 +0900

    feat : 클레릭 클래스 추가

---
+++ lib/240307/cleric.dart
@@ -0,0 +1,13 @@
+class Cleric {
+  String name;
+  int hp;
+  int maxHp;
+  int mp;
+  int maxMp;
+
+  Cleric(this.name, this.hp, this.maxHp, this.mp, this.maxMp);
+
+  void a() {
+    print('a');
+  }
+}
