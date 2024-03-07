52339b2f0d2bffa5ab760cf0c3c866665564c76b
jin song <songjin3914@gmail.com>
2024-03-07 14:12:03 +0900

    Feat: 클레릭 클래스 함수 추가
    - 생성자 함수와 selfAid

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -5,4 +5,15 @@ class Cleric {
   final int maxHp = 50;
   final int maxMp = 10;
 
+  Cleric( this.name,this.hp , this.mp);
+
+  void selfAid(){
+    if(mp<5){
+      print("mp가 부족합니다");
+    }else{
+      mp = mp - 5
+      hp = maxHp
+    }
+  }
+
 }
