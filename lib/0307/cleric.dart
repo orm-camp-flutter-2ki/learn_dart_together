18b219cfb51f086edddd55792304c36fe8cc808a
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-07 13:20:44 +0900

    feat: add field to cleric class

--- lib/0307/cleric.dart
+++ lib/0307/cleric.dart
@@ -1,3 +1,14 @@
 class Cleric {
-  
+
+  late String name;
+  late int hp;
+  late int mp;
+  final int maxHp = 50;
+  final int maxMp = 10;
+
+  Cleric(String name) {
+    this.name = name;
+    this.hp = this.maxHp;
+    this.mp = this.maxMp;
+  }
 }
