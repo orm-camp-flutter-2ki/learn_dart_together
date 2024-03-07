c27d5c8e4f63ef102406f3e4b280516499b379ca
chojja7188 <chojja7188@gmail.com>
2024-03-07 12:25:12 +0900

    feat: Cleric 클래스 수정
    - Cleric 클래스에 필드 추가
    - Cleric 클래스에 생성자 추가

--- lib/24_03_07/cleric.dart
+++ lib/24_03_07/cleric.dart
@@ -1,3 +1,9 @@
 class Cleric {
+  String name; // 이름
+  int hp; // HP
+  final int maximumHp = 50; // 최대 HP
+  int mp; // MP
+  final int maximumMp = 10;
 
+  Cleric(this.name, this.hp, this.mp);
 }
