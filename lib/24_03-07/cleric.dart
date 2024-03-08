d67fe83502d900b031822e4dac7e9621a0f2aae5
sseung <sseutta@naver.com>
2024-03-08 14:21:11 +0900

    Feat : 클래릭 클레스 기능추가
    maxHp, maxMp, 생성자 수정

--- lib/24_03-07/cleric.dart
+++ lib/24_03-07/cleric.dart
@@ -6,10 +6,10 @@ class Cleric {
   int hp;
   int mp;
 
-  final int maxHp = 50;
-  final int maxMp = 10;
+  static final int maxHp = 50;
+  static final int maxMp = 10;
 
-  Cleric(this.name, this.hp, this.mp);
+  Cleric(this.name, {this.hp = 50, this.mp = 10});
 
   void selfAid() {
     if (mp >= 5) {
@@ -27,5 +27,9 @@ class Cleric {
       mp = maxMp;
     }
     return recoveryMp;
+  }
+  static rann(){
+
+
   }
 }
--- lib/24_03-07/main.dart
+++ lib/24_03-07/main.dart
@@ -1,21 +1,12 @@
+import 'package:learn_dart_together/02_03_08/sword.dart';
 import 'cleric.dart';
 
+
 void main() {
-  Cleric cleric = Cleric('홍길동', 50, 10);
 
-  print('===== 클래릭 생성 =====');
-  print('${cleric.hp}, ${cleric.mp}');
 
-  for (int i = 0; i < 3; i++) {
-    cleric.selfAid();
-    print('===== selfAid() 발동 $i =====');
-    print('${cleric.hp}, ${cleric.mp}');
-  }
 
-  for (int i = 0; i < 3; i++) {
-    print('===== pray() 발동 $i =====');
-    int recoveryMp = cleric.pray(5);
-    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
-  }
+
+
 }
 
