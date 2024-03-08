2a29a871ceeebb0c46a63c3da5fdfc9bfd6c5c09
jin song <songjin3914@gmail.com>
2024-03-08 15:10:31 +0900

    Feat: 생성자 오버로딩 추가
    - 생성자 오버로딩 추가

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -1,14 +1,14 @@
 import 'dart:math';
 
 class Cleric {
-  String name;
+  String? name;
   int hp;
   int mp;
   static final int maxHp = 50;
   static final int maxMp = 10;
 
-  Cleric(this.name, this.hp, this.mp);
-
+  //생성자 오버로딩 추가
+  Cleric(this.name, {this.hp = 50, this.mp = 10});
 
   int spendMp = 5;
 
@@ -34,12 +34,10 @@ class Cleric {
     }
     return healPoint;
   }
-
 }
 
-
 void main() {
-  Cleric cleric = Cleric('홍길동', 50, 10);
+/*  Cleric cleric = Cleric('홍길동', 50, 10);
 
   print('===== 클래릭 생성 =====');
   print('${cleric.hp}, ${cleric.mp}');
@@ -53,7 +51,13 @@ void main() {
   for (int i = 0; i < 3; i++) {
     print('===== pray() 발동 $i =====');
     int recoveryMp = cleric.pray(5);
-    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
-  }
-}
+    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');*/
+  Cleric cleric = Cleric('아서스', hp: 40, mp: 5);
+  Cleric cleric1 = Cleric('아서스', hp: 35);
+  Cleric cleric2 = Cleric('홍길동');
 
+  print("${cleric1.hp} ${cleric1.mp}");
+  print("${cleric2.hp} ${cleric2.mp}");
+
+  print("Rmx");
+}
