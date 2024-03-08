90e4cb5153335f783ce519438e17a2ac4f123d44
BAUu <yimej07@gmail.com>
2024-03-08 15:01:38 +0900

    feat: 240308 수업 내용 정리
    static 키워드를 사용해서 maxMP와 maxHP의 값을 필드에 공유

--- lib/orm_240308/cleric_fix.dart
+++ lib/orm_240308/cleric_fix.dart
@@ -32,3 +32,21 @@ class Cleric {
     return recovery;
   }
 }
+void main() {
+  Cleric cleric = Cleric('홍길동', 50, 10);
+
+  print('===== 클래릭 생성 =====');
+  print('${cleric.hp}, ${cleric.mp}');
+
+  for (int i = 0; i < 3; i++) {
+    cleric.selfAid();
+    print('===== selfAid() 발동 ${i + 1} =====');
+    print('${cleric.hp}, ${cleric.mp}');
+  }
+
+  for (int i = 0; i < 3; i++) {
+    print('===== pray() 발동 ${i + 1} =====');
+    int recoveryMp = cleric.pray(5);
+    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
+  }
+}
