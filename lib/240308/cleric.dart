564e315de6c88f0a62a44e11199a55a8acffc59c
woong <workplayhard1@naver.com>
2024-03-08 14:38:43 +0900

    feat: cleric 기본파일 추가

---
+++ lib/240308/cleric.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+class Cleric {
+  final int maximumHp = 50; // 최대 HP
+  final int maximumMp = 10; // 최대 MP
+
+  String name; // 이름
+  int hp; // HP
+  int mp; // MP
+
+  Cleric(this.name, this.hp, this.mp);
+
+  void selfAid() {
+    mp -= 5;
+    hp = maximumHp;
+  }
+
+  int pray(int second) {
+    int healedMp = Random().nextInt(3) + second;
+
+    if (healedMp > maximumMp - mp) {
+      healedMp = maximumMp - mp;
+      mp = maximumMp;
+    } else {
+      mp += healedMp;
+    }
+
+    return healedMp;
+  }
+}
+
+void main() {
+  
+}
