69c89042b2701420ac0c6abb7be49a7f2babd3e4
jungeun272 <hanjungeun272@gmail.com>
2024-03-08 15:11:58 +0900

    feat : 생성자 생성
    성직자 기본
    아서스 hp : 40 mp : 5
    아서스2 hp : 35 mp : 기본값
    아서스3 hp : 기본값 mp : 기본값
    생성

--- lib/240307/main.dart
+++ lib/240307/main.dart
@@ -1,6 +1,16 @@
 import 'cleric.dart';
 
 void main() {
-  final cleric = Cleric('성직자1');
-  print('이름 ${cleric.name}');
+  final cleric = Cleric('성직자');
+  print('이름 ${cleric.name} hp= ${cleric.hp} mp= ${cleric.mp}');
+
+  final cleric1 = Cleric('아서스', hp: 40, mp: 5);
+  print('이름 ${cleric1.name} hp= ${cleric1.hp} mp= ${cleric1.mp}');
+
+  final cleric2 = Cleric('아서스2', hp: 35);
+  print('이름 ${cleric2.name} hp= ${cleric2.hp} mp= ${cleric2.mp}');
+
+  final cleric3 = Cleric('아서스3');
+  print('이름 ${cleric3.name} hp= ${cleric3.hp} mp= ${cleric3.mp}');
+
 }
