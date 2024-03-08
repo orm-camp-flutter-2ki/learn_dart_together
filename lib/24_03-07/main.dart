cfd49c3ddb4a064daeeabcb58f7b650ade1d4b57
sseung <sseutta@naver.com>
2024-03-08 17:09:15 +0900

    Feat: 클래릭 클레스 추가
    생성자 기능보완,

--- lib/24_03-07/main.dart
+++ lib/24_03-07/main.dart
@@ -12,7 +12,7 @@ final cleric2 = Cleric('마법사2', hp:40, mp:5);
 
 print('이름 : ${cleric2.name} HP :  ${cleric2.hp} MP : ${cleric2.mp}  ');
 
-final cleric3 = Cleric('마법사3', hp:35);
+final cleric3 = Cleric('마법사3', hp:30);
 
 print('이름 : ${cleric3.name} HP :  ${cleric3.hp} MP : ${cleric3.mp}  ');
 
