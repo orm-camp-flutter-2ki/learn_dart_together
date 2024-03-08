8c5d60a921dfb7394441b98403fa1d2e9c35e3c6
YunZi.Song <yoonji5809@gmail.com>
2024-03-08 15:01:52 +0900

    feat: Cleric 클래스 수정 - 최대 hp, 최대 mp 변수 static 선언 - 생성자 수정 - selfAid 함수 로직 수정

--- lib/24_03_07/main.dart
+++ lib/24_03_07/main.dart
@@ -1,7 +1,11 @@
 import 'package:learn_dart_together/24_03_07/cleric.dart';
 
 void main() {
-   Cleric cleric = Cleric('홍길동', 50, 10);
+
+   Cleric cleric = Cleric(name: "홍길동");
+   Cleric cleric1 = Cleric(name: "우투리", hp: 100);
+   Cleric cleric2 = Cleric(name: "전우치", hp: 100, mp: 5);
+
 
    print('===== 클래릭 생성 =====');
    print('${cleric.hp}, ${cleric.mp}');
@@ -14,7 +18,7 @@ void main() {
 
    for (int i = 0; i < 3; i++) {
       print('===== pray() 발동 $i =====');
-      int recoveryMp = cleric.pray(5);
+      int recoveryMp = cleric.pray(1);
       print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
    }
 } 
