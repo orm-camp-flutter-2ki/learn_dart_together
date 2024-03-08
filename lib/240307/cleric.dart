7fa3e1c9a2f6e2576b9dfe7f45c700f77e29e006
nala0897@naver.com <nala0897@naver.com>
2024-03-08 14:19:38 +0900

    fix: Cleric 코드 설명 추가

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -1,19 +1,22 @@
 import 'dart:math';
 
+// 성직자
 class Cleric {
-  String name;
-  int hp;
-  int mp;
   final int maxHp = 50;
   final int maxMp = 10;
 
+  int hp;
+  int mp;
+  int spendMp = 5;
+  String name;
+
   Cleric(this.name, this.hp, this.mp);
 
-  int spendMp = 5;
-
+  // '셀프 에이드'
   void selfAid() {
-    if (mp < spendMp) {
-      print("mp가 부족합니다");
+    if( mp < spendMp ) {
+      print("mp가 부족합니다.");
+
     } else {
       mp = mp - spendMp;
       hp = maxHp;
@@ -21,17 +24,24 @@ class Cleric {
   }
 
   // 불변
+  // '기도하기'
   int pray({int second = 1}) {
+    // 회복량 = 기도한 시간(초)에 랜덤하게 0~2 포인트의 보정을 한 양
     final int heal = second + Random().nextInt(3);
 
     int healPoint = 0;
-    if (mp + heal > maxMp) {
+
+    // 최대 MP 보다 더 회복은 불가능
+    if( mp + heal > maxMp ) {
       healPoint = maxMp - mp;
       mp = maxMp;
+
     } else {
       mp = mp + heal;
       healPoint = heal;
     }
+
+    // 실제로 회복된 MP 양
     return healPoint;
   }
 }
