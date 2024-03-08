bd89d8dff0bfc3a3dac34fb2b6f1ccddd307510a
YunZi.Song <yoonji5809@gmail.com>
2024-03-08 16:13:11 +0900

    feat: 성기사 프린트문 추가

--- lib/24_03_07/main.dart
+++ lib/24_03_07/main.dart
@@ -8,7 +8,10 @@ void main() {
 
 
    print('===== 클래릭 생성 =====');
-   print('${cleric.hp}, ${cleric.mp}');
+   print('나는야 성기사 ${cleric.name}!! 내 체력은 ${cleric.hp}이고, 마력은 ${cleric.mp} 이만큼 있지! 하하');
+   print('나는야 성기사 ${cleric1.name}!! 내 체력은 ${cleric1.hp}이고, 마력은 ${cleric1.mp} 이만큼 있지! 하하');
+   print('나는야 성기사 ${cleric2.name}!! 내 체력은 ${cleric2.hp}이고, 마력은 ${cleric2.mp} 이만큼 있지! 하하');
+
 
    for (int i = 0; i < 3; i++) {
       cleric.selfAid();
