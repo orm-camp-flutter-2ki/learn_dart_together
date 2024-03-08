1252588e799d691741182d6606f6abe21e83d615
Stranger Yang <ccamdol@gmail.com>
2024-03-08 16:39:58 +0900

    Cleric 이름 파라미터 수정
    - Cleric 생성자 수정

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -38,9 +38,9 @@ class Cleric {
 void main() {
   // Cleric cleric = Cleric('아서스', hp: 40, mp: 5); //문제2-A
   // Cleric cleric = Cleric('아서스', hp: 35); //문제2-B
-  // Cleric cleric = Cleric('아서스'); //문제2-C
+  Cleric cleric = Cleric('아서스'); //문제2-C
 
-  Cleric cleric2 = Cleric(); //문제2-E
+  // Cleric cleric2 = Cleric(); //문제2-E
   // 이름을 지정하지 않는 경우에는 인스턴스화 할 수 없음, 이름없는 성직자 존재할 수 없음
 
   print('===== 클래릭 생성 =====');
