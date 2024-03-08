d485d0b0d65ba58bf97a7a6afcc9100cd7fbd70c
tomoyo519 <vanessa.cheong1@gmail.com>
2024-03-08 15:51:45 +0900

    [feat] static 키워드를 이용한 maxHp, maxMp 변수 변경, named parameter, positional parameter 설정

---
+++ lib/240307/정다희/main.dart
@@ -0,0 +1,11 @@
+import 'cleric.dart';
+void main(){
+  final Hero1 = Cleric("아서스1", hp:40, mp:5);
+  final Hero2 = Cleric("아서스2", hp:35);
+  final Hero3 = Cleric("아서스3");
+  // final Hero4 = Cleric();
+
+  print(' ${Hero1.name}, ${Hero1.hp},  ${Hero1.mp}');
+  print(' ${Hero2.name}, ${Hero2.hp},  ${Hero2.mp}');
+  print(' ${Hero3.name}, ${Hero3.hp},  ${Hero3.mp}');
+}
