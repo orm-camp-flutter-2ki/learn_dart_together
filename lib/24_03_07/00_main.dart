6e98da45462f7aa780bf2ee8b7a10aa5665aafdc
leepl <yplunap25@gmail.com>
2024-03-08 16:49:17 +0900

    fix: 기존 Cleric 으로 수정
    - 새로 만든 폴더 삭제
    - 기존 클레릭에 생성자 추가

---
+++ lib/24_03_07/00_main.dart
@@ -0,0 +1,20 @@
+import '01_cleric.dart';
+
+void main() {
+  Cleric cleric = Cleric('홍길동', hp: 50, mp: 10);
+
+  print('===== 클래릭 생성 =====');
+  print('${cleric.hp}, ${cleric.mp}');
+
+  for (int i = 0; i < 3; i++) {
+    cleric.selfAid();
+    print('===== selfAid() 발동 $i =====');
+    print('${cleric.hp}, ${cleric.mp}');
+  }
+
+  for (int i = 0; i < 3; i++) {
+    print('===== pray() 발동 $i =====');
+    int recoveryMp = cleric.pray(5);
+    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
+  }
+}
