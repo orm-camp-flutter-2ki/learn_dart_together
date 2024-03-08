637718d75fba8374e4e1cd8a2b1d150a137b7dc0
KimDonghyeok <qoophon@gmail.com>
2024-03-08 16:38:41 +0900

    chore: Cleric 생성자 테스트 코드 작성 (연습문제)

---
+++ lib/240308/_main.dart
@@ -0,0 +1,8 @@
+import '../240307/cleric.dart';
+
+void main() {
+  Cleric cleric1 = Cleric(name: "아서스", hp: 40, mp: 5);
+  Cleric cleric2 = Cleric(name: "아서스", hp: 35);
+  Cleric cleric3 = Cleric(name: "아서스");
+  // Cleric cleric4 = Cleric(); // error!
+}
