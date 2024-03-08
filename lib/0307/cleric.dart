790f15ba0f28d60d290c446e448772b2037f7244
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-08 17:11:36 +0900

    feat: 답변을 토대로 코드 정리

--- lib/0307/cleric.dart
+++ lib/0307/cleric.dart
@@ -5,29 +5,14 @@ class Cleric {
   String name; // field
   int hp;
   int mp;
-
-  // static final int maxHp = 50;
-  // static final int maxMp = 10;
-  static const int maxHp = 50; // final -> const
+  static const int maxHp = 50; // 상수 + static
   static const int maxMp = 10;
 
-  Cleric(this.name, {this.hp = maxHp, this.mp = maxMp}); // 생성자 룰
-  // Cleric(this.name, {this.hp = 50, this.mp = 10}); // 생성자
-  // [Q] Cleric(this.name, {this.hp = maxHp, this.mp = maxMP}); 이렇게는 불가능할까...
-  // 팀원 답변: final 말고 const로 하면 된다. 하지만 이유는 모른다..
-  // [Q] 그러면 왜 final은 안되고 const는 되는걸까....
-/*
- * [선생님 Answer]
- * static 키워드가 붙은 변수는 다른 것보다 먼저 메모리에 올라간다, 이유는 공유자원이기 때문에.
- * 생성자 룰에 기본 값으로 주기 위해서는 .. 컴파일에 이미 값이 결정 되어 있어야 한다.
- * final은 런타임에 결정되어서 안된다. const는 컴파일 타임에 결정 되어서 된다.
- * 런타임은 버튼 눌렀을 때 되는 것이고, 런타임 전에 컴파일이 있고, 컴파일은 코드 작성 중에 일어난다
- **/
+  Cleric(this.name, {this.hp = maxHp, this.mp = maxMp}); // 생성자
 
   // selfAid 동작: mp 5를 소비하여 자신의 hp를 maxHp 까지 회복하는 동작
   void selfAid() {
     int aidHeal = 5; //소비 mp
-
     if (mp < aidHeal) {
       print('MP가 부족해 실행이 취소됩니다. (현재 MP: $mp)');
     } else {
@@ -36,7 +21,7 @@ class Cleric {
     }
   }
 
-  // pray 동작 = mp 회복, mp는 maxMp를 넘지 못한다.
+  // pray 동작: mp 회복, mp는 maxMp를 넘지 못한다.
   int pray(int second) {
     int prayHeal = Random().nextInt(3); // 회복량, 0 ~ 2까지 랜덤한 정수가 나온다
     int prayResult = second + prayHeal; // 전달받은 시간(초) + 0~2 만큼 회복한다
