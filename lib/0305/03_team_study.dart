38e06b546c4094b481fef3d55fe534ae520edac9
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-08 11:14:16 +0900

    note: 공부 내용을 작성하였습니다.

---
+++ lib/0305/03_team_study.dart
@@ -0,0 +1,21 @@
+//[?] 등차수열, 첫항이 5, 공차가 3일 때 10번째까지의 숫자를 공백으로 구분하여 출력하는 프로그램
+
+void main() {
+  print(solution04(5, 3));
+
+}
+
+solution04(int m, int n) { // 첫항 m, 공차 n
+  //[1] input
+  List<int> numbers = [];
+
+  //[2] process
+  for (int i = 0; i < 10; i++) {
+    numbers.add((m * i) + n);
+  }
+
+  return numbers;
+
+  //[3] output
+  print(numbers); // 공백 출력 어떻게 하지...
+}
