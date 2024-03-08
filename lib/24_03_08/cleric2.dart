6543d8cb227fd3866c30eb817347cedfc24cbd12
hyojung.sung <hyo8967@naver.com>
2024-03-08 23:15:51 +0900

    feat. 생성자 추가
    1) 이 클래스는 Cleric(“아서스", hp: 40, mp: 5) 와 같이, 이름, HP, MP 를 지정하여 인스턴스화 할 수 있다
    2) 이 클래스는 Cleric(“아서스", hp: 35) 와 같이, 이름과 HP만으로 지정하여 인스턴스화 할 수 있다. 이 때, MP는 최대 MP와 같은 값이 초기화 된다
    3) 이 클래스는 Cleric(“아서스") 와 같이 이름만을 지정하여 인스턴스화 할 수 있다. 이 때, HP 와 MP 는 최대 HP와 최대 MP로 초기화 된다
    4) 이 클래스는 Cleric() 과 같이 이름을 지정하지 않는 경우에는 인스턴스화 할 수 없다고 한다. (이름이 없는 성직자는 존재 할 수 없음)
    5) 생성자는 가능한 한 중복되는 코드가 없도록 작성한다.

--- lib/24_03_08/cleric2.dart
+++ lib/24_03_08/cleric2.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
+
 void main() {
-  Cleric cleric = Cleric('홍길동', 50, 10);
+  Cleric cleric = Cleric(name: '아서스', hp: 50, mp: 10); // cleric 인스턴트(붕어빵 틀)
 
   print('===== 클래릭 생성 =====');
   print('${cleric.hp}, ${cleric.mp}');
@@ -19,26 +20,26 @@ void main() {
 }
 
 class Cleric {
+  // 클래스(대문자)
   String name;
   int hp = 50;
   int mp = 10;
-  static final int maxHp = 50;
-  static final int maxMp = 10;
+  static const int maxHp = 50;
+  static const int maxMp = 10;
 
-
-  Cleric(this.name, this.hp, this.mp); // 생성자를 통해 인스턴스의 초기값을 설정
+  Cleric(
+      {required this.name,
+      this.hp = maxHp,
+      this.mp = maxMp}); // 생성자(대문자). required는 재료가 꼭 들어가야 할 때 / {}는 옵션
 
   void selfAid() {
     if (mp >= 5) {
       mp -= 5; // 현재 mp에서 5를 뺀 값을 다시 mp에 할당
       hp = maxHp; // 현재 hp를 최대(maxHp)로 설정해 회복 수행
-
     }
-
   }
 
   int pray(int prayTime) {
-
     Random random = Random(); // Random 클래스의 인스턴스를 생성
     int recoveryAmount = random.nextInt(3) + prayTime;
     //random 인스턴스의 nextInt 메서드를 호출해 0~2까지 랜덤 호출하고, +prayTime 변수의 값을 더해 보정된 회복량 계산
@@ -46,7 +47,7 @@ class Cleric {
     int actualRecoveryAmount = min(
         maxMp - mp, recoveryAmount); // 최대 MP를 넘지 않도록 회복량 조정(최대mp, 현재mp 비교해서)
 
-    mp += actualRecoveryAmount; //실제 회복된 마나 양을 현재 마나에 더함
+    mp += actualRecoveryAmount; //실제 회복된 mp 양을 현재 mp에 더함
 
     print('회복량: $actualRecoveryAmount');
     return actualRecoveryAmount;
