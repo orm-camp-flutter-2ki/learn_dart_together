20fa3c34bcc291823b6b136f6fb2d18146248343
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-08 14:23:21 +0900

    style: remove unnecessary this

--- lib/0307/cleric.dart
+++ lib/0307/cleric.dart
@@ -2,49 +2,48 @@ import 'dart:math';
 
 class Cleric {
 
-  late String name;
+  String name;
   late int hp;
   late int mp;
   final int maxHp = 50;
   final int maxMp = 10;
 
-  Cleric(String name) {
-    this.name = name;
-    this.hp = this.maxHp;
-    this.mp = this.maxMp;
+  Cleric(this.name) {
+    this.hp = maxHp;
+    mp = maxMp;
 
-    print('===== 클레릭 "${this.name}" 생성 =====');
-    print('초기 HP: ${this.hp}, 초기 MP: ${this.mp}');
+    print('===== 클레릭 "$name" 생성 =====');
+    print('초기 HP: $hp, 초기 MP: $mp');
   }
 
   void selfAid(int cnt) {
     print('===== selfAid() 발동 $cnt =====');
 
-    if (this.mp < 5) {
+    if (mp < 5) {
       print('HP 회복에 필요한 MP 5가 없습니다.');
       return;
-    } else if (this.hp == this.maxHp) {
+    } else if (hp == maxHp) {
       print('HP가 이미 최댓값입니다.');
       return;
     }
 
-    this.mp -= 5;
-    this.hp = this.maxHp;
+    mp -= 5;
+    hp = maxHp;
 
-    print('${this.hp}, ${this.mp}');
+    print('$hp, $mp');
   }
 
   void pray(int second, int cnt) {
     print('===== pray() 발동 $cnt =====');
 
-    if (this.mp == this.maxMp) {
+    if (mp == maxMp) {
       print('MP가 이미 최댓값입니다.');
       return;
     }
 
     int recovery = second + Random().nextInt(3);
-    this.mp = min(this.mp + recovery, this.maxMp);
+    mp = min(mp + recovery, maxMp);
 
-    print('${this.hp}, ${this.mp}, 회복량: $recovery');
+    print('$hp, $mp, 회복량: $recovery');
   }
 }
