c479c7b7f58541f825da6ec0cbef7f571910e663
jungeun272 <hanjungeun272@gmail.com>
2024-03-08 14:50:51 +0900

    feat : 성직자 hp,mp maxhp,maxmp = 50,10으로 할당 인스턴스를 maxhp, maxmp로 변경

--- lib/240307/main.dart
+++ lib/240307/main.dart
@@ -1,24 +1,6 @@
 import 'cleric.dart';
 
 void main() {
-  Cleric cleric = Cleric('홍길동', 50, 10);
-
-  print('===== 클래릭 생성 =====');
-  print('${cleric.hp}, ${cleric.mp}');
-
-  print('===== 전투(피 까임) =====');
-  cleric.hp -= 25;
-  print('${cleric.hp}, ${cleric.mp}');
-
-  for (int i = 1; i < 4; i++) {
-    cleric.selfAid();
-    print('===== selfAid() 발동 $i =====');
-    print('${cleric.hp}, ${cleric.mp}');
-  }
-
-  for (int i = 1; i < 4; i++) {
-    print('===== pray() 발동 $i =====');
-    int? recoveryMp = cleric.pray(5);
-    print('${cleric.hp}, ${cleric.mp}, 회복량: $recoveryMp');
-  }
+  final cleric = Cleric('성직자1');
+  print('이름 ${cleric.name});
 }
