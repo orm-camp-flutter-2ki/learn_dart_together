aa3b88157e39b9d0b6e5ece62c586967e1aa232b
jin song <songjin3914@gmail.com>
2024-03-08 14:09:28 +0900

    Feat: 상수 static로 선언
    - 상수 static로 선언

--- lib/240307/cleric.dart
+++ lib/240307/cleric.dart
@@ -4,10 +4,11 @@ class Cleric {
   String name;
   int hp;
   int mp;
-  final int maxHp = 50;
-  final int maxMp = 10;
+  static final int maxHp = 50;
+  static final int maxMp = 10;
 
   Cleric(this.name, this.hp, this.mp);
+
 
   int spendMp = 5;
 
