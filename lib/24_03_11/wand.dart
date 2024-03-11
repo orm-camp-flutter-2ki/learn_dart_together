4029b405d89b96216b3ba77c98db9069920420aa
byjun8932 <byjun8932@gmail.com>
2024-03-11 21:00:48 +0900

    feat: 1. 마법사나 지팡이의 이름은 null 일 수 없고, 반드시 3문자 이상이어야 한다

--- lib/24_03_11/wand.dart
+++ lib/24_03_11/wand.dart
@@ -1,9 +1,18 @@
 class Wand {
-  String name;
+  String _name;
   double power;
 
   Wand({
-    required this.name,
+    required String name, // Wand 클래스의 name은 다른 클래스에서 건드리지 못하게 접급제한자 붙이기
     required this.power,
-  });
+  }):_name = name;
+
+  String get name => _name;
+
+  set name(String wandName) {
+    if (wandName.length <= 3) {
+      throw Exception('마법사의 이름은 반드시 3글자 이상이어야 합니다.');
+    }
+    _name = wandName;
+  }
 }
--- lib/24_03_11/wizard.dart
+++ lib/24_03_11/wizard.dart
@@ -1,13 +1,23 @@
 import 'package:learn_dart_together/24_03_11/wand.dart';
 
 class Wizard {
-  String name;
+  String _name;
   int hp;
   Wand? wand;
 
   Wizard({
-    required this.name,
+    required String name, // Wizard 클래스의 name 필드는 다른 클래스에서 건드리지 못하게 접근 제한자 붙이기 , ex) 지팡이가(사물) 마법사의 이름을 건드릴 수 없다?
     required this.hp,
     this.wand,
-  });
+  }): _name = name;
+
+  String get name => _name;
+
+  set name(String wizardName) {
+    if (wizardName.length <= 3) {
+      throw Exception('마법사의 이름은 반드시 3글자 이상이어야 합니다.');
+    }
+    _name = wizardName;
+  }
 }
+
