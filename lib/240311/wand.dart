80bb7e388b435115e849f30624c1afe8b1748f27
KimDonghyeok <qoophon@gmail.com>
2024-03-11 20:23:54 +0900

    feat: Wand 클래스 수정
    - name, power setter 프로퍼티 수정

--- lib/240311/wand.dart
+++ lib/240311/wand.dart
@@ -4,7 +4,7 @@ class Wand {
 
   Wand(this._name, this._power);
 
-  set name(String value) {
+  set name(String? value) {
     if (value == null) {
       throw Exception('이름은 null 일 수 없습니다.');
     }
@@ -12,11 +12,15 @@ class Wand {
     if (value.length < 3) {
       throw Exception('이름은 반드시 3글자 이상이어야 합니다.');
     }
+
+    _name = value;
   }
 
   set power(double value) {
     if (value < 0.5 || value > 100.0) {
       throw Exception('지팡이의 마력이 너무 낮거나 높습니다.');
     }
+
+    _power = value;
   }
 }
