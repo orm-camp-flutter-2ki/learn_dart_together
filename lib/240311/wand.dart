3a8d5d16454ad5e0f79aaa403d5fa189f7405fee
KimDonghyeok <qoophon@gmail.com>
2024-03-11 16:45:47 +0900

    feat: Wand 클래스 수정 (연습문제1-2)
    - name, power 필드 private 으로 변경
    - name, power 필드의 set 프로퍼티 생성, 조건에 따른 유효성 검사

--- lib/240311/wand.dart
+++ lib/240311/wand.dart
@@ -1,9 +1,22 @@
 class Wand {
-  String name; // 이름
-  double power; // 마력
+  String _name;
+  double _power;
 
-  Wand({
-    required this.name,
-    required this.power
-  });
+  Wand(this._name, this._power);
+
+  set name(String value) {
+    if (value == 'null') {
+      throw Exception('이름은 null 일 수 없습니다.');
+    }
+
+    if (value.length < 3) {
+      throw Exception('이름은 반드시 3글자 이상이어야 합니다.');
+    }
+  }
+
+  set power(double value) {
+    if (value < 0.5 || value > 100.0) {
+      throw Exception('지팡이의 마력이 너무 낮거나 높습니다.');
+    }
+  }
 }
