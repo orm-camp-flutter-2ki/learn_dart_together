f6974fdf3d972b122e0d1bd0b0e54de8b8d8b3f6
byjun8932 <byjun8932@gmail.com>
2024-03-11 21:45:56 +0900

    feat: 5. HP가 음수가 되는 상황에서는 대신 0을 설정 되도록 한다. (에러 아님)

--- lib/24_03_11/wizard.dart
+++ lib/24_03_11/wizard.dart
@@ -2,16 +2,16 @@ import 'package:learn_dart_together/24_03_11/wand.dart';
 
 class Wizard {
   String _name;
-  int hp;
+  int _hp;
   int _mp;
   Wand wand;
 
   Wizard({
     required String name, // Wizard 클래스의 name 필드는 다른 클래스에서 건드리지 못하게 접근 제한자 붙이기 , ex) 지팡이가(사물) 마법사의 이름을 건드릴 수 없다?
-    required this.hp,
+    required int hp,
     required this.wand, // wand 는 not null
     required int mp,
-  }): _name = name, _mp = mp;
+  }): _name = name, _mp = mp, _hp = hp;
 
   // ========== name의 getter / setter ==========
   String get name => _name;
@@ -30,6 +30,17 @@ class Wizard {
     if (magicianMp < 0) {
       throw Exception('마법사의 mp는 0 이상이어야 합니다');
     }
+    _mp = mp;
+  }
+
+  // ========== hp의 getter / setter ==========
+  int get hp => _hp;
+
+  set hp(int wizardHp) {
+      if (wizardHp < 0) {
+        _hp = 0;
+      }
+    _hp = hp;
   }
 }
 
