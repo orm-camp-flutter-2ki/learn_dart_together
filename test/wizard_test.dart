1eb6b7f1a8edae101e2ecde90659b2ffbf689d40
woong <workplayhard1@naver.com>
2024-03-11 23:28:39 +0900

    test: wizard.mp가 음수일 때 0으로 초기화되는지

---
+++ test/wizard_test.dart
@@ -0,0 +1,14 @@
+import 'package:learn_dart_together/240309/wand.dart';
+import 'package:learn_dart_together/240309/wizard.dart';
+import 'package:test/test.dart';
+
+main() {
+  group('Wizard', () {
+    test('MP가 음수일 때, 0으로 초기화되는지', () {
+      final wand = Wand(name: 'name', power: 10);
+      final wizard = Wizard(name: 'name', hp: 50, mp: -100, wand: wand);
+
+      expect(wizard.mp, 0);
+    });
+  });
+}
