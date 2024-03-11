af8c18524dcaa7d6cc347edccb484ae36ad6335d
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-11 16:10:12 +0900

    feat: wand 클래스를 테스트 파일 입니다. 테스트 내용은 아래와 같습니다. 1) wand의 name 길이는 3글자 이상이어야 한다. 2) wand의 power 범위는 5.0이상 100.0이하여야 한다.

--- test/0308/wand_test.dart
+++ test/0308/wand_test.dart
@@ -2,19 +2,21 @@ import 'package:learn_dart_together/0308/wand.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
-  // wand의 마력은 5.0 이상 100.0 이하
-  test('wand의 마력 범위 테스트', () {
+  test('wand의 이름 범위 테스트', () {
+    // wand의 이름은 3글자 이상
     Wand wand = Wand(name: 'magic stick', power: 14.0);
 
-    // 0.5 이상
-    // wand.power = 0.0;  // error
-    wand.power = 0.4;  // error
-    // wand.power = 0.5;  // not error
+    // 오류 테스트
+    // wand.name = 'dd'; // error
+    wand.name = 'ddd'; // not error
 
-    // 100.0 이하
+    // wand02.power = 0.0;  // error
+    // wand02.power = 0.4; // error
+    // wand02.power = 0.5;  // not error
+
+    // [2] 100.0 이하
     // wand.power = 100.0; // not error
     // wand.power = 100.1; // error
     // wand.power = 1234.3; // error
-
   });
 }
