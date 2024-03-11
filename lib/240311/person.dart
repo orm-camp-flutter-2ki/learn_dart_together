01776eb9e3b22e20410965f05686bade4f051564
KimDonghyeok <qoophon@gmail.com>
2024-03-11 19:34:16 +0900

    feat: Person 클래스 생성 및 작성 (캡슐화 - 연습문제 2) (컬렉션 - 연습문제 2,3)
    - name, birthYear 필드 추가
    - name, birthYear 필드 private 지정
    - name getter 프로퍼티 추가
    - age getter 프로퍼티 추가

---
+++ lib/240311/person.dart
@@ -0,0 +1,12 @@
+class Person {
+  final String _name;
+  final int _birthYear;
+
+  Person(this._name, this._birthYear);
+
+  String get name => _name;
+
+  int get age {
+    return DateTime.now().year - _birthYear;
+  }
+}
