389e8b50a3ad06bf562b55b54e595aa315b7841f
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-11 16:37:08 +0900

    feat: wizard 클래스를 테스트 파일 입니다. 테스트 내용은 아래와 같습니다. 1) wizard의 name 길이는 3글자 이상이어야 한다. 2) wizard의 mp는 0이상이어야 합니다. 2) wizard의 hp는 0이상 입니다.

--- test/0308/wizard_test.dart
+++ test/0308/wizard_test.dart
@@ -5,21 +5,27 @@ import 'package:test/scaffolding.dart';
 void main() {
 
   test('마법사와 지팡이 이름 길이 테스트', () {
+    // 생성자
     Wand wand01 = Wand(name: 'd', power: 30.3);
     Wizard wizard = Wizard(name: 'd', hp: 40, mp: 10, wand: wand01);
 
     // 마법사 이름 3글자 이하
     // wizard.name = 'w'; // error
-    // wizard.name = 'ww'; // error
-    wizard.name = 'ww2'; // not error
+    wizard.name = 'ww'; // error
+    // wizard.name = 'ww2'; // not error
 
-    // 지팡이 이름 3글자 이하
   });
 
-  test('마법사 hp 범위 테스트', () {
+  test('마법사 mp 범위 테스트', () {
+    // 생성자
     Wand wand02 = Wand(name: 'd', power: 30.3);
     Wizard wizard2 = Wizard(name: 'water stick', hp: 44, mp: 30, wand: wand02);
 
-    wizard2.mp = 30;
+    wizard2.hp = -19;
+    print(wizard2.hp);
+    // 마법사의 mp는 0 이상으로 설정해야 한다
+    // wizard2.mp = -19; // error
+    // wizard2.mp = 0; // not error
+    // wizard2.mp = 30; // not error
   });
 }
