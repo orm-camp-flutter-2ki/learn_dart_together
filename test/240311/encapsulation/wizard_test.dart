018e6b701e3b9854bcb594be6303ffe3856b7374
chojja7188 <chojja7188@gmail.com>
2024-03-11 19:08:46 +0900

    feat: Wizard 테스트 코드 내용 추가

--- test/240311/encapsulation/wizard_test.dart
+++ test/240311/encapsulation/wizard_test.dart
@@ -6,7 +6,9 @@ void main() {
   test('Wizard Test', () {
     Wand wand = Wand('지팡팡이', 30);
     Wizard wizard = Wizard('박세훈', 30, -2, wand);
-    // wizard.name = '짧게';
-    // wizard.mp = -3;
+
+    wizard.name = '홍길동';
+    expect(wizard.name, '홍길동');
+    expect(() => wizard.mp = -3, throwsException);
   });
 }
