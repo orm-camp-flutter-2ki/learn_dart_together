999470ac2d8b647859c781c53c696b42c643d7a9
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-11 15:48:57 +0900

    feat: wand 클래스를 작성했습니다.

--- lib/0308/wand.dart
+++ lib/0308/wand.dart
@@ -1,9 +1,21 @@
 class Wand {
-  String name;
-  int damage;
+  String name; // 이름
+  double _power; // 마력
 
   Wand({
     required this.name,
-    required this.damage,
-  });
+    required double power,
+  }) : _power = power;
+
+  // getter
+  double get power => _power;
+
+  // setter
+  // 지팡이의 마력은 0.5 이상 100.0 이하여야 한다
+  set power (double powerValue) {
+    if(powerValue < 0.5 || powerValue > 100.0) {
+      throw Exception('wand의 마력은 5.0 이상 100.0 이하로 설정해주세요.');
+    }
+    _power = powerValue;
+  }
 }
