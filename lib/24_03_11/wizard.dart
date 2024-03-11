46da5efd399fafd9137d09532974152b9d95fac5
byjun8932 <byjun8932@gmail.com>
2024-03-11 21:28:01 +0900

    feat: 3. 마법사의 지팡이는 null 일 수 없다.

--- lib/24_03_11/wizard.dart
+++ lib/24_03_11/wizard.dart
@@ -3,12 +3,12 @@ import 'package:learn_dart_together/24_03_11/wand.dart';
 class Wizard {
   String _name;
   int hp;
-  Wand? wand;
+  Wand wand;
 
   Wizard({
     required String name, // Wizard 클래스의 name 필드는 다른 클래스에서 건드리지 못하게 접근 제한자 붙이기 , ex) 지팡이가(사물) 마법사의 이름을 건드릴 수 없다?
     required this.hp,
-    required this.wand,
+    required this.wand, // wand 는 not null
   }): _name = name;
 
   String get name => _name;
