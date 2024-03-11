1e1bf911b991675f1dfe16aef30221d682a451f1
nala0897@naver.com <nala0897@naver.com>
2024-03-11 18:25:22 +0900

    fix: 컬렌션 연습문제 2 test code 수정

--- test/lib/240311/01_collection/practice_2_test.dart
+++ test/lib/240311/01_collection/practice_2_test.dart
@@ -2,20 +2,38 @@ import 'package:learn_dart_together/240311/01_collection/practice_2.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Person test', () {
 
-    Person hong = Person(name: '홍길동');
-    Person han = Person(name: '한석봉');
+  // Given(준비)
+  String hong = '홍길동';
+  String han = '한석봉';
 
-    List<Person> people = [];
-    people.add(hong);
-    people.add(han);
+
+  test('Person 클래슨느 반드시 이름을 포함한다.', () {
+
+    // When(실행)
+    Person person1 = Person(name: hong);
+    Person person2 = Person(name: han);
+
+    // Then(검증)
+    expect(person1, isNotNull);
+    expect(person2, isNotNull);
+  });
+
+
+  test('List 에 담긴 Person 인스턴스의 이름을 모두 표시하시오.', () {
+
+    // When(실행)
+    Person person1 = Person(name: hong);
+    Person person2 = Person(name: han);
+
+    List<Person> peopleList = [];
+    peopleList.add(person1);
+    peopleList.add(person2);
 
     // iterator 활용
-    final iterator = people.iterator;
+    final iterator = peopleList.iterator;
     while( iterator.moveNext() ) {
-      print(iterator.current.name);
+      print('peopleList > ${iterator.current.name}');
     }
-
   });
 }
