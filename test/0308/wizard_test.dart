a78dc18a52981687cc21420123d4cff6b048541e
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-11 16:09:10 +0900

    feat: wand 클래스를 테스트 파일 입니다. 테스트 내용은 아래와 같습니다. 1) wand의 name 길이는 3글자 이상이어야 한다. 2) wand의 power 범위는 5.0이상 100.0이하여야 한다.

--- test/0308/wizard_test.dart
+++ test/0308/wizard_test.dart
@@ -1,11 +1,25 @@
+import 'package:learn_dart_together/0308/wand.dart';
 import 'package:learn_dart_together/0308/wizard.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
-  test('마법사와 지팡이 이름 값 테스트', () {
-    Wizard wizard = Wizard(name: 'd', hp: 40, mp: 10);
 
-    wizard.name = 'w';
+  test('마법사와 지팡이 이름 길이 테스트', () {
+    Wand wand01 = Wand(name: 'd', power: 30.3);
+    Wizard wizard = Wizard(name: 'd', hp: 40, mp: 10, wand: wand01);
 
+    // 마법사 이름 3글자 이하
+    // wizard.name = 'w'; // error
+    // wizard.name = 'ww'; // error
+    wizard.name = 'ww2'; // not error
+
+    // 지팡이 이름 3글자 이하
+  });
+
+  test('마법사 hp 범위 테스트', () {
+    Wand wand02 = Wand(name: 'd', power: 30.3);
+    Wizard wizard2 = Wizard(name: 'water stick', hp: 44, mp: 30, wand: wand02);
+
+    wizard2.mp = 30;
   });
 }
