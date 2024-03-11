7fc6d162d8db179be6e48df2509964548c6b2bf2
gyubit <gyubros@gmail.com>
2024-03-12 01:06:41 +0900

    Edit

--- lib/240311/personMap.dart
+++ lib/240311/personMap.dart
@@ -6,15 +6,23 @@ class Person {
 }
 
 void main() {
-  Map<String, dynamic> herosMap = {};
+  List<Map<String, dynamic>> heroList = [];
 
   Person personA = Person('홍길도', 20);
-  herosMap['name'] = personA.name;
-  herosMap['age'] = personA.age;
+  Map<String, dynamic> hero1 = {'name': personA.name, 'age': personA.age};
 
   Person personB = Person('한석봉', 25);
-  herosMap['name'] = personB.name;
-  herosMap['age'] = personB.age;
+  Map<String, dynamic> hero2 = {'name': personB.name, 'age': personB.age};
 
-  print(herosMap.addAll);
+  // hero1,2를 List에 추가
+  heroList.add(hero1);
+  heroList.add(hero2);
+
+  // List 안의 map 순차적으로 출력하기
+  for (Map<String, dynamic> heroMap in heroList) {
+    String name = heroMap['name']; // Map에서 "name" 키에 해당하는 값 축출 -> 변수지정
+    int age = heroMap['age']; // Map에서 "age" 키에 해당하는 값 축출 -> 변수지정
+
+    print('$name의 나이는 $age세입니다.');
+  }
 }
