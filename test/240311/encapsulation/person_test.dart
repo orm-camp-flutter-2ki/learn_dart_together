6ae6eabfb5517ad24f996cef6d975f3e4614d6ed
chojja7188 <chojja7188@gmail.com>
2024-03-11 15:30:11 +0900

    style: 테스트 명칭에 맞게 수정

--- test/240311/encapsulation/person_test.dart
+++ test/240311/encapsulation/person_test.dart
@@ -2,7 +2,7 @@ import 'package:learn_dart_together/240311/encapsulation/person.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Wizard Test', () {
+  test('Person Test', () {
     Person person = Person('박세훈', 2001);
     print(person.age);
   });
