2487f12fa1f1c94066c5c3bb561e132ee45681b3
hee462 <a01080809887@gmail.com>
2024-03-11 20:14:28 +0900

    feat : 문제지문 작성

--- lib/240311/person collection.dart
+++ lib/240311/person collection.dart
@@ -1,3 +1,8 @@
+//다음을 수행하는 코드를 작성하시오.
+// 이름을 가지는 Person 클래스를 작성하시오. Person 은 반드시 이름을 포함해야 합니다.
+// 이름이 ‘홍길동', ‘한석봉' 인 Person 인스턴스를 생성하고, List에 담습니다.
+// List에 담긴 모든 Person 인스턴스의 이름을 표시하시오.
+
 class Person {
   final String _name;
   final int _age;
--- lib/test/240311/person collection_test.dart
+++ lib/test/240311/person collection_test.dart
@@ -1,4 +1,7 @@
-
+//연습문제 2 에서 작성한 Person 클래스로 생성한 ‘홍길동’, ‘한석봉'의 나이를 각각 20, 25살 이라고 할 때, 이름과 나이를 쌍으로 적당한 컬렉션에 넣습니다.
+// 그 다음, 컬렉션에 저장한 값을 하나씩 다음과 같이 출력합니다.
+// “홍길동의 나이는 20살”
+// “한석봉의 나이는 25살”
 import 'package:learn_dart_together/240311/person collection.dart';
 import 'package:test/test.dart';
 
