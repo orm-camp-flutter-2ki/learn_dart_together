1a3d90ee9f9c866671c7af2bfb9e729c58ab4e6a
chojja7188 <chojja7188@gmail.com>
2024-03-11 14:16:27 +0900

    feat: 클래스 생성
    - Wand 클래스, Wizard 클래스 생성

---
+++ lib/240311/wizard.dart
@@ -0,0 +1,12 @@
+import 'package:learn_dart_together/240311/wand.dart';
+
+class Wizard {
+  String name;
+  int hp;
+  Wand? wand;
+
+  Wizard({
+    required this.name,
+    required this.hp
+  });
+}
