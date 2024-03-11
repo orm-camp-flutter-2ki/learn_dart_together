9a357f8de0853d0cbf193bb9c01c5e3dc0a92cac
woong <workplayhard1@naver.com>
2024-03-11 14:29:46 +0900

    feat: PRA01.collection 구현

---
+++ lib/240309/collection.dart
@@ -0,0 +1,23 @@
+class Collection {
+  final Set<String> cityInKorea = {
+    'seoul',
+    'incheon',
+    'daejeon',
+    'daegu',
+    'busan'
+  };
+
+  final List<int> testPoint = [
+    65,
+    80,
+    90,
+    95
+  ];
+
+  final Map<String, int> cityPopulation = {
+    '서울': 100000,
+    '인천': 200000,
+    '대전': 300000,
+    '대구': 400000
+  };
+}
