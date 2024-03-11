322590933a3c13d54a0fb395f03d98c787e58ae5
woong <workplayhard1@naver.com>
2024-03-11 23:30:20 +0900

    chore: given when then 주석 마크

--- test/wand_test.dart
+++ test/wand_test.dart
@@ -3,10 +3,12 @@ import 'package:test/test.dart';
 
 void main() {
   group('Wand', () {
+    // given when then
     test('name 속성이 3글자 미만일 때 예외를 발생시키는가', () {
       expect(() => Wand(name: 'tw', power: 10), throwsException);
     });
 
+    // given when then
     test('name 속성이 3글자 이상일 때 정상값으로 초기화하는가', () {
       expect(() => Wand(name: 'twWand', power: 100), returnsNormally);
     });
--- test/wizard_test.dart
+++ test/wizard_test.dart
@@ -5,9 +5,11 @@ import 'package:test/test.dart';
 main() {
   group('Wizard', () {
     test('MP가 음수일 때, 0으로 초기화되는지', () {
+      // given when
       final wand = Wand(name: 'name', power: 10);
       final wizard = Wizard(name: 'name', hp: 50, mp: -100, wand: wand);
 
+      // then
       expect(wizard.mp, 0);
     });
   });
