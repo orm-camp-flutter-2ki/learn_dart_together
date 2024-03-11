3f18b81547b3fa328881500fc4743f3de17fbb77
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-11 16:41:00 +0900

    feat: wizard 클래스를 파일 내용 추가 추가 내용 1) wizard의 name 길이는 3글자 이상이어야 한다. 2) wizard의 hp가 음수라면 0으로 설정합니다. 3) wizard의 mp는 0 이상 이어야 합니다.

--- test/0308/wizard_test.dart
+++ test/0308/wizard_test.dart
@@ -16,16 +16,26 @@ void main() {
 
   });
 
-  test('마법사 mp 범위 테스트', () {
-    // 생성자
+  test('마법사 hp 음수라면 0으로 출력되는지 체크', () {
     Wand wand02 = Wand(name: 'd', power: 30.3);
     Wizard wizard2 = Wizard(name: 'water stick', hp: 44, mp: 30, wand: wand02);
 
-    wizard2.hp = -19;
+    // wizard2.hp = -3; // 0 출력
+    // wizard2.hp = -1; // 0 출력
+    // wizard2.hp = 0; // 0 출력
+    wizard2.hp = 3; // 3 출력
     print(wizard2.hp);
+
+  });
+
+  test('마법사 mp 범위 테스트', () {
+    // 생성자
+    Wand wand03 = Wand(name: 'd', power: 30.3);
+    Wizard wizard3 = Wizard(name: 'water stick', hp: 44, mp: 30, wand: wand03);
+
     // 마법사의 mp는 0 이상으로 설정해야 한다
-    // wizard2.mp = -19; // error
-    // wizard2.mp = 0; // not error
-    // wizard2.mp = 30; // not error
+    // wizard3.mp = -19; // error
+    // wizard3.mp = 0; // not error
+    // wizard3.mp = 30; // not error
   });
 }
