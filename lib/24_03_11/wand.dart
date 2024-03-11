ba95a3f449f1616a5e8588098cf1f701b9ff477a
SeongJongHo <whdgh9595@gmail.com>
2024-03-11 15:44:17 +0900

    [MODIFY] Wand, Wizard 클래스 유효성 검사 로직 추가

--- lib/24_03_11/wand.dart
+++ lib/24_03_11/wand.dart
@@ -1,6 +1,27 @@
 class Wand {
-  String _name;
-  int _power;
+  late final String _name;
+  late final double _power;
 
-  Wand(this._name, this._power);
+  Wand({required String name, required double power}) {
+    _name = _checkNameValidity(name);
+    _power = _checkPowerValidity(power);
+  }
+
+  String get name => _name;
+
+  double get power => _power;
+
+  String _checkNameValidity(String name) {
+    if (name.length < 3) _exceptionHandler('이름은 3글자 이상이어야 합니다.');
+
+    return name;
+  }
+
+  double _checkPowerValidity(double power) {
+    return power < 0.5 ? 0.5 : power;
+  }
+
+  void _exceptionHandler(String message) {
+    throw Exception(message);
+  }
 }
--- lib/24_03_11/wizard.dart
+++ lib/24_03_11/wizard.dart
@@ -1,9 +1,33 @@
 import 'wand.dart';
 
 class Wizard {
-  String _name;
-  int _hp;
-  Wand? _wand;
+  late final String _name;
+  late final int _hp;
+  final Wand _wand;
 
-  Wizard(this._name, this._hp, this._wand);
+  Wizard({required String name, required int hp, required Wand wand})
+      : _wand = wand {
+    _name = _checkNameValidity(name);
+    _hp = _checkHPValidity(hp);
+  }
+
+  String get name => _name;
+
+  int get hp => _hp;
+
+  Wand get wand => _wand;
+
+  int _checkHPValidity(int hp) {
+    return hp < 0 ? 0 : hp;
+  }
+
+  String _checkNameValidity(String name) {
+    if (name.length < 3) _exceptionHandler('이름은 3글자 이상이어야 합니다.');
+
+    return name;
+  }
+
+  void _exceptionHandler(String message) {
+    throw Exception(message);
+  }
 }
