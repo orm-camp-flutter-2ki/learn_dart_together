8b83ba904f52e408d5996b5f2440ec134937b222
byjun8932 <byjun8932@gmail.com>
2024-03-11 22:35:42 +0900

    feat: wizard 클래스의 테스트 코드 작성

---
+++ test/24_03_11/wizard_test.dart
@@ -0,0 +1,13 @@
+
+import 'package:learn_dart_together/24_03_11/wand.dart';
+import 'package:learn_dart_together/24_03_11/wizard.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Wizard Test', () {
+    Wand wand = Wand(name: "츄츄", power: 10);
+    Wizard w1 = Wizard(name: '루루', hp: 100, mp: 50, wand: wand);
+
+    w1.hp = -100;
+  });
+}
