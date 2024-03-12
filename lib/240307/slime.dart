d36df72d70382ee190150e5b2d9186a077fb92ed
KimDonghyeok <qoophon@gmail.com>
2024-03-12 23:06:31 +0900

    feat: Slime 클래스 수정 (상속 연습문제)
    - 연습문제의 예시와 같이 클래스 수정

--- lib/240307/slime.dart
+++ lib/240307/slime.dart
@@ -1,10 +1,16 @@
+import 'package:dart_cli_practice/240307/hero.dart';
+
 class Slime {
-  final int level = 10;
+  int hp = 50;
+  final String suffix;
 
-  int hp;
-  String suffix;
+  Slime({required this.suffix});
 
-  Slime(this.hp, this.suffix);
+  void attack(Hero hero) {
+    print('슬라임 $suffix 이/가 공격했다.');
+    print('10의 데미지');
+    hero.hp -= 10;
+  }
 
   void run() {
     print('[슬라임 $suffix 가 도망쳤다.]');
