9fa38418d17b4a3bb3848cf9c201fbe5db8143d5
kimhyungmin <jeen14rla@gmail.com>
2024-03-12 15:36:22 +0900

    feat: dog, cat, bird 클래스 속성 및 메서드 추가

--- lib/24_03_12/practice_04/bird.dart
+++ lib/24_03_12/practice_04/bird.dart
@@ -1,8 +1,13 @@
 import 'package:learn_dart_together/24_03_12/practice_04/animal.dart';
 
 class Bird extends Animal {
+  String name;
+  String featherColor;
+
   Bird({
     required super.type,
     required super.sound,
+    required this.name,
+    required this.featherColor,
   });
 }
--- lib/24_03_12/practice_04/cat.dart
+++ lib/24_03_12/practice_04/cat.dart
@@ -1,8 +1,15 @@
 import 'package:learn_dart_together/24_03_12/practice_04/animal.dart';
 
 class Cat extends Animal {
+  String name;
+
   Cat({
     required super.type,
     required super.sound,
+    required this.name,
   });
+
+  void meow() {
+    print('meow!');
+  }
 }
