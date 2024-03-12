c5dd86c80d8d84941c3569ecc712063977c658c9
Yejin.Shin <mingmi_27@naver.com>
2024-03-12 18:40:13 +0900

    feat: modifying class and diagram

--- lib/240312/book.dart
+++ lib/240312/book.dart
@@ -1,8 +1,8 @@
 void main() {
-  Fiction fiction = Fiction('엄마는 외계인', '베라', 'SF', '미스테리');
-  NonFiction nonFiction = NonFiction('Java를 잡아라', '엄준식', '자기계발서', 'IT');
-  print(fiction);
-  print(nonFiction);
+  Fiction fiction = Fiction('엄마는 외계인', '베라', 'SF', '미스테리','엄마');
+  NonFiction nonFiction = NonFiction('Java를 잡아라', '엄준식', '자기계발서', 'IT',7000);
+  fiction.summary();
+  nonFiction.summary();
 }
 
 class Book {
@@ -12,19 +12,33 @@ class Book {
 
   Book(this.title, this.author, this.genre);
 
-  void summary() {}
+  void summary() {
+    print("이 책의 제목은 '$title' 이다.'$author'에 의해 작성되었으며 $genre이다.");
+  }
 }
 
 class Fiction extends Book {
-  //주인공
-  String subgenre;
 
-  Fiction(super.title, super.author, super.genre, this.subgenre);
+  String subgenre;
+  String hero;
+  Fiction(super.title, super.author, super.genre, this.subgenre,this.hero);
+
+  @override
+  void summary(){
+    super.summary();
+    print("주인공 '$hero'의 이야기 이다.");
+  }
 }
 
 class NonFiction extends Book {
-  //토탈세일즈
-  String subject;
 
-  NonFiction(super.title, super.author, super.genre, this.subject);
+  String subject;
+  int totalSales;
+
+  NonFiction(super.title, super.author, super.genre, this.subject,this.totalSales);
+  @override
+  void summary(){
+    super.summary();
+    print("약 $totalSales부가 팔렸다.");
+  }
 }
