810cd2c4cf032aea32504f9572d3ffcc8e16e451
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-12 21:08:49 +0900

    feat: create cat class

--- lib/0312/02/cat.dart
+++ lib/0312/02/cat.dart
@@ -1,6 +1,10 @@
-import 'package:learn_dart_together/0312/animal.dart';
+import 'animal.dart';
 
 class Cat extends Animal {
 
-  Cat(super.type, super.sound);
+  Cat(super.type, super.sound, super.leg);
+
+  void groom() {
+    print('${super.type}가 털을 정돈한다.');
+  }
 }
