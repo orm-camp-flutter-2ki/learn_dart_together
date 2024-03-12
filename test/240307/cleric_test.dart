0dfc606fe52c9be6cb85a8a50e6f3eeae3bddd96
Stranger Yang <ccamdol@gmail.com>
2024-03-12 21:58:59 +0900

    Test 코드 연습

---
+++ test/240307/cleric_test.dart
@@ -0,0 +1,20 @@
+import 'package:learn_dart_together/240307/cleric.dart';
+import 'package:test/test.dart';
+
+void main(){
+  test('Cleric test', (){
+    Cleric cleric = Cleric('홍길동');
+
+    expect(cleric.hp, Cleric.maxhp);
+
+
+    cleric.hp = 1;
+
+    cleric.selfAid();
+
+    expect(cleric.hp, Cleric.maxhp);
+
+
+  });
+
+}
