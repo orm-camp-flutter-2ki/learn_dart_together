f25e7445047b1624953c904cc95b548369f394f8
nala0897@naver.com <nala0897@naver.com>
2024-03-12 14:00:33 +0900

    feat: 새 Slime 과 Hero class 생성

---
+++ lib/240312/Slime.dart
@@ -0,0 +1,16 @@
+import 'hero.dart';
+
+class Slime {
+
+  int hp = 50;
+  final String suffix;
+
+  Slime(this.suffix);
+
+  void attack(Hero hero) {
+    print('슬람인$suffix이/가 공격했다.');
+    print('10의 데미지');
+
+    hero.hp = -10;
+  }
+}
---
+++ lib/240312/hero.dart
@@ -0,0 +1,40 @@
+import 'package:learn_dart_together/240311/00_encapsulation/king.dart';
+
+import 'slime.dart';
+
+class Hero {
+  String name;
+  int _hp;
+
+  Hero({required this.name, required int hp}) : _hp = hp;
+
+
+  int get hp => _hp;
+  set hp(int value) {
+    _hp = value;
+  }
+
+  // 인사
+  void bye() {
+    print('Bye ~');
+  }
+
+
+  // 공격
+  void attack(Slime slime) {
+
+    print('슬라임의 반격을 받았다.');
+
+    _hp -= 5;
+
+    if( _hp <= 0 ) {
+      _die();
+    }
+  }
+
+
+  // 사망
+  void _die() {
+    print('죽었다.');
+  }
+}
