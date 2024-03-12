5c617527054f2a89ad17293e2b457afad5189efa
chojja7188 <chojja7188@gmail.com>
2024-03-12 22:44:58 +0900

    feat: rectangle_test 테스트 코드 expect 추가

--- test/240312/rectangle_test.dart
+++ test/240312/rectangle_test.dart
@@ -7,5 +7,17 @@ void main() {
     Rectangle rectangle = Rectangle(topLeft: Point(2, 3), bottomRight: Point(4, 9));
     print('직사각형의 넓이 : ${rectangle.getArea()}');
     print('직사각형의 둘레 : ${rectangle.getRound()}');
+
+    expect(rectangle.getArea(), rectangle.width * rectangle.height);
+    expect(rectangle.getRound(), (rectangle.width + rectangle.height) * 2);
+  });
+
+  test('좌표에 음수가 들어갈 경우', () {
+    Rectangle rectangle = Rectangle(topLeft: Point(-3, 6), bottomRight: Point(4, -2));
+    print('직사각형의 넓이 : ${rectangle.getArea()}');
+    print('직사각형의 둘레 : ${rectangle.getRound()}');
+
+    expect(rectangle.getArea(), rectangle.width * rectangle.height);
+    expect(rectangle.getRound(), (rectangle.width + rectangle.height) * 2);
   });
 }
