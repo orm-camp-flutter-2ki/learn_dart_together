2d2f348eeff50c7ddf07788eed35e5b0be2f664d
YunZi.Song <yoonji5809@gmail.com>
2024-03-12 15:56:30 +0900

    fix: wand 테스트 코드 수정

--- test/24_03_11/wand_test.dart
+++ test/24_03_11/wand_test.dart
@@ -3,25 +3,26 @@ import 'package:test/test.dart';
 
 void main() {
   group('wand class', () {
-
     Wand wand = Wand(0.4, 50, name: '지팡이');
 
     test('name property', () {
       // 이름 변경 확인하기
       expect(wand.name = '지', throwsException);
+      expect(() => wand.name = '지팡', throwsException);
+      expect(() => wand.name = '지팡이야', returnsNormally);
     });
 
     test('MP property', () {
+      Wand wand = Wand(0.4, 50, name: '지팡이');
+
       // 마력이 제대로 변경되는지 확인
-       expect(wand.mp = 80.0, 80.0);
+      expect(wand.mp = 80.0, 80.0);
 
       // 유효하지 않은 마력 값(0.5 미만)을 설정하면 예외가 발생하는지 확인
-      expect(wand.mp = 0.3, throwsException);
+      expect(() => wand.mp = 0.3, throwsException);
 
       // 유효하지 않은 마력 값(100.0 초과)을 설정하면 예외가 발생하는지 확인
-      expect(wand.mp = 110.0, throwsException);
+      expect(() => wand.mp = 110.0, throwsException);
     });
-
   });
-
 }
