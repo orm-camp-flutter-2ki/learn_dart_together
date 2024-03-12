7e32f42e93dd21ed26a275d0a86c57eb154528e3
KimDonghyeok <qoophon@gmail.com>
2024-03-12 11:11:13 +0900

    test: Wizard 테스트 코드 추가 (캡슐화 연습문제)
    - name.length > 3 테스트 코드 추가
    - hp < 0 테스트 코드 추가
    - mp < 0 테스트 코드 추가

--- test/240311/wizard_test.dart
+++ test/240311/wizard_test.dart
@@ -3,26 +3,34 @@ import 'package:dart_cli_practice/240311/wizard.dart';
 import 'package:test/test.dart';
 
 void main() {
-
   test('wizard name test', () {
     // give
     Wand wand = Wand("지팡이", 10);
-    Wizard w1 = Wizard('멀린', 100, 50, wand);
+    Wizard w1 = Wizard('김멀린', 100, 50, wand);
 
-    w1.name = null;
+    expect(() => w1.name = '멀린', throwsException); // Exception 발생을 기대
+    expect(() => w1.name = '박멀린', returnsNormally); // 정상 처리 됨을 기대
   });
 
   test('wizard hp test', () {
+    // give
     Wand wand = Wand("지팡이", 10);
-    Wizard w1 = Wizard('멀린', 100, 50, wand);
+    Wizard w1 = Wizard('박멀린', 100, 50, wand);
 
-    w1.hp = -100;
+    // when, then
+    expect(() => w1.hp = -200, returnsNormally); // 정상 처리 됨을 기대
+
+    w1.hp = -200;
+    expect(w1.hp, 0);
   });
 
   test('wizard mp test', () {
+    // give
     Wand wand = Wand("지팡이", 10);
-    Wizard w1 = Wizard('멀린', 100, 50, wand);
+    Wizard w1 = Wizard('최멀린', 100, 50, wand);
 
-    w1.mp = -10;
+    // when, then
+    expect(() => w1.mp = -10, throwsException); // Exception 발생을 기대
+    expect(() => w1.mp = 10, returnsNormally); // 정상 처리 됨을 기대
   });
 }
