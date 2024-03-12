18e0519a056b203a9ab743362aa896f3cb13c70a
nala0897@naver.com <nala0897@naver.com>
2024-03-12 16:19:07 +0900

    fix: RectangleExtendsPoint class 의 면적, 둘레 계산 함수 수정

--- lib/240312/practice_5/rectangle_extends_point.dart
+++ lib/240312/practice_5/rectangle_extends_point.dart
@@ -1,30 +1,21 @@
 import 'dart:math';
 
-class Rectangle extends Point {
+class RectangleExtendsPoint extends Point {
 
   int width;
   int height;
 
-  Rectangle(double super.x, double super.y, this.width, this.height);
+  RectangleExtendsPoint(double super.x, double super.y, this.width, this.height);
 
   // 면적
-  int calculateArea() {
-    return width * height;
-  }
-
+  int get calculateArea => width * height;
   // 둘레
-  int calculateRound() {
-    return (width + height) * 2;
-  }
+  int get calculateRound => (width + height) * 2;
 }
 
 void main() {
-  Rectangle rectangle = Rectangle(15, 10, 100, 33);
+  RectangleExtendsPoint rectangle = RectangleExtendsPoint(15, 10, 100, 33);
 
-  // 계산
-  int getArea = rectangle.calculateArea();
-  int getRound = rectangle.calculateRound();
-
-  print('면적은 $getArea');
-  print('둘레는 $getRound');
+  print('면적은 ${rectangle.calculateArea}');
+  print('둘레는 ${rectangle.calculateRound}');
 }
