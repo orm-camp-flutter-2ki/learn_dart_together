43e0c55689f3cb36cc6ba557149b4dee9523907e
SeongJongHo <whdgh9595@gmail.com>
2024-03-12 16:23:34 +0900

    [ADD] rectangle 클래스 작성

---
+++ lib/24_03_12/rectangle.dart
@@ -0,0 +1,19 @@
+import 'package:learn_dart_together/24_03_12/point.dart';
+
+class Rectangle {
+  int _width;
+  int _height;
+  Point _topLeft;
+
+  Rectangle(this._width, this._height, this._topLeft);
+
+  int get calculateArea => _width * _height;
+
+  int get calculatePerimeter => 2 * (_width * _height);
+
+  set width(int width) => _width = width;
+
+  set height(int height) => _height = height;
+
+  set topLeft(Point topLeft) => _topLeft = topLeft;
+}
