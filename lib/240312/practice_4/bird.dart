d1210c6978f9b315e94b712d39b4c2d2fd84f619
nala0897@naver.com <nala0897@naver.com>
2024-03-13 08:35:38 +0900

    fix: 코드 정리

--- lib/240312/practice_4/bird.dart
+++ lib/240312/practice_4/bird.dart
@@ -1,15 +1,15 @@
 import 'package:learn_dart_together/240312/practice_4/animal.dart';
 
-class Bird extends Animal{
+class Bird extends Animal {
   Bird(super.type, super.sound);
 
-  void canFly(String type) {
+  void canFly() {
     print("'$type'은/는 날 수 있나요?");
   }
 }
 
 void main() {
   Bird penguin = Bird('황제펭귄', '꽈깎');
-  penguin.soundIs(penguin.type, penguin.sound);
-  penguin.canFly(penguin.type);
+  penguin.soundIs();
+  penguin.canFly();
 }
--- lib/240312/practice_6/bicycle.dart
+++ lib/240312/practice_6/bicycle.dart
@@ -1,15 +1,15 @@
 import 'package:learn_dart_together/240312/practice_6/vehicle.dart';
 
 class Bicycle extends Vehicle {
-
   bool hasBasket;
   double weight;
 
-  Bicycle(super.maker,
-      super.model,
-      super.year,
-      super.numberOfWheels,
-      this.hasBasket,
-      this.weight
-      );
+  Bicycle(
+    super.maker,
+    super.model,
+    super.year,
+    super.numberOfWheels,
+    this.hasBasket,
+    this.weight,
+  );
 }
