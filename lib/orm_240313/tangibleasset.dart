772dfd7f74ffd41fbe5b7fd70e880bf737d88c4c
BAUu <yimej07@gmail.com>
2024-03-14 00:30:26 +0900

    fix: 옛날 코드 수정

--- lib/orm_240313/tangibleasset.dart
+++ lib/orm_240313/tangibleasset.dart
@@ -2,15 +2,38 @@ import 'package:learn_dart_together/orm_240313/thing.dart';
 
 import 'asset.dart';
 
+// abstract class TangibleAsset extends Asset implements Thing {
+//   String color;
+//   double _weight;
+//
+//   TangibleAsset(
+//       {required super.name,
+//       required super.price,
+//       required this.color,
+//       required double _weight})
+//       : _weight = weight;
+//
+//   @override
+//   double get weight => _weight;
+//
+//   @override
+//   set weight(double value) {
+//     if (value > 0) {
+//       _weight = value;
+//     } else {
+//       throw Exception('Weight must be positive');
+//     }
+//   }
+// }
+
 abstract class TangibleAsset extends Asset implements Thing {
   String color;
   double _weight;
 
-
+  TangibleAsset(super.name,super.price,this.color, this._weight);
 
   @override
   double get weight => _weight;
-
 
   @override
   set weight(double value) {
@@ -21,42 +44,37 @@ abstract class TangibleAsset extends Asset implements Thing {
     }
   }
 
-  TangibleAsset(super.name, super.price, this.color, this._weight);
+  @override
+  String toString() {
+    return '제목: $name, 가격: $price원, 색깔: $color, 무게: $weight kg';
+  }
 }
-
-
-
 
 class Book extends TangibleAsset {
   String isbn;
 
-  Book(String name, int price, String color, this.isbn)
-      : super(name, price, color, 0.5);
-
+  Book(super.name, super.price, super.color,  this.isbn, super.weight);
 
   @override
   String toString() {
-    return '제목: $name, 가격: $price원, 색깔: $color, 고유 번호: $isbn';
+    return '제목: $name, 가격: $price원, 색깔: $color, 고유 번호: $isbn 무게: $weight Kg';
   }
 }
 
 class Computer extends TangibleAsset {
   String makerName;
 
-  Computer(String name, int price, String color, this.makerName, double weight)
-      : super(name, price, color, weight);
+  Computer(super.name, super.price, super.color, this.makerName,super.weight);
 
 
   @override
   String toString() {
-  return '제품명: $name, 가치: $price원, 색깔: $color, 제조사: $makerName';
-   }
-
-
+    return '제품명: $name, 가치: $price원, 색깔: $color, 제조사: $makerName';
+  }
 }
 
 void main() {
-  Book book = Book('톰 소여의 모험', 6500, '흰색', '64800');
+  Book book = Book('톰 소여의 모험', 6500, '흰색', '64800', 0.5);
   Computer computer = Computer('Macbook Air 15', 216000, '미드나이트', 'Apple', 2.5);
   print(book);
   print(computer);
