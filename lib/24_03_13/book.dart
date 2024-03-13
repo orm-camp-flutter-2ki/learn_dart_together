879a2bb2020fda07360df6774dbb8933dfc0d87d
SeongJongHo <whdgh9595@gmail.com>
2024-03-13 15:00:00 +0900

    [MODIFY] Computer, Book 클래스 수정

--- lib/24_03_13/book.dart
+++ lib/24_03_13/book.dart
@@ -1,12 +1,13 @@
 import 'package:learn_dart_together/24_03_13/tangible_asset.dart';
 
 class Book extends TangibleAsset {
-  String isbn;
+  final String _isbn;
 
-  Book(super.name, super.price, super.color, super.weight, this.isbn);
+  Book({required String name, required int price, required String color, required int weight, required String isbn})
+      : _isbn = isbn, super(name, price, color, weight);
 
   @override
   String generateDescription() {
-    return '이번에 소개할 책은 $name입니다. 가격은 ${super.valuationPrice}원이며, 색상은 ${super.color}입니다. 무게는 ${super.weight}이며, ISBN은 $isbn입니다.';
+    return '이번에 소개할 책은 $name입니다. 가격은 ${super.valuationPrice}원이며, 색상은 ${super.color}입니다. 무게는 ${super.weight}이며, ISBN은 $_isbn입니다.';
   }
 }
--- lib/24_03_13/computer.dart
+++ lib/24_03_13/computer.dart
@@ -1,13 +1,14 @@
 import 'package:learn_dart_together/24_03_13/tangible_asset.dart';
 
 class Computer extends TangibleAsset {
-  String makerName;
+  final String _makerName;
 
-  Computer(super.name, super.price, super.color, super.weight, this.makerName);
+  Computer({required String name, required int price, required String color, required int weight, required String makerName})
+      : _makerName = makerName, super(name, price, color, weight);
 
   @override
   String generateDescription() {
     return '이번에 소개할 컴퓨터는 $name입니다. 가격은 ${super.valuationPrice}원이며, 색상은 '
-        '${super.color}입니다. 무게는 ${super.weight}이며, 제조사는 $makerName입니다.';
+        '${super.color}입니다. 무게는 ${super.weight}이며, 제조사는 $_makerName입니다.';
   }
 }
