4d23340be10915cf6587a2303aa39987ce98df6b
KimDonghyeok <qoophon@gmail.com>
2024-03-13 14:48:53 +0900

    feat: Book 클래스 작성 (연습문제)
    - TangibleAsset 추상클래스 상속
    - isbn 필드 선언
    - 생성자 정의

---
+++ lib/240313/pracitce/book.dart
@@ -0,0 +1,12 @@
+import 'tangible_asset.dart';
+
+class Book extends TangibleAsset {
+  String isbn;
+
+  Book({
+    required super.name,
+    required super.price,
+    required super.color,
+    required this.isbn,
+  });
+}
