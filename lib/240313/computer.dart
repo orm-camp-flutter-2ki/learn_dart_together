4e59d9d3f35c3ff5752033f08fc6fd655e3ec0e0
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-03-13 14:20:36 +0900

    feat : compueter 생성

---
+++ lib/240313/computer.dart
@@ -0,0 +1,12 @@
+class Computer {
+  String name;
+  int price;
+  String color;
+  String makerName;
+
+  Computer(
+      {required this.name,
+      required this.price,
+      required this.color,
+      required this.makerName});
+}
