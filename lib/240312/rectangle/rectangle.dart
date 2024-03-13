3d9e8423b9b5f2133c8e9e583a41c990196ce5ec
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-03-13 09:32:15 +0900

    fix : 선언과 대입을 한줄로 변경

--- lib/240312/rectangle/rectangle.dart
+++ lib/240312/rectangle/rectangle.dart
@@ -7,23 +7,17 @@ class RectanglePoint {
   RectanglePoint(this.bottomLeft, this.topRight);
 
   int area() {
-    int width;
-    int height;
-    int area;
-    width = topRight.x.toInt() - bottomLeft.x.toInt();
-    height = topRight.y.toInt() - bottomLeft.y.toInt();
-    area = width * height;
+    int width = topRight.x.toInt() - bottomLeft.x.toInt();
+    int height = topRight.y.toInt() - bottomLeft.y.toInt();
+    int area = width * height;
     print(area);
     return area;
   }
 
   int round() {
-    int width;
-    int height;
-    int round;
-    width = topRight.x.toInt() - bottomLeft.x.toInt();
-    height = topRight.y.toInt() - bottomLeft.y.toInt();
-    round = (width + height) * 2;
+    int width = topRight.x.toInt() - bottomLeft.x.toInt();
+    int height = topRight.y.toInt() - bottomLeft.y.toInt();
+    int round = (width + height) * 2;
     print(round);
     return round;
   }
