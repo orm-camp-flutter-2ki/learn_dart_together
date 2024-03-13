629d9bc5c1e34bafb60a0644367b859dd541884f
chojja7188 <chojja7188@gmail.com>
2024-03-13 15:54:11 +0900

    style: 코드 정리

--- lib/240313/intangible_asset.dart
+++ lib/240313/intangible_asset.dart
@@ -1,6 +1,6 @@
 import 'package:learn_dart_together/240313/asset.dart';
 
-abstract class IntangibleAsset extends Asset{
+abstract class IntangibleAsset extends Asset {
   IntangibleAsset({
     required super.name,
     required super.price,
