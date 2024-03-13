0c918e4e22b2ba9a3890ce758fd83fcb8fb4dcc7
woong <workplayhard1@naver.com>
2024-03-13 14:23:41 +0900

    feat: TangibleAsset 초기구현

---
+++ lib/240313/TangibleAsset.dart
@@ -0,0 +1,14 @@
+
+abstract class TangibleAsset {
+  String name;
+  int pirce;
+  String color;
+  String makerName;
+
+  TangibleAsset({
+    required this.name,
+    required this.pirce,
+    required this.color,
+    required this.makerName
+  });
+}
