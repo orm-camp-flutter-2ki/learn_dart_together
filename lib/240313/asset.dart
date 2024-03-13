b8f2b81138b23678d7dac65b360830793b1b283b
Stranger Yang <ccamdol@gmail.com>
2024-03-13 15:26:11 +0900

    feat: Asset, 무형자산 클래스

--- lib/240313/asset.dart
+++ lib/240313/asset.dart
@@ -6,13 +6,23 @@ abstract class Asset {
   Asset(this.name, this.price);
 }
 
+//무형자산 클래스
+abstract class IntangibleAsset extends Asset {
+  // String name;
+  // int price;
+  // String color;
+
+  IntangibleAsset(String name, int price) : super(name, price);
+}
+
+
 //유형자산 클래스
-abstract class TangibleAsset extends Asset{
-  String name;
-  int price;
+abstract class TangibleAsset extends Asset {
+  // String name;
+  // int price;
   String color;
 
-  TangibleAsset(this.name, this.price, this.color);
+  TangibleAsset(String name, int price, this.color) : super(name, price);
 }
 
 class Book extends TangibleAsset {
@@ -41,4 +51,15 @@ class Computer extends TangibleAsset {
     required String color,
     required this.makerName,
   }) : super(name, price, color);
+}
+
+//무형자산 특허권
+class Patent extends IntangibleAsset {
+  String patentNumber;
+
+  Patent({
+    required String name,
+    required int price,
+    required this.patentNumer;
+  }) :super(name, price);
 }
