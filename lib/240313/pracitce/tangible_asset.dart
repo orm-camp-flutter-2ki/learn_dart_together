e4bd96cda84f40b7b0bd6bcb1bf987d47d3ca93b
KimDonghyeok <qoophon@gmail.com>
2024-03-13 14:47:35 +0900

    feat: TangibleAsset 추상클래스 작성 (연습문제)
    - name, price, color 필드 선언
    - 생성자 정의

---
+++ lib/240313/pracitce/tangible_asset.dart
@@ -0,0 +1,11 @@
+abstract class TangibleAsset {
+  String name;
+  int price;
+  String color;
+
+  TangibleAsset({
+    required this.name,
+    required this.price,
+    required this.color,
+  });
+}
