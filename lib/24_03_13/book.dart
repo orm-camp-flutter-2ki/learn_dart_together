fea485cb24b791aa34cc801eaee4f735b4468e97
kimhyungmin <jeen14rla@gmail.com>
2024-03-13 14:21:17 +0900

    feat: book, computer 클래스 작성

---
+++ lib/24_03_13/book.dart
@@ -0,0 +1,13 @@
+class Book {
+  String name;
+  int price;
+  String color;
+  String isbn;
+
+  Book({
+    required this.name,
+    required this.price,
+    required this.color,
+    required this.isbn,
+  });
+}
