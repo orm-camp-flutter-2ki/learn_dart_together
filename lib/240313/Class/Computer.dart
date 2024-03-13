f580a883ad18c09e3c8d52ccedd0170bfe48b139
woong <workplayhard1@naver.com>
2024-03-13 15:10:13 +0900

    feat: Computer에 Override구현

--- lib/240313/Class/Computer.dart
+++ lib/240313/Class/Computer.dart
@@ -2,28 +2,24 @@ import 'package:learn_dart_together/240313/Abstract/TangibleAsset.dart';
 
 class Computer extends TangibleAsset {
   String os;
+  double _weight;
+
+  @override
+  double get weight => _weight;
+
+  @override
+  set weight(double value) {
+    _weight = value;
+  }
 
   Computer({
     required super.name,
     required super.pirce,
     required super.color,
     required super.makerName,
+    required super.owner,
+    required super.productionYear,
     required this.os,
-    required super.owner,
-    required super.productionYear
-  });
-}
-
-class Book extends TangibleAsset {
-  String publisher;
-
-  Book({
-    required super.name,
-    required super.pirce,
-    required super.color,
-    required super.makerName,
-    required this.publisher, 
-    required super.owner,
-    required super.productionYear
-  });
+    required double weight
+  }) : _weight = weight;
 }
