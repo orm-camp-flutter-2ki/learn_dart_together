7f2d493ec0279ac92bae142242d6c8055236a7ef
jiwon Choi <jiwon31273@naver.com>
2024-03-13 16:09:09 +0900

    feat : book 클래스

--- lib/24_03_12/book/book.dart
+++ lib/24_03_12/book/book.dart
@@ -1,5 +1,35 @@
-class Book {
-  String title, author, genre;
+import 'package:learn_dart_together/24_03_13/tangible_asset.dart';
 
-  Book(this.title, this.author, this.genre);
+class Book extends TangibleAsset {
+  String isbn;
+  double? weight;
+
+  Book(
+    super.name,
+    super.price,
+    super.color,
+    this.isbn
+  );
+
+  @override
+  void displayInfo() {
+    print("BookName: $name, Price: $price, Color: $color, Isbn: $isbn");
+  }
+
+  @override
+  double getWeight() {
+    return weight ?? 0.0;
+  }
+
+  @override
+  void setMass(double value) {
+    weight = value;
+  }
+}
+
+void main() {
+  Book book = Book('퍼즐로 이해하는 알고리즘', 3, 'white', 'BJPublic');
+  book.setMass(10);
+  book.displayInfo();
+  print('${book.weight}');
 }
