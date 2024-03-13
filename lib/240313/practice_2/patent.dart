9b5510000c04a1eba3cf98b5aacacb88c36f801f
nala0897@naver.com <nala0897@naver.com>
2024-03-13 15:43:12 +0900

    fix: Patent 클래스 수정

--- lib/240313/practice_2/patent.dart
+++ lib/240313/practice_2/patent.dart
@@ -2,14 +2,12 @@ import 'package:learn_dart_together/240313/practice_2/intangible_asset.dart';
 
 // 특허권
 class Patent extends IntangibleAsset {
-  String nickName; // 특허 명칭
   int number; // 특허 번호
-  DateTime expirationPeriod; // 유효 기간
 
   Patent(
+    super.price,
+    super.name,
     super.ownerName,
-    this.nickName,
     this.number,
-    this.expirationPeriod,
   );
 }
