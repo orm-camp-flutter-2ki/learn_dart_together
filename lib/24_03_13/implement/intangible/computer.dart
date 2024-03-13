85546881d4e72b6a9412fea4468d8f6ae283c0cf
JEON-Sungsu <anagma@naver.com>
2024-03-13 16:21:21 +0900

    refactor: 유형자산 구현체 생성자 수정

--- lib/24_03_13/implement/intangible/computer.dart
+++ lib/24_03_13/implement/intangible/computer.dart
@@ -4,15 +4,16 @@ class Computer extends IntangibleAsset {
   final bool _isLaptop;
   final String _OS;
 
-  Computer(
-      {required super.name,
-      required super.price,
-      required super.color,
-      required super.quantity,
-      required super.weight,
-      required bool isLaptop,
-      required String OS})
-      : _isLaptop = isLaptop,
+  Computer({
+    required super.name,
+    required super.price,
+    required super.color,
+    required super.quantity,
+    required super.department,
+    required super.weight,
+    required bool isLaptop,
+    required String OS,
+  })  : _isLaptop = isLaptop,
         _OS = OS;
 
   bool get isLaptop => _isLaptop;
