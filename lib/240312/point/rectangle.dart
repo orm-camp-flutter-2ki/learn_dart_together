e0e835eaa114281e4ca9ca0fca5e273094a46723
icicle_4 <dwayne.han4@gmail.com>
2024-03-13 13:47:49 +0900

    refactor: 필요없는 변수 삭제

--- lib/240312/point/rectangle.dart
+++ lib/240312/point/rectangle.dart
@@ -5,13 +5,10 @@ import 'point.dart';
 class Rectangle {
   final double _width;
   final double _height;
-  final Point _topLeft;
-  final Point _bottomRight;
 
+  // topLeft, bottoomRight 변수로 선언할 필요없었음
   Rectangle({required Point topLeft, required Point bottomRight})
-      : _topLeft = topLeft,
-        _bottomRight = bottomRight,
-        _height = (topLeft.y - bottomRight.y).abs(),
+      : _height = (topLeft.y - bottomRight.y).abs(),
         _width = (topLeft.x - bottomRight.x).abs();
   // abs() 함수 -> 절대값
   // height, width 계산을 생성자 안에서 해야
