a36f31c14f64e6291df50a317fa0ac5ae4f5848b
nala0897@naver.com <nala0897@naver.com>
2024-03-13 11:22:10 +0900

    feat: Book, Computer class 생성

---
+++ lib/240313/practice_1/book.dart
@@ -0,0 +1,13 @@
+class Book {
+  String name;
+  String color;
+  String isbn;
+  int price;
+
+  Book({
+    required this.name,
+    required this.color,
+    required this.isbn,
+    required this.price,
+  });
+}
---
+++ lib/240313/practice_1/computer.dart
@@ -0,0 +1,13 @@
+class Computer {
+  String name;
+  String color;
+  String makerName;
+  int price;
+
+  Computer({
+    required this.name,
+    required this.color,
+    required this.makerName,
+    required this.price,
+  });
+}
