1db57a85656eec67e3327d238324af8cf603a799
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-13 18:19:51 +0900

    rename: test name

--- test/0313/computer_test.dart
+++ test/0313/computer_test.dart
@@ -2,7 +2,7 @@ import 'package:learn_dart_together/0313/computer.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Computer 인스턴스 생성', () {
+  test('유형 Computer 인스턴스 생성', () {
     Computer computer = Computer('유형', '2023 iMac 24', 3370000, 4.43, '퍼플', '애플');
 
     computer.weight = 4.48;
