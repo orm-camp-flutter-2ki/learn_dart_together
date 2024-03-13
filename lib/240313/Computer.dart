1d3c63c120e88f626f099652d86a0018ceef1555
woong <workplayhard1@naver.com>
2024-03-13 14:30:46 +0900

    feat: computer, book이 TangibleAsset 추상클래스를 상속받도록 함.

---
+++ lib/240313/Computer.dart
@@ -0,0 +1,25 @@
+import 'package:learn_dart_together/240313/TangibleAsset.dart';
+
+class Computer extends TangibleAsset {
+  String os;
+
+  Computer({
+    required super.name,
+    required super.pirce,
+    required super.color,
+    required super.makerName,
+    required this.os
+  });
+}
+
+class Book extends TangibleAsset {
+  String publisher;
+
+  Book({
+    required super.name,
+    required super.pirce,
+    required super.color,
+    required super.makerName,
+    required this.publisher
+  });
+}
