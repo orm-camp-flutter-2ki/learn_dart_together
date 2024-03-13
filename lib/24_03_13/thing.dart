15661a4074bc68e3837f923b7b366d547213be52
YunZi.Song <yoonji5809@gmail.com>
2024-03-13 17:47:29 +0900

    feat: Thing 인터페이스 추가

---
+++ lib/24_03_13/thing.dart
@@ -0,0 +1,8 @@
+// 자산인지 아닌지 따지지 말고, 형태가 있는 것 (Thing) 이면, 무게가 있다.
+// 그래서, double 형으로 무게(weight)를 얻을 수 있도록
+// getter/setter를 가지는 인터페이스 Thing 을 만드시오.
+abstract interface class Thing {
+  double get weight;
+
+  set weight(double value);
+}
