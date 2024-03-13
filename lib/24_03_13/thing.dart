0a9aa189c4fd31ce8f9da014ba96885d3b7085a9
YunZi.Song <yoonji5809@gmail.com>
2024-03-13 19:34:58 +0900

    command: Thing 인터페이스 주석 수정

--- lib/24_03_13/thing.dart
+++ lib/24_03_13/thing.dart
@@ -2,6 +2,8 @@
 // 그래서, double 형으로 무게(weight)를 얻을 수 있도록
 // getter/setter를 가지는 인터페이스 Thing 을 만드시오.
 abstract interface class Thing {
+  // 인터페이스에서는 필드 구현을 하지 않고 메소드 선언만 한다.
+
   double get weight;
 
   set weight(double value);
