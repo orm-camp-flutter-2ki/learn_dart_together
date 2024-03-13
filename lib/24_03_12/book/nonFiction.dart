31d859616e761a7841fa22e87905edbb57cb5526
jiwon Choi <jiwon31273@naver.com>
2024-03-13 16:10:16 +0900

    feat : nonfiction 클래스

--- lib/24_03_12/book/nonFiction.dart
+++ lib/24_03_12/book/nonFiction.dart
@@ -1,12 +1,13 @@
 import 'package:learn_dart_together/24_03_12/book/book.dart';
 
-class NonFiction extends Book{
-
+class NonFiction extends Book {
   String subgenre;
 
-  NonFiction(super.title, super.author, super.genre, this.subgenre);
-
-  void information() {
-    print('$title의 저자는 $author이고 장르는 $genre, $subgenre입니다.}');
-  }
+  NonFiction(
+    super.name,
+    super.price,
+    super.color,
+    super.isbn,
+    this.subgenre
+  );
 }
