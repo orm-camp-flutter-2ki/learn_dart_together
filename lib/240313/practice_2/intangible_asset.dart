546f599ad9acb9084a74eb58f0cacecfc74e1108
nala0897@naver.com <nala0897@naver.com>
2024-03-13 18:07:29 +0900

    fix: IntangibleAsset 클래스에서 Thing 인터페이스 구현

--- lib/240313/practice_2/intangible_asset.dart
+++ lib/240313/practice_2/intangible_asset.dart
@@ -5,12 +5,20 @@ import 'asset.dart';
 // 무형자산
 abstract class IntangibleAsset extends Asset implements Thing {
   String ownerName;
+  double _weight;
 
-  IntangibleAsset({
+  IntangibleAsset(
+    this._weight, {
     required super.price,
     required super.name,
     required this.ownerName,
   });
+
+  @override
+  double get weight => _weight;
+
+  @override
+  set weight(double value) => _weight;
 
   // 가치 평가
   void value();
--- lib/240313/practice_2/patent.dart
+++ lib/240313/practice_2/patent.dart
@@ -5,15 +5,12 @@ class Patent extends IntangibleAsset {
   int number; // 특허 번호
 
   Patent(
-    this.weight, {
+    super.weight, {
     required super.price,
     required super.name,
     required super.ownerName,
     required this.number,
   });
-
-  @override
-  double weight;
 
   @override
   void value() {
