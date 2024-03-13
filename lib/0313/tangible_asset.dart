3cc455c67f7a13fc0f1f5ef0c5d77361fb16489c
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-13 15:48:01 +0900

    feat: create thing interface

--- lib/0313/tangible_asset.dart
+++ lib/0313/tangible_asset.dart
@@ -1,10 +1,20 @@
 import 'Asset.dart';
+import 'thing.dart';
 
-abstract class TangibleAsset extends Asset {
+abstract class TangibleAsset extends Asset implements Thing {
 
-  final double weight;
-  final String color;
+  final double _weight;
+  final String _color;
 
-  TangibleAsset(this.weight, this.color);
+  TangibleAsset(super.type, this._weight, this._color);
 
+  @override
+  double get weight => _weight;
+  @override
+  String get color => _color;
+
+  @override
+  set weight(double weight);
+  @override
+  set color(String color);
 }
---
+++ lib/0313/thing.dart
@@ -0,0 +1,8 @@
+abstract interface class Thing {
+
+  double get weight;
+  String get color;
+
+  set weight(double weight);
+  set color(String color);
+}
