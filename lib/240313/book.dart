6fa20a79b173aba430fb619142f98382c3ae3e64
icicle_4 <dwayne.han4@gmail.com>
2024-03-13 16:00:48 +0900

    feat: 추상클래스와 인터페이스 연습문제3.

--- lib/240313/book.dart
+++ lib/240313/book.dart
@@ -1,7 +1,25 @@
 import 'tangible_asset.dart';
+import 'thing.dart';
 
-class Book extends TangibleAsset {
+class Book extends TangibleAsset implements Thing {
   String isbn;
+  double _weight;
 
-  Book({required super.name, required super.price, required super.color, required this.isbn});
+  // getter를 사용(불가, 이름이 같아서), 변수 재정의가 아니라
+  // 추상클래스에서 _붙인거는 어차피 접근 안됨
+  Book({
+    required super.name,
+    required super.price,
+    required super.color,
+    required this.isbn,
+    required double weight,
+  }) : _weight = weight;
+
+  @override
+  set weight(double value) {
+    _weight = weight;
+  }
+
+  @override
+  double get weight => _weight;
 }
--- lib/240313/computer.dart
+++ lib/240313/computer.dart
@@ -1,7 +1,23 @@
 import 'tangible_asset.dart';
+import 'thing.dart';
 
-class Computer extends TangibleAsset {
+class Computer extends TangibleAsset implements Thing {
   String makerName;
+  double _weight;
 
-  Computer({required super.name, required super.price, required super.color, required this.makerName});
+  Computer({
+    required super.name,
+    required super.price,
+    required super.color,
+    required this.makerName,
+    required double weight,
+  }) : _weight = weight;
+
+  @override
+  set weight(double value) {
+    _weight = weight;
+  }
+
+  @override
+  double get weight => _weight;
 }
