c438d5cbfcb83508c2e6dc68e703d5c5519e9a6e
nala0897@naver.com <nala0897@naver.com>
2024-03-13 14:19:54 +0900

    feat: Patent 클래스에 Thing 인터페이스를 구현

--- lib/240313/practice_2/patent.dart
+++ lib/240313/practice_2/patent.dart
@@ -1,10 +1,13 @@
 import 'package:learn_dart_together/240313/practice_2/intangible_asset.dart';
 
+import '../practice_3/thing.dart';
+
 // 특허권
-class Patent extends IntangibleAsset {
+class Patent extends IntangibleAsset implements Thing {
   String nickName; // 특허 명칭
   int number; // 특허 번호
   DateTime expirationPeriod; // 유효 기간
+  double _weight = 0.0;
 
   Patent(
     super.ownerName,
@@ -12,4 +15,13 @@ class Patent extends IntangibleAsset {
     this.number,
     this.expirationPeriod,
   );
+
+  @override
+  double get weight => _weight;
+
+  @override
+  set weight(double value) {
+    _weight = value;
+  }
+
 }
