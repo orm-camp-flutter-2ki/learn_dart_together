68e1581dd7e5dbb390f7d1870e8db6f54fd262f0
jiwon Choi <jiwon31273@naver.com>
2024-03-14 18:44:03 +0900

    feat : b 클래스 작성

---
+++ lib/24_03_14/b.dart
@@ -0,0 +1,17 @@
+import 'package:learn_dart_together/24_03_14/xy.dart';
+
+class B extends Y {
+  @override
+  void a() {
+    print('Ba');
+  }
+
+  @override
+  void b() {
+    print('Bb');
+  }
+
+  void c() {
+    print('Bc');
+  }
+}
