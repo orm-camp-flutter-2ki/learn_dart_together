8867b787e4eea4c376e426adf38e4ac931e3ed33
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-14 21:01:08 +0900

    test: task

---
+++ lib/0314/main.dart
@@ -0,0 +1,29 @@
+import 'a.dart';
+import 'b.dart';
+import 'x.dart';
+import 'y.dart';
+
+void main() {
+  // [1]
+  X obj = A();
+  obj.a();
+
+
+  // [2]
+  Y y1 = A();
+  Y y2 = B();
+  y1.a();
+  y2.a();
+
+
+  // [3]
+  A a = A();
+  B b = B();
+
+  List<Y> list = [a, b];
+
+  for (Y item in list) {
+    item.b();
+  }
+}
+
