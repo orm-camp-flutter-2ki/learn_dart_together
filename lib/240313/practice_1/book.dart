f87c0ccf7c6abf14ae09dcfb1cf03c33692d208a
nala0897@naver.com <nala0897@naver.com>
2024-03-15 07:03:16 +0900

    fix: weight 도 named parameter 로 만들어줌

--- lib/240313/practice_1/book.dart
+++ lib/240313/practice_1/book.dart
@@ -3,10 +3,10 @@ import 'package:learn_dart_together/240313/practice_1/tangible_asset.dart';
 class Book extends TangibleAsset {
   String isbn;
 
-  Book(
-    super.weight, {
+  Book({
     required super.name,
     required super.price,
+    required super.weight,
     required super.color,
     required super.warrantyExpiredYear,
     required super.warrantyExpiredMonth,
