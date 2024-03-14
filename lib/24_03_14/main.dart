f689bdd889542ade2308c651129bda9cc1a2bc23
byjun8932 <byjun8932@gmail.com>
2024-03-14 22:42:16 +0900

    feat: 연습문제 3

---
+++ lib/24_03_14/main.dart
@@ -0,0 +1,30 @@
+import 'a.dart';
+import 'b.dart';
+import 'x.dart';
+import 'y.dart';
+
+void main() {
+  // [Q-1]
+  X obj = A();
+  obj.a();
+
+
+
+  // [Q-2]
+  Y y1 = A();
+  Y y2 = B();
+  y1.a();
+  y2.a();
+
+
+
+  // [Q-3]
+  A a = A();
+  B b = B();
+
+  List<Y> list = [a, b];
+
+  for (Y item in list) {
+    item.b();
+  }
+}
