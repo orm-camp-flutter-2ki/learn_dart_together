b51c15827f9d4f76cd30874f09a0cf312faccdb6
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-03-14 17:08:49 +0900

    feat : ab class 추가

--- lib/240314/ab_class.dart
+++ lib/240314/ab_class.dart
@@ -38,20 +38,27 @@ class B extends Y {
   }
 }
 
-void main(List<String> arguments) {
-  // X obj = A();
-  // obj.a();
+void main() {
+  //연습2-1
+  X obj = A();
+  print('2-1 답');
+  obj.a();
   // obj.b;
   // obj.c;
 
-  // Y y1 = A();
-  // Y y2 = B();
-  // y1.a();
-  // y2.a();
+  //연습2-2
+  Y y1 = A();
+  Y y2 = B();
+
+  print('2-2 답');
+  y1.a();
+  y2.a();
 
   final abs = <Y>[];
   abs.add(A());
   abs.add(B());
+
+  print('3 답');
 
   abs.forEach((abs) {
     abs.b();
@@ -59,6 +66,8 @@ void main(List<String> arguments) {
 }
 
 
-//1. a()
+//2-1. a()
 
-//2. 1)Aa 2) Ba
+//2-2. 1)Aa 2) Ba
+
+//3. Ab Bb
