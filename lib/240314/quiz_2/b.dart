57dd3728e80df26e8c3a3abe586e50c7781f56cc
jungeun272 <hanjungeun272@gmail.com>
2024-03-15 00:27:38 +0900

    feat : 연습문제 2,3번

---
+++ lib/240314/quiz_2/b.dart
@@ -0,0 +1,19 @@
+import 'y.dart';
+
+
+class B extends Y {
+  @override
+  void a() {
+    print('Ba');
+  }
+
+  @override
+  void b() {
+    print('Bb');
+  }
+
+  @override
+  void c() {
+    print('Bc');
+  }
+}
