b8ae28641e1039ed83afcb95f20fd48d2002f34a
byjun8932 <byjun8932@gmail.com>
2024-03-14 22:33:20 +0900

    feat: B 클래스에서 Y 추상클래스 상속

--- lib/24_03_14/b.dart
+++ lib/24_03_14/b.dart
@@ -1,3 +1,17 @@
-class B {
+import 'package:learn_dart_together/24_03_14/y.dart';
 
+class B extends Y {
+  @override
+  void a() {
+    print('Ba');
+  }
+
+  @override
+  void b() {
+    print('Bb');
+  }
+
+  void c() {
+    print('Bc');
+  }
 }
