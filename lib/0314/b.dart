f096abf33f932fd11e97a94c6a428da6d02b29d3
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-14 20:46:14 +0900

    feat: create b class

---
+++ lib/0314/b.dart
@@ -0,0 +1,18 @@
+import 'y.dart';
+
+class B extends Y {
+
+  @override
+  void a() {
+    print('Ba');
+  }
+
+  @override
+  void b() {
+    print('Bb');
+  }
+
+  void c() {
+    print('Bc');
+  }
+}
