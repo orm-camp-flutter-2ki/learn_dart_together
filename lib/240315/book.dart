f2ce3c5426f1b86faa40d41925b1d787e6ea8709
Stranger Yang <ccamdol@gmail.com>
2024-03-15 15:44:36 +0900

    feat: 연습문제
    -book 클래스

---
+++ lib/240315/book.dart
@@ -0,0 +1,22 @@
+class Book implements Comparable<Book> {
+  String title;
+  DateTime publishDate;
+  String comment;
+
+  Book({
+    required this.title,
+    required this.comment,
+    DateTime? publishDate,
+  }) : publishDate = publishDate ?? DateTime.now();
+
+  @override
+  int compareTo(Book other) {
+    return publishDate.compareTo(other.publishDate);
+  }
+
+  @override
+  bool operator ==(object other) =>
+  identical(this, otehr) ||
+  other is Book &&
+  ;
+}
