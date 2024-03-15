476d0c975e091032bcf5e1e20be4b3402e273a6e
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-15 15:38:09 +0900

    feat: create book class

---
+++ lib/0315/book.dart
@@ -0,0 +1,12 @@
+class Book {
+
+  String title;
+  DateTime publishDate;
+  String comment;
+
+  Book({
+    required this.title,
+    required this.comment,
+    DateTime? publishDate,
+  }) : publishDate = publishDate ?? DateTime.now();
+}
