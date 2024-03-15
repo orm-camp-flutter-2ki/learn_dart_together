374826c6269f61de7360b1d0c278ae1fb4e474f3
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-15 16:08:03 +0900

    feat: override equal operator

--- lib/0315/book.dart
+++ lib/0315/book.dart
@@ -9,4 +9,11 @@ class Book {
     required this.comment,
     DateTime? publishDate,
   }) : publishDate = publishDate ?? DateTime.now();
+
+  @override
+  bool operator ==(Object other) {
+    String thisDate = '${publishDate.year}-${publishDate.month}-${publishDate.day}';
+    String otherDate = '${publishDate.year}-${publishDate.month}-${publishDate.day}';
+    return identical(this, other) || other is Book && runtimeType == other.runtimeType && title == other.title && thisDate == otherDate;
+  }
 }
