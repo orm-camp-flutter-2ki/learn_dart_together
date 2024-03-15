e2748857454861a89768969ae0760fd029fce2ff
chojja7188 <chojja7188@gmail.com>
2024-03-15 16:25:36 +0900

    feat: Book 클래스 작성
    - 제목과 출간일이 같으면 같은 책으로 판단 (override operator ==)
    - 테스트 코드에서 오름차순 sort() 실행
    - copyWith() 메소드 작성

---
+++ lib/240315/book.dart
@@ -0,0 +1,37 @@
+class Book {
+  String title;
+  DateTime publishDate;
+  String comment;
+
+  Book({
+    required this.title,
+    required this.comment,
+    DateTime? publishDate,
+  }) : publishDate = DateTime.now();
+
+  String getFormattedDate(DateTime date) {
+    return '${date.year}-${date.month}-${date.day}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Book && runtimeType == other.runtimeType &&
+      title == other.title &&
+      getFormattedDate(publishDate) == getFormattedDate(other.publishDate);
+
+  @override
+  int get hashCode => int.parse('${title.hashCode}${publishDate.hashCode}');
+
+  Book copyWith({
+    String? title,
+    String? comment,
+    DateTime? publishDate,
+  }) {
+    return Book(
+      title: title ?? this.title,
+      comment: comment ?? this.comment,
+      publishDate: publishDate ?? this.publishDate
+    );
+  }
+}
---
+++ test/240315/book_test.dart
@@ -0,0 +1,40 @@
+import 'package:learn_dart_together/240315/book.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Book Test', () {
+    print('=========Test 1=========');
+    Book book = Book(title: '하이', comment: '코멘트다');
+    Book book2 = Book(title: '하이', comment: '코멘트라네');
+    Book book3 = Book(title: '안녕', comment: '안녕하세요');
+
+    print(book == book2);
+    print(book == book3);
+
+    expect(book, equals(book2)); // book과 book2는 이름이 같아서 통과
+    expect(book, isNot(equals(book3))); // book과 book3는 이름이 달라서 다른 요소. isNot(equals()) 사용
+  });
+
+  test('Book Test 2', () {
+    print('=========Test 2=========');
+    Book book = Book(title: '책1', comment: '코멘트');
+    Book book2 = Book(title: '책2', comment: '코멘트');
+    Book book3 = Book(title: '책3', comment: '코멘트');
+    Book book4 = Book(title: '책4', comment: '코멘트');
+
+    List<Book> bookList = [book, book2, book3, book4];
+    // publishDate를 기준으로 오름차순으로 정렬
+    bookList.sort((a, b) => a.publishDate.compareTo(b.publishDate));
+    for(var item in bookList) {
+      print(item.title);
+    }
+  });
+
+  test('Book Test 3', () {
+    print('=========Test 3=========');
+    Book book = Book(title: '책1', comment: '코멘트');
+    Book book2 = book.copyWith(comment: '커멘트');
+    print(book == book2);
+    expect(book, book2);
+  });
+}
