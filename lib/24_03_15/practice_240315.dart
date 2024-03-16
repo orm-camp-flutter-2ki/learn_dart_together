0a91b9c6d3080588a3261ce16c78b17f6312fca4
kimhyungmin <jeen14rla@gmail.com>
2024-03-16 17:47:19 +0900

    chore: main() 제거

--- lib/24_03_15/practice_240315.dart
+++ lib/24_03_15/practice_240315.dart
@@ -40,37 +40,3 @@ class Book {
     return 'title: $title, publishDate: $publishDate';
   }
 }
-
-void main() {
-  Book book = Book(
-      title: 'goodDay', comment: 'good', publishDate: DateTime(1955, 10, 20));
-
-  Book book1 = Book(
-      title: 'goodDay', comment: 'bad', publishDate: DateTime(1955, 10, 20));
-
-  Book book2 = Book(
-      title: 'badDay', comment: 'bad', publishDate: DateTime(1965, 10, 20));
-
-  Book book3 = Book(
-      title: 'badMonth', comment: 'hmm', publishDate: DateTime(1965, 9, 21));
-
-  Book book4 = book.copyWith();
-
-  print('${book4.title}, ${book4.comment}, ${book4.publishDate}');
-
-  print(book == book1);
-
-  print('here ${book.hashCode}, ${book4.hashCode}');
-
-  final books = <Book>[];
-
-  books.add(book);
-  books.add(book1);
-  books.add(book2);
-  books.add(book3);
-
-  // Book 인스턴스를 담고 있는 컬렉션에 대해 sort() 를 수행하여 출간일이 오래된 순서대로 정렬한다.
-  books.sort((a, b) => a.publishDate.compareTo(b.publishDate));
-
-  print(books.toString());
-}
