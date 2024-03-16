18baa0c5f24581b463e0ce9df479011a4d5acfaf
sseung <sseutta@naver.com>
2024-03-16 16:31:07 +0900

    Feat: Book 클래스 작성
    ==연산자 오버라이딩, sort변경, copyWith() 메소드 작성

--- lib/24_03_15/book.dart
+++ lib/24_03_15/book.dart
@@ -7,20 +7,26 @@ class Book implements Comparable<Book> {
     required this.title,
     required this.comment,
     DateTime? publishDate,
-  }) : publishDate = publishDate ?? DateTime.now();
+  }) : publishDate = publishDate ??
+            DateTime(
+                DateTime.now().year, DateTime.now().month, DateTime.now().day);
 
-  @override
-  String toString() {
-    return 'Book { title: $title, publishDate: $publishDate }';
-  } //타이틀과 출간일을 문자열로 변환
+  String getDatetoString(DateTime date) {
+    return '${publishDate.year}-${publishDate.month}-${publishDate.day}';
+  } //출간일을 문자열로 변환 'yy-mm-dd'
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-          other is Book &&
-              toString() ==
-                  other
-                      .toString(); //this book에 문자열로 반환한것과 other book에 문자열로 반환한것이 같은지 확인
+      other is Book && // Book 형 인지 확인 name is var
+          runtimeType == other.runtimeType && //
+          title == other.title &&
+          getDatetoString(publishDate) == getDatetoString(other.publishDate);
+
+  @override
+  String toString() {
+    return '제목 : $title 코멘트 : $comment 출간일 $publishDate }';
+  }
 
   // @override
   // bool operator ==(Object other) =>
@@ -31,6 +37,7 @@ class Book implements Comparable<Book> {
   int get hashCode =>
       title.hashCode ^
       publishDate.hashCode; //^ 연산자는 두 비트가 서로 다를 때 = 1, 같을 때 = 0을 반환
+  // 년 월 일 까지만 hashcode
 
   @override
   int compareTo(Book other) {
@@ -38,11 +45,12 @@ class Book implements Comparable<Book> {
   } // 오래된 책들부터 비교하여 정렬해주는 코드
 
   Book copyWith({
-    String? title, //파라미터임 매개변수 x임 y(x)
+    String? title,
     DateTime? publishDate,
     String? comment,
   }) {
-    return Book( //book 클래스 타입 반환
+    return Book(
+      //book 클래스 타입 반환
       title: title ?? this.title, //타이틀이 널이 아니면 디스타이틀 타이틀이 없으면 그놈의 타이틀 넣을거다.
       comment: comment ?? this.comment,
       publishDate: publishDate ?? this.publishDate,
@@ -51,46 +59,27 @@ class Book implements Comparable<Book> {
 }
 
 void main() {
-  final Book book1 = Book(
-      title: '신승찬1', comment: '코멘트', publishDate: DateTime.parse('2024-01-03'));
-  final Book book2 = Book(
-      title: '신승찬2', comment: '코멘트', publishDate: DateTime.parse('2024-01-01'));
-  final Book book3 = Book(
-      title: '신승찬3', comment: '코멘트', publishDate: DateTime.parse('2024-01-02'));
-  final Book book4 = Book(
-      title: '신승찬3', comment: '코멘트', publishDate: DateTime.parse('2024-01-02'));
-  final Book book5 = book4; //book5는 book 객체의 book4 인스턴스의 주소를 가지고 있다(?)
-
-  Book book6;
-
-  book4.title = '신승찬5';
-  print(book5.title); //공유하고 있다는 코드
-  print(book4.title);
-
+  Book book1 = Book(
+      title: '신승찬', comment: '코멘트', publishDate: DateTime.parse('2024-05-16'));
+  Book book2 = Book(
+      title: '신승찬2', comment: '코멘트', publishDate: DateTime.parse('2024-05-17'));
+  Book book3 = Book(
+      title: '신승찬', comment: '코멘트', publishDate: DateTime.parse('2024-05-16'));
+  Book book4 = Book(
+      title: '신승찬', comment: '코멘트', publishDate: DateTime.parse('2024-05-18'));
+  Book book5;
   print(book1 == book2);
-  print(book2 == book3);
-  print(book4.hashCode);
-  print(book5.hashCode); //책이 같은지 같지 않은지 테스트
-  print(book1.publishDate);
-  print(book3.publishDate);
+  print(book1 == book3);
 
-  final booklist = [book1, book2, book3];
-  booklist.sort();
-  for (var book in booklist) {
-    print(book);
+  List<Book> bookList = []; //book형 리스트 생성 초기화
+  bookList.add(book1);
+  bookList.add(book2);
+  bookList.add(book3);
+  bookList.add(book4);
+  bookList.sort(); //출간일 순서대로 정렬됨
+  for (var book in bookList) {
+    print(book.toString());
   }
-  print(booklist);
-
-
-  final Book copiedBook = book3.copyWith();
-
-  print(book3); // 출력: Book { title: Title, publishDate: 2024-01-01 00:00:00.000 }
-  print(copiedBook);   // 출력: Book { title: New Title, publishDate: 2024-01-01 00:00:00.000 }
-
+  book5 = book1.copyWith();
+  print(book5);
 }
-
-//다음 동작을 할 수 있도록 Book 클래스를 수정하시오.
-//
-//제목과 출간일(2024-01-01)이 같으면 같은 책으로 판단한다.
-//Book 인스턴스를 담고 있는 컬렉션에 대해 sort() 를 수행하여 출간일이 오래된 순서대로 정렬한다.
-//deep copy 를 위한 copyWith() 메서드를 제공한다.
