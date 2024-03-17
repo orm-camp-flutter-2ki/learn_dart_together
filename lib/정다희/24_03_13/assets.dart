ebba966af462475c3e462ca9ff6e66fe14026d63
tomoyo519 <vanessa.cheong1@gmail.com>
2024-03-17 14:59:43 +0900

    [feat] 스타크래프트 구조

--- lib/정다희/24_03_13/assets.dart
+++ lib/정다희/24_03_13/assets.dart
@@ -21,6 +21,10 @@ class TangibleAsset extends Assets implements Thing{
   // this 키워드는 현재 인스턴스의 필드를 참조할때 사용되며(현재 객체가 가지고있는 필드나 메소드를 참조), 필드를 직접 참조하는 방식은
   // 생성자의 매개변수 리스트에서는 허용되지 않기 때문.
   // _weight 필드를 초기화 하는 올바른 방법은 초기화 리스트를 사용하는것.
+
+  // 게터세터 변형이 필요없을때는 이렇게 쓰면 된다.
+  // @override
+  // double weight
   TangibleAsset( super.name, super.code, super.purchaseDate, super.purchasePrice, super.category, this.location, double weight):_weight=weight;
   @override
   void disposeAsset() {
