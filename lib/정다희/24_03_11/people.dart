c69c613750ae44769aa1365ebedfc8e6f91af361
tomoyo519 <vanessa.cheong1@gmail.com>
2024-03-17 20:53:31 +0900

    [feat] 3/11 과제 제출

---
+++ lib/정다희/24_03_11/people.dart
@@ -0,0 +1,11 @@
+class Person {
+
+  final String _name;
+  final int _birthYear
+  int _age;
+
+  get age => DateTIme.now().year - _birthYear;
+
+  Person({ required this.name, required this.birthYear}) : _name = name, _birthYear = birthYear
+
+}
