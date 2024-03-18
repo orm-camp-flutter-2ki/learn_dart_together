48fdad491320a032860e1c770320df5fb3c0d8eb
nala0897@naver.com <nala0897@naver.com>
2024-03-18 15:49:37 +0900

    feat: i 번째 글자가 자음인지 알려주는 isConsonant() 함수 생성

--- lib/240318/practice_string.dart
+++ lib/240318/practice_string.dart
@@ -1,12 +1,14 @@
 // i 번째 글자가 모음인지 알려주는 isVowel() 함수를 완성하시오
+// i 번째 글자가 자음인지 알려주는 isConsonant() 함수를 완성하시오
 // 영어 모음: a, e, i, o, u
 class Word {
   String word = '';
 
   Word(this.word);
 
+  // 모음
   bool isVowel(int i) {
-    if( i > word.length ) {
+    if (i >= word.length) {
       return false;
     }
     String value = word.substring(i, i + 1).toLowerCase();
@@ -19,10 +21,27 @@ class Word {
     }
     return false;
   }
+
+  // 자음
+  bool isConsonant(int i) {
+    if (i >= word.length) {
+      return false;
+    }
+
+    String value = word[i].toLowerCase();
+    print(value);
+    return (value != 'a' &&
+        value != 'e' &&
+        value != 'i' &&
+        value != 'o' &&
+        value != 'u');
+  }
 }
 
 void main() {
-  Word word = Word('word');
-  bool result = word.isVowel(0);
+  Word word = Word('chair');
+  bool result = word.isVowel(5);
+  bool result2 = word.isConsonant(5);
   print(result);
+  print(result2);
 }
