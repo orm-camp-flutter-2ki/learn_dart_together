65f87dc5338b345acb3c3c806e815033dab00f0f
kimhyungmin <jeen14rla@gmail.com>
2024-03-18 16:27:45 +0900

    fix: StrongBox class 금고에서 get() 메서드를 호출할 때 마다 사용횟수를 카운트하고 각 열쇠의 사용횟수에 도달하기 전에는 null을 리턴한다.

--- lib/24_03_18/strong_box.dart
+++ lib/24_03_18/strong_box.dart
@@ -1,11 +1,45 @@
-class StrongBox<E> {
-  E? _item;
+enum KeyType {
+  // 열거형 열쇠 종류
+  padlock,
+  button,
+  dial,
+  finger,
+}
 
-  void put(E item) {
+class StrongBox<T> {
+  // 1. 인스턴스 타입 미정
+  T? _item;
+  KeyType keyType; // 열쇠 종류 필드 변수
+  int openNum = 0;
+  int useNum = 0;
+
+  StrongBox({required this.keyType});
+
+  // 열쇠 종류 받는 생성자
+
+  void put(T item) {
+    // 3. 인스턴스 저장
     _item = item;
   }
 
-  E? get() {
+  T? get() {
+    // 3. 인스턴스 얻기
+    switch (keyType) {
+      case KeyType.padlock:
+        openNum = 1024;
+      case KeyType.button:
+        openNum = 10000;
+      case KeyType.dial:
+        openNum = 30000;
+      case KeyType.finger:
+        openNum = 1000000;
+    }
+
+    useNum++;
+
+    if (useNum <= openNum) {
+      return null;
+    }
     return _item;
   }
 }
