1eaaffe2a4f0314660c9d07b07a741cc3882e332
hoogom88 <ziziwhdgus@naver.com>
2024-03-18 16:25:25 +0900

    Update.Word클래스 판별 메서드 수정

--- lib/24_03_18/string_manipulation.dart
+++ lib/24_03_18/string_manipulation.dart
@@ -1,20 +1,13 @@
 class Word {
   String word;
 
-  Word(this.word);
+  Word({required this.word});
 
-  bool isVowel(int i) {
-    var target = word[i].toLowerCase();
-    return target == 'a' ||
-        target == 'e' ||
-        target == 'i' ||
-        target == 'o' ||
-        target == 'u';
-  }
+  bool isVowel(int i) => 'aeiou'.contains(word[i].toLowerCase());
 
   bool isConsonant(int i) {
     var target = word[i].toLowerCase().codeUnitAt(0);
-    return (!isVowel(i)) &&
-        ((target >= 'a'.codeUnitAt(0) && target <= 'z'.codeUnitAt(0)));
+    return ((target >= 'a'.codeUnitAt(0) && target <= 'z'.codeUnitAt(0))) &&
+        (!isVowel(i));
   }
 }
