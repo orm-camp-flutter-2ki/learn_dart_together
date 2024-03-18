de4f7c9ecb13c6971c1e12599375c8a137a3cf17
hoogom88 <ziziwhdgus@naver.com>
2024-03-18 12:03:37 +0900

    Update. Word클래스 - 모음 판별 메서드 수정

--- lib/24_03_18/string_manipulation.dart
+++ lib/24_03_18/string_manipulation.dart
@@ -2,10 +2,11 @@ class Word {
   String word = '';
 
   bool isVowel(int i) {
-    return word[i] == 'a' ||
-        word[i] == 'e' ||
-        word[i] == 'i' ||
-        word[i] == 'o' ||
-        word[i] == 'u';
+    var target = word[i].toLowerCase();
+    return target == 'a' ||
+        target == 'e' ||
+        target == 'i' ||
+        target == 'o' ||
+        target == 'u';
   }
 }
