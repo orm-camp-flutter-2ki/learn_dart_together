07dcb4ef979d59a4fb70aa3721ee34147e6e176f
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-03-18 16:59:59 +0900

    faet : switch 추가 및 테스트 코드 작성

--- lib/240318/strong_box.dart
+++ lib/240318/strong_box.dart
@@ -1,15 +1,53 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class StrongBox<E> {
-  KeyType? _key;
+  E _item;
+  int _count = 0;
+  KeyType _key;
 
-  StrongBox(KeyType key) : _key = key;
-
-  void put(KeyType key) {
-    _key = key;
+  StrongBox(
+    E item,
+    KeyType key,
+  )   : _item = item,
+        _key = key {
+    switch (key) {
+      case KeyType.padlock:
+        _count = 1024;
+      case KeyType.buttom:
+        _count = 10000;
+      case KeyType.dial:
+        _count = 30000;
+      case KeyType.finger:
+        _count = 1000000;
+    }
   }
 
-  get() {
-    return _key;
+  void put(E item) {
+    _item = item;
   }
+
+  E? get() {
+    if (_count == 0) {
+      return _item;
+    } else {
+      _count -= 1;
+      return null;
+    }
+  }
+
+  // int setCount(KeyType key) {
+  //   switch (key) {
+  //     case KeyType.padlock:
+  //       return 1024;
+  //     case KeyType.buttom:
+  //       return 10000;
+  //     case KeyType.dial:
+  //       return 30000;
+  //     case KeyType.finger:
+  //       return 1000000;
+  //   }
+  // }
 }
 
 enum KeyType {
@@ -18,5 +56,3 @@ enum KeyType {
   dial,
   finger,
 }
-
-void main() {}
