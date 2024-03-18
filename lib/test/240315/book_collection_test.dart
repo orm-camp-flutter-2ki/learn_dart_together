dcf7321baaeb52daed13c9e45f4cc89cc24c64e9
hee462 <a01080809887@gmail.com>
2024-03-18 16:09:49 +0900

    feat : 15일 과제 수정코드 추가

--- lib/test/240315/book_collection_test.dart
+++ lib/test/240315/book_collection_test.dart
@@ -5,10 +5,10 @@ import 'package:learn_dart_together/240315/book_collection.dart';
 void main() {
   test('book Test', () {
     List<Book> bookList = [
-      Book(title: '논어', comment: '삶', publishDate: DateTime(2024, 1, 1)),
-      Book(title: '논어', comment: '삶', publishDate: DateTime(2025, 1, 1)),
-      Book(title: '논어', comment: '삶', publishDate: DateTime(2028, 1, 1)),
-      Book(title: '논어', comment: '삶', publishDate: DateTime(2004, 1, 1)),
+      Book(title: '논어', comment: '삶', publishDate: DateTime(2024, 1, 1, 1)),
+      Book(title: '논어', comment: '삶', publishDate: DateTime(2025, 1, 1, 2)),
+      Book(title: '논어', comment: '삶', publishDate: DateTime(2028, 1, 1, 3)),
+      Book(title: '논어', comment: '삶', publishDate: DateTime(2004, 1, 1, 4)),
     ];
     bookList.sort((a, b) => a.compareTo(b));
 
