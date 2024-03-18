3f347c844d9f5b829576b3f65436905e27881443
Yuna.Kim <yunaoasiskim@gmail.com>
2024-03-18 17:35:48 +0900

    feat: -연습문제1번 StrongBox 클래스 정의 -인스턴스 타입 미정 -put으로 저장, get으로 얻을 수 있음 -get으로 얻을 때 별도 타입 캐스팅 사용x

--- lib/240318/strongbox.dart
+++ lib/240318/strongbox.dart
@@ -1,11 +1,32 @@
-class StrongBox<E> {
-  E? _data;
+enum KeyType {padlock, button, dial, finger}
 
-  void put(E data) {
-    _data = data;
+class StrongBox<T> { //오브젝트
+  final KeyType _keyType;
+  T _savedInstance;
+  int _usedCnt = 0;
+
+  //생성자
+  StrongBox({required value, required keyType})
+    : _savedInstance = value,
+      _keyType = keyType;
+
+  void put(T value) => _savedInstance = value; //setter
+
+  T? get() { //getter
+    var limitCnt = switch (_keyType) {
+      KeyType.padlock => 1024,
+      KeyType.button => 10000,
+      KeyType.dial => 30000,
+      KeyType.finger => 1000000
+    };
+    _usedCnt++; //시행할 때 마다 값이 올라
+    return _usedCnt < limitCnt ? null : _savedInstance; //삼항연산자로 시행횟수까지 null 반환
+    }
   }
 
-  E? get() {
-    return _data;
+void main() {
+  var sb = StrongBox<String>(value: "룰루랄라", keyType: KeyType.dial);
+  for (int i=0; i < 10; i++) {
+    print(sb.get());
   }
 }
