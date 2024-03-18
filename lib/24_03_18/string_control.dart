6a46f897818a85ec3cd399b88b89bd89edcd5a26
YunZi.Song <yoonji5809@gmail.com>
2024-03-18 16:18:57 +0900

    Feat: 문자열 조작 실습 문제 2 풀이

--- lib/24_03_18/string_control.dart
+++ lib/24_03_18/string_control.dart
@@ -18,4 +18,18 @@ class Word {
 
     return vowel.contains(letter);
   }
+
+  // i 번째 글자가 자음인지 알려주는 isConsonant() 함수를 완성하시오
+  bool isConsonant(int i) {
+    if (i > word.length) {
+      throw Exception('입력값이 글자 길이보다 큽니다.');
+    }
+
+    int beforeLetter = (i - 1) >= 0 ? (i - 1) : 0;
+
+    String letter = word.substring(beforeLetter, i);
+    String vowel = 'aeiou';
+
+    return !vowel.contains(letter);
+  }
 }
