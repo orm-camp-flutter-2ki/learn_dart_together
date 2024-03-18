aa2d8c8a338a567ece6eaf0afa2704086e418776
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-18 22:10:27 +0900

    feat: add if statement

--- lib/0318/strong_box.dart
+++ lib/0318/strong_box.dart
@@ -1,10 +1,20 @@
+import 'key_type.dart';
+
 class StrongBox<T> {
 
   T? _data;
+  KeyType key;
+  int tryCount = 0;
+
+  StrongBox(this.key);
 
   void put(T data) {
     _data = data;
   }
 
-  T? get data => _data;
+  T? get() {
+    tryCount += 1;
+    if (tryCount >= key.needToTry) return _data;
+    return null;
+  }
 }
