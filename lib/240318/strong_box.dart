ba901bb8ec758a8498c804b5b98e65e251404e51
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-03-18 19:10:15 +0900

    fix : 쓸모없는 코드 제거

--- lib/240318/strong_box.dart
+++ lib/240318/strong_box.dart
@@ -1,16 +1,13 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
-
 class StrongBox<E> {
   E _item;
   int _count;
-  KeyType _key;
+  // KeyType _key;
 
   StrongBox(
     E item,
     KeyType key,
   )   : _item = item,
-        _key = key,
+        // _key = key,
         _count = key.value;
 
   void put(E item) {
