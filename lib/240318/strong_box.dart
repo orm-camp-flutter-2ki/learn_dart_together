63f8bebbeaa9823fc1d901831648610ae9283509
hee462 <a01080809887@gmail.com>
2024-03-18 16:15:37 +0900

    feat : 주석 추가

--- lib/240318/strong_box.dart
+++ lib/240318/strong_box.dart
@@ -2,12 +2,15 @@ import 'package:learn_dart_together/240318/key_type.dart';
 
 class StrongBox<T> {
   T? _thing;
-  KeyType _keyType;
+
+  // 왜 , final _keyType 써야되냐,,,? 일단 IDE가 하란대로 하긴함...
+  final KeyType _keyType;
   int count = 0;
 
   StrongBox(this._keyType);
 
   void put(T thing) {
+    // _thing 값이 null 이면(thing에 저장된게 없으면) _thing 을 thing에 넣기
     if (_thing == null) {
       _thing = thing;
       print('저장되었습니다!');
@@ -19,9 +22,12 @@ class StrongBox<T> {
   T? get() {
     if (_thing != null && count < getMaxCount()) {
       count++;
+      // 새로운 thing 값 복사
       T? thing = _thing;
+      // _thing에 null 값 할당
       _thing = null;
       print('${thing}을 얻었다!!');
+      // thing 값 반환
       return thing;
     } else {
       print('아무것도없어!');
