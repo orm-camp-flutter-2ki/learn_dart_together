e351dc6600c06ee5d3fc680853a7df7dfafa0e7a
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-18 21:09:26 +0900

    feat: strong_box 클래스 수정
    - 다른 분 의견을 참고해 키타입별 횟수 제한을 리턴하는 메서드를 두게 정리했다.

--- lib/0318/strong_box.dart
+++ lib/0318/strong_box.dart
@@ -3,66 +3,43 @@
 // 2. put() 메서드로 인스턴스를 저장하고 get()메서드로 인스턴스를 얻을 수 있음
 // 3. get() 으로 얻을 때는 별도의 타입 캐스팅을 사용하지 않아도 된다.
 
-enum KeyType {
-  /// strong box 열쇠의 종류 정ㅊ
-  padlock, // 1_024회
-  button, // 10_000회
-  dial, // 30_000회
-  finger, // 1_000_000
-}
-
-int padlockCountLimit = 1024;
-int buttonCountLimit = 10000;
-int dialCountLimit = 1024;
-int fingerCountLimit = 1000000;
+import 'package:learn_dart_together/0318/key_type.dart';
 
 class StrongBox<T> {
   /// field
-  /// 금고에 담는 인스턴스 타입
+  /// 금고에 담는 인스턴스의 타입 (단 1개)
   T? _dataType;
-  KeyType _keyType;
+  final KeyType _keyType;
+  int _tryCount = 0;
 
   StrongBox({required KeyType keyType}) : _keyType = keyType;
 
   /// put
   /// dataType 인스턴스를 저장하는 메서드
-  void putDataType(T dataValue) {
-    _dataType = dataValue;
-  }
+  void putDataType(T dataValue) => _dataType = dataValue;
 
   /// getter
-  /// keyType 인스턴스를 얻는 메서드
-  /// 반복 코드가 너무 많은데 어떻게 줄일 수 없을까....
-  T? getKeyType(KeyType keyValue) {
-    switch (keyValue) {
-      case KeyType.padlock:
-        for (int i = 0; i <= padlockCountLimit; i++) {
-          print(_dataType);
-          _keyType = keyValue;
-        }
-        break;
-      case KeyType.button:
-        for (int i = 0; i <= buttonCountLimit; i++) {
-          print(_dataType);
-          _keyType = keyValue;
-        }
-        break;
-      case KeyType.dial:
-        for (int i = 0; i <= dialCountLimit; i++) {
-          print(_dataType);
-          _keyType = keyValue;
-        }
-        break;
-      case KeyType.finger:
-        for (int i = 0; i <= fingerCountLimit; i++) {
-          print(_dataType);
-          _keyType = keyValue;
-        }
-        break;
-      default:
-        print('제시된 keyType 중에 선택해주세요');
+  /// keyType 을 통해 _dataType을 얻는 메서드
+  T? getKeyType() {
+    _tryCount++;
+    for (int i = 0; _tryCount < typeCountLimit(); i++) {
+      return null;
     }
-    return null;
+      return _dataType;
+  }
+
+  /// key type별 횟수 제한을 return하는 메서드
+  int typeCountLimit() {
+    switch (_keyType) {
+      case KeyType.padlock:
+        return padlockCountLimit;
+      case KeyType.button:
+        return buttonCountLimit;
+      case KeyType.dial:
+        return dialCountLimit;
+      case KeyType.finger:
+        return fingerCountLimit;
+    }
   }
 }
 
