cb876553c2bbb68496dcda6ca9273f7e29cfaf49
kimhyungmin <jeen14rla@gmail.com>
2024-03-18 17:23:59 +0900

    fix: Word 클래스 vowel 리스트에 대문자 추가

--- lib/24_03_18/word.dart
+++ lib/24_03_18/word.dart
@@ -1,6 +1,6 @@
 class Word {
   String word = '';
-  List<String> vowel = ['a', 'e', 'i', 'o', 'u'];
+  List<String> vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
   bool isVowel(int i) {
     return vowel.contains(word[i]);
