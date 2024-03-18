a5616901410c4a695a1ace5c8f971c175ec40429
nala0897@naver.com <nala0897@naver.com>
2024-03-18 15:35:14 +0900

    feat: 연습 문제 3(문자열) test code 작성

---
+++ test/lib/240318/practice_3_test.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'package:learn_dart_together/240318/practice_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  // 영어 모음: a, e, i, o, u
+  group('i 번째 글자가 모음인지 알려주는 isVowel() 함수를 완성하시오', () {
+
+    int i = 5;
+
+    test('i 번째 글자가 주어진 단어 글자 수 보다 클 경우 false 가 반환된다.', () {
+
+      Word word = Word('word');
+      bool result = word.isVowel(i);
+
+      // 검증
+      expect(result, false);
+    });
+
+    test('i 번째 글자가 모음이면 true 가 반환된다.', () {
+
+      Word word = Word('generate');
+      bool result = word.isVowel(i);
+
+      // 검증
+      expect(result, true);
+    });
+  });
+
+}
