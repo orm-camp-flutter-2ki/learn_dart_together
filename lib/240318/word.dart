7c27291eb108112dbbb07ddf3f4afbf31ab05956
hee462 <a01080809887@gmail.com>
2024-03-18 17:25:22 +0900

    feat : 수정코드

--- lib/240318/word.dart
+++ lib/240318/word.dart
@@ -2,30 +2,19 @@ class Word {
   String word = '';
 
   bool isVowel(int i) {
-    String moem = word.substring(i, i + 1);
+    String moem = word.substring(i, i + 1).toLowerCase();
     return moem == 'a' ||
         moem == 'e' ||
         moem == 'i' ||
         moem == 'o' ||
-        moem == 'u' ||
-        moem == 'A' ||
-        moem == 'E' ||
-        moem == 'I' ||
-        moem == 'O' ||
-        moem == 'U';
+        moem == 'u';
   }
 
   bool isConsonant(int i) {
-    String jaem = word.substring(i, i + 1);
+    String jaem = word.substring(i, i + 1).toLowerCase();
     return jaem != 'a' &&
         jaem != 'e' &&
         jaem != 'i' &&
         jaem != 'o' &&
-        jaem != 'u' &&
-        jaem != 'A' &&
-        jaem != 'E' &&
-        jaem != 'I' &&
-        jaem != 'O' &&
-        jaem != 'U';
+        jaem != 'u';
   }
-}
