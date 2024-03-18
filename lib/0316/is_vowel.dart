f1be18b3fa9ea3a6f0275210e2e9f9d49f5dc896
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-18 16:49:01 +0900

    feat: is_vowel 클래스 생성
    - if 문을 사용하려다 자음을 set 으로 묶으면 어떨까... 싶어서 일단 묶어봤다...

--- lib/0316/is_vowel.dart
+++ lib/0316/is_vowel.dart
@@ -5,27 +5,27 @@ class Word {
 
   Word({required this.word});
 
+  Set<String> vowelList = {'a', 'e', 'i', 'u', 'o', 'A', 'E', 'I', 'U', 'O'};
+
   /// 다른 분들 대문자 소문자일 때도... 비교....
   /// 특정 값이라고 생각해 swhich를 사용했는데... 중복 코드가 많아서 if나 다른 것으로 고치는게 나을 것 같다.
-    bool isVowel(int i) {
-      switch (word[i]) {
-        case 'a':
-          return word.substring(i, i + 1) == 'a';
-        case 'e':
-          return word.substring(i, i + 1) == 'e';
-        case 'i':
-          return word.substring(i, i + 1) == 'i';
-        case 'o':
-          return word.substring(i, i + 1) == 'o';
-        case 'u':
-          return word.substring(i, i + 1) == 'u';
-        default:
-          print('$word의 $i번째 글자는 모음이 아닙니다.');
-          return false;
+  /// 길이도 고려!
+  bool isVowel(int i) {
+    if(i > word.length) {
+      print('다시 입력해주세요');
+      return false;
     }
+
+    return vowelList.contains(word[i]);
+
+    if (word[i]){}
+
+    if (word[i] == 'a' ||
+        word[i] == 'e' ||
+        word[i] == 'i' ||
+        word[i] == 'u' ||
+        word[i] == 'o') {}
   }
 }
 
-void main () {
-
-}
+void main() {}
