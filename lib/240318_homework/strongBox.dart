c1e257853610ebdf9cddf70c58144ce03975e4d4
Yejin.Shin <mingmi_27@naver.com>
2024-03-18 19:15:51 +0900

    feat: strongBox class refactor

--- lib/240318_homework/strongBox.dart
+++ lib/240318_homework/strongBox.dart
@@ -16,34 +16,20 @@ class StrongBox<E> {
   StrongBox({required this.keyValue});
 
   E? tryUnlock() {
-    switch (keyValue) {
-      case KeyType.padlock:
-        if (_count++ < 1024) return null;
-      case KeyType.button:
-        if (_count++ < 10000) return null;
-      case KeyType.dial:
-        if (_count++ < 30000) return null;
-      case KeyType.finger:
-        if (_count++ < 1000000) return null;
-    }
+    if (_count++ < keyValue.count) return null;
+
     return _treasure;
   }
 }
 
 enum KeyType {
-  padlock,
-  button,
-  dial,
-  finger,
+  padlock(type: 'padlock', count: 1024),
+  button(type: 'button', count: 10000),
+  dial(type: 'dial', count: 30000),
+  finger(type: 'finger', count: 1000000);
+
+  final String type;
+  final int count;
+
+  const KeyType({required this.type, required this.count});
 }
-// enum KeyType {
-//   padlock(type: 'padlock', count: 1024),
-//   button(type: 'button', count: 10000),
-//   dial(type: 'dial', count: 30000),
-//   finger(type: 'finger', count: 1000000);
-//
-//   final String type;
-//   final int count;
-//
-//   const KeyType({required this.type, required this.count});
-// }
