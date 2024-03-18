1dd33c912d29aa3e1ec55ea9dc347f99263d15b6
David Kim <mycodingtool9@gmail.com>
2024-03-18 17:36:35 +0900

    240318 문자열 조작 실습 2 feat : i 번째 글자가 자음인지 알려주는 isConsonant() 함수를 완성하시오.

--- lib/24_03_18/03_word.dart
+++ lib/24_03_18/03_word.dart
@@ -1,3 +1,7 @@
+// 실습 1
+// i 번째 글자가 모음인지 알려주는 isVowel() 함수를 완성하시오
+// 영어에서의 모음은 a, e, i, o, u 다섯가지이다
+
 class Word {
   String word = '';
 
@@ -19,11 +23,20 @@ class Word {
         return false;
     }
   }
+
+  // 실습 2
+  // i 번째 글자가 자음인지 알려주는 isConsonant() 함수를 완성하시오
+  bool isConsonant(int i) {
+    String char = word.substring(i, i + 1).toLowerCase();
+    List<String> vowels = ['a', 'e', 'i', 'o', 'u'];
+    return !vowels.contains(char); // vowels이 포함이 안된값을 찾기위해 '!'사용
+  }
 }
 
 void main() {
   Word word = Word('Hello world');
-  print(word.isVowel(1)); // true를 반환
-  print(word.isVowel(3)); // false를 반환
-
+  // print(word.isVowel(1)); // 결괏값이 'e'라서 true를 반환
+  // print(word.isVowel(3)); // 결괏값이 'l'이라서 false를 반환
+  print(word.isConsonant(0)); // 결괏밧이 'h'라서 true를 반환
+  print(word.isConsonant(1)); // 결괏값이 'e'라서 false를 반환
 }
