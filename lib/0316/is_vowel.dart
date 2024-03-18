f9ca0ac4c1449cd6cca4f59c21f24268a09f57ac
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-18 17:37:12 +0900

    feat: is_vowel 클래스 수정
    - 글자 수 범위 체크 후, 대문자가 있다면 소문자로 치환 한 후 검사합니다.

--- lib/0316/is_vowel.dart
+++ lib/0316/is_vowel.dart
@@ -1,30 +1,19 @@
-// enum VowelList { a, e, i, u, o }
-
 class Word {
-  String word;
+  final String _word;
 
-  Word({required this.word});
+  Word({required String word}) : _word = word;
 
-  Set<String> vowelList = {'a', 'e', 'i', 'u', 'o', 'A', 'E', 'I', 'U', 'O'};
+  /// 자음들 리스트, 소문자와 자음을 담았다.
+  final Set<String> _vowelsList = {'a', 'e', 'i', 'u', 'o'};
 
-  /// 다른 분들 대문자 소문자일 때도... 비교....
-  /// 특정 값이라고 생각해 swhich를 사용했는데... 중복 코드가 많아서 if나 다른 것으로 고치는게 나을 것 같다.
-  /// 길이도 고려!
   bool isVowel(int i) {
-    if(i > word.length) {
-      print('다시 입력해주세요');
+    if (i < 0 || i > _word.length) {
+      print('글자 수 범위가 아닙니다. 다시 입력해주세요');
       return false;
     }
 
-    return vowelList.contains(word[i]);
-
-    if (word[i]){}
-
-    if (word[i] == 'a' ||
-        word[i] == 'e' ||
-        word[i] == 'i' ||
-        word[i] == 'u' ||
-        word[i] == 'o') {}
+    String wordValue = _word[i].toLowerCase();
+    return _vowelsList.contains(wordValue);
   }
 }
 
