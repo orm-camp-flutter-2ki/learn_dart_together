75a0fb888888985a1f34008a471f3720c21c3e78
algochemy <algochemy@gmail.com>
2024-03-18 13:48:43 +0900

    feat: 생성자 사용 안 한 버전

---
+++ lib/240318/vowel_consonant.dart
@@ -0,0 +1,36 @@
+class Word {
+  String word = '';
+
+  bool isVowel(int i) {
+    String character = word.substring(i, i + 1).toLowerCase();
+    return character == 'a' ||
+        character == 'e' ||
+        character == 'i' ||
+        character == 'o' ||
+        character == 'u';
+  }
+
+  bool isConsonant(int i) {
+    String character = word.substring(i, i + 1).toLowerCase();
+    return !(character == 'a' &&
+        character == 'e' ||
+        character == 'i' ||
+        character == 'o' ||
+        character == 'u');
+  }
+}
+
+void main() {
+  Word word = Word();
+  word.word = "Hello";
+  print('주어진 문자의 개수는 ${word.word.length} 개입니다.');
+
+  for (int i = 0; i < word.word.length; i++) {
+    if (word.isVowel(i) == true) {
+      print('${i+1}번째 글자는 모음: ${word.isVowel(i)}');
+    }
+    if (word.isConsonant(i) == true) {
+      print('${i+1}번째 글자는 자음: ${word.isConsonant(i)}');
+    }
+  }
+}
