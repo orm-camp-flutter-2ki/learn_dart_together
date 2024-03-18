f2ae4077505f3ef8819916b44b2507b8785ffe50
SeongJongHo <whdgh9595@gmail.com>
2024-03-18 14:46:23 +0900

    [MODIFY] StrongBox 클래스에 key, keyUsageCount 필드 추가 및 get 메소드 수정

--- lib/24_03_18/-1/strong_box.dart
+++ lib/24_03_18/-1/strong_box.dart
@@ -1,9 +1,22 @@
+import 'package:learn_dart_together/24_03_18/-1/key_type.dart';
+
 class StrongBox<T> {
   T? _item;
+  final KeyType _key;
+  int _keyUsageCount = 0;
 
-  StrongBox({T? item}) : _item = item;
+  StrongBox({required KeyType key, T? item})
+      : _item = item,
+        _key = key;
 
   void put(T item) => _item = item;
 
-  T? get() => _item;
+  T? get() {
+    T? result;
+    if (_key.value > _keyUsageCount) {
+      result = _item;
+      ++_keyUsageCount;
+    }
+    return result;
+  }
 }
