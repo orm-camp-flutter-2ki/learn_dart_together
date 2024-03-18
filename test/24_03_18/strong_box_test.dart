f3d5b7e0f4d5ad1d600694ff2963721804426859
kimhyungmin <jeen14rla@gmail.com>
2024-03-18 16:28:12 +0900

    test: test 코드 수정

--- test/24_03_18/strong_box_test.dart
+++ test/24_03_18/strong_box_test.dart
@@ -2,16 +2,19 @@ import 'package:learn_dart_together/24_03_18/strong_box.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
-void main(){
+void main() {
   test('인스턴스 타입 미정', () {
     // given
-    StrongBox box = StrongBox();
+    StrongBox box = StrongBox(keyType: KeyType.padlock);
 
     // when
     box.put('a');
-    final inside = box.get();
+
+    for (int i = 0; i < 1024; i++) {
+      box.get();
+    }
 
     // then
-    expect(inside, 'a');
+    expect(box.get(), 'a');
   });
 }
