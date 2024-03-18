35d2421e757df1200ffcd968ca1a1dc3dfaa022f
JEON-Sungsu <anagma@naver.com>
2024-03-18 15:27:34 +0900

    feat: Word 클래스 작성
    - 모음검사 메서드 작성
    - 자음검사 메서드 작성

---
+++ lib/24_03_18/word.dart
@@ -0,0 +1,24 @@
+class Word {
+  String word;
+
+  Word({required this.word});
+
+  bool isVowel(int i) {
+    final List<String> vowel = ['a','e','i','o','u'];
+    final char = word.substring(i, i + 1);
+
+    if (vowel.contains(char)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  bool isConsonant(int i) {
+    if (isVowel(i)) {
+      return false;
+    }
+
+    return true;
+  }
+}
