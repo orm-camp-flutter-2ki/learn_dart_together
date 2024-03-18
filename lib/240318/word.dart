8a35d0c6d145bad027ffb2ce9379a0007754aa38
icicle_4 <dwayne.han4@gmail.com>
2024-03-19 00:32:09 +0900

    refactor: 코드정리 주석추가, 필요없는 괄호삭제, 사용하지 않는 코드 삭제

--- lib/240318/word.dart
+++ lib/240318/word.dart
@@ -1,31 +1,23 @@
 class Word {
   final String _word;
-  final String vowel = ('aeiou');
+  final String vowel = 'aeiou';
 
   Word(this._word);
-
+  // _word의 i 인덱스의 글자가 모음인지 확인
   bool isVowel(int i) {
-    final List<String> splited = vowel.split(',');
-
+    // aeiou가 _word의 i 인덱스 글자를 포함하고 있는지 확인
     if (vowel.contains(_word.substring(i, i + 1))) {
       return true;
     }
     return false;
   }
 
+  // _word의 i 인덱스의 글자가 자음인지 확인
   bool isConsonant(int i) {
+    // aeiou가 _word의 i 인덱스 글자를 포함하고 있지 않은지 확인
     if (!vowel.contains(_word.substring(i, i + 1))) {
       return true;
     }
     return false;
-  }
-}
-
-void main() {
-  String word = 'tomato';
-  String word1 = 'apple';
-  Word w = Word(word1);
-  for (int i = 0; i < word1.length; i++) {
-    print('$i는 모음=> ${w.isConsonant(i)}');
   }
 }
