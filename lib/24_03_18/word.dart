5b79bb5a918b0c0afc8ddbae2a192ecf7f8775d2
kimhyungmin <jeen14rla@gmail.com>
2024-03-18 17:33:06 +0900

    fix: Word 클래스 vowel 리스트에 대문자 삭제
    isConsonant 수정

--- lib/24_03_18/word.dart
+++ lib/24_03_18/word.dart
@@ -1,13 +1,15 @@
 class Word {
   String word = '';
-  List<String> vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  List<String> vowel = ['a', 'e', 'i', 'o', 'u'];
 
   bool isVowel(int i) {
-    return vowel.contains(word[i]);
+    return vowel.contains(word[i].toLowerCase());
   }
 
   bool isConsonant(int i) {
-    if (!vowel.contains(word[i])) {
+    if (word[i].toLowerCase().codeUnits[0] <= 97 &&
+        word[i].toLowerCase().codeUnits[0] >= 122 &&
+        !vowel.contains(word[i])) {
       return true;
     }
 
