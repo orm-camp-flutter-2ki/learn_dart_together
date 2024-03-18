a1ccf49309dde4459563212a581dee4b3b9e4b0a
Yejin.Shin <mingmi_27@naver.com>
2024-03-18 16:47:39 +0900

    feat:Word class 정의 & isVowel,isConsonant 정의(연습문제 1,2)

---
+++ lib/240318_homework/word.dart
@@ -0,0 +1,28 @@
+void main() {
+  Word beautiful = Word('beautiful');
+  for (int i = 1; i <= beautiful.word.length; i++) {
+    print('${beautiful.word[i - 1]}이(가) 모음 인가? : ${beautiful.isVowel(i)}');
+    beautiful.isVowel(i);
+  }
+  for (int i = 1; i <= beautiful.word.length; i++) {
+    print('${beautiful.word[i - 1]}이(가) 자음 인가? : ${beautiful.isConsonant(i)}');
+    beautiful.isConsonant(i);
+  }
+}
+
+class Word {
+  String word;
+  Set vowel = {'a', 'e', 'i', 'o', 'u'};
+
+  Word(this.word);
+
+  bool isVowel(int i) {
+    var findVowel = vowel.contains(word.substring(i - 1, i));
+    return findVowel;
+  }
+
+  bool isConsonant(int i) {
+    var findConsonant = !(vowel.contains(word.substring(i - 1, i)));
+    return findConsonant;
+  }
+}
