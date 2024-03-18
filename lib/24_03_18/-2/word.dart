32b82105d96b52a0dc0d97fa1321a81b75b11156
SeongJongHo <whdgh9595@gmail.com>
2024-03-18 17:49:12 +0900

    [MODIFY] Word 클래스 isConsonant, isVowel 메소드 작성

--- lib/24_03_18/-2/word.dart
+++ lib/24_03_18/-2/word.dart
@@ -3,9 +3,22 @@ class Word {
 
   bool isVowel(int i) {
     if (i >= word.length) return false;
-    return switch (word.substring(i, i + 1)) {
+    return _isVowelCharacter(word.substring(i, i + 1));
+  }
+
+  bool isConsonant(int i) {
+    if (i >= word.length) return false;
+    return _isConsonantCharacter(word.substring(i, i + 1));
+  }
+
+  bool _isVowelCharacter(String character) {
+    return switch (character) {
       'a' || 'e' || 'i' || 'o' || 'u' => true,
       _ => false
     };
+  }
+
+  bool _isConsonantCharacter(String character) {
+    return !_isVowelCharacter(character);
   }
 }
