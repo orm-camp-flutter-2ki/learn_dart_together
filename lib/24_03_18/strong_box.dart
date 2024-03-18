0a13bdd8a6eaa036d0ddc223aa6d34745aa4f0b9
JEON-Sungsu <anagma@naver.com>
2024-03-18 14:54:02 +0900

    feat: StrongBox 클래스 작성, KeyType Enum 작성

---
+++ lib/24_03_18/strong_box.dart
@@ -0,0 +1,38 @@
+import 'package:learn_dart_together/24_03_18/key_type.dart';
+
+class StrongBox<E> {
+  E? _stuff;
+  int _count;
+  final KeyType _key;
+
+  StrongBox({required E stuff, required KeyType key})
+      : _stuff = stuff,
+        _key = key,
+        _count = _setCount(key);
+
+  static int _setCount(KeyType key) {
+    switch (key) {
+      case KeyType.padlock:
+        return 1024;
+      case KeyType.button:
+        return 10000;
+      case KeyType.dial:
+        return 30000;
+      case KeyType.finger:
+        return 1000000;
+    }
+  }
+
+  void put(E thing) {
+    _stuff = thing;
+  }
+
+  E? get() {
+    if (_count != 0) {
+      _count -= 1;
+      return null;
+    } else {
+      return _stuff;
+    }
+  }
+}
