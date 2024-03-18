f31330f8c271ce1074c6513c7ca5d18d8639fcf8
YunZi.Song <yoonji5809@gmail.com>
2024-03-18 16:18:41 +0900

    Feat: 문자열 조작 실습 문제 1

---
+++ lib/24_03_18/string_control.dart
@@ -0,0 +1,21 @@
+// i 번째 글자가 모음인지 알려주는 isVowel() 함수를 완성하시오
+// 영어에서의 모음은 a, e, i, o, u 다섯가지이다.
+//
+class Word {
+  String word;
+
+  Word(this.word);
+
+  bool isVowel(int i) {
+    if (i > word.length) {
+      throw Exception('입력값이 글자 길이보다 큽니다.');
+    }
+
+    int beforeLetter = (i - 1) >= 0 ? (i - 1) : 0;
+
+    String letter = word.substring(beforeLetter, i);
+    String vowel = 'aeiou';
+
+    return vowel.contains(letter);
+  }
+}
