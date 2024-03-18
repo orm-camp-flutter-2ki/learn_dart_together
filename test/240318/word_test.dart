c57c8b2e23586c4b725e7f6acb9ef8a76049b5b5
chojja7188 <chojja7188@gmail.com>
2024-03-18 17:40:14 +0900

    style: word_test 테스트 코드 설명 수정

--- test/240318/word_test.dart
+++ test/240318/word_test.dart
@@ -2,7 +2,7 @@ import 'package:learn_dart_together/240318/word.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('소문자 판별', () {
+  test('Word Test', () {
     Word a = Word('Hello world');
     expect(a.isVowel(1) == false, true); // 1번째 글자인 H는 모음이 아님
     expect(a.isVowel(2) == true, true); // 2번째 글자인 e는 모음이 맞음
