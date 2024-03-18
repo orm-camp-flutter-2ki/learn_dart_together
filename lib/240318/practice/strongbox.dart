2b6f7d97e25688451e3b6a44a285a5eccf364552
KimDonghyeok <qoophon@gmail.com>
2024-03-18 14:59:03 +0900

    feat: StrongBox 클래스 수정 (제네릭, 열거형 연습문제)
    - KeyType 열거형 keyType 필드 추가
    - requiredNumOfOpen private 필드 추가
    - get() 메소드 수정

--- lib/240318/practice/strongbox.dart
+++ lib/240318/practice/strongbox.dart
@@ -1,9 +1,18 @@
+import 'key_type.dart';
+
 class StrongBox<E> {
   E? _item;
+  KeyType keyType;
+  int _requiredNumOfOpen;
 
-  void put(E item) => _item = item;
+  StrongBox({required this.keyType}) : _requiredNumOfOpen = 0;
+
+  void put(E item) {
+    _item = item;
+  }
 
   E? get() {
-    return _item;
+    _requiredNumOfOpen++;
+    return _requiredNumOfOpen >= keyType.requiredNumOfOpen ? _item : null;
   }
 }
