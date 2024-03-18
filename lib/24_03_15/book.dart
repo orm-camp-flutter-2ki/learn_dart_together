b0ba5b1b4ef5854b284ae3cc9c4a792777f9b7e6
hyojung.sung <hyo8967@naver.com>
2024-03-18 09:43:00 +0900

    feat. 인스턴트의 기본조작
    월, 일도 추가함

--- lib/24_03_15/book.dart
+++ lib/24_03_15/book.dart
@@ -9,14 +9,15 @@ class Book {
     DateTime? publishDate,
   }) : publishDate = publishDate ?? DateTime.now();
 
-
   @override
   bool operator ==(Object other) => //제목과 출간일이 같으면 같은 책으로 판단
-  identical(this, other) ||
+      identical(this, other) ||
       other is Book &&
           runtimeType == other.runtimeType &&
           title == other.title &&
-          publishDate == other.publishDate;
+          publishDate.year == other.publishDate.year &&
+          publishDate.month == other.publishDate.month &&
+          publishDate.day == other.publishDate.day;
 
   @override
   int get hashCode => title.hashCode ^ publishDate.hashCode; //두개 다 포함(^)
@@ -36,4 +37,14 @@ class Book {
       publishDate: publishDate ?? this.publishDate,
       comment: comment ?? this.comment,
     );
-  }}
+  }
+}
+
+void main() {
+  final now = DateTime.now();
+  final now2 = DateTime.now();
+
+  print(now.hashCode);
+  print(now2.hashCode);
+  print(now == now2);
+}
