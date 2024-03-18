2a25fd61552f89bfa0503e88109692fb3784694e
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-03-18 17:21:05 +0900

    feat : isconsonant 추가

--- lib/test/240318/is_vowel.dart
+++ lib/test/240318/is_vowel.dart
@@ -1,7 +1,31 @@
 class Word {
   String word = '';
 
+  Word(this.word);
+
   bool isVowel(int i) {
-    return word.substring(i, i + 1) == 'a';
+    switch (word.substring(i - 1, i)) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  bool isConsonant(int i) {
+    switch (word.substring(i - 1, i)) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        return false;
+      default:
+        return true;
+    }
   }
 }
