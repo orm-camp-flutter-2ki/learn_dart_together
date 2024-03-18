6ab374ccf2eab5562262c2663c0844cb30c194be
chojja7188 <chojja7188@gmail.com>
2024-03-18 14:41:58 +0900

    feat: Generic 연습문제2 작성
    - KeyType enum 생성
    - keyType에 따라 keyPoint 설정. 0이 되기 전까지는 null을 반환

--- lib/240318/strong_box.dart
+++ lib/240318/strong_box.dart
@@ -1,11 +1,40 @@
-class StrongBox<E> {
-  E? _data;
+import 'package:learn_dart_together/240318/key_type.dart';
 
-  void put(E? data) {
+class StrongBox<E> {
+  E _data;
+  KeyType _keyType;
+  int _keyPoint = 0;
+
+  StrongBox({required E data, required KeyType keyType})
+      : _data = data, _keyType = keyType
+  {
+    switch(keyType) {
+      case KeyType.padlock:
+        _keyPoint = 1024;
+      case KeyType.button:
+        _keyPoint = 10000;
+      case KeyType.dial:
+        _keyPoint = 30000;
+      case KeyType.finger:
+        _keyPoint = 1000000;
+    }
+}
+
+  void put(E data) {
     _data = data;
   }
 
   E? get() {
-    return _data;
+    if (_keyPoint == 0) return _data;
+    _keyPoint--;
+    return null;
   }
+}
+
+void main() {
+  StrongBox<int> a = StrongBox<int>(data: 3, keyType: KeyType.dial);
+  a.put(3);
+  print(a.get());
+  print(a.get());
+  print(a._keyPoint);
 }
