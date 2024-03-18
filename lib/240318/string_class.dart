771f0ec4157c578423d05d8263b985203b2e4808
Stranger Yang <ccamdol@gmail.com>
2024-03-18 16:30:27 +0900

    feat: 실습 1
    - i번째 글자가 모음인지 알려주는 isVowel() 함수

--- lib/240318/string_class.dart
+++ lib/240318/string_class.dart
@@ -2,6 +2,26 @@ class Word {
   String word = '';
 
   bool isVowel(int i) {
-    return word.substring(i, i + 1) == 'a';
+    if (i < 0 || i >= word.length) {
+      return false;
+    }
+
+    // i 번째 문자를 추출
+    final char = word.substring(i, i + 1);
+
+    return ['a', 'e', 'i', 'o', 'u'].contains(char.toLowerCase());
+  }
+}
+
+void main(){
+  final word = Word();
+  word.word = 'Hello, world!';
+
+  for (var i = 0; i < word.word.length; i++) {
+    if (word.isVowel(i)) {
+      print('$i 번째 문자는 모음입니다.');
+    } else {
+      print('$i 번째 문자는 자음입니다.');
+    }
   }
 }
