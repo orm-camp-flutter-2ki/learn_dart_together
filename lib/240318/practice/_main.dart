ce453dd5fa49398094b3f72e09c580522809f9af
KimDonghyeok <qoophon@gmail.com>
2024-03-18 16:52:06 +0900

    chore: Word 클래스 테스트 (문자열 조작 연습문제)

--- lib/240318/practice/_main.dart
+++ lib/240318/practice/_main.dart
@@ -1,5 +1,6 @@
 import 'package:dart_cli_practice/240318/practice/StrongBox.dart';
 import 'package:dart_cli_practice/240318/practice/key_type.dart';
+import 'package:dart_cli_practice/240318/practice/word.dart';
 
 void main() {
   StrongBox<String> vault = StrongBox(keyType: KeyType.finger);
@@ -11,4 +12,11 @@ void main() {
   for(int i = 0; i < vault.keyType.requiredNumOfOpen + 1; i++) {
     print(vault.get());
   }
+
+  final word = Word();
+  word.word = 'Lorem ipsum dolor sit amet.';
+
+  print('문자열의 길이는 ${word.word.length}');
+  print(word.isVowel(1));
+  print(word.isConsonant(1));
 }
