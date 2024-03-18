e2b0ee2a0bdfd03efb197268e168fe7233e9396a
hoogom88 <ziziwhdgus@naver.com>
2024-03-18 11:31:11 +0900

    Update. StrongBox 클래스 KeyType 변수 생성 및 get 메서드 수정

--- lib/24_03_18/StrongBox.dart
+++ lib/24_03_18/StrongBox.dart
@@ -6,11 +6,22 @@ enum KeyType {
 }
 
 class StrongBox<T> {
+  final KeyType _keyType;
   T _savedInstance;
+  int _usedCnt = 0;
 
-  StrongBox(this._savedInstance);
+  StrongBox(this._savedInstance, this._keyType);
 
   void put(T newInstance) => _savedInstance = newInstance;
 
-  T get() => _savedInstance;
+  T? get() {
+    var limitCnt = switch (_keyType) {
+      KeyType.padlock => 1024,
+      KeyType.button => 10000,
+      KeyType.dial => 30000,
+      KeyType.finger => 1000000,
+    };
+    _usedCnt++;
+    return _usedCnt < limitCnt ? null : _savedInstance;
+  }
 }
