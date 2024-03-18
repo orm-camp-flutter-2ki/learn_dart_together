55cc97b9bb880721834f5c3ebdb73ee33837ec98
icicle_4 <dwayne.han4@gmail.com>
2024-03-18 17:01:10 +0900

    feat: 제너릭 연습문제2. StrongBox 클래스 생성

--- lib/240318/strong_box.dart
+++ lib/240318/strong_box.dart
@@ -1,15 +1,57 @@
-class StrongBox<E> {
-  /**
-   * 금고 클래스에 담는 인스턴스의 타입은 미정
-    금고에는 1개의 인스턴스를 담을 수 있음
-    put() 메서드로 인스턴스를 저장하고 get() 메서드로 인스턴스를 얻을 있음
-    get() 으로 얻을 때는 별도의 타입 캐스팅을 사용하지 않아도 됨   
- */
-  E? _data;
+enum KeyType {
+  padlock,
+  button,
+  dial,
+  finger,
+}
 
-  void put(E? data) {
-    _data = data;
+class StrongBox<E> {
+  E? _data;
+  int _usageCount = 0;
+  bool _locked = false;
+
+  final KeyType _keyType;
+
+  StrongBox(this._keyType) {
+    int keyLitmit = getKeyLimit();
   }
 
-  E? get() => _data;
+  int getKeyLimit() {
+    switch (_keyType) {
+      case KeyType.padlock:
+        return 1024;
+      case KeyType.button:
+        return 10000;
+      case KeyType.dial:
+        return 30000;
+      case KeyType.finger:
+        return 1000000;
+    }
+  }
+
+  bool isLocked() {
+    return _usageCount >= getKeyLimit();
+  }
+
+  void lock() {
+    _locked = true;
+  }
+
+  E? get() {
+    if (_locked != true && !isLocked() && _usageCount < getKeyLimit()) {
+      _usageCount += 1;
+      return _data;
+    } else {
+      print("상자가 잠겨 있거나 사용 횟수를 초과하여 물건을 꺼낼 수 없습니다.");
+      return null;
+    }
+  }
+
+  void put(E? data) {
+    if (!isLocked()) {
+      _data = data;
+    } else {
+      print("상자가 잠겨 있어서 물건을 넣을 수 없습니다.");
+    }
+  }
 }
