3dab6da538ded19323c3b7e3003de22fe3897da6
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-03-18 15:57:06 +0900

    feat: 확인

---
+++ lib/240318/04_exam.dart
@@ -0,0 +1,23 @@
+void main() {
+  int luckyNumber1 = 13;
+  int luckyNumber2 = luckyNumber1;
+  luckyNumber2 = 12;
+
+  print(luckyNumber1);
+  print(luckyNumber2);
+
+
+  // String greeting1 = 'Hello, World!';
+  // String greeting2 = greeting1;
+  // greeting2.toUpperCase();
+  //
+  // print(greeting1);
+  // print(greeting2);
+
+
+  // String greeting = 'Hello';
+  //
+  // print(greeting.replaceAll('H', 'J'));
+  // print(greeting);
+
+}
