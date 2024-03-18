00c4d81363c25f6f2bb02f0fea2523f4cd35ca12
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-18 16:37:42 +0900

    feat: add isConsonant method

--- test/0318/word_test.dart
+++ test/0318/word_test.dart
@@ -2,10 +2,15 @@ import 'package:learn_dart_together/0318/word.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('a, e, i, o, u 확인', () {
-    Word word = Word('HELLO');
+  Word word = Word('HELLO');
 
+  test('모음 확인', () {
     expect(word.isVowel(0), false);
     expect(word.isVowel(1), true);
+  });
+
+  test('자음 확인', () {
+    expect(word.isConsonant(0), true);
+    expect(word.isConsonant(1), false);
   });
 }
