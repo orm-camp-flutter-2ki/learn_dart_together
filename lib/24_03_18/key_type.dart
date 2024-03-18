8eb97a144e818b74900011a0700b99b53b4cbf48
JEON-Sungsu <anagma@naver.com>
2024-03-18 16:10:32 +0900

    reafctor: KeyType Enum 코드 개선
    - switch문 추가

--- lib/24_03_18/key_type.dart
+++ lib/24_03_18/key_type.dart
@@ -3,5 +3,20 @@ enum KeyType {
   button,
   dial,
   finger,
-  once,
+  once;
+
+  int get value {
+    switch (this) {
+      case KeyType.padlock:
+        return 1024;
+      case KeyType.button:
+        return 10000;
+      case KeyType.dial:
+        return 30000;
+      case KeyType.finger:
+        return 1000000;
+      case KeyType.once:
+        return 0;
+    }
+  }
 }
--- lib/24_03_18/strong_box.dart
+++ lib/24_03_18/strong_box.dart
@@ -8,22 +8,7 @@ class StrongBox<E> {
   StrongBox({required E stuff, required KeyType key})
       : _stuff = stuff,
         _key = key,
-        _count = _setCount(key);
-
-  static int _setCount(KeyType key) {
-    switch (key) {
-      case KeyType.padlock:
-        return 1024;
-      case KeyType.button:
-        return 10000;
-      case KeyType.dial:
-        return 30000;
-      case KeyType.finger:
-        return 1000000;
-      case KeyType.once:
-        return 0;
-    }
-  }
+        _count = key.value;
 
   void put(E stuff) {
     _stuff = stuff;
