7820644a6692312f11ae61d7526ee55b90527500
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-18 14:26:48 +0900

    feat: strong_box 클래스 수정
    - put과 get 메서드를 사용하여 금고에 담는 인스턴스의 타입을 얻어 저장하도록 수정하였습니다.

--- lib/0316/strong_box.dart
+++ lib/0316/strong_box.dart
@@ -3,7 +3,21 @@
 // 2. put() 메서드로 인스턴스를 저장하고 get()메서드로 인스턴스를 얻을 수 있음
 // 3. get() 으로 얻을 때는 별도의 타입 캐스팅을 사용하지 않아도 된다.
 class strongBox<E> {
+  /// field
+  /// 금고에 담는 인스턴스 타입
   E? _dataType;
+
+  /// put
+  /// 인스턴스를 저장하는 메서드
+  void put(E dataValue) {
+    _dataType = dataValue;
+  }
+
+  /// getter
+  /// 인스턴스를 얻는 메서드
+  E? get() {
+    return _dataType;
+  }
 }
 
 void main() {}
