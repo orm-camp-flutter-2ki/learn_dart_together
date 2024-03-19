9e4ef05c52d9f2b66c9ed22a772779b430354eac
gyubit <gyubros@gmail.com>
2024-03-19 15:31:25 +0900

    ADD: 파일 복사 코드 추가

--- lib/exception_240319/file_manipulation /fille_manipulation.dart
+++ lib/exception_240319/file_manipulation /fille_manipulation.dart
@@ -1,19 +1,22 @@
 import 'dart:io';
 
-void copy(String source, String target){
+// 파일 복사 함수
+void copy(String source, String target) {
   final sourceFile = File('save.txt');
   final targetFile = File('target.txt');
-
 }
 
-void main(){
-  // 파일 열기
+void main() {
+  // 파일 생성 or 열기
   final myFile = File('save.txt');
-  // 파일 내용 쓰기 (덮어쓰기)
+
+  // 파일 내용 쓰기 (방식: 덮어 쓰기)
   myFile.writeAsStringSync('Hello, world!');
+  // 파일 내용 쓰기 (방식: 덮어 쓰기)
   myFile.writeAsStringSync('Hello, World Cup!');
-  // 파일 내용 쓰기 (이어쓰기)
+  // 파일 내용 쓰기 (방식: 이어 쓰기)
   myFile.writeAsStringSync('\nAgain World 2002', mode: FileMode.append);
 
-
+  // 파일 복사 실행
+  myFile.copy;
 }
