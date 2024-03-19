7acb4425716315230808467d3dd9018dd137c7c9
tomoyo519 <vanessa.cheong1@gmail.com>
2024-03-19 21:03:00 +0900

    [feat] 3/18 comment 수정 - 코드정리, const final 정리 등

--- lib/정다희/24_03_18/exercise.dart
+++ lib/정다희/24_03_18/exercise.dart
@@ -5,7 +5,6 @@
 // get() 으로 얻을 때는 별도의 타입 캐스팅을 사용하지 않아도 됨
 import 'enum.dart';
 
-
 //파일 따로 빼서 import 처리 해주면 됨
 // enum 이라는 키워드를 선언하면 static 처럼 사용할 수 있음.
 // static 처리할수 있는 방법 ???
@@ -13,47 +12,45 @@ import 'enum.dart';
 //   padlock, button, dial, finger
 // }
 
-class StrongBox<E, T> {
+class StrongBox<E, Key> {
   int count = 0;
   E? _data;
-  T? _variable;
+  KeyType _variable;
 
-  StrongBox( E data,  T variable ) : _data = data,  _variable = variable;
-
+  StrongBox(E data, KeyType variable)
+      : _data = data,
+        _variable = variable;
 
   void put(E data) {
     _data = data;
   }
 
   E? get() {
-
     count++;
-    switch(_variable){
+    switch (_variable) {
       case KeyType.button:
-        if(count < 10000) {
+        if (count < 10000) {
           return null;
-        } return _data;
+        }
+        return _data;
       case KeyType.dial:
-        if(count < 30000){
+        if (count < 30000) {
           return null;
-        } return _data;
+        }
+        return _data;
 
       case KeyType.finger:
-
-        if(count < 1000000){
+        if (count < 1000000) {
           return null;
-        } return _data;
+        }
+        return _data;
 // dart문에서는 break 을 쓰지 않아도 된다.
       case KeyType.padlock:
-        if(count < 1024){
+        if (count < 1024) {
           return null;
         }
         return _data;
     }
     return _data;
-
-
   }
 }
-
-
