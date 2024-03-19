bfcb963a3fedeb8ea592ef7a9111b1396326be83
hee462 <a01080809887@gmail.com>
2024-03-19 17:37:09 +0900

    feat: Department  수정 사항

--- lib/240319/employee.dart
+++ lib/240319/employee.dart
@@ -13,5 +13,6 @@ class Department {
 
   Department(this.name, this.leader);
 
-  Map<String, dynamic> toJson() => {'name': name, 'leader': leader};
+// Employee 불러와서 사용하기 때문에 , 추가적으로 .tojson()을 해서 구석구석 json 형태로 만들어줄것!
+  Map<String, dynamic> toJson() => {'name': name, 'leader': leader.toJson()};
 }
