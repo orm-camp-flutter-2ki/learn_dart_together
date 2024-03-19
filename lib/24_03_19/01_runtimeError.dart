e4bc94a33b6c5e3333c477d11635499125707226
leepl <yplunap25@gmail.com>
2024-03-19 14:27:41 +0900

    feat: runtime error 발생
    - runtimeError 파일 생성

---
+++ lib/24_03_19/01_runtimeError.dart
@@ -0,0 +1,13 @@
+void main() {
+  // // 여기에 발생시키면 아래 코드는 다 날아간다.
+  // throw Exception('에러발생');
+
+  final numString = '10.5';
+
+  int num = int.parse(numString);
+
+  print(num);
+
+  // // 이미 위에서 터져서 안뜸
+  // throw Exception('에러발생');
+}
