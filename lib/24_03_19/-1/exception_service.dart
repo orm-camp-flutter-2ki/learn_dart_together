6771b2c8f688ce3a5af6aae1bf0af321ab3dc522
SeongJongHo <whdgh9595@gmail.com>
2024-03-19 14:35:44 +0900

    [MODIFY] exception service 클래스 try catch문 추가

--- lib/24_03_19/-1/exception_service.dart
+++ lib/24_03_19/-1/exception_service.dart
@@ -1,7 +1,19 @@
 class ExceptionService {
-  int run() {
-      final numString = '10.5';
+  static const int defaultResult = 0;
+  String _numString;
 
+  ExceptionService({required String numString}) : _numString = numString;
+
+  String get numString => _numString;
+
+  set numString(String value) => _numString = value;
+
+  int run() {
+    try {
       return int.parse(numString);
+    } on FormatException catch (e) {
+      print('FormatException: $e');
+      return defaultResult;
+    }
   }
 }
