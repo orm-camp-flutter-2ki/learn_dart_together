4584fa3b0c8f6dcb9cde49df23a65bc06ff5f85a
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-20 00:38:41 +0900

    feat: add fromJson named constructor

--- lib/0319/department.dart
+++ lib/0319/department.dart
@@ -1,11 +1,14 @@
 import 'employee.dart';
 
 class Department {
-
   String name;
   Employee leader;
 
   Department(this.name, this.leader);
+
+  Department.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        leader = json['leader'];
 
   Map<String, dynamic> toJson() => {'name': name, 'leader': leader.toJson()};
 }
--- lib/0319/employee.dart
+++ lib/0319/employee.dart
@@ -1,9 +1,12 @@
 class Employee {
-
   String name;
   int age;
 
   Employee(this.name, this.age);
+
+  Employee.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        age = json['age'];
 
   Map<String, dynamic> toJson() => {'name': name, 'age': age};
 }
