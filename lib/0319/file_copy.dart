2c4e1afcb7494908dd790823085629ca662e7030
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-19 16:59:12 +0900

    feat: file copy test 파일 수정
    - test 코드 작성 :
    1) 오늘 날짜의 directory로 경로를 직접 지정하도록 수정

--- lib/0319/file_copy.dart
+++ lib/0319/file_copy.dart
@@ -1,14 +1,16 @@
 import 'dart:io'; // html말고 io로 import 하는 것이 좋다.
 
 void copyFile(String sourcePath, String targetPath) {
-  // 원본 파일 경로
+  /// 원본 파일 경로
   final getSource = File(sourcePath);
-  // 내용 읽고 변수에 담음
+
+  /// 내용 읽고 변수에 담음
   final readSource = getSource.readAsStringSync();
 
-  // 복사할 파일 경로
+  /// 복사할 파일 경로
   final fileWithCopy = File(targetPath);
-  // 내용 쓰기
+
+  /// 내용 쓰기
   fileWithCopy.writeAsStringSync(readSource);
 }
 
@@ -16,4 +18,5 @@ void copyFile(String sourcePath, String targetPath) {
 * 파일을 복사하는 함수를 작성
 * : 원본파일 경로와 복사할 파일 경로는 프로그램 실행 시 파라미터로 전달된다.
 * : 버퍼링이나 에러처리는 하지 않는다.
+* : 내부에서 .path를 하는게 에러가 덜 나나? 라는 생각도 했는데 <- 이건 뭔가 잘못된 접근같았다...
 * */
--- test/0319/file_copy_test.dart
+++ test/0319/file_copy_test.dart
@@ -5,22 +5,22 @@ import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
-  /// 경로를 내가 지정해서 넣을 수 있다..!? 시도해보기
+  /// Directory 경로 변수: 너무 길어서 변수에 담았다.
+  final String todayDirectory = '/Users/yong/Desktop/learn_dart_together/lib/0319';
 
-  test('파일 생성 및 내용 작성 잘 되는지 test', () {
-    // 원본에 내용 쓰기
-    final originFile = File('origin_test_file.txt');
+  /// 원본 경로
+  final originFile = File('$todayDirectory/origin_test_file.txt');
+  /// 카피 할 경로
+  final fileCopyTest = File('$todayDirectory/copy_test_file.txt');
+
+  test('파일 생성 후 내용 작성 잘 되는지 test', () {
+    /// 원본에 내용 쓰기
     originFile.writeAsStringSync('Hello World!');
     originFile.writeAsStringSync('Hello Dart!'); // 새로 덮어 쓰는 것
   });
 
-  // 원본 경로
-  final originFile = File('origin_test_file.txt');
-  // 카피 할 경로
-  final fileCopyTest = File('copy_test_file.txt');
-
-  test('내용 추가 잘 되는지 test', () {
-    // 원본에 내용 추가
+  test('기존 파일 내용에 추가가 잘 되는지 test', () {
+    /// 원본에 내용 추가
     originFile.writeAsStringSync('\nMy name is Yong.', mode: FileMode.append);
     originFile.writeAsStringSync('\nnice to meet you.', mode: FileMode.append);
 
@@ -28,15 +28,16 @@ void main() {
   });
 
   test('카피가 잘 되는지 test', () {
+    /// copyFile 메서드 실행
     copyFile(originFile.path, fileCopyTest.path);
 
-    expect(
-        originFile.readAsStringSync() == fileCopyTest.readAsStringSync(), true);
-    expect(originFile.readAsStringSync() != fileCopyTest.readAsStringSync(),
-        false);
+    expect(originFile.readAsStringSync() == fileCopyTest.readAsStringSync(), true);
+    expect(originFile.readAsStringSync() != fileCopyTest.readAsStringSync(), false);
   });
 
-  // 파일 지우기
-  // originFile.delete();
-  // fileCopyTest.delete();
+  test('파일 지우기 test', () {
+    /// 파일 지우기
+    originFile.delete();
+    fileCopyTest.delete();
+  });
 }
