77712f189e77b75712706ada01ba79c23ccb7e5e
chojja7188 <chojja7188@gmail.com>
2024-03-19 16:21:08 +0900

    style: 문제에 맞게 수정

--- test/240319/serialize_test.dart
+++ test/240319/serialize_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 void main() {
   test('Serialize Test', () {
     Employee employee = Employee('홍길동', 41);
-    Department department = Department('그냥회사', employee);
+    Department department = Department('총무부', employee);
 
     department.createTxtFile();
   });
