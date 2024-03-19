222148b14bc356a58d4731edc846c15fa0f28bdd
gyubit <gyubros@gmail.com>
2024-03-19 14:27:41 +0900

    FEAT: 예외 연습문제2._재에러 발샐

--- lib/exception_240319/exception.dart
+++ lib/exception_240319/exception.dart
@@ -1,7 +1,22 @@
-void main(){
+void main() {
   final numString = '10.5';
 
-  int num = int.parse(numString);
+  void someError() {
+    int num = int.parse(numString);
+    print(num);
+  }
 
-  print(num);
+  try {
+    // 예외가 발생할 수 있는 코드 작성
+    someError();
+  } catch (e) {
+    // 예외 처리
+    int num = 0;
+    print('예외 처리됨');
+  } finally {
+    // 예외가 발생하든 안 하든 항상 실행되는 코드
+    print('Finally block executed'); // 항상 출력됨
+  }
 }
+
+
