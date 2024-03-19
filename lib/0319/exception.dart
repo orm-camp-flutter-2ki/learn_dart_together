3cad3bb2e60567b513458a4d358fd35d53ca719a
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-19 14:46:19 +0900

    feat: exception 클래스 작성
    - 에러 처리 : 예외가 발생하면 0으로 처리하도록 try-catch 문을 작성했습니다.

--- lib/0319/exception.dart
+++ lib/0319/exception.dart
@@ -1,10 +1,18 @@
 void main() {
   final numString = '10.5';
-  int num = int.parse(numString); // runtime error;
+  int num;
+  try {
+    num = int.parse(numString);
+  } catch (e) {
+    print('[error message] $e');
+    num = 0;
+  }
   print(num);
 }
 
 /*
 * try-catch()문을 사용하여 위 코드의 예외처리 하기
 * : 예외가 발생하면 0으로 처리
+*
+* try catch() 안에서 선언한 변수/함수는 catch 코드 블럭 브레스 안에서만 유효하다.
 * */
