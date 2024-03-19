50f2d3eb321a66cf766d7d8e6ba5d345539bc9e2
kimhyungmin <jeen14rla@gmail.com>
2024-03-19 15:30:28 +0900

    comment: file_manipulation에 주석 수정

--- lib/24_03_19/file_manipulation/file_manipulation.dart
+++ lib/24_03_19/file_manipulation/file_manipulation.dart
@@ -1,9 +1,5 @@
 import 'dart:io';
 
-// 1. 원본과 복사본 경로를 받는다
-// 2. 원본 경로의 파일을 읽는다 (readAsStringSync)
-// 3. 복사본 경로의 파일에 쓴다 (writeAsStringSync)
-
 void copy(String source, String target) {
   final origin = File(source);
   final copy = File(target);
@@ -12,11 +8,14 @@ void copy(String source, String target) {
 }
 
 void main() {
+  // 원본 파일
   final originFile = File('origin.txt');
+
+  // 복사본 파일
   final copyFile = File('copy.txt');
 
+  // 원본에 내용 작성
   originFile.writeAsStringSync('Hello, World!');
-  copyFile.writeAsStringSync('');
 
   copy(originFile.path, copyFile.path);
 }
