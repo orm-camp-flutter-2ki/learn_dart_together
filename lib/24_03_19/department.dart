3fd0c3bb6f26f6c7420a446bd6d17fb97afc0225
JEON-Sungsu <anagma@naver.com>
2024-03-19 14:40:02 +0900

    feat: department 클래스 작성

---
+++ lib/24_03_19/department.dart
@@ -0,0 +1,8 @@
+import 'package:learn_dart_together/24_03_19/employee.dart';
+
+class Department {
+  String name;
+  Employee leader;
+
+  Department({required this.name, required this.leader});
+}
