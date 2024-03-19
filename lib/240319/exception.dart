594af488a2e7264fb880612e109d8b2a8ea122a6
chojja7188 <chojja7188@gmail.com>
2024-03-19 14:14:07 +0900

    feat: 예외 연습문제 1번 작성

--- lib/240319/exception.dart
+++ lib/240319/exception.dart
@@ -1,7 +1,12 @@
 void main() {
-  final numString = '10.5';
+  final numString = '10å5';
 
-  int num = int.parse(numString);
+  int num;
+  try {
+    num = int.parse(numString);
+  } catch(e) {
+    num = 0;
+  }
 
   print(num);
 }
