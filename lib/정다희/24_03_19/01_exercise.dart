026b1903ab999f7e2fd02402d9fe156556901d02
tomoyo519 <vanessa.cheong1@gmail.com>
2024-03-19 20:57:06 +0900

    [feat] 3/19 예외처리 연습문제1

--- lib/정다희/24_03_19/01_exercise.dart
+++ lib/정다희/24_03_19/01_exercise.dart
@@ -3,9 +3,10 @@ void main() {
     final numString = '10.5';
     int num = int.parse(numString);
   } catch (e) {
-    print(e);
+    print('e:$e');
+    int num = 0;
+    print('thisisnum: $num');
   }
-
   // on catch 문과 함께 사용은 안됨
   // on formatException{
   //
