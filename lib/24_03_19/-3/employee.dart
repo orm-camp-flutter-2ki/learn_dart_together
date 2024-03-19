6097673d864bd0cf14a57612602b25083ed29364
SeongJongHo <whdgh9595@gmail.com>
2024-03-19 17:20:49 +0900

    [MODIFY] employee, department eqauls, hashcode 재정의

--- lib/24_03_19/-3/employee.dart
+++ lib/24_03_19/-3/employee.dart
@@ -1,10 +1,21 @@
 import 'package:learn_dart_together/24_03_19/-3/mappable.dart';
 
 class Employee implements Mappable {
-  String _name;
-  int _age;
+  final String _name;
+  final int _age;
 
   Employee(this._name, this._age);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Employee &&
+          runtimeType == other.runtimeType &&
+          _name == other._name &&
+          _age == other._age;
+
+  @override
+  int get hashCode => _name.hashCode ^ _age.hashCode;
 
   @override
   Map<String, dynamic> toJson() {
