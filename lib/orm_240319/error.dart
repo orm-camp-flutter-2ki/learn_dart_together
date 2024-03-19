2deabd231b6493028e3bf4f90d312d14deff6809
BAUu <yimej07@gmail.com>
2024-03-19 14:22:52 +0900

    feat: runtime error 코드를 try-catch로 예외 상황 구문 만들기

--- lib/orm_240319/error.dart
+++ lib/orm_240319/error.dart
@@ -1,7 +1,13 @@
 void main() {
+  try {
+    errorCode();
+  } catch (e) {
+    print(0);
+  }
+}
+
+void errorCode() {
   final numString = '10.5';
-
   int num = int.parse(numString);
-
   print(num);
 }
