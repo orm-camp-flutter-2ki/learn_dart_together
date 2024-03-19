673f4de4310e19593fe6e822c96892696e9bd4f4
SeongJongHo <whdgh9595@gmail.com>
2024-03-19 15:30:27 +0900

    [ADD] file service 테스트코드 추가

--- lib/24_03_19/-2/file_service.dart
+++ lib/24_03_19/-2/file_service.dart
@@ -1,16 +1,21 @@
 import 'dart:io';
 
 class FileService {
-  String _readFile(String path) {
+  String readFile(String path) {
     final file = File(path);
 
     return file.readAsStringSync();
   }
 
   void copyFile(String sourcePath, String targetPath) {
-    final sourceFile = _readFile(sourcePath);
+    final sourceFile = readFile(sourcePath);
     final targetFile = File(targetPath);
 
     targetFile.writeAsStringSync(sourceFile);
+  }
+
+  void deleteFile(String path) {
+    final file = File(path);
+    file.deleteSync();
   }
 }
