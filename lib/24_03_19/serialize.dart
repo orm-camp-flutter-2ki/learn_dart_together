5fab9280e596aeef3464f0a92a78fe7bd07accec
hoogom88 <ziziwhdgus@naver.com>
2024-03-19 21:15:53 +0900

    Update. Serializable 구현 실패

--- lib/24_03_19/serialize.dart
+++ lib/24_03_19/serialize.dart
@@ -1,6 +1,31 @@
 import 'dart:convert';
 import 'package:learn_dart_together/24_03_19/file.dart';
 
+// Serializable 구현 실패.. 다른 방법을 찾아봐야할 듯
+// - Dart는 리플렉션이 없음
+// - 생성자는 추상메서드로 추상화 불가
+// - Static은 컴파일 타임에 선언되므로 제네릭 파라미터에 접근 불가
+//
+// abstract interface class Serializable {
+//     Serializable.fromJson(Map<String, dynamic> json);
+//     Map<String, dynamic> toJson();
+//
+//     factory Serializable(Map<String, dynamic> json) {
+//       return T.fr
+//     }
+// }
+//
+//
+// class Serializer {
+//   void serializeAndSaveAsFile(Serializable instance, String title) {
+//     writeWithString(title, jsonEncode(instance.toJson()));
+//   }
+//
+//   T deserializedFromFile<T extends Serializable>(String title) {
+//     return T.fromJson(jsonDecode(readAsString(title)));
+//   }
+// }
+
 class Employee {
   String name;
   int age;
