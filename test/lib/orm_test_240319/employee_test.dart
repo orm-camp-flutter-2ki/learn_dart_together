b2377e3c177055a08bb392dea2bb0992a7feef30
BAUu <yimej07@gmail.com>
2024-03-19 17:56:28 +0900

    feat: 정렬화

---
+++ test/lib/orm_test_240319/employee_test.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:learn_dart_together/orm_240318/employee.dart';
+import 'package:test/test.dart';
+
+void main(){
+  test('jsontest', () {
+    Employee leader = Employee('홍길동', 41);
+    Department department = Department('총무부', leader);
+
+    final testText = department.toJson();
+    String testString = jsonEncode(testText);
+
+    final sourceFile = File('company.text');
+    sourceFile.writeAsStringSync(testString);
+    final sourcetext = sourceFile.readAsStringSync();
+    final fileJson = jsonDecode(sourcetext);
+    String departName = fileJson['name'];
+    expect(departName , equals('총무부'));
+
+  });
+}
