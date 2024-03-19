fabe952cf9b5cb88cabc88742f7f70ff092d0fd3
gyubit <gyubros@gmail.com>
2024-03-19 14:29:47 +0900

    FIXED: 예외 연습문제2._에러 수정

--- lib/exception_240319/exception.dart
+++ lib/exception_240319/exception.dart
@@ -12,7 +12,7 @@ void main() {
   } catch (e) {
     // 예외 처리
     int num = 0;
-    print('예외 처리됨');
+    print('예외 처리됨. 값${num}으로 처리 ');
   } finally {
     // 예외가 발생하든 안 하든 항상 실행되는 코드
     print('Finally block executed'); // 항상 출력됨
