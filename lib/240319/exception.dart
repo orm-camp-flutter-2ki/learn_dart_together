e3e6237a65fed6bf03b875c2d44ce2d5cd3173e4
algochemy <algochemy@gmail.com>
2024-03-19 13:43:59 +0900

    feat : 예외가 발생하면 0으로 처리

--- lib/240319/exception.dart
+++ lib/240319/exception.dart
@@ -1,5 +1,12 @@
 void main() {
   final numString = '10.5';
-  int num = int.parse(numString);
+  late int num;
+  
+  try {
+    num = int.parse(numString);
+  } catch (error) {
+    num = 0; // 예외가 발생하면 0으로 처리
+  }
+
   print(num);
 }
