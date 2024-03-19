83cb2b91acfbce7f063021606b475f5a2cbc3d08
kaggle68 <kaggle68@gmail.com>
2024-03-19 14:32:08 +0900

    try-catch를 사용한 예외처리
    이전 코드의 print(nums[3]); 을 그대로 오류로 사용
    
    catch에서 오류 발생 시 0으로 처리하기 위해 num=0 설정

--- lib/24_03_19/error.dart
+++ lib/24_03_19/error.dart
@@ -7,6 +7,17 @@ void main() {
   // runtime에러를 위한 리스트 생성
   List<double> nums = [1, 2, 3];
 
-  // 초과한 값 출력 시도해서 오류 발생
-  print(nums[3]);
+  // try-catch를 사용한 예외처리
+  try {
+    // 리스트를 초과하는 값을 출력 시도 <- 에러발생
+    print(nums[3]);
+  }
+  // 에러 발생에 대한 예외처리
+  catch (e) {
+    // 0으로 처리하기 위해 num = 0 설정
+    num = 0;
+
+    // 예외처리한 값을 출력하기 위한 print
+    print(num);
+  }
 }
