66b5f590adcfa70abb133e86e7a3be88041a8dc2
BAUu <yimej07@gmail.com>
2024-03-19 23:09:58 +0900

    fix: 파일 error 문제 코드 수정
    따로 했던 클래스를 하나의 클래스로 변경후 입력되는 String으로 모두 풀이되게 변경

--- lib/orm_240319/error.dart
+++ lib/orm_240319/error.dart
@@ -1,18 +1,12 @@
 class Error1 {
-void errorCode() {
-  final numString = '10.5';
-  int num = int.parse(numString);
-  print(num);
-}
 
-void errorSome() {
+int errorSome(String numString) {
+  int num;
   try {
-    errorCode();
+    num = int.parse(numString);
   } catch (e) {
-    int num = 0;
-    print(num == 0);
+    num = 0;
   }
-}
-}
-void main() {
+  return num;
+  }
 }
