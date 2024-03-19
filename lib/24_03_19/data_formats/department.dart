b96eebf8bff3f15644446dca2ba9a7a97b0565d0
kimhyungmin <jeen14rla@gmail.com>
2024-03-19 14:45:02 +0900

    feat: Employee, department 클래스 작성

---
+++ lib/24_03_19/data_formats/department.dart
@@ -0,0 +1,8 @@
+import 'package:learn_dart_together/24_03_19/data_formats/employee.dart';
+
+class Department {
+  String name;
+  Employee leader;
+
+  Department(this.name, this.leader);
+}
---
+++ lib/24_03_19/data_formats/employee.dart
@@ -0,0 +1,6 @@
+class Employee {
+  String name;
+  int age;
+
+  Employee(this.name, this.age);
+}
