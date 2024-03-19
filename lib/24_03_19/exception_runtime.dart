b6eb1eed42bb461fe5fc146e1369c02152bd9538
byjun8932 <byjun8932@gmail.com>
2024-03-19 14:38:46 +0900

    feat: try-catch 구문을 이용해 예외를 catch 하고 num 값을 0으로 변경

--- lib/24_03_19/exception_runtime.dart
+++ lib/24_03_19/exception_runtime.dart
@@ -1,10 +1,16 @@
 void main() {
   final numString = '10.5';
+  int num;
 
-  int num = double.parse(numString);
-
+  try {
+    num = int.parse(numString); // catch 에서 num = 0 값을 이용해야 하니까 다시 바꿈 ;;
+  } catch (e) {
+    print("예외 발생: $e");
+    num = 0;
+  }
   print(num);
 }
+
 
 
 
