f43898917cf97ba18aca6e9cf781708734c8d3b3
icicle_4 <dwayne.han4@gmail.com>
2024-03-20 00:30:50 +0900

    feat: serialization 연습문제 파일의 내용이 직렬화한 데이터와 동일한지 확인하는 테스트코드 작성

--- test/240319/serialization_test.dart
+++ test/240319/serialization_test.dart
@@ -40,5 +40,24 @@ void main() {
       expect(result2, isTrue);
       expect(result3, isTrue);
     });
+    test('파일의 내용이 직렬화한 데이터와 동일한지 확인한다..', () {
+      Employee employee = Employee('홍길동', 41);
+      Department department = Department('총무부', employee);
+
+      final String targetPath = 'lib/240319/company.txt';
+      final File targetFile = File(targetPath);
+
+      // 정렬
+      JsonEncoder encoder = JsonEncoder.withIndent(' ');
+      String jsonStr = encoder.convert(department.toJson());
+      // 파일 쓰기
+      targetFile.writeAsStringSync(jsonStr);
+
+      // 파일 내용 읽어오기
+      String fileContent = targetFile.readAsStringSync();
+
+      // 원본과 파일 내용이 동일한지 확인
+      expect(fileContent, equals(jsonStr));
+    });
   });
 }
