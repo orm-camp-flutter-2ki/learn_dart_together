94ef7824e9052a8c864a0c6684bc1734d9550d85
kimhyungmin <jeen14rla@gmail.com>
2024-03-19 15:36:22 +0900

    feat: data_format 작성
    총무부 리더 ‘홍길동(41세)’의 인스턴스를 생성하고 직렬화하여 company.txt 파일에 쓰는 프로그램을 작성

---
+++ lib/24_03_19/data_formats/data_formats.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:learn_dart_together/24_03_19/data_formats/department.dart';
+import 'package:learn_dart_together/24_03_19/data_formats/employee.dart';
+
+void main() {
+  // 인스턴스 생성
+  Employee hong = Employee('홍길동', 41);
+  Department generalAffair = Department('총무부', hong);
+
+  // 직렬화
+  final serialized = hong.toJson();
+
+  // String으로 변경
+  String jsonToString = serialized.toString();
+
+  // company.txt
+  final companyFile = File('company.txt');
+
+  // company.txt에 String 작성
+  companyFile.writeAsStringSync(jsonToString);
+}
