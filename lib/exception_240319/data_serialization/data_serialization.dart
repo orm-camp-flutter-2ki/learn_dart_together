a8cc2fb30e5a7c2361e1b7138016be979be80a50
gyubit <gyubros@gmail.com>
2024-03-19 23:21:10 +0900

    EDIT: 최종 결과 완료. (향후 보강)

--- lib/exception_240319/data_serialization/data_serialization.dart
+++ lib/exception_240319/data_serialization/data_serialization.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 class Employee {
@@ -6,7 +7,7 @@ class Employee {
 
   Employee({required this.name, required this.age});
 
-  //직렬화
+  //toJson: Data(직원 정보)의 String type 직렬화
   Map<String, dynamic> toJson() => {
         'name': name,
         'age': age,
@@ -17,15 +18,29 @@ class Department {
   String name;
   Employee leader;
 
-  Department(this.name, this.leader);
+  Department({required this.name, required this.leader});
+
+  //직원 정보의 역직렬화
+  Map<String, dynamic> fromJson() => {
+        'name': name,
+        'leader': leader.toJson(),
+      };
 }
 
 void main() {
   // txt파일 생성
   final myFile = File('company.txt');
 
+  // instance > insert to Map > send to 'company.txt'
   Employee employee = Employee(name: '홍길동', age: 41);
+  // Employee employee2 = Employee(name: '김만덕', age: 36);
+  Map<String, dynamic> employeeMap = {};
+  employeeMap.addAll(employee.toJson());
+  // employeeMap.addAll(employee2.toJson());
+  myFile.writeAsStringSync('$employeeMap', mode: FileMode.append);
 
-  employee.toJson();
-  myFile.writeAsStringSync('${employee.toJson()}', mode: FileMode.append);
+  // instance > insert to Map > send to 'company.txt'
+  Department department = Department(name: '디자인팀', leader: employee);
+  Map<String, dynamic> departmentMap = department.fromJson();
+  myFile.writeAsStringSync('$departmentMap', mode: FileMode.append);
 }
