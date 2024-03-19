32476294e77c1a17e3a87691aea2db788ff8bbba
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-19 17:39:52 +0900

    feat: company 파일 수정
    - Department class 변수 수정
    - Employee class 변수 수정 :
    1) Department 클래스를 상속받도록 하였다.
    2) toString override

--- lib/0319/company.dart
+++ lib/0319/company.dart
@@ -1,21 +1,35 @@
-/// 사원 클래스
-class Employee {
-  String name;
-  int age;
-
-  Employee(this.name, this.age);
-}
-
 /// 부서 클래스
 class Department {
+  String departmentTeam;
+  String teamNumber;
+
+  Department({required this.departmentTeam, required this.teamNumber});
+}
+
+/// 사원 클래스
+class Employee extends Department {
   String name;
   int age;
 
-  Department(this.name, this.age);
+  Employee({
+    required super.departmentTeam,
+    required super.teamNumber,
+    required this.name,
+    required this.age,
+  });
+
+  @override
+  String toString() {
+    return '[$departmentTeam부 $teamNumber팀] $name ($age세)';
+  }
+}
+
+void main() {
+  // Department GeneralAffairs = Department('총무', 6);
 }
 
 /*
-*
+* dart의 직렬화/역직렬화는 class <-> Json(Map과 형태 같음)
 * 총무부 리더 '홍길동(41세)'의 인스턴스를 생성
 * 인스턴스를 직렬화 하여 company.txt 파일에 쓰는 프로그램 작성
 * */
