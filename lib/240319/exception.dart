f7e18093c49d98bd4a49e03517b2f98b1e57c31f
Yuna.Kim <yunaoasiskim@gmail.com>
2024-03-19 14:51:22 +0900

    feat: - exception 연습문제2 - try/catch 사용해서 예외 처리 - numString 값을 정수로 바꾸면 try 실행 - numString 값을 소수로 바꾸면 catch 실행

--- lib/240319/exception.dart
+++ lib/240319/exception.dart
@@ -1,5 +1,12 @@
-void main(){
-final numString = '10.5';
-int num = int.parse(numString);
-print(num);
+void main() {
+
+  try {
+    //에러가 없을 때 실행할 로직
+    final numString = '10.5';
+    int num = int.parse(numString);
+    print(num);
+  } catch (e) {
+    int num = 0;
+    print(num);
+  }
 }
