e6716e1fbffa1575c2465412ca2fa9a1511ffd99
jungeun272 <hanjungeun272@gmail.com>
2024-03-19 14:52:01 +0900

    feat : try catch 문으로 문자열 변환 실패시 예외처리

--- lib/240319/quiz_1.dart
+++ lib/240319/quiz_1.dart
@@ -1,5 +1,10 @@
-void main() {
-  final numString = '10.5';
-  int num = int.parse(numString);
-  print(num);
+void main () {
+  try {
+    final numString = '10.5';
+    int num = int.parse(numString);
+    print(num);
+  } catch (e) {
+    int num = 0;
+    print(num);
+  }
 }
