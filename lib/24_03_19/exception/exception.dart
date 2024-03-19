c6104e7efa556684a2cd945af856ddb222a70220
kimhyungmin <jeen14rla@gmail.com>
2024-03-19 14:19:02 +0900

    fix: try catch로 예외가 발생하면 0으로 처리

--- lib/24_03_19/exception/exception.dart
+++ lib/24_03_19/exception/exception.dart
@@ -1,7 +1,13 @@
-void main(){
+void main() {
   final numString = '10.5';
 
-  int num = int.parse(numString);
+  int num;
+
+  try {
+    num = int.parse(numString);
+  } catch (e) {
+    num = 0;
+  }
 
   print(num);
 }
