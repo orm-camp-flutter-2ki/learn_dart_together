71cd637594cdc1e579840f86146493512ef802fd
hee462 <a01080809887@gmail.com>
2024-03-20 17:13:24 +0900

    feat: comment  수정 사항

--- lib/240319/copy.dart
+++ lib/240319/copy.dart
@@ -2,9 +2,13 @@ import 'dart:io';
 
 void Copy(String source, String target) {
   try {
+    // 파일 경로 저장
     final sourceFile = File(source);
+    // 복사할 파일
     final targetFile = File(target);
+    // 복사할 파일 경로에 있는 파일 읽어서 내용값에 저장
     final contents = sourceFile.readAsStringSync();
+    // 복사할 파일에 파일 내용 적기
     targetFile.writeAsStringSync(contents);
     print('파일복사완료');
   } catch (e) {
--- lib/240319/employee.dart
+++ lib/240319/employee.dart
@@ -13,6 +13,7 @@ class Department {
 
   Department(this.name, this.leader);
 
-// Employee 불러와서 사용하기 때문에 , 추가적으로 .tojson()을 해서 구석구석 json 형태로 만들어줄것!
+// Employee 불러와서 사용하기 때문에 ,
+// 추가적으로 .tojson()을 해서 구석구석 json 형태로 만들어줄것!
   Map<String, dynamic> toJson() => {'name': name, 'leader': leader.toJson()};
 }
