8de672c8e65089ffd26dc41eeea38b017adbe449
Stranger Yang <ccamdol@gmail.com>
2024-03-20 16:26:17 +0900

    feat: 쿼리 연습문제
    2.거래자(trader)가 근무하는 모든 도시(city)를 중복없이 나열하시오

--- lib/240320/query.dart
+++ lib/240320/query.dart
@@ -5,6 +5,16 @@ class Trader {
   String city;
 
   Trader(this.name, this.city);
+
+  Trader.formJson(Map<String, dynamic> json)
+    : name = json['name'],
+      city = json['city'];
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'city': city,
+  };
+
 }
 
 class Transaction {
@@ -30,10 +40,19 @@ void main(){
 //오름차순
 //이름만 나열
 
-transactions
-    .where((transaction) => transaction.year == 2011) //where 이터러블, 리스트로 바꿔야 함
-    .toList()
-    .sorted((a,b) => a.value.compareTo(b.value)) //오름차순 정렬
-    .map((transaction) => transaction.trader.name) //트렌젝션 트레이더의 이름으로 다 바꾸겠음, 스트링만 남는다
-    .forEach(print);
+// transactions
+//     .where((transaction) => transaction.year == 2011) //where 이터러블, 리스트로 바꿔야 함
+//     .toList()
+//     .sorted((a,b) => a.value.compareTo(b.value)) //오름차순 정렬
+//     .map((transaction) => transaction.trader.name) //트렌젝션 트레이더의 이름으로 다 바꾸겠음, 스트링만 남는다
+//     .forEach(print);
+
+
+//2.거래자(trader)가 근무하는 모든 도시(city)를 중복없이 나열하시오
+  final cities = transactions
+      .map((transaction) => transaction.trader.city) // 모든 트랜잭션의 도시 목록을 가져온다.
+      .toSet(); // 중복된 도시를 제거하기 위해 Set으로 변환한다.
+
+  // 도시 이름을 오름차순으로 정렬하여 출력한다.
+  cities.toList()..sort()..forEach(print);
 }
