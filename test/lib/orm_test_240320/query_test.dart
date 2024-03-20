4719702912d2b0b399dccf3923be17bc972b13be
BAUu <yimej07@gmail.com>
2024-03-20 18:04:20 +0900

    feat: query 테스트 문서 작성
    문제 1

---
+++ test/lib/orm_test_240320/query_test.dart
@@ -0,0 +1,14 @@
+import 'package:learn_dart_together/orm_240320/query.dart';
+import 'package:collection/collection.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('query 문제 1', () {
+    transactions
+        .where((transactions) => transactions.year == 2011)
+        .toList()
+        .sorted((a, b) => a.value.compareTo(b.value))
+        .map((transaction) => transaction.trader.name)
+        .forEach(print);
+  });
+}
