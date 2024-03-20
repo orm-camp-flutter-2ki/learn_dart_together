5470d9266da449d1148ce4037995481f732a4d56
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-20 16:29:43 +0900

    test: task 05 06 07

--- test/0320/transaction_test.dart
+++ test/0320/transaction_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:learn_dart_together/0320/transaction.dart';
 import 'package:test/test.dart';
 
@@ -8,5 +10,24 @@ void main() {
     transactions.forEach((element) { set.add(element.trader.city); });
 
     expect(set.toString(), '{Cambridge, Milan}');
+  });
+
+  test('5. 밀라노에 거래자가 있는가?', () {
+    bool resultTrue = transactions.any((element) => element.trader.city == 'Milan');
+    bool resultFalse = transactions.any((element) => element.trader.city == 'Korea');
+
+    expect(resultTrue, true);
+    expect(resultFalse, false);
+  });
+
+  test('6. 케임브리지에 거주하는 거래자의 모든 트랙잭션 값을 출력한다.', () {
+    transactions
+        .where((element) => element.trader.city == 'Cambridge')
+        .forEach((element) => print(element.value));
+  });
+
+  test('7. 전체 트랜잭션 중 최댓값은 얼마인가?', () {
+    Transaction result = transactions.reduce((value, element) => value.value > element.value ? value: element);
+    expect(result.value, 1000);
   });
 }
