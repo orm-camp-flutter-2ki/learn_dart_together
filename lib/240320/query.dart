b2bd91d4c25c7921ca901c31ce55e8ad4d55c689
Stranger Yang <ccamdol@gmail.com>
2024-03-20 17:11:38 +0900

    feat: 쿼리 연습문제
    3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오

--- lib/240320/query.dart
+++ lib/240320/query.dart
@@ -1,4 +1,5 @@
 import 'package:collection/collection.dart';
+import 'dart:math';
 
 class Trader {
   String name;
@@ -6,14 +7,34 @@ class Trader {
 
   Trader(this.name, this.city);
 
-  Trader.formJson(Map<String, dynamic> json)
-    : name = json['name'],
-      city = json['city'];
 
-  Map<String, dynamic> toJson() => {
-    'name': name,
-    'city': city,
-  };
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Trader &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          city == other.city;
+
+  @override
+  int get hashCode => name.hashCode ^ city.hashCode;
+
+  @override
+  String toString() {
+    return 'Trader{name: $name, city: $city}';
+  }
+
+// Trader.fromJson(Map<String, dynamic> json)
+  //     : name = json['name'],
+  //       city = json['city'],
+  //       trader = Trader(json['name'], json['city']);
+  //
+  //
+  // Map<String, dynamic> toJson() =>
+  //     {
+  //       name: name,
+  //       city: city,
+  //     };
 
 }
 
@@ -23,6 +44,35 @@ class Transaction {
   int value;
 
   Transaction(this.trader, this.year, this.value);
+
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Transaction &&
+          runtimeType == other.runtimeType &&
+          trader == other.trader &&
+          year == other.year &&
+          value == other.value;
+
+  @override
+  int get hashCode => trader.hashCode ^ year.hashCode ^ value.hashCode;
+
+  @override
+  String toString() {
+    return 'Transaction{trader: $trader, year: $year, value: $value}';
+  }
+
+//   Transaction.fromJson(Map<String, dynamic> json)
+//     : trader = Trader.fromJson(json['trader']),
+//       year = json['year'],
+//       value = json['value'];
+//
+//   Map<String, dynamic> toJson() => {
+//     'trader': trader,
+//     'year': year,
+//     'value': value,
+// };
 }
 
 final transactions = [
@@ -35,7 +85,8 @@ final transactions = [
 ];
 
 void main(){
-  //1. 2011년에 일어난 모든 트랜잭션을 찾아 가격기준 오름차순으로 정리하여 이름을 나열하시오.
+
+//1. 2011년에 일어난 모든 트랜잭션을 찾아 가격기준 오름차순으로 정리하여 이름을 나열하시오.
 //가격기준,
 //오름차순
 //이름만 나열
@@ -49,10 +100,20 @@ void main(){
 
 
 //2.거래자(trader)가 근무하는 모든 도시(city)를 중복없이 나열하시오
-  final cities = transactions
-      .map((transaction) => transaction.trader.city) // 모든 트랜잭션의 도시 목록을 가져온다.
-      .toSet(); // 중복된 도시를 제거하기 위해 Set으로 변환한다.
+//   final cities = transactions
+//       .map((transaction) => transaction.trader.city) // 모든 트랜잭션의 도시 목록을 가져온다.
+//       .toSet(); // 중복된 도시를 제거하기 위해 Set으로 변환한다.
 
-  // 도시 이름을 오름차순으로 정렬하여 출력한다.
-  cities.toList()..sort()..forEach(print);
+// 도시 이름을 오름차순으로 정렬하여 출력한다.
+// cities.toList()..sort()..forEach(print);
+
+//3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
+ final cambridgeTraders = transactions
+     .where((transaction) => transaction.trader.city == "Cambridge")
+    // .map((transaction) => transaction.trader.city == "Cambridge") // 케임브리지에 거주하는 거래자만
+    .map((transaction) => transaction.trader)
+    .toList()
+    .sorted((a,b) => a.name.compareTo(b.name));
+
+cambridgeTraders.forEach((trader) => print(trader.name));
 }
