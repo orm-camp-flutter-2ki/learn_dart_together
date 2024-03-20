8aee70664f018dacd2a7f4537e6a209c50862825
JEON-Sungsu <anagma@naver.com>
2024-03-20 14:10:36 +0900

    refactor: 전날 과제 수정

--- lib/24_03_19/exception.dart
+++ lib/24_03_19/exception.dart
@@ -1,12 +1,12 @@
 void main() {
- final numString = '10.5';
- int num;
+  final numString = '10.5';
+  int num;
 
- try {
-   num = int.parse(numString);
- } catch(e) {
-   num = 0;
- }
+  try {
+    num = int.parse(numString);
+  } catch (e) {
+    num = 0;
+  }
 
- print(num);
+  print(num);
 }
