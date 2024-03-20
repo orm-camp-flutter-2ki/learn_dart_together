491e999e555a3b5c566a35f6197dd8935b014522
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-03-21 01:12:10 +0900

    feat : copy_file_test 파일 자동 추가 자동삭제 기능 추가

--- lib/test/240319/copy_file_test.dart
+++ lib/test/240319/copy_file_test.dart
@@ -4,6 +4,16 @@ import 'package:learn_dart_together/240319/copy_file.dart';
 import 'package:test/test.dart';
 
 void main() {
+  // 매 test 시작시 실행할 부분
+  setUp(() {
+    File('lib/240319/save.txt').writeAsStringSync('Hello World');
+  });
+  // 매 test 끝날 때 실행할 부분
+  tearDown(() {
+    File('lib/240319/save.txt').deleteSync();
+    File('lib/240319/savecopy.txt').deleteSync();
+    File('lib/240319/savecopy2.txt').deleteSync();
+  });
   test('Copy File Test', () {
     copy('lib/240319/save.txt', 'lib/240319/savecopy.txt');
     copy('lib/240319/savecopy.txt', 'lib/240319/savecopy2.txt');
