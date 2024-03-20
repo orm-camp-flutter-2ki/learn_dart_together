5d59c80ae5a254b4888006be9ac2dacd7a91e328
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-20 20:54:58 +0900

    feat: convert trader class to data class

--- lib/0320/trader.dart
+++ lib/0320/trader.dart
@@ -1,7 +1,28 @@
 class Trader {
-  
-  String name;
-  String city;
+
+  final String name;
+  final String city;
 
   Trader(this.name, this.city);
+
+  Trader.fromJson(Map<String, dynamic> json) : name = json['name'], city = json['leader'];
+
+  Map<String, dynamic> toJson() => {'name': name, 'city': city};
+
+  Trader copyWith(String? name, String? city) {
+    return Trader(name ?? this.name, city ?? this.city);
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Trader && runtimeType == other.runtimeType && name == other.name && city == other.city;
+
+  @override
+  int get hashCode => name.hashCode ^ city.hashCode;
+
+  @override
+  String toString() {
+    return 'Trader{name: $name, city: $city}';
+  }
 }
