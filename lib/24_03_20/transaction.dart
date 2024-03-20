47245dd2ed9217df7f63e3364332802ead1707c9
SeongJongHo <whdgh9595@gmail.com>
2024-03-20 20:17:42 +0900

    [ADD] Transaction 클래스 작성

---
+++ lib/24_03_20/transaction.dart
@@ -0,0 +1,9 @@
+import 'package:learn_dart_together/24_03_20/trader.dart';
+
+class Transaction {
+  Trader trader;
+  int year;
+  int value;
+
+  Transaction(this.trader, this.year, this.value);
+}
