4ff3922fe817713fe674586f67a2905365ba97f3
Stranger Yang <ccamdol@gmail.com>
2024-03-20 13:53:59 +0900

    수정

--- test/240319/file_copy_test.dart
+++ test/240319/file_copy_test.dart
@@ -6,13 +6,15 @@ import 'package:test/test.dart';
 
 void main() {
   //매 테스트 시작시에 실행
-  setUp(() => File('save.txt').writeAsStringSync('Hello World'));
+  setUp(() {
+    File('save.txt').writeAsStringSync('Hello World');
+  });
 
   //매 테스트 종료시 실행
-  tearDown(() =>
-      File('save.txt').deleteSync(); File('save2.txt').deleteSync();
-
-
+  tearDown(() {
+    File('save.txt').deleteSync();
+    File('save2.txt').deleteSync();
+  });
   // test('copy 테스트', () => null)
   test('복제 테스트', () {
     copy('save.txt', 'save2.txt');
