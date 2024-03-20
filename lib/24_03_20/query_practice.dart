073cc3ca73e82d84ca5df73faaed70106984aff3
YunZi.Song <yoonji5809@gmail.com>
2024-03-21 05:10:06 +0900

    Refactor: 쿼리 연습문제 리팩토링

--- lib/24_03_20/query_practice.dart
+++ lib/24_03_20/query_practice.dart
@@ -17,71 +17,66 @@ class Transaction {
   Transaction(this.trader, this.year, this.value);
 }
 
-final transactions = [
-  Transaction(Trader("Brian", "Cambridge"), 2011, 300),
-  Transaction(Trader("Raoul", "Cambridge"), 2012, 1000),
-  Transaction(Trader("Raoul", "Cambridge"), 2011, 400),
-  Transaction(Trader("Mario", "Milan"), 2012, 710),
-  Transaction(Trader("Mario", "Milan"), 2012, 700),
-  Transaction(Trader("Alan", "Cambridge"), 2012, 950),
-];
+class Query {
+  final transactions = [
+    Transaction(Trader("Brian", "Cambridge"), 2011, 300),
+    Transaction(Trader("Raoul", "Cambridge"), 2012, 1000),
+    Transaction(Trader("Raoul", "Cambridge"), 2011, 400),
+    Transaction(Trader("Mario", "Milan"), 2012, 710),
+    Transaction(Trader("Mario", "Milan"), 2012, 700),
+    Transaction(Trader("Alan", "Cambridge"), 2012, 950),
+  ];
 
-void main() {
-  /// 1. 2011년에 일어난 모든 트랜잭션을 찾아 가격 기준 오름차순으로 정리하여 이름을 나열하시오
-  print('1번 문제 결과 -1');
-
-  for (var e in (transactions.where((e) => e.year == 2011).toList()
-    ..sort((a, b) => a.value.compareTo(b.value)))) {
-    print(e.trader.name);
+  /// 1. 2011년에 일어난 모든 트랜잭션을 찾아 가격 기준 오름차순으로 정리 후 이름을 반환
+  List<String> question1() {
+    return transactions
+        .where((element) => element.year == 2011)
+        .map((e) => e.trader.name)
+        .sorted((a, b) => a.compareTo(b))
+        .toList();
   }
-  print('\n1번 문제 결과 -2');
-  transactions
-      .where((element) => element.year == 2011)
-      .map((e) => e.trader.name)
-      .sorted((a, b) => a.compareTo(b))
-      .forEach(print);
-  // 원본을 건드리지 않고 결과를 리턴 해준다.
 
-  // 디버깅할 때는 이런식으로 쪼개서 해보는 것도 나쁘지 않음.
-  // var stage1 = transactions.where((element) => e.year ==2011);
-  // var stage2 = stage1.toList();
-  // var stage3 = stage2.sorted((a, b) => a.value.compareTo(b.value));
+  /// 2. 거래자가 근무하는 모든 도시를 중복 없이 반환
+  Set<String> question2() {
+    return transactions.map((e) => e.trader.city).toSet();
+  }
 
-  /// 2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오 => set
-  print('\n2번 문제 결과');
-  transactions.map((e) => e.trader.city).toSet().forEach(print);
+  /// 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬 후 반환
+  Set<String> question3() {
+    return transactions
+        .where((e) => e.trader.city == 'Cambridge')
+        .map((e) => e.trader.name)
+        .toSet();
+  }
 
-  /// 3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
-  print('\n3번 문제 결과');
-  transactions
-      .where((e) => e.trader.city == 'Cambridge')
-      .map((e) => e.trader.name)
-      .toSet()
-      .forEach(print);
+  /// 모든 거래자의 이름을 알파벳순으로 정렬 후 반환
+  List<String> question4() {
+    return transactions
+        .sorted((a, b) => a.trader.name.compareTo(b.trader.name))
+        .map((e) => e.trader.name)
+        .toList();
+  }
 
-  /// 4. 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오
-  print('\n4번 문제 결과');
-  transactions
-      .sorted((a, b) => a.trader.name.compareTo(b.trader.name))
-      .forEach((e) => print(e.trader.name));
+  /// 5. 밀라노에 거래자가 있는지 여부
+  bool question5() {
+    return transactions.any((element) => element.trader.city == 'Milan');
+  }
 
-  /// 5. 밀라노에 거래자가 있는가?
-  print('\n5번 문제 결과');
-  print(transactions.any((element) => element.trader.city == 'Milan'));
+  /// 케임브리지에 거주하는 거래자의 모든 트랙잭션값을 반환
+  List<int> question6() {
+    return transactions
+        .where((element) => element.trader.city == 'Cambridge')
+        .map((e) => e.value)
+        .toList();
+  }
 
-  /// 6. 케임브리지에 거주하는 거래자의 모든 트랙잭션값을 출력하시오
-  print('\n6번 문제 결과');
-  transactions
-      .where((element) => element.trader.city == 'Cambridge')
-      .forEach((element) {
-    print(element.value);
-  });
+  /// 7. 전체 트랜잭션 중 최대값
+  int question7() {
+    return transactions.map((e) => e.value).reduce(max);
+  }
 
-  /// 7. 전체 트랜잭션 중 최대값은 얼마인가?
-  print('\n7번 문제 결과');
-  print(transactions.map((e) => e.value).reduce((v, e) => max(v, e)));
-
-  /// 8. 전체 트랜잭션 중 최소값은 얼마인가?
-  print('\n8번 문제 결과');
-  print(transactions.map((e) => e.value).reduce((v, e) => min(v, e)));
+  /// 7. 전체 트랜잭션 중 최소값
+  int question8() {
+    return transactions.map((e) => e.value).reduce(min);
+  }
 }
