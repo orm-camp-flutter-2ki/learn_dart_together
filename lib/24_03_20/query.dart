98328ddab41b4d46fc15947083496e16abfebc60
SeongJongHo <whdgh9595@gmail.com>
2024-03-20 20:26:10 +0900

    [ADD] Query 클래스 작성

---
+++ lib/24_03_20/query.dart
@@ -0,0 +1,21 @@
+import 'package:learn_dart_together/24_03_20/trader.dart';
+import 'package:learn_dart_together/24_03_20/transaction.dart';
+
+class Query {
+  final _transactions = [
+    Transaction(Trader("Brian", "Cambridge"), 2011, 300),
+    Transaction(Trader("Raoul", "Cambridge"), 2012, 1000),
+    Transaction(Trader("Raoul", "Cambridge"), 2011, 400),
+    Transaction(Trader("Mario", "Milan"), 2012, 710),
+    Transaction(Trader("Mario", "Milan"), 2012, 700),
+    Transaction(Trader("Alan", "Cambridge"), 2012, 950),
+  ];
+
+  List<Transaction> copyList(){
+    return [..._transactions];
+  }
+
+  List<Transaction> firstTask() {
+    return copyList().where((transaction) => transaction.year == 2011).toList();
+  }
+}
