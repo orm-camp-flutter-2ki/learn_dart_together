50df68a32b80438afad90d6535ea199ca099ab77
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-03-20 14:56:04 +0900

    feat: Json List 를 역직렬화 하는 방법

---
+++ lib/240320/03_json_list.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+void main() {
+  String jsonString = '''
+  {
+    "name": "홍길동",
+    "hobby": ["축구", "농구"]
+  }
+  ''';
+
+  Map<String, dynamic> json = jsonDecode(jsonString);
+  List hobbies = json['hobby'];
+
+  List<String> results = hobbies.map((e) => e as String).toList();
+
+  print(results);
+}
