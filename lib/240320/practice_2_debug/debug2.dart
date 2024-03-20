c52cfe5eac96dd4059237376cbb7e97f14459fd3
nala0897@naver.com <nala0897@naver.com>
2024-03-21 08:10:56 +0900

    fix: 디버깅 문제 제거

--- lib/240320/practice_2_debug/debug2.dart
+++
@@ -1,7 +0,0 @@
-import 'yukymController.dart';
-
-void main() {
-  YukymController yukymController = YukymController();
-  print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시
-  print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시
-}
