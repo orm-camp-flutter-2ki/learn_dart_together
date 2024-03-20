97e937428b7a5a7c18940fc2da0c70ade74d74fb
kimhyungmin <jeen14rla@gmail.com>
2024-03-20 17:00:01 +0900

    remove: query - main에 print 숫자 삭제

--- lib/24_03_20/query.dart
+++ lib/24_03_20/query.dart
@@ -34,13 +34,13 @@ void main() {
       .sorted((a, b) => a.value.compareTo(b.value))
       .map((transaction) => transaction.trader.name)
       .forEach(print);
-  print('-1-');
+
   // 2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오
   transactions
       .map((transaction) => transaction.trader.city)
       .toSet()
       .forEach(print);
-  print('-2-');
+
   // 3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
   transactions
       .where((transaction) => transaction.trader.city == 'Cambridge')
@@ -48,24 +48,23 @@ void main() {
       .sorted((a, b) => a.trader.name.compareTo(b.trader.name))
       .map((transaction) => transaction.trader.name)
       .forEach(print);
-  print('-3-');
+
   // 4. 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오
   transactions
       .map((transaction) => transaction.trader.name)
       .toList()
       .sorted((a, b) => a.compareTo(b))
       .forEach(print);
-  print('-4-');
+
   // 5. 밀라노에 거래자가 있는가?
   print(transactions.any((transaction) => transaction.trader.city == 'Milan'));
 
-  print('-5-');
   // 6. 케임브리지에 거주하는 거래자의 모든 트랙잭션값을 출력하시오
   transactions
       .where((transaction) => transaction.trader.city == 'Cambridge')
       .toList()
       .forEach((e) => print('${e.value}'));
-  print('-6-');
+
   // 7. 전체 트랜잭션 중 최대값은 얼마인가?
   print(transactions
       .map((transaction) => transaction.value)
