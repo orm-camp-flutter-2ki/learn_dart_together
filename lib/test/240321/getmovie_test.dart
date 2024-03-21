3f1c021cad4cba3c56efca8d80cf463c23bc0a33
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-03-21 17:38:01 +0900

    test : getmovie 타이틀, 출시년도 테스트 추가

--- lib/test/240321/getmovie_test.dart
+++ lib/test/240321/getmovie_test.dart
@@ -3,15 +3,39 @@ import 'package:test/test.dart';
 import '../../240321/getmovie.dart';
 
 void main() {
-  test('movie', () async {
-    //given
-    String sol2 = 'George Lucas';
-    //when
+  group('movie', () {
+    test('제작자 출력', () async {
+      //given
+      String sol = 'George Lucas';
+      //when
 
-    Movie movie = await getMovieInFo();
-    String data = movie.director;
+      Movie movie = await getMovieInFo();
+      String data = movie.director;
 
-    //then
-    expect(data, sol2);
+      //then
+      expect(data, sol);
+    });
+    test('타이틀 출력', () async {
+      //given
+      String sol = 'Star Ward';
+      //when
+
+      Movie movie = await getMovieInFo();
+      String data = movie.title;
+
+      //then
+      expect(data, sol);
+    });
+    test('출시년도 출력', () async {
+      //given
+      int sol = 1977;
+      //when
+
+      Movie movie = await getMovieInFo();
+      int data = movie.year;
+
+      //then
+      expect(data, sol);
+    });
   });
 }
--- lib/test/240321/rewrite_test.dart
+++ lib/test/240321/rewrite_test.dart
@@ -10,7 +10,7 @@ void main() {
   });
   test('한석봉 김석봉 수정', () async {
     //given
-    String sol1 = '1, 홍길동, 30\r\n2, 김석봉, 20';
+    String sol = '1, 홍길동, 30\r\n2, 김석봉, 20';
 
     //when
     reWrite('lib/240321/sample.csv', 'lib/240321/sample_copy.csv');
@@ -18,6 +18,17 @@ void main() {
     String data = await rewrite.readAsString();
 
     //then
-    expect(data, sol1);
+    expect(data, sol);
   });
+  // test('파일 없음', () async {
+  //   //given
+
+  //   //when
+  //   reWrite('lib/2403213/1sample.csv', 'lib/240321/sample_copy.csv');
+  //   File rewrite = File('lib/240321/sample_copy.csv');
+  //   String data = await rewrite.readAsString();
+
+  //   //then
+  //   expect(data, returnsNormally);
+  // });
 }
