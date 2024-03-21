9ea6dc63424284d6f506dc93c5eda8f5d67455a7
Stranger Yang <ccamdol@gmail.com>
2024-03-21 15:44:21 +0900

    feat: 파일처리 연습문제2
    다음과 같은 영화 정보 Json 을 반환하는 함수가 있다. (async - await 사용할 것)

--- lib/240321/director.dart
+++ lib/240321/director.dart
@@ -1,9 +1,37 @@
-Future<Movie> getMovieInfo() async {
- var delay = Future.delayed(Duration(seconds: 5));
+class Movie {
+  final String title;
+  final String director;
+  final int year;
 
-final String jsonString = '''{
- "title": "Star Ward",
- "director":
-}
- 
+  Movie(this.title, this.director, this.year);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Movie &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          director == other.director &&
+          year == other.year;
+
+  @override
+  int get hashCode => title.hashCode ^ director.hashCode ^ year.hashCode;
+
+  @override
+  String toString() {
+    return 'Movie{title: $title, director: $director, year: $year}';
+  }
+
+  Movie copyWith({
+    String? title,
+    String? director,
+    int? year,
+}) {
+    return Movie(
+      title ?? this.title,
+      director?? this.director,
+      year ?? this.year,
+    );
+  }
+
 }
