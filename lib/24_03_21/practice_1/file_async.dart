ccbcd7452340a083b0b0e94d75e87e3ec4df9ff3
kimhyungmin <jeen14rla@gmail.com>
2024-03-21 17:27:52 +0900

    add: file_async try catch 추가

--- lib/24_03_21/practice_1/file_async.dart
+++ lib/24_03_21/practice_1/file_async.dart
@@ -1,23 +1,23 @@
 import 'dart:io';
 
 void copyModify(String source) async {
-  final originDetail = await File(source).readAsString();
-  final copyFile = File('lib/24_03_21/practice_1/sample_copy.csv');
+  try {
+    final originDetail = await File(source).readAsString();
+    final copyFile = File('lib/24_03_21/practice_1/sample_copy.csv');
 
-  final bool isHan = originDetail
-      .replaceAll(" ", "") // 공백제거
-      .split(RegExp(r'[,\r\n]')) // 잡문자 제거
-      .any((element) => element == '한석봉');
+    final bool isHan = originDetail
+        .replaceAll(" ", "") // 공백제거
+        .split(RegExp(r'[,\r\n]')) // 잡문자 제거
+        .any((element) => element == '한석봉');
 
-  if (isHan) {
-    await copyFile.writeAsString(originDetail.replaceAll('한석봉', '김석봉'));
+    if (isHan) {
+      await copyFile.writeAsString(originDetail.replaceAll('한석봉', '김석봉'));
+    }
+  } catch (e) {
+    print('error');
   }
 }
 
 void main() {
-  try {
-    copyModify('lib/24_03_21/practice_1/sample.csv');
-  } catch (e) {
-    print(e);
-  }
+  copyModify('lib/24_03_21/practice_1/sample.csv');
 }
