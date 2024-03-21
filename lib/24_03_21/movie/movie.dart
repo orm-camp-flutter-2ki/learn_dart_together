e04039601c6c4bba98caadd025ccdebb1b860ee2
JEON-Sungsu <anagma@naver.com>
2024-03-21 14:26:14 +0900

    feat: movie 클래스 작성

---
+++ lib/24_03_21/movie/movie.dart
@@ -0,0 +1,39 @@
+class Movie {
+  final String title;
+  final String director;
+  final int year;
+
+  Movie({required this.title, required this.director, required this.year});
+
+  Movie.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        director = json['director'],
+        year = json['year'];
+
+  Map<String, dynamic> toJson() =>
+      {'title': title, 'director': director, 'year': year};
+
+  Movie copyWith(String? title, String? director, int? year) {
+    return Movie(
+        title: title ?? this.title,
+        director: director ?? this.director,
+        year: year ?? this.year);
+  }
+
+  @override
+  String toString() {
+    return 'Movie{title: $title, director: $director, year: $year}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Movie &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          director == other.director &&
+          year == other.year;
+
+  @override
+  int get hashCode => title.hashCode ^ director.hashCode ^ year.hashCode;
+}
