da646a78c6c3eaf2249928460860a52821eee654
Stranger Yang <ccamdol@gmail.com>
2024-03-21 16:22:02 +0900

    feat: 파일처리 연습문제3
    -Future.timeout(시간) 함수를 사용하면 Future가 특정 시간안에 종료되지 않았을 때 타임아웃 처리를 할 수 있다.
     다음 Future 함수를 실행하고 5초 이내에 완료되지 않으면 “timeout”을 출력하시오.

---
+++ lib/240321/timeout.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+Future<String> timeoutFuture() async {
+  await Future.delayed(Duration(seconds: 6));
+  return 'ok';
+}
+
+Future<void> main() async {
+  try {
+    final result = await Future.timeout(
+      Duration(seconds: 5),
+      timeoutFuture(),
+    );
+    print(result);
+  } on TimeoutException catch(e) {
+    print('timeout : $e');
+  }
+}
