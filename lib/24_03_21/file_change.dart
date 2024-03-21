7b05ab6ab0af1764ea4377df3e41bdfa2c5003e6
sseung <sseutta@naver.com>
2024-03-21 23:14:31 +0900

    Feat:  파일 바꾸기 테스트 작성

---
+++ lib/24_03_21/file_change.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'dart:core';
+
+void main() async {
+  final myFile = File('sample.csv');
+  final copyedFile = File('sample._copy.csv');
+  myFile.writeAsStringSync('1, 홍길동, 30\n2, 한석봉, 20');
+
+  String context = await myFile.readAsString(); // Future 앞에 await 써줘야한다.
+
+  String changedContext =
+      context.replaceAll('한석봉', '김석봉');
+  copyedFile.writeAsString(changedContext);
+
+}
