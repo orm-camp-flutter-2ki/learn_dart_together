2f157c3cd59362be1fbd96acc9471d3b4f9ddfe7
SeongJongHo <whdgh9595@gmail.com>
2024-03-21 15:28:26 +0900

    [MODIFY] AsyncPractice 클래스 주석, try-catch문 추가

--- lib/24_03_21/async_practice.dart
+++ lib/24_03_21/async_practice.dart
@@ -1,13 +1,20 @@
+import 'dart:io';
+
 import 'package:learn_dart_together/24_03_19/-2/file_service.dart';
 
 class AsyncPractice {
   final FileService _fileService = FileService();
 
+  // 과제1. 파일처리에 대한 비동기 처리
   Future<void> fileHandler(String sourcePath, String targetPath) async {
-    final sourceFile = await _fileService.asyncReadFile(sourcePath);
-    final result = sourceFile.split('\n').map(_replaceName).join('\n');
+    try {
+      final sourceFile = await _fileService.asyncReadFile(sourcePath);
+      final result = sourceFile.split('\n').map(_replaceName).join('\n');
 
-    await _fileService.asyncWriteToFile(targetPath, result);
+      await _fileService.asyncWriteToFile(targetPath, result);
+    } on PathNotFoundException catch (e) {
+      print('파일 경로를 찾을 수 없습니다. ${e.path}');
+    }
   }
 
   String _replaceName(String el) {
