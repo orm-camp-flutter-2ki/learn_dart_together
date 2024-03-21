ab2ab47e73c83e7841ca2d4bae43e0886750a7fa
Yejin.Shin <mingmi_27@naver.com>
2024-03-21 18:17:43 +0900

    feat: copy 함수 await 추가

--- lib/240321_homework/fileHandling.dart
+++ lib/240321_homework/fileHandling.dart
@@ -9,8 +9,11 @@ void main() {
 
 Future<void> copy(File file, File copyFile) async {
   try {
-    copyFile.writeAsString(await file.readAsString());
-    //await copyFile.writeAsString(await file.readAsString()); 앞에도 붙여야 하는지??
+    await copyFile.writeAsString(await file.readAsString());
+    //await이 맨앞에도 붙어야 하는 이유!
+    //1.writeAsString & readAsString future타입이기 때문에
+    //2.지금은 리턴 값이 void 라서 안터지지만, 리턴값이 있다면 터질것이다!
+    //3.Why? 끝나는 시점을 모르니 main함수가 끝나버리면 강제로 종료되기때문
   } catch (e) {
     print('파일이 없습니다.');
   }
