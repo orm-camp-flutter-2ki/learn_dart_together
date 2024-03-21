e1e0092fe41ae51e021ef0289b8fcb91c493ba1f
Yejin.Shin <mingmi_27@naver.com>
2024-03-21 16:59:01 +0900

    feat: main 변수 생성

--- lib/240321_homework/getMovieInfo.dart
+++ lib/240321_homework/getMovieInfo.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
 
 void main() async {
-  print((await getMovieInfo()).director);
+  Movie movieInfo = await getMovieInfo();
+  String director = movieInfo.director;
+  print(director);
 }
 
 Future<Movie> getMovieInfo() async {
   //TODO: 2초동안 기다리는 코드 작성
-  //Future.delayed(const Duration(seconds: 2));
-  Movie movie;
+  Future.delayed(const Duration(seconds: 2));
+
   //서버에서 들어오는 데이터라고 상상
   final String jsonstring = '''{
   "title": "Star Ward",
@@ -15,13 +17,12 @@ Future<Movie> getMovieInfo() async {
   "year": 1977
   }''';
 
-  Map<String, dynamic> dataMap =  jsonDecode(jsonstring);
-
+  Map<String, dynamic> dataMap = jsonDecode(jsonstring);
+  Movie movie = Movie(dataMap['title'], dataMap['director'], dataMap['year']);
   //to jason
 //TODO: Movie 데이터 클래스를 리턴하도록 작성
-  return movie = Movie(dataMap['title'], dataMap['director'], dataMap['year']);
+  return movie;
 }
-
 class Movie {
   String title;
   String director;
