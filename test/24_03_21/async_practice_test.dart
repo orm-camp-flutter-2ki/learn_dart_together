c289146519b21982e5faac0cee2164f8ae113bf8
SeongJongHo <whdgh9595@gmail.com>
2024-03-21 16:26:26 +0900

    [ADD] AsyncPractive 클래스 getMovieInfo 테스트코드 작성

--- test/24_03_21/async_practice_test.dart
+++ test/24_03_21/async_practice_test.dart
@@ -1,5 +1,6 @@
 import 'package:learn_dart_together/24_03_19/-2/file_service.dart';
 import 'package:learn_dart_together/24_03_21/async_practice.dart';
+import 'package:learn_dart_together/24_03_21/movie.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -24,6 +25,24 @@ void main() {
 
         // Then
         expect(await fileService.asyncReadFile(targetPath), expected);
+      });
+    });
+
+    group('getMovieInfo 메소드는', () {
+      test('2초 딜레이 후 json을 영화객체로 역직렬화 후 반환한다.', () async {
+        // Given
+        String expected = '성종호';
+        String json = '''{
+          "title": "Star ward",
+          "director": "$expected",
+          "year": 1977
+        }''';
+
+        // When
+        Movie result = await asyncPractice.getMovieInfo(json);
+
+        // Then
+        expect(result.director, expected);
       });
     });
   });
