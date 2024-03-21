3f42d88eaab584b08d3b0692b61106408dbdaf1c
Yejin.Shin <mingmi_27@naver.com>
2024-03-21 18:14:23 +0900

    feat: try catch, timeout 프린트

--- lib/240321_homework/timeoutFuture.dart
+++ lib/240321_homework/timeoutFuture.dart
@@ -1,7 +1,12 @@
-void main(){
-  timeoutFuture();
+void main() async {
+  try{
+    await timeoutFuture().timeout(Duration(seconds: 5));
+  }catch(e){
+    print('timeout');
+  }
 }
+
 Future<String> timeoutFuture() async {
-  await Future.delayed(Duration(seconds: 6)).timeout(Duration(seconds: 5));
+  await Future.delayed(Duration(seconds: 6));
   return 'ok';
 }
