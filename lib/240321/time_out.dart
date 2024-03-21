e180c743aaeaf82ffb5dacbe5aa3c7194d7ca213
icicle_4 <dwayne.han4@gmail.com>
2024-03-21 18:10:27 +0900

    feat: 비동기프로그래밍 연습문제3 5초 이내 완료되지 않으면 timeout출력

--- lib/240321/time_out.dart
+++ lib/240321/time_out.dart
@@ -6,3 +6,9 @@ class TimeOut {
     return 'ok';
   }
 }
+
+void main() async {
+  TimeOut timeOut = TimeOut();
+  // 5초 경과 후 timeout 오류 발생하도록 수정
+  await timeOut.timeoutFuture().timeout(Duration(seconds: 5));
+}
