2e3b73f1e5385f1e9b97ce21c444e5ff9b7bbfb8
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-22 08:18:16 +0900

    fix: add try catch block to copyFile method

--- lib/0321/task01.dart
+++ lib/0321/task01.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 
 void main() async {
-  String fileName = '/Users/leeseowoo/git/learn_dart_together/lib/0321/sample.csv';
+  String fileName = 'lib/0321/sample.csv';
   List<String> readFileData =  await readFile(fileName);
 
   String targetData = '한석봉';
   String newData = '김석봉';
   List<String> modifiedData = await modifyData(readFileData, targetData, newData);
 
-  String newFileName = '/Users/leeseowoo/git/learn_dart_together/lib/0321/sample_copy.csv';
+  String newFileName = 'lib/0321/sample_copy.csv';
   copyFile(newFileName, modifiedData);
 }
 
@@ -36,12 +36,22 @@ Future<List<String>> modifyData(List<String> fileData, String targetData, String
 
 void copyFile(String fileName, List<String> fileData) async {
   File file = File(fileName);
-  IOSink sink = file.openWrite(mode: FileMode.writeOnly);
 
-  for (String line in fileData) {
-    sink.writeln(line);
+  try {
+    IOSink sink = file.openWrite(mode: FileMode.writeOnly);
+
+    for (String line in fileData) {
+      sink.writeln(line);
+    }
+
+    await sink.flush();
+    await sink.close();
+
+  } on PathNotFoundException {
+    print('파일을 찾을 수 없습니다.');
+
+  } catch(e, s) {
+    print(e);
+    print(s);
   }
-
-  await sink.flush();
-  await sink.close();
 }
