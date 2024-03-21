7de0fe9beed4f9010c87ad9a687a210c2a09d152
Yejin.Shin <mingmi_27@naver.com>
2024-03-21 18:14:51 +0900

    feat: Movie 데이터클래스화

--- lib/240321_homework/getMovieInfo.dart
+++ lib/240321_homework/getMovieInfo.dart
@@ -7,7 +7,8 @@ void main() async {
   String director = movieInfo.director;
   print('director:$director');
   stopwatch.stop();
-  print('getMovieInfo 함수는 ${stopwatch.elapsed.toString().substring(5)}초가 걸렸습니다.');
+  print(
+      'getMovieInfo 함수는 ${stopwatch.elapsed.toString().substring(5)}초가 걸렸습니다.');
 }
 
 Future<Movie> getMovieInfo() async {
@@ -27,6 +28,7 @@ Future<Movie> getMovieInfo() async {
 //TODO: Movie 데이터 클래스를 리턴하도록 작성
   return movie;
 }
+
 class Movie {
   String title;
   String director;
@@ -34,8 +36,43 @@ class Movie {
 
   Movie(this.title, this.director, this.year);
 
-  // Movie.fromJson(Map<String, dynamic> json)
-  //     : title = json['title'],
-  //       director = json['director'],
-  //       year = json['year'];
+  @override
+  String toString() {
+    return 'Movie{title: $title, director: $director, year: $year}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Movie &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          director == other.director &&
+          year == other.year;
+
+  @override
+  int get hashCode => title.hashCode ^ director.hashCode ^ year.hashCode;
+
+  Movie copyWith({
+    String? title,
+    String? director,
+    int? year,
+  }) {
+    return Movie(
+      title ?? this.title,
+      director ?? this.director,
+      year ?? this.year,
+    );
+  }
+
+  Movie.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        director = json['director'],
+        year = json['year'];
+
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'director': director,
+        'year': year,
+      };
 }
