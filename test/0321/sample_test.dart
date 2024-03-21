6c5bc3afdbd1df4e685db8094eecc94c85e6ac63
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-21 16:54:28 +0900

    fear: sample.csv 와 테스트 파일 수정
    - 테스트 하나가 어렵다... 질문하기...

--- test/0321/sample_test.dart
+++ test/0321/sample_test.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -6,37 +7,68 @@ import 'package:csv/csv.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
-void main() async {
-  final eq = DeepCollectionEquality().equals;
+final String todayDirectory =
+    '/Users/yong/Desktop/learn_dart_together/lib/0320';
 
-  final sample =
-      File('/Users/yong/Desktop/learn_dart_together/lib/0321/sample.csv')
-          .openRead(); // 원본 경로
-  final sampleCopy =
-      File('/Users/yong/Desktop/learn_dart_together/lib/0321/sample_copy.csv')
-          .openRead(); // 카피 경로
+Future<void> copyReplaceFile(String sourcePath, String targetPath) async {
+  // 소스 파일에서 문자열을 읽고 -> 바꾼 것을 저장
+  try {
+    String originFile = await File(sourcePath).readAsString();
+    String replaceFile = originFile.replaceAll('한석봉', '김석봉');
 
-  // csv toList() : 잘된건지는 모르겠다...
-  // https://pub.dev/packages/csv
-  final sampleString = await sample
-      .transform(utf8.decoder)
-      .transform(CsvToListConverter())
-      .toList();
+    if (!(originFile.contains('한석봉'))) {
+      throw Exception('[error]\n 한석봉을 찾을 수 없습니다.');
+    }
 
-  final sampleCopyString = await sampleCopy
-      .transform(utf8.decoder)
-      .transform(CsvToListConverter())
-      .toList();
-
-  // file copy 메서드
-  void copyFile(String sourcePath, String targetPath) {
-    File(sourcePath).copySync(targetPath);
+    // 복사할 파일에 쓰기
+    await File(targetPath).writeAsString(replaceFile);
+  } catch (e) {
+    print(e);
   }
+}
 
-  test('csv file copy test', () {
-    copyFile('/Users/yong/Desktop/learn_dart_together/lib/0321/sample.csv',
+Future<void> main() async {
+  final eq = DeepCollectionEquality().equals;
+  final sample = File('$todayDirectory/sample.csv').openRead(); // 원본 경로
+  final sampleCopy =
+      File('$todayDirectory/sample_copy.csv').openRead(); // 카피 경로
+
+  test('csv file copy test', () async {
+    copyReplaceFile(
+        '/Users/yong/Desktop/learn_dart_together/lib/0321/sample.csv',
         '/Users/yong/Desktop/learn_dart_together/lib/0321/sample_copy.csv');
 
-    expect(eq(sampleString, sampleCopyString), true);
+    expect(eq(sample, sampleCopy), false);
   });
+
+  test('한석봉이 없다!', () async {
+    String originFile = await File(
+            '/Users/yong/Desktop/learn_dart_together/lib/0321/sample.csv')
+        .readAsString();
+    String replaceFile = originFile.replaceAll('한석봉', '한라봉');
+
+    copyReplaceFile(
+        '/Users/yong/Desktop/learn_dart_together/lib/0321/sample.csv',
+        '/Users/yong/Desktop/learn_dart_together/lib/0321/sample_copy.csv');
+
+    expect(replaceFile.contains('한석봉'), false);
+    expect(replaceFile.contains('한라봉'), true);
+    // expect(replaceFile.contains('한석봉'), true); // error
+    // expect(replaceFile.contains('한라봉'), false);
+  });
+
+  // test('description', () async {
+  //   // 내가 무엇을 작성한건지는 잘 모르겠다...
+  //   //   https: //pub.dev/packages/csv
+  //
+  //   final sampleString = await sample
+  //       .transform(utf8.decoder)
+  //       .transform(CsvToListConverter())
+  //       .toList();
+  //
+  //   final sampleCopyString = await sampleCopy
+  //       .transform(utf8.decoder)
+  //       .transform(CsvToListConverter())
+  //       .toList();
+  // });
 }
