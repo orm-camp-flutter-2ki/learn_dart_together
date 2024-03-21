dff9ee2e2bdaf928c5dde71ef78d104d6567f79b
SeongJongHo <whdgh9595@gmail.com>
2024-03-21 14:33:14 +0900

    [MODIFY] FileService asyncCopy, asyncRead 메소드 추가

--- lib/24_03_19/-2/file_service.dart
+++ lib/24_03_19/-2/file_service.dart
@@ -21,4 +21,17 @@ class FileService {
   void writeToFile(String path, String content) {
     File(path).writeAsStringSync(content);
   }
+
+  void asyncCopyFile(String sourcePath, String targetPath) async {
+    final sourceFile = await asyncReadFile(sourcePath);
+    final targetFile = File(targetPath);
+
+    await targetFile.writeAsString(sourceFile);
+  }
+
+  Future<String> asyncReadFile(String path) async {
+    final file = File(path);
+
+    return await file.readAsString();
+  }
 }
