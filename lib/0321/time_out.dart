e336bd16361382e790df49316bff0d6583da21aa
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-22 10:43:36 +0900

    fear: time out 클래스 수정
    1) 문제를 자세히 읽지 않아 future timeout 사용해야 하는지 몰랐던 실수... 언제나 글을 잘 읽자.
    2) 수정 완료

--- lib/0321/time_out.dart
+++ lib/0321/time_out.dart
@@ -1,27 +1,20 @@
 import 'dart:async';
 
 Future<void> timeOutFuture(int seconds) async {
-  String text = '';
-  const int timeLimit = 5;
-  final stopWatch = Stopwatch()..start();
-
-  switch (seconds) {
-    case <= 5:
-      await Future.delayed(Duration(seconds: seconds));
-      text = 'ok';
-      print('(경과시간 : ${stopWatch.elapsed.inSeconds}초)');
-    case >= timeLimit:
-      await Future.delayed(Duration(seconds: timeLimit));
-      text = 'time out';
+  try {
+    await Future.delayed(Duration(seconds: seconds)).timeout(Duration(seconds: 5));
+    print('OK');
+  } catch (e) {
+    print('Timeout');
   }
-  print(text);
-  return;
 }
 
 Future<void> main() async {
-  // await timeOutFuture(-1); // 바로 ok 출력
-  // await timeOutFuture(0); // 바로 ok 출력
-  await timeOutFuture(3); // ok 출력
-  // await timeOutFuture(5); // ok 출력
+  final stopWatch = Stopwatch()..start();
+
+  // await timeOutFuture(4); // ok 출력
+  await timeOutFuture(5); // ok 출력
   // await timeOutFuture(6); // time out 출력
+
+  print('(경과시간 : ${stopWatch.elapsed})');
 }
