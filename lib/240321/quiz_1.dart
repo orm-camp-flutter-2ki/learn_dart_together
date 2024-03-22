ecfb9be0abb0f293cbdf09e55667dfd39f361180
jungeun272 <hanjungeun272@gmail.com>
2024-03-22 09:54:58 +0900

    feat : 연습문제 3번 timeout 메서드 사용 try catch 문으로 결과값 print

--- lib/240321/quiz_1.dart
+++ lib/240321/quiz_1.dart
@@ -4,7 +4,7 @@ Future<void> editFile() async {
   try {
     final file = File('lib/240321/sample.csv');
     final editFile = File('lib/240321/sample_copy.csv'); //만들어진 파일에 내용이 카피가 됐다.
-    String contents = await file.readAsString();
+    String contents = await file.readAsString(); //awiat를 넣어줘야지 Future을 벗길 수 있다.
 
     if (contents.contains('한석봉')) {
       final editContents = contents.replaceAll('한석봉', '김석봉');
@@ -23,3 +23,7 @@ void main() async {
 
 // sample.csv 파일을 읽어와서 “한석봉" 이라는 문자열이 있는지 찾고, 있다면 sample_copy.csv 파일에 “김석봉"으로 수정하는 함수를 작성하시오.
 // (async - await 사용할 것)
+
+
+
+
