ef878e919e6152fd35d74be28c5a61800cc16191
icicle_4 <dwayne.han4@gmail.com>
2024-03-23 13:37:09 +0900

    refactor: Department fromJson추가

--- lib/240319/department.dart
+++ lib/240319/department.dart
@@ -5,7 +5,9 @@ class Department {
   Employee leader;
 
   Department(this.name, this.leader);
-
+  Department.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        leader = json['leader'];
   Map<String, dynamic> toJson() => {
         'name': name,
         'leader': leader.toJson(),
