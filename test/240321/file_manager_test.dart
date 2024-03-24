a6feec772b5f3ae9e05c04a83f005f23768e7777
icicle_4 <dwayne.han4@gmail.com>
2024-03-24 12:57:58 +0900

    refactor: setUp(), tearDown() 수정 동기화 -> 비동기로 변경

--- test/240321/file_manager_test.dart
+++ test/240321/file_manager_test.dart
@@ -4,17 +4,17 @@ import 'package:learn_dart_together/240321/file_manager.dart';
 import 'package:test/test.dart';
 
 void main() {
-  setUp(() {
+  setUp(() async {
     final sourcePath = 'lib/240321/sample.csv';
     File file = File(sourcePath);
-    file.create();
+    await file.create();
     final String contents = '1, 홍길동, 30\n2, 한석봉, 20';
-    file.writeAsString(contents);
+    await file.writeAsString(contents);
   });
 
-  tearDown(() {
-    File('lib/240321/sample.csv').delete();
-    File('lib/240321/sample_copy.csv').delete();
+  tearDown(() async {
+    await File('lib/240321/sample.csv').delete().then((_) => print('file1 deleted'));
+    await File('lib/240321/sample_copy.csv').delete().then((_) => print('file2 deleted'));
   });
 
   test('target file의 한석봉을 김석봉으로 바꾼다', () async {
@@ -24,9 +24,6 @@ void main() {
     final manager = FileManager();
     // When
     manager.fileManipulate(sourcePath, targetPath);
-
-    // sample_copy.csv 생성 전에 readAsString()이 실행되는 경우가 있어서 delay추가
-    await Future.delayed(Duration(seconds: 1));
 
     final String futureStr = await File('lib/240321/sample_copy.csv').readAsString();
 
