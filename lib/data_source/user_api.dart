0afaafdcfd2d22744b97e1e321c3547cf37147d7
YunZi.Song <yoonji5809@gmail.com>
2024-03-25 22:40:18 +0900

    Feat: http 코드 리팩토링

--- lib/data_source/user_api.dart
+++ lib/data_source/user_api.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
+import 'package:learn_dart_together/http/http_service.dart';
+
 import '../24_03_25/user.dart';
 import 'package:http/http.dart' as http;
 
 class UserApi {
   Future<List<User>> getUsers() async {
-    final http.Response response =
-        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    final response =
+        await HttpService().get('https://jsonplaceholder.typicode.com/users');
 
     List jsonList = jsonDecode(response.body);
     return jsonList.map((e) => User.fromJson(e)).toList();
