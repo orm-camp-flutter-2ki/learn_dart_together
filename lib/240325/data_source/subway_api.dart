5f3f320877144b2d855943842109aa0d5840eed0
chojja7188 <shoun7188@naver.com>
2024-03-25 21:42:05 +0900

    style: 코드 정리

--- lib/240325/data_source/subway_api.dart
+++ lib/240325/data_source/subway_api.dart
@@ -5,7 +5,6 @@ import 'package:learn_dart_together/240325/model/xml/row.dart';
 import 'package:learn_dart_together/240325/service/xml_service.dart';
 
 class SubwayApi {
-
   Future<List<Row>> getSubwayInfo() async {
     final result = await XmlService().callGet('http://swopenapi.seoul.go.kr/api/subway/sample/xml/realtimeStationArrival/0/5/서울');
     final Map<String, dynamic> json = jsonDecode(result);
