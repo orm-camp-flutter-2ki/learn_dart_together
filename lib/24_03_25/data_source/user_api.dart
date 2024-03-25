0b0f458bc4c2046e248e5c818311599beec310d7
sseung <sseutta@naver.com>
2024-03-25 23:20:09 +0900

    Feat: User 클래스, Api 작성
    테스트 작성

---
+++ lib/24_03_25/data_source/user_api.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:learn_dart_together/24_03_25/data_source/user.dart';
+
+class UserApi {
+  Future<List<User>> getTodos() async {
+    final http.Response response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    List jsonList = jsonDecode(response.body); //String
+
+    return jsonList.map((e) => User.fromJson(e)).toList();
+  }
+
+  Future<User> getTodo(int id) async {
+// try catch 해야
+    final http.Response response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/users/$id'));
+// response.body == Json String
+    final Map<String, dynamic> json = jsonDecode(response.body);
+    return User.fromJson(json);
+  }
+}
