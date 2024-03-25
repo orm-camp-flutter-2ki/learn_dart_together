1f54fb3005b86616e0541071f5b5b2f18261e377
chojja7188 <shoun7188@naver.com>
2024-03-25 22:09:49 +0900

    test: stock_test 테스트 코드 내용 추가

--- test/240325/stock_test.dart
+++ test/240325/stock_test.dart
@@ -3,13 +3,22 @@ import 'package:learn_dart_together/240325/model/stock/stock.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Csv 테스트', () async {
+  group('CSV 테스트', () {
     print('========csv========');
     final StockApi api = StockApi();
     final Stock expected = Stock('A', 'Agilent Technologies Inc', 'NYSE', 'Stock', '1999-11-18', 'null', 'Active');
+    final int expectedLength = 11612;
+    test('첫 번째 데이터 검증', () async {
+      final data = await api.getStock();
 
-    final data = await api.getStock();
+      expect(data[0] == expected, true);
+    });
 
-    expect(data[0] == expected, true);
+    test('첫 번째 데이터 검증', () async {
+      final data = await api.getStock();
+      print(data.length);
+
+      expect(data.length == expectedLength, true);
+    });
   });
 }
