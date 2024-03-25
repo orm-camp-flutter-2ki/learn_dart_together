299a6a581bb723f5fb1500f7303f75f213854598
KimDonghyeok <qoophon@gmail.com>
2024-03-25 17:03:10 +0900

    feat: User 데이터 클래스 작성 (DataSource 연습문제)
    - id, name, username, email, address, phone, website, company 필드 추가
    - 역직렬화 User.fromJson 생성자 정의
    - 직렬화 toJson() 메소드 정의
    - ==, hashCode, toString 재정의
    - copyWith 메소드 선언

---
+++ lib/240325/practice/user.dart
@@ -0,0 +1,126 @@
+// https://jsonplaceholder.typicode.com/users
+// {
+//   "id": 1,
+//   "name": "Leanne Graham",
+//   "username": "Bret",
+//   "email": "Sincere@april.biz",
+//   "address": {
+//     "street": "Kulas Light",
+//     "suite": "Apt. 556",
+//     "city": "Gwenborough",
+//     "zipcode": "92998-3874",
+//     "geo": {
+//       "lat": "-37.3159",
+//       "lng": "81.1496"
+//     }
+//   },
+//   "phone": "1-770-736-8031 x56442",
+//   "website": "hildegard.org",
+//   "company": {
+//     "name": "Romaguera-Crona",
+//     "catchPhrase": "Multi-layered client-server neural-net",
+//     "bs": "harness real-time e-markets"
+//   }
+// }
+
+import 'address.dart';
+import 'company.dart';
+
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final Address address;
+  final String phone;
+  final String website;
+  final Company company;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.address,
+    required this.phone,
+    required this.website,
+    required this.company,
+  });
+
+  User.fromJson(Map<String, dynamic> json)
+      : this(
+          id: json['id'],
+          name: json['name'],
+          username: json['username'],
+          email: json['email'],
+          address: Address.fromJson(json['address']),
+          phone: json['phone'],
+          website: json['website'],
+          company: Company.fromJson(json['company']),
+        );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'username': username,
+      'email': email,
+      'address': address,
+      'phone': phone,
+      'website': website,
+      'company': company,
+    };
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is User &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          name == other.name &&
+          username == other.username &&
+          email == other.email &&
+          address == other.address &&
+          phone == other.phone &&
+          website == other.website &&
+          company == other.company;
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      name.hashCode ^
+      username.hashCode ^
+      email.hashCode ^
+      address.hashCode ^
+      phone.hashCode ^
+      website.hashCode ^
+      company.hashCode;
+
+  @override
+  String toString() {
+    return 'User{id: $id, name: $name, username: $username, email: $email, address: $address, phone: $phone, website: $website, company: $company}';
+  }
+
+  User copyWith({
+    int? id,
+    String? name,
+    String? username,
+    String? email,
+    Address? address,
+    String? phone,
+    String? website,
+    Company? company,
+  }) {
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      address: address ?? this.address,
+      phone: phone ?? this.phone,
+      website: website ?? this.website,
+      company: company ?? this.company,
+    );
+  }
+}
