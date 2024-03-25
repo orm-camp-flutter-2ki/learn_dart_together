987d2252271f3692da714c63c5a01d1a87609b46
jungeun272 <hanjungeun272@gmail.com>
2024-03-25 23:32:05 +0900

    feat : Test

--- test/240325/test.dart
+++ test/240325/test.dart
@@ -1,4 +1,6 @@
 import 'package:learn_dart_together/240325/data_source/todo_api.dart';
+import 'package:learn_dart_together/240325/data_source/user.dart';
+import 'package:learn_dart_together/240325/data_source/user_api.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -14,6 +16,15 @@ void main() {
     try {
       final todo = await TodoList();
       print(todo.toString());
+    } catch (e) {
+      print('error');
+    }
+  });
+  test('유저 List 출력', () async {
+    UserApi userApi = UserApi();
+    try {
+      List<User> userList = await userApi.getUser();
+      print(userList.toString());
     } catch (e) {
       print('error');
     }
