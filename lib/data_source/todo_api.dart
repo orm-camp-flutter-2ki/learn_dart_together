ab6a4bd396c236275b928825271bef0a7cc4dbd8
YunZi.Song <yoonji5809@gmail.com>
2024-03-25 21:24:23 +0900

    Feat: todo Api 클래스 작성

---
+++ lib/data_source/todo_api.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../24_03_25/todo.dart';
+
+/// 데이터를 돌려주는 기능을 하는 클래스
+class TodoApi {
+  Future<List<Todo>> getTodos() async {
+    final http.Response response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+
+    List jsonList = jsonDecode(response.body) as List;
+    return jsonList.map((e) => Todo.fromJson(e)).toList();
+  }
+
+  Future<Todo> getTodo(int id) async {
+    final http.Response response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/$id'));
+    final Map<String, dynamic> json = jsonDecode(response.body) ;
+    return Todo.fromJson(json);
+  }
+}
