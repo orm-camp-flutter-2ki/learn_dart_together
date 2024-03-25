ff5193e5024bcb2a33fc097c68787dad94bb9a9a
byjun8932 <byjun8932@gmail.com>
2024-03-25 20:43:53 +0900

    feat: add try-catch logic to TodoApi

--- lib/24_03_25/data_source/task1_todoApi.dart
+++ lib/24_03_25/data_source/task1_todoApi.dart
@@ -4,11 +4,22 @@ import 'package:learn_dart_together/24_03_25/data_source/task1_todo.dart';
 
 class TodoApi {
   Future<List> getTodos() async {
-    final http.Response res = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    try {
+      final http.Response res =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
 
-    List madeJsonList = jsonDecode(res.body);
-
-    return madeJsonList;
+      if (res.statusCode == 200) {
+        // 서버에서 정상적으로 데이터를 받아왔을 때
+        List<dynamic> jsonData = jsonDecode(res.body);
+        return jsonData;
+      } else {
+        // 상태 코드가 200이 아닌 경우
+        throw Exception('Failed to get todos. Status code: ${res.statusCode}');
+      }
+    } catch (e) {
+      // 네트워크 오류나 다른 예외가 발생한 경우
+      throw Exception('Failed to get todos: $e');
+    }
   }
 
   Future<Todo> getTodo(int id) async {
