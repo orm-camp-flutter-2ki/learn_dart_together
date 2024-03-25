628b2b893214a858fc57b697f33441a4380588b7
SeongJongHo <whdgh9595@gmail.com>
2024-03-25 15:11:06 +0900

    [ADD] FakeHttpProxy 클래스 작성, test resource json파일 생성

---
+++ test/24_03_25/fake_http_proxy.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+import 'package:learn_dart_together/24_03_19/-2/file_service.dart';
+
+import 'package:learn_dart_together/24_03_25/http_proxy.dart';
+
+class FakeHttpProxy implements HttpProxy {
+  FileService fileService = FileService();
+
+  @override
+  Future<http.Response> get(Uri uri) async {
+    return await _getResponse(uri);
+  }
+
+  Future<http.Response> _getResponse(Uri uri) async {
+    return http.Response(await fileService.asyncReadFile(_getUriToPath(uri)), 200);
+  }
+
+  String _getUriToPath(Uri uri) {
+    return switch (uri.toString()) {
+      'https://jsonplaceholder.typicode.com/todos/1' => 'test/24_03_25/resource/todo.json',
+      _ => throw Exception('Unknown URI: $uri')
+    };
+  }
+}
