8c6ddc4b5b9e51414064ad94a794c3c778628a30
KimDonghyeok <qoophon@gmail.com>
2024-03-25 17:04:54 +0900

    feat: Company 데이터 클래스 작성 (DataSource 연습문제)
    - name, catchPhrase, bs 필드 추가
    - 역직렬화 Company.fromJson 생성자 정의
    - 직렬화 toJson() 메소드 정의
    - ==, hashCode, toString 재정의
    - copyWith 메소드 선언

---
+++ lib/240325/practice/company.dart
@@ -0,0 +1,61 @@
+//   "company": {
+//     "name": "Romaguera-Crona",
+//     "catchPhrase": "Multi-layered client-server neural-net",
+//     "bs": "harness real-time e-markets"
+//   }
+
+class Company {
+  final String name;
+  final String catchPhrase;
+  final String bs;
+
+  Company({
+    required this.name,
+    required this.catchPhrase,
+    required this.bs,
+  });
+
+  Company.fromJson(Map<String, dynamic> json)
+      : this(
+          name: json['name'],
+          catchPhrase: json['catchPhrase'],
+          bs: json['bs'],
+        );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'catchPhrase': catchPhrase,
+      'bs': bs,
+    };
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Company &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          catchPhrase == other.catchPhrase &&
+          bs == other.bs;
+
+  @override
+  int get hashCode => name.hashCode ^ catchPhrase.hashCode ^ bs.hashCode;
+
+  @override
+  String toString() {
+    return 'Company{name: $name, catchPhrase: $catchPhrase, bs: $bs}';
+  }
+
+  Company copyWith({
+    String? name,
+    String? catchPhrase,
+    String? bs,
+  }) {
+    return Company(
+      name: name ?? this.name,
+      catchPhrase: catchPhrase ?? this.catchPhrase,
+      bs: bs ?? this.bs,
+    );
+  }
+}
