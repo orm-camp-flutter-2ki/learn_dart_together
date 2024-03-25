47ef0f55aa75859ea697ab575ff3d83d3f258533
BAUu <yimej07@gmail.com>
2024-03-25 22:54:22 +0900

    add: test 코드에 List<Users>가 맞는지 확인

--- test/lib/orm_test_240325/users_test.dart
+++ test/lib/orm_test_240325/users_test.dart
@@ -1,21 +1,29 @@
 import 'package:learn_dart_together/orm_240325/data_source/users_api.dart';
+import 'package:learn_dart_together/orm_240325/users.dart';
 import 'package:test/test.dart';
 
 void main() {
   final usersApi = UserApi();
   test('User List 형태 변환', () async {
     final users = await usersApi.getUsers();
+
     print(users);
   });
-  test('Users List 타입이 List<users>인지 확인', () async {
-    final userRuntime = UserApi().runtimeType;
 
-    expect(userRuntime, 'List<User>');
+  test('Users List 타입이 List<users>인지 확인', () async {
+    List<Users> userList = await usersApi.getUsers();
+
+    final userRuntime = userList.runtimeType;
+
+    print(userRuntime);
+
+    expect(userRuntime == List<Users>, true);
   });
+
   test('User List 한개 확인', () async {
     final id = 1;
     final user = await usersApi.getUser(id);
-    print(user);
+    print(user.name);
 
     expect(user.name, 'Leanne Graham');
   });
