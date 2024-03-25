adf0d2a276682493d4ba2a8bc6c134f87a31c121
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-25 15:57:25 +0900

    tset: convert json list to data class

--- test/0325/todo_api_test.dart
+++ test/0325/todo_api_test.dart
@@ -14,5 +14,18 @@ void main() {
     expect(todo.completed, false);
   });
 
+  test('Json List를 데이터 클래스로 변환', () async {
+    List<Todo> todos = await todoApi.getTodos();
+
+    expect(todos.first.userId, 1);
+    expect(todos.first.id, 1);
+    expect(todos.first.title, 'delectus aut autem');
+    expect(todos.first.completed, false);
+
+    expect(todos.last.userId, 10);
+    expect(todos.last.id, 200);
+    expect(todos.last.title, 'ipsam aperiam voluptates qui');
+    expect(todos.last.completed, false);
+  });
 
 }
