b0b96f97976a75fe742eb0b6bb56679a062de6a1
KimDonghyeok <qoophon@gmail.com>
2024-03-25 16:35:52 +0900

    test: Todo 클래스 테스트 코드 수정 (DataSource 연습문제)
    - 역직렬화 테스트 위해 http로 json string을 fetch 하여 저장하는 코드 추가
    - 역직렬화한 todo 객체를 다시 직렬화하여 actualJson 데이터와 String 비교하는 코드 추가

--- test/240325/practice/todo_test.dart
+++ test/240325/practice/todo_test.dart
@@ -1,17 +1,27 @@
 import 'dart:convert';
 
 import 'package:dart_cli_practice/data_source/todo_api.dart';
+import 'package:http/http.dart' as http;
+import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
-void main() {
+void main() async {
   JsonEncoder encoder = JsonEncoder.withIndent('  ');
   TodoApi api = TodoApi();
+  final todoJsonString = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'))
+      .then((response) => response.body);
+  final todosJsonString = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/todos'))
+      .then((response) => response.body);
 
   group('Todo api는', () {
     test('/todo/\$id  path로 특정 id의 todo를 fetch 한다.', () async {
       try {
         final todo = await api.getTodo(1);
-        print(todo);
+        String actualJson = encoder.convert(todo);
+
+        expect(actualJson, equals(todoJsonString));
       } catch (e) {
         print(e);
       }
@@ -20,7 +30,9 @@ void main() {
     test('/todo path로 todo list를 fetch 한다.', () async {
       try {
         final todoList = await api.getTodos();
-        print(todoList);
+        String actualJson = encoder.convert(todoList);
+
+        expect(actualJson, equals(todosJsonString));
       } catch (e) {
         print(e);
       }
