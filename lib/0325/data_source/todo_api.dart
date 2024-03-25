3779265bf9d983f3fd9f82cdd574203ea18dc419
leeseowoo <leeseowoo.kr@gmail.com>
2024-03-25 19:41:12 +0900

    refactor: check status code using if statement

--- lib/0325/data_source/todo_api.dart
+++ lib/0325/data_source/todo_api.dart
@@ -5,13 +5,31 @@ import '../todo.dart';
 class TodoApi {
   Future<List<Todo>> getTodos() async {
     final http.Response response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
-    List jsonList = jsonDecode(response.body);
-    return jsonList.map((e) => Todo.fromJson(e)).toList();
+    List<Todo> todoList = [];
+
+    if (response.statusCode == 200) {
+      List jsonList = jsonDecode(response.body);
+      todoList = jsonList.map((e) => Todo.fromJson(e)).toList();
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+
+    return todoList;
+
   }
 
   Future<Todo> getTodo(int id) async {
     final http.Response response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/$id'));
-    final Map<String, dynamic> json = jsonDecode(response.body);
-    return Todo.fromJson(json);
+    Todo? todo;
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> json = jsonDecode(response.body);
+      todo = Todo.fromJson(json);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+      throw Exception('Todo is null');
+    }
+
+    return todo;
   }
 }
