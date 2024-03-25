5380d5634f4736dba5e94593ce5c9ab7d34688dd
jungeun272 <hanjungeun272@gmail.com>
2024-03-25 17:29:23 +0900

    feat : Created TODO with List

--- lib/240325/data_source/todo_api.dart
+++ lib/240325/data_source/todo_api.dart
@@ -3,6 +3,7 @@ import 'dart:convert'; //Decode 하기 위함
 import 'package:http/http.dart'
     as http; //as 패키지 이름을 생략하고 간결하게 코드를 작성 가능. 동일 함수 방지
 
+//제이슨을 어플리케이션에서 사용할 수 있게 하는 것.  인스턴스를 만들기 위해 클래스를 만들어라
 class Todo {
   final int userId;
   final int id;
@@ -15,33 +16,42 @@ class Todo {
     required this.title,
     required this.completed,
   });
-}
 
-Future<void> getTodo() async {
-  //비동기 함수
-  final response = //http 응답 요청 성공했는지의 여부 판단
-      await http.get(Uri.parse(
-          'https://jsonplaceholder.typicode.com/todos/1')); //URL에서 데이터를 가지고 옴 await 키워드를 사용하여 요청 기다림
-
-  if (response.statusCode == 200) {
-    final Map<String, dynamic> jsonData = json
-        .decode(response.body); //HTTP 응답 본문을 JSON 형식으로 디코딩하여 jsonData 변수에 저장
-
-    final todo = Todo(
-      //팩토리 메서드를 사용하여 JSON 데이터에서 Todo객체를 생성
-      userId: jsonData['userId'],
-      id: jsonData['id'],
-      title: jsonData['title'],
-      completed: jsonData['completed'],
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      completed: json['completed'],
     );
+  }
 
-    print(
-        'userId: ${todo.userId} id: ${todo.id} Title: ${todo.title} Completed: ${todo.completed}');
-  } else {
-    print('Failed to load todo');
+  @override
+  String toString() {
+    return 'Userid = $userId id = $id title = $title completed = $completed';
   }
 }
 
-void main() {
-  getTodo();
+Future<List<Todo>> getTodos() async {
+  //비동기 함수
+  final response = //http 응답 요청 성공했는지의 여부 판단
+      await http.get(Uri.parse(
+          'https://jsonplaceholder.typicode.com/todos')); //URL에서 데이터를 가지고 옴 await 키워드를 사용하여 요청 기다림
+
+  if (response.statusCode == 200) {
+    final List<dynamic> jsonData = json
+        .decode(response.body); //HTTP 응답 본문을 JSON 형식으로 디코딩하여 jsonData 변수에 저장
+    return jsonData.map((json) => Todo.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load todos');
+  }
 }
+
+void main() async {
+  try {
+    final todos = await getTodos();
+    todos.forEach(print);
+  } catch (e) {
+    print('에러');
+  }
+} //tostring 오버라이딩 Use
