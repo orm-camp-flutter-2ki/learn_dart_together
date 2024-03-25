170d3777c77971d8123d5e24c9c03f6b1ba1bd4f
SeongJongHo <whdgh9595@gmail.com>
2024-03-25 16:36:52 +0900

    [ADD] User클래스 작성

---
+++ lib/24_03_25/models/user.dart
@@ -0,0 +1,49 @@
+import 'address.dart';
+import 'company.dart';
+
+class User {
+  final num id;
+  final String name;
+  final String username;
+  final String email;
+  final Address address;
+  final String phone;
+  final String website;
+  final Company company;
+
+  User(
+      {required this.id,
+      required this.name,
+      required this.username,
+      required this.email,
+      required this.address,
+      required this.phone,
+      required this.website,
+      required this.company});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'username': username,
+      'email': email,
+      'address': address.toJson(),
+      'phone': phone,
+      'website': website,
+      'company': company.toJson(),
+    };
+  }
+
+  factory User.fromJson(Map<String, dynamic> json){
+    return User(
+      id: json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+      address: Address.fromJson(json['address']),
+      phone: json['phone'],
+      website: json['website'],
+      company: Company.fromJson(json['company']),
+    );
+  }
+}
