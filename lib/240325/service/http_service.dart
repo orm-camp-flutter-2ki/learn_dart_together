497e2b65f9f3df640d5def42c5870e3fb6f6626c
chojja7188 <shoun7188@naver.com>
2024-03-25 16:42:48 +0900

    feat: HttpService 수정

--- lib/240325/service/http_service.dart
+++ lib/240325/service/http_service.dart
@@ -6,6 +6,8 @@ class HttpService {
   Future<String> callGet(url) async {
     final response = await http.get(Uri.parse(url))
         .onError((error, stackTrace) => throw Exception('에러 발생 : $error'));
+
+    if (response.statusCode != 200) throw Exception('에러 발생 : ${response.statusCode}');
     return response.body;
   }
 }
