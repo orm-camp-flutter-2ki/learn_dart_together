f874873359f1bdba2c93ab983761e646c70be6ca
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-03-25 15:14:21 +0900

    feat : todo class 추가

---
+++ lib/240325/todo.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class Todo {
+  int userId;
+  int id;
+  String title;
+  bool completed;
+
+  Todo(
+      {required this.userId,
+      required this.id,
+      required this.title,
+      required this.completed});
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'userId': userId,
+      'id': id,
+      'title': title,
+      'completed': completed,
+    };
+  }
+
+  Todo.fromJson(Map<String, dynamic> json)
+      : userId = json['userId'],
+        id = json['id'],
+        title = json['title'],
+        completed = json['completed'];
+
+  Todo copyWith({
+    int? userId,
+    int? id,
+    String? title,
+    bool? completed,
+  }) {
+    return Todo(
+      userId: userId ?? this.userId,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      completed: completed ?? this.completed,
+    );
+  }
+
+  @override
+  bool operator ==(covariant Todo other) {
+    if (identical(this, other)) return true;
+
+    return other.userId == userId &&
+        other.id == id &&
+        other.title == title &&
+        other.completed == completed;
+  }
+
+  @override
+  int get hashCode {
+    return userId.hashCode ^ id.hashCode ^ title.hashCode ^ completed.hashCode;
+  }
+
+  @override
+  String toString() {
+    return 'Todo(userId: $userId, id: $id, title: $title, completed: $completed)';
+  }
+}
