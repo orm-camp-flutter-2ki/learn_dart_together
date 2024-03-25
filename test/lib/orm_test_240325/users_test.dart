44b56d0687e38eae905bcdaa6a43357ecc5d522a
BAUu <yimej07@gmail.com>
2024-03-25 18:51:10 +0900

    add: test 코드에 name이 일치하는지 검사 코드 추가

--- test/lib/orm_test_240325/users_test.dart
+++ test/lib/orm_test_240325/users_test.dart
@@ -1,10 +1,22 @@
 import 'package:learn_dart_together/orm_240325/data_source/users_api.dart';
-import 'package:test/scaffolding.dart';
+import 'package:test/test.dart';
 
 void main() {
+  final usersApi = UserApi();
   test('User List 형태 변환', () async {
-    final usersApi = UserApi();
     final users = await usersApi.getUsers();
     print(users);
+  });
+  test('Users List 타입이 List<users>인지 확인', () async {
+    final userRuntime = UserApi().runtimeType;
+
+    expect(userRuntime, 'List<User>');
+  });
+  test('User List 한개 확인', () async {
+    final id = 1;
+    final user = await usersApi.getUser(id);
+    print(user);
+
+    expect(user.name, 'Leanne Graham');
   });
 }
