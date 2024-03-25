6483001c80fc61afc2c9de7f0f0a3ed09b823a95
KimDonghyeok <qoophon@gmail.com>
2024-03-25 17:00:37 +0900

    test: Todo 클래스 테스트 코드 수정 (DataSource 연습문제)
    - 코멘트 추가

--- test/240325/practice/todo_test.dart
+++ test/240325/practice/todo_test.dart
@@ -8,6 +8,7 @@ import 'package:test/scaffolding.dart';
 void main() async {
   JsonEncoder encoder = JsonEncoder.withIndent('  ');
   TodoApi api = TodoApi();
+
   final todoJsonString = await http
       .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'))
       .then((response) => response.body);
@@ -18,6 +19,7 @@ void main() async {
   group('Todo api는', () {
     test('/todo/\$id  path로 특정 id의 todo를 fetch 한다.', () async {
       try {
+        // 역직렬화하여 생성한 객체를 다시 직렬화하여 비교
         final todo = await api.getTodo(1);
         String actualJson = encoder.convert(todo);
 
@@ -29,6 +31,7 @@ void main() async {
 
     test('/todo path로 todo list를 fetch 한다.', () async {
       try {
+        // 역직렬화하여 생성한 객체를 다시 직렬화하여 비교
         final todoList = await api.getTodos();
         String actualJson = encoder.convert(todoList);
 
