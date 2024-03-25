743fa5ccc0b866967d44bbb85b39b08f0c6e9b92
KimDonghyeok <qoophon@gmail.com>
2024-03-25 17:19:55 +0900

    test: User 테스크 클래스 작성 (DataSource 연습문제)
    - user list fetch 테스트 코드 작성

---
+++ test/240325/practice/user_test.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:dart_cli_practice/240325/practice/user.dart';
+import 'package:dart_cli_practice/data_source/user_api.dart';
+import 'package:http/http.dart' as http;
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() async {
+  JsonEncoder encoder = JsonEncoder.withIndent('  ');
+  UserApi api = UserApi();
+
+  final userJsonString = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/users'))
+      .then((response) => response.body);
+
+  test('User api는 /users path로 user list를 fetch 한다.', () async {
+    try {
+      // 역직렬화하여 생성한 List<User>를 다시 직렬화하여 비교
+      List<User> userList = await api.getUsers();
+      final actualJson = encoder.convert(userList);
+
+      expect(actualJson, equals(userJsonString));
+    } catch (e) {
+      print(e);
+    }
+  });
+}
