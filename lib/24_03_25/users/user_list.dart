b22c7f33bc6bd37c4f634a11dd2999d4b7868822
kimhyungmin <jeen14rla@gmail.com>
2024-03-25 15:05:00 +0900

    add: user_list에 변수, 프린트 추가

--- lib/24_03_25/users/user_list.dart
+++ lib/24_03_25/users/user_list.dart
@@ -3,6 +3,8 @@ import 'package:learn_dart_together/24_03_25/users/user_api.dart';
 
 void main() async {
   List<User> users = await UserApi().getUsers();
+  User userIdOne = await UserApi().getUser(1);
 
   print(users);
+  print(userIdOne);
 }
