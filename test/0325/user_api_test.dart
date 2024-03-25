a41b20d6a86cef50615e5a672dbdc3cb0e2e2ca6
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-26 04:01:07 +0900

    fix: user test 파일 수정

--- test/0325/user_api_test.dart
+++ test/0325/user_api_test.dart
@@ -1,15 +1,42 @@
+import 'package:collection/collection.dart';
 import 'package:http/http.dart';
 import 'package:learn_dart_together/0325/user_api.dart';
 import 'package:learn_dart_together/0325/user_data_class.dart';
+import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 Future<void> main() async {
-  test('description', () async {
-    try {
-      final UserApi userApi = UserApi();
-      final List<User> users = await userApi.getUsers();
-    } catch (e) {
-      print('[error]\n$e');
-    }
+  group('todo Api', () {
+    final UserApi userApi = UserApi();
+    final eq = DeepCollectionEquality().equals;
+
+    test('특정 id 메서드', () async {
+      final expectedResult = User(
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+          address: Address(
+              street: 'Kulas Light',
+              suite: 'Apt. 556',
+              city: 'Gwenborough',
+              zipcode: '92998-3874',
+              geo: Geo(lat: '-37.3159', lng: '81.1496')),
+          phone: '1-770-736-8031 x56442',
+          website: 'hildegard.org',
+          company: Company(
+              name: 'Romaguera-Crona',
+              catchPhrase: 'Multi-layered client-server neural-net',
+              bs: 'harness real-time e-markets'));
+
+      final actualResult = await userApi.getUser(1);
+
+      expect(actualResult == expectedResult, true);
+    });
+
+    test('전체 길이가 10인지', () async {
+      final actualResult = await userApi.getUsers();
+      expect(actualResult.length, 10);
+    });
   });
 }
