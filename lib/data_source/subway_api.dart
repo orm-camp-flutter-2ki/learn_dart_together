8aa510ffdcc04ebfcac56069e0cd69d11c6be7c2
gyubit <gyubros@gmail.com>
2024-03-26 02:00:01 +0900

    TRY: PRACTICE4 시(현재 실행오류)

---
+++ lib/data_source/subway_api.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart' as xml;
+
+import '../240325/practice4.dart';
+
+class SubwayApi {
+  Future<List<Subway>> getRealTimeXml() async {
+    final http.Response response = await http.get(Uri.parse(
+        'http://swopenapi.seoul.go.kr/api/subway/sample/xml/realtimeStationArrival/0/5/서울'));
+
+    if (response.statusCode == 200) {
+      var document = xml.XmlDocument.parse(response.body);
+      var arrivals = document.findAllElements('row');
+      List<Subway> subways = [];
+
+      arrivals.forEach((arrival) {
+        var trainLine = arrival.findElements('trainLine').first.text;
+        var station = arrival.findElements('station').first.text;
+        var arrivalTime = arrival.findElements('arrivalTime').first.text;
+
+        var subway = Subway(trainLine, station, arrivalTime);
+        subways.add(subway);
+      });
+
+      return subways;
+    } else {
+      throw Exception('Failed to load subway data');
+    }
+  }
+}
