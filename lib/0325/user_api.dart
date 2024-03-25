ab14faeab82672316edbf5380968cca088a2706c
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-25 19:03:03 +0900

    fear: User Api, Todo Api 클래스 생성

---
+++ lib/0325/user_api.dart
@@ -0,0 +1,54 @@
+// 데이터 클래스
+class User {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  User(
+      {required this.userId,
+      required this.id,
+      required this.title,
+      required this.completed});
+
+  User.fromJson(Map<String, dynamic> json) // 생성자
+      : userId = json['userId'],
+        id = json['id'],
+        title = json['title'],
+        completed = json['completed'];
+
+  Map<String, dynamic> toJson() => // 함수
+      {'userId': userId, 'id': id, 'title': title, 'completed': completed};
+
+  @override
+  String toString() {
+    return 'User{userId: $userId, id: $id, title: $title, completed: $completed}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is User &&
+          runtimeType == other.runtimeType &&
+          userId == other.userId &&
+          id == other.id &&
+          title == other.title &&
+          completed == other.completed;
+
+  @override
+  int get hashCode =>
+      userId.hashCode ^ id.hashCode ^ title.hashCode ^ completed.hashCode;
+
+  User copyWith(
+      {required int? userId,
+      required int? id,
+      required String? title,
+      required bool? completed}) {
+    return User(
+      userId: userId ?? this.userId,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      completed: completed ?? this.completed,
+    );
+  }
+}
