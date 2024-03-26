88af9e0f298f5db0517f230c3093b0fb974daf60
Yejin.Shin <mingmi_27@naver.com>
2024-03-26 19:08:21 +0900

    feat: userRepository 할 수 있는 곳까지

---
+++ lib/240326_homework/user_repository.dart
@@ -0,0 +1,30 @@
+import 'package:collection/collection.dart';
+import 'package:learn_dart_together/240325_homework/model/user.dart';
+import 'package:learn_dart_together/240325_homework/user_api.dart';
+
+void main() async {
+  final repo1 = await TodoRepositoryImpl().getTodos();
+  final repo2 = await TodoRepositoryImpl().getUsersTop10ByUserName();
+}
+
+class TodoRepositoryImpl implements TodoRepository {
+  final UserApi _api = UserApi();
+
+  @override
+  Future<List<User>> getTodos() async {
+    List<User> data = await _api.getUsers();
+    return data;
+  }
+
+  @override
+  Future<List<User>> getUsersTop10ByUserName() async {
+    List<User> data = await _api.getUsers();
+    return data.sorted((a, b) => a.username.compareTo(b.username)).toList();
+  }
+}
+
+abstract interface class TodoRepository {
+  Future<List<User>> getTodos();
+
+  Future<List<User>> getUsersTop10ByUserName();
+}
