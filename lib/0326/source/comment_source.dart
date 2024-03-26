9598f59e5c148a19d32049637ed195e53896b7ce
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-26 15:13:54 +0900

    feat: comment_repository 와 source 작성
    - comment_repository :
    1. 추상 인터페이스 클래스로 작성해서 어떤 동작을 할 지 정의
    
    - comment_source:
    1. 데이터 소스를 사용해 인터페이스를 구현

---
+++ lib/0326/source/comment_source.dart
@@ -0,0 +1,37 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:learn_dart_together/0326/model/comment_model.dart';
+import 'package:learn_dart_together/0326/repository/comment_repository.dart';
+
+class CommentImpl implements CommentRepository {
+  @override
+  // http.get()을 사용하여 특정 postId에 해당하는 데이터를 가져오는 메서드
+  Future<List<Comment>> getComments(int postId) async {
+    final http.Response response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/comments'));
+    final List json = await jsonDecode(response.body);
+
+    // 특정 postId에 해당하는 댓글만
+    final List postIdCommentsList =
+        json.where((element) => element['postId'] == postId).toList();
+
+    return postIdCommentsList.map((e) => Comment.fromJson(e)).toList();
+  }
+
+  @override
+  Future<List<Comment>> getCommentsList() async {
+    // http.get()을 사용하여 웹 서버에서 데이터를 가져오는 메서드
+    http.Response response;
+    try {
+      response = await http
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/comments'));
+    } catch (error) {
+      print('[Error]\n$error');
+      return []; // // 예외가 발생하면 빈 리스트를 반환하거나 다른 처리할 수 있다.
+    }
+
+    final List jsonList = await jsonDecode(response.body);
+    return jsonList.map((e) => Comment.fromJson(e)).toList();
+  }
+}
