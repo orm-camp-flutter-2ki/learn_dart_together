6b3b2f18fba6e2565e97df2718ae680da7a110ee
jungeun272 <hanjungeun272@gmail.com>
2024-03-26 20:18:22 +0900

    feat : edit file name Album -> album

---
+++ lib/240326/data_source/photo_api.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../model/photo.dart';
+import '../repository/photo_repository.dart';
+import '../repository/photo_repository_implements.dart';
+
+class PhotoApi {
+  Future<List<Photo>> getPhotos(int albumId) async {
+    final response = await http.get(Uri.parse(
+        'https://jsonplaceholder.typicode.com/photos?albumId=$albumId'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = jsonDecode(response.body);
+      return jsonData.map((json) => Photo.fromJson(json)).toList();
+    } else {
+      throw Exception('Error: ${response.statusCode}');
+    }
+  }
+}
+
+void main() async {
+  PhotoRepository photoRepo = PhotoRepositoryImpl();
+  final result = await photoRepo.getPhotos(5);
+  print(result);
+}
