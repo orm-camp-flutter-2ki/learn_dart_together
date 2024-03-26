05826c9682a413e4518ebfe538b9a1c610a5a9a0
byjun8932 <byjun8932@gmail.com>
2024-03-26 21:44:58 +0900

    feat: create task5 user Model Class, API, Repository, Interface

---
+++ lib/24_03_26/model_class/task5_user.dart
@@ -0,0 +1,103 @@
+import 'package:learn_dart_together/24_03_26/model_class/task5_address.dart';
+import 'package:learn_dart_together/24_03_26/model_class/task5_company.dart';
+
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final Address address;
+  final String phone;
+  final String website;
+  final Company company;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.address,
+    required this.phone,
+    required this.website,
+    required this.company,
+  });
+
+  User.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        username = json['username'],
+        email = json['email'],
+        address = Address.fromJson(json['address']),
+        phone = json['phone'],
+        website = json['website'],
+        company = Company.fromJson(json['company']);
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'username': username,
+        'email': email,
+        'address': address.toJson(),
+        'phone': phone,
+        'website': website,
+        'company': company.toJson()
+      };
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is User &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          name == other.name &&
+          username == other.username &&
+          email == other.email &&
+          address == other.address &&
+          phone == other.phone &&
+          website == other.website &&
+          company == other.company;
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      name.hashCode ^
+      username.hashCode ^
+      email.hashCode ^
+      address.hashCode ^
+      phone.hashCode ^
+      website.hashCode ^
+      company.hashCode;
+
+  @override
+  String toString() {
+    return 'User{'
+        'id: $id,'
+        ' name: $name,'
+        ' username: $username,'
+        ' email: $email,'
+        ' address: $address,'
+        ' phone: $phone,'
+        ' website: $website,'
+        ' company: $company}';
+  }
+
+  User copyWith(
+      {int? id,
+      String? name,
+      String? username,
+      String? email,
+      Address? address,
+      String? phone,
+      String? website,
+      Company? company}) {
+    return User(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        username: username ?? this.username,
+        email: email ?? this.email,
+        address: address ?? this.address,
+        phone: phone ?? this.phone,
+        website: website ?? this.website,
+        company: company ?? this.company);
+  }
+}
