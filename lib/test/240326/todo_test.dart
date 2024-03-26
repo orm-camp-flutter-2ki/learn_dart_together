d141f210b9365e258491c02f42d2e98b6118fbf8
hee462 <a01080809887@gmail.com>
2024-03-26 23:05:33 +0900

    feat: Test code 변경
    테장(성종호님) comment와 함께 작성..

--- lib/test/240326/todo_test.dart
+++ lib/test/240326/todo_test.dart
@@ -2,9 +2,16 @@ import 'package:learn_dart_together/240326/repository/todo_repository.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Todo Test', () async {
+  test('모든 Todo Test', () async {
+    final repository = TodoRepositoryImpl();
+    final todos = await repository.getTodos();
+    expect(todos.length, 200);
+  });
+  test('completed가 ture인 경우 todo 출력', () async {
     final repository = TodoRepositoryImpl();
     final completed = await repository.getCompletedTodos();
     print(completed);
+    expect(completed.first.completed, true);
+    expect(completed.last.completed, true);
   });
 }
