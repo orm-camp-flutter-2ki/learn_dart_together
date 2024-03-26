ff4e7b1de5afa09bdef88f9afb0f0c992fc2a226
YunZi.Song <yoonji5809@gmail.com>
2024-03-27 03:01:00 +0900

    Feat: 레포지토리 변경에 따른 테스트코드 수정

--- test/24_03_25/api_test.dart
+++ test/24_03_25/api_test.dart
@@ -1,29 +1,29 @@
 import 'package:learn_dart_together/24_03_25/todo.dart';
 import 'package:learn_dart_together/24_03_25/user.dart';
 import 'package:learn_dart_together/data_source/todo_api.dart';
-import 'package:learn_dart_together/data_source/user_api.dart';
+import 'package:learn_dart_together/repository/todo_repository_impl.dart';
+import 'package:learn_dart_together/repository/user_repository_impl.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
   group('api 테스트', () {
     test('Q1, 간단한 json을 데이터 클래스로 변환', () async {
-      Todo todo = await TodoApi().getTodo(1);
+      final Todo todo = await TodoApi().getTodo(1);
 
       expect(todo.title == 'delectus aut autem', true);
     });
 
     test('Q2, json List를 List<Todo>로 변환', () async {
-      List<Todo> todos = await TodoApi().getTodos();
+      final List<Todo> todos = await TodoRepositoryImpl().getTodos();
 
       expect(todos[0].id == 1, true);
     });
 
-    test('Q3, json List를 List<Todo>로 변환', () async {
-      List<User> users = await UserApi().getUsers();
+    test('Q3, json user를 List<User>로 변환', () async {
+      final List<User> users = await UserRepositoryImpl().getUsers();
 
       expect(users[0].id == 1, true);
     });
-
   });
 }
