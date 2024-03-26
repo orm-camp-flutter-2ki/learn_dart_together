5f0c5111092cdee4c536b7bc8f71ff41578795fc
JEON-Sungsu <anagma@naver.com>
2024-03-26 17:00:51 +0900

    fix: UserApi , User DTO 수정

--- test/24_03_25/data_source/user_api_test.dart
+++ test/24_03_25/data_source/user_api_test.dart
@@ -6,17 +6,11 @@ void main() {
   group('User JSON 파싱 테스트', () {
     UserApi userApi = UserApi();
 
-    test('UserApi 의 getUser 메서드가 잘 동작하는지', () async {
-      List jsonList = await userApi.getUser();
-
-      expect(jsonList is List, true);
-    });
 
     test('User DTO의 userInfo 필드의 length 는 10이다.', () async {
-      List jsonList = await userApi.getUser();
-      User user = User.fromJson(jsonList);
+      List<User> userList = await userApi.getUsers();
 
-      expect(user.userInfo.length, 10);
+      expect(userList.length, 10);
     });
   });
 }
