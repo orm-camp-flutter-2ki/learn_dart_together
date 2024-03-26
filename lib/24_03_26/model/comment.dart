ff0e002b670e5ec69e11a4f5bb7a74ad9727275a
SeongJongHo <whdgh9595@gmail.com>
2024-03-26 14:23:42 +0900

    [ADD] Comment 클래스 작성

---
+++ lib/24_03_26/model/comment.dart
@@ -0,0 +1,59 @@
+class Comment {
+  final num postId;
+  final num id;
+  final String name;
+  final String email;
+  final String body;
+
+  Comment({
+    required this.postId,
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.body,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'postId': postId,
+      'id': id,
+      'name': name,
+      'email': email,
+      'body': body,
+    };
+  }
+
+  factory Comment.fromJson(Map<String, dynamic> json) {
+    return Comment(
+      postId: json['postId'],
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      body: json['body'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Comment{postId: $postId, id: $id, name: $name, email: $email, body: $body}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Comment &&
+          runtimeType == other.runtimeType &&
+          postId == other.postId &&
+          id == other.id &&
+          name == other.name &&
+          email == other.email &&
+          body == other.body;
+
+  @override
+  int get hashCode =>
+      postId.hashCode ^
+      id.hashCode ^
+      name.hashCode ^
+      email.hashCode ^
+      body.hashCode;
+}
