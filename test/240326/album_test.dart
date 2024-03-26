d42a3f55749f64d31525ac6286ddff9975c5112b
chojja7188 <shoun7188@naver.com>
2024-03-26 15:34:02 +0900

    test: 테스트용 resource json 추가

--- test/240326/album_test.dart
+++ test/240326/album_test.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:collection/collection.dart';
 import 'package:learn_dart_together/240326/impl/album_repository_impl.dart';
 import 'package:learn_dart_together/240326/model/album.dart';
@@ -6,58 +9,7 @@ import 'package:test/test.dart';
 void main() {
   group('Album Test', () {
     final repository = AlbumRepositoryImpl();
-    List<Map<String, dynamic>> json = [
-      {
-        "userId": 1,
-        "id": 1,
-        "title": "quidem molestiae enim"
-      },
-      {
-        "userId": 1,
-        "id": 2,
-        "title": "sunt qui excepturi placeat culpa"
-      },
-      {
-        "userId": 1,
-        "id": 3,
-        "title": "omnis laborum odio"
-      },
-      {
-        "userId": 1,
-        "id": 4,
-        "title": "non esse culpa molestiae omnis sed optio"
-      },
-      {
-        "userId": 1,
-        "id": 5,
-        "title": "eaque aut omnis a"
-      },
-      {
-        "userId": 1,
-        "id": 6,
-        "title": "natus impedit quibusdam illo est"
-      },
-      {
-        "userId": 1,
-        "id": 7,
-        "title": "quibusdam autem aliquid et et quia"
-      },
-      {
-        "userId": 1,
-        "id": 8,
-        "title": "qui fuga est a eum"
-      },
-      {
-        "userId": 1,
-        "id": 9,
-        "title": "saepe unde necessitatibus rem"
-      },
-      {
-        "userId": 1,
-        "id": 10,
-        "title": "distinctio laborum qui"
-      }
-    ];
+    List json = jsonDecode(File('test/240326/resource/expected_album.json').readAsStringSync());
     final expected = json.map((e) => Album.fromJson(e)).toList();
 
     test('getAlbums() 첫 번째 Index 비교', () async {
--- test/240326/comment_test.dart
+++ test/240326/comment_test.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:collection/collection.dart';
 import 'package:learn_dart_together/240326/impl/comment_repository_impl.dart';
 import 'package:learn_dart_together/240326/model/comment.dart';
@@ -6,43 +9,7 @@ import 'package:test/test.dart';
 void main() {
   group('Comment Test', () {
     final repository = CommentRepositoryImpl();
-    List<Map<String, dynamic>> json = [
-      {
-        "postId": 1,
-        "id": 1,
-        "name": "id labore ex et quam laborum",
-        "email": "Eliseo@gardner.biz",
-        "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
-      },
-      {
-        "postId": 1,
-        "id": 2,
-        "name": "quo vero reiciendis velit similique earum",
-        "email": "Jayne_Kuhic@sydney.com",
-        "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
-      },
-      {
-        "postId": 1,
-        "id": 3,
-        "name": "odio adipisci rerum aut animi",
-        "email": "Nikita@garfield.biz",
-        "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
-      },
-      {
-        "postId": 1,
-        "id": 4,
-        "name": "alias odio sit",
-        "email": "Lew@alysha.tv",
-        "body": "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
-      },
-      {
-        "postId": 1,
-        "id": 5,
-        "name": "vero eaque aliquid doloribus et culpa",
-        "email": "Hayden@althea.biz",
-        "body": "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
-      }
-    ];
+    List json = jsonDecode(File('test/240326/resource/expected_comment.json').readAsStringSync());
     final expected = json.map((e) => Comment.fromJson(e)).toList();
 
     test('List 비교', () async {
