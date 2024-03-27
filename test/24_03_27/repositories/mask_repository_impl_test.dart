79289df6113d187ea32af78ad4be75f7d345a80f
SeongJongHo <whdgh9595@gmail.com>
2024-03-27 18:27:20 +0900

    [MODIFY] MaskRepositoryImpl getMask 테스트코드 정리

--- test/24_03_27/repositories/mask_repository_impl_test.dart
+++ test/24_03_27/repositories/mask_repository_impl_test.dart
@@ -42,22 +42,6 @@ void main() {
     group('getMask 메소드는', () {
       test('api.getMask를 1번 호출한다.', () async {
         // Given
-        final mask = Mask(
-          count: 1,
-          stores: [
-            Stores(
-              addr: '서울시 중구',
-              code: '1111',
-              lat: 37.1234,
-              lng: 126.1234,
-              name: '마스크판매처1',
-              remainStat: 'plenty',
-              stockAt: '2022-03-27 10:00:00',
-              type: '01',
-              createdAt: '2022-03-27 10:00:00',
-            ),
-          ],
-        );
         when(maskApi.getMask()).thenAnswer((_) async => mask);
 
         // When
