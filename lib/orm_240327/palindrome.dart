a098261e3e682fe8c9dceeebbe75119b989b7661
BAUu <yimej07@gmail.com>
2024-03-27 17:40:03 +0900

    feat: 문제 3번 리스트 가장 큰값 2개 더한 값 구하기

--- lib/orm_240327/palindrome.dart
+++ lib/orm_240327/palindrome.dart
@@ -1,14 +1,17 @@
-class Palindrome {
-  String word;
-
-  Palindrome(this.word);
-
+extension Palindrome on String {
   bool isPalindrome() {
-    // Remove non-alphanumeric characters and convert to lowercase
-    String cleanWord =
-        word.replaceAll(RegExp(r'[^a-zA-Z0-9]'), '').toLowerCase();
+    // // 알파벳 숫자를 제외한 모든 코드를 지우고 알파벳에 경우 소문자로 만드는 코드
+    // String cleanWord =
+    //     word.replaceAll(RegExp(r'[^a-zA-Z]'), '').toLowerCase();
+    //
+    // // 화문이 맞는지 확인하는 코드
+    // return cleanWord == cleanWord.split('').reversed.join('');
 
-    // Check if the clean word is equal to its reverse
-    return cleanWord == cleanWord.split('').reversed.join('');
+    for (int i = 0; i < length / 2; i++) {
+      if (this[i] != this[length - 1 - i]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
--- test/lib/orm_test_240327/palindrome_test.dart
+++ test/lib/orm_test_240327/palindrome_test.dart
@@ -2,10 +2,16 @@ import 'package:learn_dart_together/orm_240327/palindrome.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('palindrome 화문 구별', () async {
-    Palindrome word = Palindrome('level');
+  test('palindrome 화문 구별 true', () async {
+    final word = 'level';
     final result = word.isPalindrome();
 
     expect(result, true);
+  });
+  test('palindrome 화문 구별 false', () async {
+    final word = 'level2';
+    final result = word.isPalindrome();
+
+    expect(result, false);
   });
 }
