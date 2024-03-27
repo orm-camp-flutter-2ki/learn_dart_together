cfbf48780bfc2228447fc1af25cb7339cabde573
Yejin.Shin <mingmi_27@naver.com>
2024-03-27 14:53:37 +0900

    feat: 해답

--- lib/240327_homework/model/bank.dart
+++ lib/240327_homework/model/bank.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
 
+import 'bank_json.dart';
+
+void main() {
+  final result = Bank.fromJson(jsonDecode(jsonString));
+  print(result);
+}
 
 class Bank {
   final String name;
@@ -7,14 +14,24 @@ class Bank {
 
   Bank(this.name, this.address, this.phone);
 
+  @override
+  String toString() {
+    return 'Bank{name: $name, address: $address, phone: $phone}';
+  }
+
   Bank.fromJson(Map<String, dynamic> json)
-      : name = json['namê'] as String,
-        address = json['addrêss'],
-        phone = json['ph0ne'] as String;
+      : name = json['name'] as String,
+        address = json['address'],
+        phone = json['phone'] as String;
 
   Map<String, dynamic> toJson() => {
         'name': name,
-        'αddress': address,
-        'plone': phone,
+        'address': address,
+        'phone': phone,
       };
 }
+
+//from json 영어 바꿈
+//tojson plone -> phone
+//객체값을 알기위해 toSring() 정의
+//as Strin 괜찮은가?
