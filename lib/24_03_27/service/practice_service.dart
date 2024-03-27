12db16788d2126c9e9ba8f2649dba541179e9512
SeongJongHo <whdgh9595@gmail.com>
2024-03-27 15:08:18 +0900

    [ADD] sumOfLargestTwoNumbers 메소드 및 테스트코드 작성

--- lib/24_03_27/service/practice_service.dart
+++ lib/24_03_27/service/practice_service.dart
@@ -1,5 +1,16 @@
+import 'package:collection/collection.dart';
+
 class PracticeService {
   bool isPalindrome(String text) {
     return text.toLowerCase() == text.toLowerCase().split('').reversed.join();
+  }
+
+  int sumOfLargestTwoNumbers(List<int> numbers) {
+    final [one, two, ...] = numbers.sorted(_compareDescending);
+    return one + two;
+  }
+
+  int _compareDescending(int a, int b) {
+    return b - a;
   }
 }
--- test/24_03_27/service/practice_service_test.dart
+++ test/24_03_27/service/practice_service_test.dart
@@ -1,4 +1,3 @@
-import 'package:learn_dart_together/24_03_27/model/bank.dart';
 import 'package:learn_dart_together/24_03_27/service/practice_service.dart';
 import 'package:test/test.dart';
 
@@ -28,6 +27,55 @@ void main() {
 
         // Then
         expect(result, expected);
+      });
+    });
+
+    group('isPalindrome 메소드는', () {
+      test('인자로 받은 값이 회문이면 true를 반환한다.', () {
+        // Given
+        final expected = true;
+        final text = 'level';
+
+        // When
+        final result = practiceService.isPalindrome(text);
+
+        // Then
+        expect(result, expected);
+      });
+
+      test('인자로 받은 값이 회문이 아니면 false를 반환한다.', () {
+        // Given
+        final expected = false;
+        final text = 'hello';
+
+        // When
+        final result = practiceService.isPalindrome(text);
+
+        // Then
+        expect(result, expected);
+      });
+    });
+
+    group('sumOfLargestTwoNumbers 메소드는', () {
+      test('인자로 받은 리스트에서 제일 큰 두 수를 더해서 반환한다.', () {
+        // Given
+        final expected = 9;
+        final numbers = [1, 2, 3, 4, 5];
+
+        // When
+        final result = practiceService.sumOfLargestTwoNumbers(numbers);
+
+        // Then
+        expect(result, expected);
+      });
+
+      test('인자로 받은 리스트의 크기가 2 미만일경우 StateError 예외를 던진다.', () {
+        // Given
+        final numbers = [1];
+
+        // When Then
+        expect(() => practiceService.sumOfLargestTwoNumbers(numbers),
+            throwsA(predicate((e) => e is StateError)));
       });
     });
   });
