a7ab0c96d992480af595e61b48130b5a43f16098
YunZi.Song <yoonji5809@gmail.com>
2024-03-27 14:33:19 +0900

    Feat: 직렬/역직렬화 연습문제1 - 오타 수정

--- lib/24_03_27/bank.dart
+++ lib/24_03_27/bank.dart
@@ -1,3 +1,10 @@
+// 데이터 소스
+// {
+//  "name": "홍길동",
+//  "address": "서울시 어쩌구 저쩌구",
+//  "phone": "010-1111-2222"
+// }
+
 class Bank {
   final String name;
   final String address;
@@ -6,13 +13,13 @@ class Bank {
   Bank(this.name, this.address, this.phone);
 
   Bank.fromJson(Map<String, dynamic> json)
-      : name = json['namê'] as String,
-        address = json['addrêss'],
-        phone = json['ph0ne'] as String;
+      : name = json['name'] as String,
+        address = json['address'],
+        phone = json['phone'] as String;
 
   Map<String, dynamic> toJson() => {
     'name': name,
-    'αddress': address,
-    'plone': phone,
+    'address': address,
+    'phone': phone,
   };
 }
