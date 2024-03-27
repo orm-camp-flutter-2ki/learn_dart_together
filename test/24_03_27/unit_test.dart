5eb7b61c179c8e268c5ef91b43339940320642a2
YunZi.Song <yoonji5809@gmail.com>
2024-03-27 14:48:27 +0900

    Feat: 회문 연습문제2 - 함수 및 테스트 코드 작성

--- test/24_03_27/unit_test.dart
+++ test/24_03_27/unit_test.dart
@@ -6,7 +6,7 @@ import 'package:test/scaffolding.dart';
 
 void main() {
   group('unit test', () {
-    test('bank test', () async {
+    test('Q1. bank test', () {
       Map<String, dynamic> b = {
         "name": "홍길동",
         "address": "서울시 어쩌구 저쩌구",
@@ -18,4 +18,24 @@ void main() {
       expect(bank.name == '홍길동', true);
     });
   });
+
+  test('Q2. palindrome test', () {
+    expect(palindromeCheck('racecar') == true, true);
+  });
+}
+
+bool palindromeCheck(String text) {
+  int start = 0;
+  int end = text.length - 1;
+
+  while (start < end) {
+    // 시작과 끝에서 한 글자씩 비교. 다르면 false
+    if (text[start] != text[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
 }
