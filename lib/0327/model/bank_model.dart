3aa16c5e0eaf4fe2f67b83c8514659206a9c471b
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-27 16:23:27 +0900

    fix: 기존 bank 파일 수정
    - 수정 사항
    1. 생성자: const, required 키워드 추가
    2. fromJson(), toJson() 수정
    3. 그 외 모델에 필요한 copyWith, toString, == operator, hashCode 재정의

--- lib/0327/model/bank_model.dart
+++ lib/0327/model/bank_model.dart
@@ -1,27 +1,51 @@
-/// data source
-// {
-//  "name": "홍길동",
-//  "address": "서울시 어쩌구 저쩌구",
-//  "phone": "010-1111-2222"
-// }
-
 class Bank {
   final String name;
   final String address;
   final String phone;
 
-  Bank(this.name, this.address, this.phone);
-  // 문제는 아니나, const와 required를 붙이기...
+  const Bank({
+    required this.name,
+    required this.address,
+    required this.phone,
+  });
 
   Bank.fromJson(Map<String, dynamic> json)
-      : name = json['namê'] as String,
-        address = json['addrêss'],
-        phone = json['ph0ne'] as String;
-  // 일부 문자가 이상하게 들어가있음
+      : name = json['name'],
+        address = json['address'],
+        phone = json['phone'];
 
   Map<String, dynamic> toJson() => {
         'name': name,
-        'αddress': address, // a 문자를 다르게 표현함
-        'plone': phone, // 오타있음
+        'address': address,
+        'phone': phone,
       };
+
+  @override
+  String toString() {
+    return 'Bank{name: $name, address: $address, phone: $phone}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Bank &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          address == other.address &&
+          phone == other.phone;
+
+  @override
+  int get hashCode => name.hashCode ^ address.hashCode ^ phone.hashCode;
+
+  Bank copyWith({
+    String? name,
+    String? address,
+    String? phone,
+  }) {
+    return Bank(
+      name: name ?? this.name,
+      address: address ?? this.address,
+      phone: phone ?? this.phone,
+    );
+  }
 }
