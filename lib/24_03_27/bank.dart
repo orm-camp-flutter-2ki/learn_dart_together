5989e4b6d7aa54666f24bc909167ea2c63a79439
YunZi.Song <yoonji5809@gmail.com>
2024-03-27 14:35:41 +0900

    Feat: 직렬/역직렬화 연습문제1 - 모델 클래스 6종 함수 추가

--- lib/24_03_27/bank.dart
+++ lib/24_03_27/bank.dart
@@ -10,11 +10,23 @@ class Bank {
   final String address;
   final String phone;
 
-  Bank({
+  const Bank({
     required this.name,
     required this.address,
     required this.phone,
   });
+
+  Bank copyWith({
+    String? name,
+    String? address,
+    String? phone,
+  }) {
+    return Bank(
+      name: name ?? this.name,
+      address: address ?? this.address,
+      phone: phone ?? this.phone,
+    );
+  }
 
   Bank.fromJson(Map<String, dynamic> json)
       : name = json['name'] as String,
@@ -26,4 +38,21 @@ class Bank {
         'address': address,
         'phone': phone,
       };
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is Bank &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          address == other.address &&
+          phone == other.phone);
+
+  @override
+  int get hashCode => name.hashCode ^ address.hashCode ^ phone.hashCode;
+
+  @override
+  String toString() {
+    return 'Bank{ name: $name, address: $address, phone: $phone,}';
+  }
 }
