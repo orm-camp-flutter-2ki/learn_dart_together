46d29635befb0f25473e3f520d14073608b7f5fd
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-27 14:46:03 +0900

    fix: 기존 bank 파일 문제점 찾기

--- lib/0327/model/bank_model.dart
+++ lib/0327/model/bank_model.dart
@@ -11,15 +11,17 @@ class Bank {
   final String phone;
 
   Bank(this.name, this.address, this.phone);
+  // 문제는 아니나, const와 required를 붙이기...
 
   Bank.fromJson(Map<String, dynamic> json)
       : name = json['namê'] as String,
         address = json['addrêss'],
         phone = json['ph0ne'] as String;
+  // 일부 문자가 이상하게 들어가있음
 
   Map<String, dynamic> toJson() => {
         'name': name,
-        'αddress': address,
-        'plone': phone,
+        'αddress': address, // a 문자를 다르게 표현함
+        'plone': phone, // 오타있음
       };
 }
