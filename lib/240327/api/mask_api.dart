52475b257e1ae47fa0a637cc3dc9a055aab70b00
KimDonghyeok <qoophon@gmail.com>
2024-03-27 23:46:59 +0900

    feat: MaskApi 클래스 작성 (연습문제 5)

---
+++ lib/240327/api/mask_api.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:dart_cli_practice/240327/model/mask.dart';
+import 'package:http/http.dart' as http;
+
+class MaskApi {
+  Future<Mask> getMaskInfo() async {
+    final http.Response response =
+        await http.get(Uri.parse('http://104.198.248.76:3000/mask'));
+
+    Map<String, dynamic> jsonList = (response.statusCode == 200)
+        ? jsonDecode(response.body)
+        : throw Exception('데이터 불러오기에 실패했습니다.');
+
+    return Mask.fromJson(jsonList);
+  }
+}
