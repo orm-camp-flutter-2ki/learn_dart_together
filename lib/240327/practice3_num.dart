77e49e2eb01014b1e0cb6082997132c5c9aa88b1
gyubit <gyubros@gmail.com>
2024-03-27 18:07:00 +0900

    EDIT: Practice 3_햠수 (조 리뷰 후)

--- lib/240327/practice3_num.dart
+++ lib/240327/practice3_num.dart
@@ -1,13 +1,9 @@
-Future<int> addNum(numList) async {
-  List<int> numList = [1, 2, 5, 3, 9, 8];
-
+Future<int> addNum(List<int> numList) async {
+  if (numList.length < 2) {
+    throw ArgumentError('숫자는 최소 2개 이상이 필요합니다');
+  }
   numList.sort((a, b) => b.compareTo(a));
   int add2Num = numList[0] + numList[1];
 
-  return add2Num; // 리스트를 정렬
+  return Future.value(add2Num); // Wrap result in a Future
 }
-
-// void main() async {
-//   int result = await addNum();
-//   print(result);
-// }
