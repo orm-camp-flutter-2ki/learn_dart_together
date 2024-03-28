9e2b010845f0cfd890ed4c2db5dbb73980e37cbc
SeongJongHo <whdgh9595@gmail.com>
2024-03-28 16:00:05 +0900

    [ADD] User 클래스 작성

---
+++ lib/24_03_28/model/user.dart
@@ -0,0 +1,27 @@
+class User {
+  final String name;
+  final String email;
+  final num latitude;
+  final num longitude;
+
+  User({required this.name, required this.email, required this.latitude, required this.longitude});
+
+  @override
+  String toString() {
+    return 'User{name: $name, email: $email, latitude: $latitude, longitude: $longitude}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is User &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          email == other.email &&
+          latitude == other.latitude &&
+          longitude == other.longitude;
+
+  @override
+  int get hashCode =>
+      name.hashCode ^ email.hashCode ^ latitude.hashCode ^ longitude.hashCode;
+}
