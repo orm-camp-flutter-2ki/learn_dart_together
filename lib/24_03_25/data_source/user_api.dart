d09fc053cd03b2dcfc60f9c949bc9e016c59d29d
JEON-Sungsu <anagma@naver.com>
2024-03-28 17:24:37 +0900

    refactor: User Model, Api 메서드 수정

--- lib/24_03_25/data_source/user_api.dart
+++ lib/24_03_25/data_source/user_api.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:learn_dart_together/24_03_25/DTO/user.dart';
+import 'package:learn_dart_together/24_03_28/dto/user_dto.dart';
 
 class UserApi {
-  Future<List<User>> getUsers() async {
+  Future<List<UserDto>> getUserDtoList() async {
     final http.Response response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
     final jsonList = jsonDecode(response.body) as List;
-    final List<User> userList = jsonList.map((e) => User.fromJson(e)).toList();
+    final List<UserDto> userList = jsonList.map((e) => UserDto.fromJson(e)).toList();
 
     return userList;
   }
