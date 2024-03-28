b8ec698131ea5b547a4f027103fa8e1e2dca1e89
SeongJongHo <whdgh9595@gmail.com>
2024-03-28 16:18:28 +0900

    [ADD] UserRepositoryImpl 및 UserRepository 작성

---
+++ lib/24_03_28/repository/impl/user_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'package:learn_dart_together/24_03_28/data_source/user_api.dart';
+import 'package:learn_dart_together/24_03_28/model/user.dart';
+import 'package:learn_dart_together/24_03_28/repository/interface/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final UserApi _userApi;
+
+  UserRepositoryImpl({required UserApi userApi}) : _userApi = userApi;
+
+  @override
+  Future<List<User>> getUsers() async {
+    return _userApi.getUsers();
+  }
+}
