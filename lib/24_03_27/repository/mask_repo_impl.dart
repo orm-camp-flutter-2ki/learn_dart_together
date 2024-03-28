1eb87e356a732421bafc2a25e6986587f3b5b08a
kimhyungmin <jeen14rla@gmail.com>
2024-03-28 11:48:19 +0900

    add: MaskRepositoryImpl 수정

--- lib/24_03_27/repository/mask_repo_impl.dart
+++ lib/24_03_27/repository/mask_repo_impl.dart
@@ -3,8 +3,12 @@ import 'package:learn_dart_together/24_03_27/model/mask.dart';
 import 'package:learn_dart_together/24_03_27/repository/mask_repository.dart';
 
 class MaskRepositoryImpl implements MaskRepository {
+  final MaskApi _api;
+
+  MaskRepositoryImpl(this._api);
+
   @override
   Future<Mask> getMask() async {
-    return await MaskApi().getMask();
+    return await _api.getMask();
   }
 }
--- test/24_03_27/mask_test.dart
+++ test/24_03_27/mask_test.dart
@@ -1,4 +1,5 @@
 import 'package:collection/collection.dart';
+import 'package:learn_dart_together/24_03_27/data_source/mask_api.dart';
 import 'package:learn_dart_together/24_03_27/model/mask.dart';
 import 'package:learn_dart_together/24_03_27/model/store.dart';
 import 'package:learn_dart_together/24_03_27/repository/mask_repo_impl.dart';
@@ -9,7 +10,7 @@ import 'mask_map.dart';
 
 void main() {
   test('mask test', () async {
-    Mask mask = await MaskRepositoryImpl().getMask();
+    Mask mask = await MaskRepositoryImpl(MaskApi()).getMask();
 
     final expectedData = expected.map((e) => Store.fromJson(e)).toList();
 
