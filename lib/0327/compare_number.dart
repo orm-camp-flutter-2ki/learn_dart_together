6f96a6d07ddf5a9d1168d97401ce6a888f4c1292
Jiyeong Yu <jiyeong.yu.kr@gmail.com>
2024-03-28 09:43:09 +0900

    fix: compare_number 수정

--- lib/0327/compare_number.dart
+++ lib/0327/compare_number.dart
@@ -3,6 +3,6 @@ import 'package:collection/collection.dart';
 int compareNumber(List<int> intList) {
   return intList
       .sorted((a, b) => a.compareTo(b) * -1)
-      .sublist(0, 2)
+      .take(2)
       .reduce((value, element) => value + element);
 }
