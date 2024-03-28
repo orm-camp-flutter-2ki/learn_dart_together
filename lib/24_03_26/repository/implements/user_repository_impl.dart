3c033d1097fdaaf681edbd183c27c042acef8e63
JEON-Sungsu <anagma@naver.com>
2024-03-28 17:25:21 +0900

    refactor: User Repository 수정

--- lib/24_03_26/repository/implements/user_repository_impl.dart
+++ lib/24_03_26/repository/implements/user_repository_impl.dart
@@ -2,22 +2,26 @@ import 'package:learn_dart_together/24_03_25/DTO/user.dart';
 import 'package:learn_dart_together/24_03_25/data_source/user_api.dart';
 import 'package:learn_dart_together/24_03_26/repository/interface/user_repository.dart';
 import 'package:collection/collection.dart';
+import 'package:learn_dart_together/24_03_28/dto/user_dto.dart';
+import 'package:learn_dart_together/24_03_28/mapper/user_mapper.dart';
 
 class UserRepositoryImpl implements UserRepository {
   final UserApi _api = UserApi();
 
   @override
-  Future<List<User>> getUsers() async {
-    final list = await _api.getUsers();
-    return list;
+  Future<List<User>> getUsersList() async {
+    final userDto = await _api.getUserDtoList();
+    final userList = userDto.map((e) => e.transferDtoUser()).toList();
+
+    return userList;
   }
 
   @override
   Future<List<User>> getUserTop10ByUserName() async {
-    final list = await _api.getUsers();
-    final filtered = list.sorted((a, b) => a.name.compareTo(b.name)).sublist(0, 10);
+    final userDto = await _api.getUserDtoList();
+    final userList = userDto.map((e) => e.transferDtoUser()).toList();
+    final filtered = userList.sorted((a, b) => a.name.compareTo(b.name)).sublist(0, 10);
 
     return filtered;
   }
-
 }
