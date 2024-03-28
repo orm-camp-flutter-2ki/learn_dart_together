91a051680c31896edcb81939e21a98b29dbc1e7d
SeongJongHo <whdgh9595@gmail.com>
2024-03-28 16:14:28 +0900

    [ADD] UserApiImpl 및 UserApi 작성

---
+++ lib/24_03_28/data_source/user_api_impl.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:learn_dart_together/24_03_25/http_proxy.dart';
+import 'package:learn_dart_together/24_03_28/data_source/user_api.dart';
+import 'package:learn_dart_together/24_03_28/dto/user_dto.dart';
+import 'package:learn_dart_together/24_03_28/mapper/user_mapper.dart';
+import 'package:learn_dart_together/24_03_28/model/user.dart';
+
+class UserApiImpl implements UserApi {
+  // 상수라서 정적으로 메모리에 한번만 올리고 싶어 static으로 선언하였습니다.
+  static const _baseUrl = 'https://jsonplaceholder.typicode.com';
+  final HttpProxy _http;
+
+  UserApiImpl({required http}) : _http = http;
+
+  @override
+  Future<List<User>> getUsers() async {
+    final response = await _http.get(Uri.parse('$_baseUrl/users'));
+    return (jsonDecode(response.body) as List)
+        .map((e) => UserDto.fromJson(e as Map<String, dynamic>).toUser())
+        .toList();
+  }
+}
