0ad0b6b095a87473e7cbdceab0ec388155e6251f
chojja7188 <shoun7188@naver.com>
2024-03-28 21:38:50 +0900

    feat: user_dto 수정

--- lib/240328/dto/user/user_dto.dart
+++ lib/240328/dto/user/user_dto.dart
@@ -1,29 +1,51 @@
-class UserDto {
-  String? name;
-  String? email;
-  double? latitude;
-  double? longitude;
+import 'address_dto.dart';
+import 'company_dto.dart';
 
+class UserDto {
   UserDto({
-    this.name,
-    this.email,
-    this.latitude,
-    this.longitude
-  });
+      this.id, 
+      this.name, 
+      this.username, 
+      this.email, 
+      this.address, 
+      this.phone, 
+      this.website, 
+      this.company,});
 
   UserDto.fromJson(dynamic json) {
+    id = json['id'];
     name = json['name'];
+    username = json['username'];
     email = json['email'];
-    latitude = double.parse(json['address']['geo']['lat']);
-    longitude = double.parse(json['address']['geo']['lng']);
+    address = json['address'] != null ? AddressDto.fromJson(json['address']) : null;
+    phone = json['phone'];
+    website = json['website'];
+    company = json['company'] != null ? CompanyDto.fromJson(json['company']) : null;
   }
+  num? id;
+  String? name;
+  String? username;
+  String? email;
+  AddressDto? address;
+  String? phone;
+  String? website;
+  CompanyDto? company;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
+    map['id'] = id;
     map['name'] = name;
+    map['username'] = username;
     map['email'] = email;
-    map['latitude'] = latitude;
-    map['longitude'] = longitude;
+    if (address != null) {
+      map['address'] = address?.toJson();
+    }
+    map['phone'] = phone;
+    map['website'] = website;
+    if (company != null) {
+      map['company'] = company?.toJson();
+    }
     return map;
   }
+
 }
