1adcee433292cf4e391b5e9ed10d70458cee0d21
kimhyungmin <jeen14rla@gmail.com>
2024-03-30 00:03:41 +0900

    feat: movie_info 작성

---
+++ lib/24_03_29/model/movie_info.dart
@@ -0,0 +1,58 @@
+class MovieInfo {
+  final int id;
+  final String overview;
+  final String releaseDate;
+  final String title;
+  final double voteAverage;
+
+//<editor-fold desc="Data Methods">
+  const MovieInfo({
+    required this.id,
+    required this.overview,
+    required this.releaseDate,
+    required this.title,
+    required this.voteAverage,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is MovieInfo &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          overview == other.overview &&
+          releaseDate == other.releaseDate &&
+          title == other.title &&
+          voteAverage == other.voteAverage);
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      overview.hashCode ^
+      releaseDate.hashCode ^
+      title.hashCode ^
+      voteAverage.hashCode;
+
+  @override
+  String toString() {
+    return 'MovieInfo{ id: $id, overview: $overview, releaseDate: $releaseDate, title: $title, voteAverage: $voteAverage,}';
+  }
+
+  MovieInfo copyWith({
+    int? id,
+    String? overview,
+    String? releaseDate,
+    String? title,
+    double? voteAverage,
+  }) {
+    return MovieInfo(
+      id: id ?? this.id,
+      overview: overview ?? this.overview,
+      releaseDate: releaseDate ?? this.releaseDate,
+      title: title ?? this.title,
+      voteAverage: voteAverage ?? this.voteAverage,
+    );
+  }
+
+//</editor-fold>
+}
