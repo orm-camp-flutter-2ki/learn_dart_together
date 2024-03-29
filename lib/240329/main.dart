440f4e3d799a9bfd6118911550c1e29c5604ec98
KimDonghyeok <qoophon@gmail.com>
2024-03-30 00:58:34 +0900

    chore: main 함수 수정 (연습문제 2)
    - MovieDetail 모델에 담고 JSON 형태로 출력 확인

--- lib/240329/main.dart
+++ lib/240329/main.dart
@@ -5,14 +5,22 @@ import 'package:dart_cli_practice/240329/repository/movie_repository.dart';
 import 'package:dart_cli_practice/240329/repository/movie_repository_impl.dart';
 
 void main() async {
+  JsonEncoder encoder = JsonEncoder.withIndent(' ');
+
   // 다형성을 이용함
   MovieRepository repository = MovieRepositoryImpl();
 
-  final movieList = await repository.getMovieInfoList();
+  // 연습문제 1
+  final movieList = await repository.getMovieInfoList(); // json -> movie
+  final movieListDto =
+      movieList.map((e) => e.toMovieDto()).toList(); // movie -> json
+  final movieListJsonString = encoder.convert(movieListDto);
 
-  JsonEncoder encoder = JsonEncoder.withIndent(' ');
+  print(movieListJsonString.runtimeType); // String
+  print(movieListJsonString); // pretty-print json string
 
-  final json = movieList.map((e) => e.toMovieDto()).toList();
-
-  print(encoder.convert(json));
+  // 연습문제 2
+  final movieDetail = await repository.getMovieDetail(1011985);
+  final movieDetailJson = movieDetail.toJson();
+  print(encoder.convert(movieDetailJson));
 }
