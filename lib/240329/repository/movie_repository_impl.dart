a0de7884936baed0333d72208a8c8d8d4213fa34
KimDonghyeok <qoophon@gmail.com>
2024-03-30 00:57:47 +0900

    feat: MovieRepositoryImpl 수정 (연습문제 2)
    - getMovieDetail() 추상 메소드 구현

--- lib/240329/repository/movie_repository_impl.dart
+++ lib/240329/repository/movie_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dart_cli_practice/240329/data_source/movie_api.dart';
 import 'package:dart_cli_practice/240329/mapper/movie_mapper.dart';
 import 'package:dart_cli_practice/240329/model/movie.dart';
+import 'package:dart_cli_practice/240329/model/movie_detail.dart';
 import 'package:dart_cli_practice/240329/repository/movie_repository.dart';
 
 class MovieRepositoryImpl implements MovieRepository {
@@ -16,5 +17,10 @@ class MovieRepositoryImpl implements MovieRepository {
     final List<Movie> movieList = movieDtoList.map((e) => e.toMovie()).toList();
 
     return movieList;
+  }
+
+  @override
+  Future<MovieDetail> getMovieDetail(int id) async {
+    return await _api.getMovieDetail(id);
   }
 }
