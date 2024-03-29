d676ee5d0c67fba36715d54b87d130918f9d5fce
tomoyo519 <vanessa.cheong1@gmail.com>
2024-03-29 16:57:01 +0900

    [feat] serilization 할때 에러나는 파일들 수정

--- lib/정다희/24_03_11/people.dart
+++ lib/정다희/24_03_11/people.dart
@@ -1,11 +1,11 @@
 class Person {
-
   final String _name;
-  final int _birthYear
+  final int _birthYear;
   int _age;
 
   get age => DateTIme.now().year - _birthYear;
 
-  Person({ required this.name, required this.birthYear}) : _name = name, _birthYear = birthYear
-
+  Person({required this.name, required this.birthYear})
+      : _name = name,
+        _birthYear = birthYear;
 }
---
+++ lib/정다희/24_03_28/dto/Address.dart
@@ -0,0 +1,42 @@
+import 'Geo.dart';
+
+/// street : "Kulas Light"
+/// suite : "Apt. 556"
+/// city : "Gwenborough"
+/// zipcode : "92998-3874"
+/// geo : {"lat":"-37.3159","lng":"81.1496"}
+
+class Address {
+  Address({
+    this.street,
+    this.suite,
+    this.city,
+    this.zipcode,
+    this.geo,
+  });
+
+  Address.fromJson(dynamic json) {
+    street = json['street'];
+    suite = json['suite'];
+    city = json['city'];
+    zipcode = json['zipcode'];
+    geo = json['geo'] != null ? Geo.fromJson(json['geo']) : null;
+  }
+  String? street;
+  String? suite;
+  String? city;
+  String? zipcode;
+  Geo? geo;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['street'] = street;
+    map['suite'] = suite;
+    map['city'] = city;
+    map['zipcode'] = zipcode;
+    if (geo != null) {
+      map['geo'] = geo?.toJson();
+    }
+    return map;
+  }
+}
