9a89f009a2af4797f1e38ec3282374a3a63ffc7a
SeongJongHo <whdgh9595@gmail.com>
2024-03-29 19:17:04 +0900

    [ADD] Movie Repository에 getMovieDetail메소드 추가

--- lib/24_03_29/repository/impl/movie_info_repository_impl.dart
+++ lib/24_03_29/repository/impl/movie_info_repository_impl.dart
@@ -1,5 +1,7 @@
 import 'package:learn_dart_together/24_03_29/api/interface/movie_info_api.dart';
+import 'package:learn_dart_together/24_03_29/mapper/movie_detail_mapper.dart';
 import 'package:learn_dart_together/24_03_29/mapper/movie_info_mapper.dart';
+import 'package:learn_dart_together/24_03_29/model/movie_detail.dart';
 import 'package:learn_dart_together/24_03_29/model/movie_info.dart';
 import 'package:learn_dart_together/24_03_29/repository/interface/movie_info_repository.dart';
 
@@ -13,5 +15,12 @@ class MovieInfoRepositoryImpl implements MovieInfoRepository {
     final movieInfoDtoList = await _movieInfoApi.getMovieInfoList();
 
     return movieInfoDtoList.map((e) => e.toMovieInfo()).toList();
+  }
+
+  @override
+  Future<MovieDetail> getMovieDetail(int movieId) async {
+    final movieDetailDto = await _movieInfoApi.getMovieDetail(movieId);
+
+    return movieDetailDto.toMovieDetail();
   }
 }
