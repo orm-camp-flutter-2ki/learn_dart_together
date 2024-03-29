f45debfbbe56eda5c52b1e9d780cf89561de860b
hee462 <a01080809887@gmail.com>
2024-03-29 17:43:25 +0900

    feat:  연습문제 2번 test 완료

---
+++ lib/test/240329/movie_test.dart
@@ -0,0 +1,17 @@
+import 'package:learn_dart_together/240329/repository/movie_repository_Impl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('movie list test', () async {
+    final repository = MovieRepositoryImpl();
+    final result = await repository.getMovieInfoList();
+    expect(result.first.id, 1011985);
+    expect(result.length, 20);
+  });
+  test('movie id search test', () async {
+    final repository = MovieRepositoryImpl();
+    final result = await repository.getMovie(1011985);
+    // print(result);
+    expect(result.id, 1011985);
+  });
+}
