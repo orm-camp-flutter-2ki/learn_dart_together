6545555f9e402aa5be4b9ec88e5676a5ccd373d1
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-03-29 16:15:04 +0900

    refactor: json_serializable 적용

--- lib/240326/model/post.dart
+++ lib/240326/model/post.dart
@@ -1,10 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'post.g.dart';
+
+@JsonSerializable(explicitToJson: true)
 class Post {
   final int userId;
   final int id;
   final String title;
   final String body;
 
-//<editor-fold desc="Data Methods">
   const Post({
     required this.userId,
     required this.id,
@@ -12,56 +16,7 @@ class Post {
     required this.body,
   });
 
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      (other is Post &&
-          runtimeType == other.runtimeType &&
-          userId == other.userId &&
-          id == other.id &&
-          title == other.title &&
-          body == other.body);
+  factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
 
-  @override
-  int get hashCode =>
-      userId.hashCode ^ id.hashCode ^ title.hashCode ^ body.hashCode;
-
-  @override
-  String toString() {
-    return 'Post{ userId: $userId, id: $id, title: $title, body: $body,}';
-  }
-
-  Post copyWith({
-    int? userId,
-    int? id,
-    String? title,
-    String? body,
-  }) {
-    return Post(
-      userId: userId ?? this.userId,
-      id: id ?? this.id,
-      title: title ?? this.title,
-      body: body ?? this.body,
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'userId': userId,
-      'id': id,
-      'title': title,
-      'body': body,
-    };
-  }
-
-  factory Post.fromJson(Map<String, dynamic> map) {
-    return Post(
-      userId: map['userId'] as int,
-      id: map['id'] as int,
-      title: map['title'] as String,
-      body: map['body'] as String,
-    );
-  }
-
-//</editor-fold>
+  Map<String, dynamic> toJson() => _$PostToJson(this);
 }
