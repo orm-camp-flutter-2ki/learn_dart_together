d148af07644ff12ab795cde3c363307591099bb6
Yejin.Shin <mingmi_27@naver.com>
2024-03-29 21:04:21 +0900

    feat: movieDetail 코드작성

---
+++ lib/240329_homework/dto/movieDetail_dto.dart
@@ -0,0 +1,279 @@
+class MovieDetailDto {
+  bool? adult;
+  String? backdropPath;
+  BelongsToCollectionDto? belongsToCollection;
+  num? budget;
+  List<GenresDto>? genres;
+  String? homepage;
+  num? id;
+  String? imdbId;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  num? popularity;
+  String? posterPath;
+  List<ProductionCompaniesDto>? productionCompanies;
+  List<ProductionCountriesDto>? productionCountries;
+  String? releaseDate;
+  num? revenue;
+  num? runtime;
+  List<SpokenLanguagesDto>? spokenLanguages;
+  String? status;
+  String? tagline;
+  String? title;
+  bool? video;
+  num? voteAverage;
+  num? voteCount;
+
+  MovieDetailDto({
+    this.adult,
+    this.backdropPath,
+    this.belongsToCollection,
+    this.budget,
+    this.genres,
+    this.homepage,
+    this.id,
+    this.imdbId,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.productionCompanies,
+    this.productionCountries,
+    this.releaseDate,
+    this.revenue,
+    this.runtime,
+    this.spokenLanguages,
+    this.status,
+    this.tagline,
+    this.title,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+
+  MovieDetailDto.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    belongsToCollection = json['belongs_to_collection'] != null
+        ? BelongsToCollectionDto.fromJson(
+            json['belongs_to_collection'] as Map<String, dynamic>)
+        : null;
+    budget = json['budget'];
+    if (json['genres'] != null) {
+      genres = [];
+      json['genres'].forEach((v) {
+        genres?.add(GenresDto.fromJson(v));
+      });
+    }
+    homepage = json['homepage'];
+    id = json['id'];
+    imdbId = json['imdb_id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'];
+    posterPath = json['poster_path'];
+    if (json['production_companies'] != null) {
+      productionCompanies = [];
+      json['production_companies'].forEach((v) {
+        productionCompanies?.add(ProductionCompaniesDto.fromJson(v));
+      });
+    }
+    if (json['production_countries'] != null) {
+      productionCountries = [];
+      json['production_countries'].forEach((v) {
+        productionCountries?.add(ProductionCountriesDto.fromJson(v));
+      });
+    }
+    releaseDate = json['release_date'];
+    revenue = json['revenue'];
+    runtime = json['runtime'];
+    if (json['spoken_languages'] != null) {
+      spokenLanguages = [];
+      json['spoken_languages'].forEach((v) {
+        spokenLanguages?.add(SpokenLanguagesDto.fromJson(v));
+      });
+    }
+    status = json['status'];
+    tagline = json['tagline'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['adult'] = adult;
+    map['backdrop_path'] = backdropPath;
+    if (belongsToCollection != null) {
+      map['belongs_to_collection'] = belongsToCollection?.toJson();
+    }
+    map['budget'] = budget;
+    if (genres != null) {
+      map['genres'] = genres?.map((v) => v.toJson()).toList();
+    }
+    map['homepage'] = homepage;
+    map['id'] = id;
+    map['imdb_id'] = imdbId;
+    map['original_language'] = originalLanguage;
+    map['original_title'] = originalTitle;
+    map['overview'] = overview;
+    map['popularity'] = popularity;
+    map['poster_path'] = posterPath;
+    if (productionCompanies != null) {
+      map['production_companies'] =
+          productionCompanies?.map((v) => v.toJson()).toList();
+    }
+    if (productionCountries != null) {
+      map['production_countries'] =
+          productionCountries?.map((v) => v.toJson()).toList();
+    }
+    map['release_date'] = releaseDate;
+    map['revenue'] = revenue;
+    map['runtime'] = runtime;
+    if (spokenLanguages != null) {
+      map['spoken_languages'] =
+          spokenLanguages?.map((v) => v.toJson()).toList();
+    }
+    map['status'] = status;
+    map['tagline'] = tagline;
+    map['title'] = title;
+    map['video'] = video;
+    map['vote_average'] = voteAverage;
+    map['vote_count'] = voteCount;
+    return map;
+  }
+}
+
+class SpokenLanguagesDto {
+  SpokenLanguagesDto({
+    this.englishName,
+    this.iso6391,
+    this.name,
+  });
+
+  SpokenLanguagesDto.fromJson(dynamic json) {
+    englishName = json['english_name'];
+    iso6391 = json['iso_639_1'];
+    name = json['name'];
+  }
+
+  String? englishName;
+  String? iso6391;
+  String? name;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['english_name'] = englishName;
+    map['iso_639_1'] = iso6391;
+    map['name'] = name;
+    return map;
+  }
+}
+
+class ProductionCountriesDto {
+  ProductionCountriesDto({
+    this.iso31661,
+    this.name,
+  });
+
+  ProductionCountriesDto.fromJson(dynamic json) {
+    iso31661 = json['iso_3166_1'];
+    name = json['name'];
+  }
+
+  String? iso31661;
+  String? name;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['iso_3166_1'] = iso31661;
+    map['name'] = name;
+    return map;
+  }
+}
+
+class ProductionCompaniesDto {
+  ProductionCompaniesDto({
+    this.id,
+    this.logoPath,
+    this.name,
+    this.originCountry,
+  });
+
+  ProductionCompaniesDto.fromJson(dynamic json) {
+    id = json['id'];
+    logoPath = json['logo_path'];
+    name = json['name'];
+    originCountry = json['origin_country'];
+  }
+
+  num? id;
+  String? logoPath;
+  String? name;
+  String? originCountry;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['logo_path'] = logoPath;
+    map['name'] = name;
+    map['origin_country'] = originCountry;
+    return map;
+  }
+}
+
+class GenresDto {
+  GenresDto({
+    this.id,
+    this.name,
+  });
+
+  GenresDto.fromJson(dynamic json) {
+    id = json['id'];
+    name = json['name'];
+  }
+
+  num? id;
+  String? name;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['name'] = name;
+    return map;
+  }
+}
+
+class BelongsToCollectionDto {
+  num? id;
+  String? name;
+  String? posterPath;
+  String? backdropPath;
+
+  BelongsToCollectionDto({
+    this.id,
+    this.name,
+    this.posterPath,
+    this.backdropPath,
+  });
+
+  BelongsToCollectionDto.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    posterPath = json['poster_path'];
+    backdropPath = json['backdrop_path'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['name'] = name;
+    map['poster_path'] = posterPath;
+    map['backdrop_path'] = backdropPath;
+    return map;
+  }
+}
