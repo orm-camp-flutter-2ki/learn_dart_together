7120c06122adba095fb84a741532c944c7987ee5
KimDonghyeok <qoophon@gmail.com>
2024-03-30 00:57:36 +0900

    feat: MovieRepository 수정 (연습문제 2)
    - getMovieDetail() 추상 메소드 추가

--- lib/240329/repository/movie_repository.dart
+++ lib/240329/repository/movie_repository.dart
@@ -1,5 +1,8 @@
 import 'package:dart_cli_practice/240329/model/movie.dart';
+import 'package:dart_cli_practice/240329/model/movie_detail.dart';
 
 abstract interface class MovieRepository {
   Future<List<Movie>> getMovieInfoList();
+
+  Future<MovieDetail> getMovieDetail(int id);
 }
