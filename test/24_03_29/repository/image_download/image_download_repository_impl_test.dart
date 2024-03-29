65c3abeb6279ffd393b536ef06ce0b465c8a5657
JEON-Sungsu <anagma@naver.com>
2024-03-29 18:05:28 +0900

    feat: 연습문제 5번 완료

--- test/24_03_29/repository/image_download/image_download_repository_impl_test.dart
+++ test/24_03_29/repository/image_download/image_download_repository_impl_test.dart
@@ -23,8 +23,32 @@ void main() {
       final File file = await ImageDownloadRepositoryImpl().saveFile(bytes, "icon2.ico", path);
       print('다운로드 끝');
       print('=========');
-      print('소요시간 : ${stopwatch.elapsed}');
+      print('소요시간 : ${stopwatch.elapsed}'); //결과 0:00:00.068987
       print('용량 : ${file.lengthSync()}');
+    });
+
+    test('파일 3개 순차 다운로드 시간 체크', () async {
+      final stopwatch = Stopwatch()..start();
+      print('다운로드 시작');
+      final Uint8List bytes = await ImageDownloadApi().downloadImage(imageUrl);
+      final File file = await ImageDownloadRepositoryImpl().saveFile(bytes, "icon1.ico", path);
+      final File file2 = await ImageDownloadRepositoryImpl().saveFile(bytes, "icon2.ico", path);
+      final File file3 = await ImageDownloadRepositoryImpl().saveFile(bytes, "icon3.ico", path);
+      print('다운로드 끝');
+      print('=========');
+      print('소요시간 : ${stopwatch.elapsed}'); //결과 0:00:00.069860
+    });
+
+    test('파일 3개 병렬 다운로드 시간 체크', () async {
+      final stopwatch = Stopwatch()..start();
+      print('다운로드 시작');
+      final Uint8List bytes = await ImageDownloadApi().downloadImage(imageUrl);
+      ImageDownloadRepositoryImpl().saveFile(bytes, "icon1.ico", path);
+      ImageDownloadRepositoryImpl().saveFile(bytes, "icon2.ico", path);
+      ImageDownloadRepositoryImpl().saveFile(bytes, "icon3.ico", path);
+      print('다운로드 끝');
+      print('=========');
+      print('소요시간 : ${stopwatch.elapsed}'); //결과 0:00:00.069253
     });
   });
 }
