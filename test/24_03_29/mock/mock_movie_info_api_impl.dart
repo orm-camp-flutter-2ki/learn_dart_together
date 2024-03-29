11f01d07b8a5a0ad401de00d977fed2edc58ed75
SeongJongHo <whdgh9595@gmail.com>
2024-03-29 19:24:48 +0900

    [ADD] Movie Repository getMovieDetail메소드 테스트코드 추가

--- test/24_03_29/mock/mock_movie_info_api_impl.dart
+++ test/24_03_29/mock/mock_movie_info_api_impl.dart
@@ -1,5 +1,11 @@
+import 'package:learn_dart_together/24_03_28/dto/geo_dto.dart';
 import 'package:learn_dart_together/24_03_29/api/interface/movie_info_api.dart';
+import 'package:learn_dart_together/24_03_29/dto/belongs_to_collection_dto.dart';
+import 'package:learn_dart_together/24_03_29/dto/genre_dto.dart';
+import 'package:learn_dart_together/24_03_29/dto/movie_detail_dto.dart';
 import 'package:learn_dart_together/24_03_29/dto/movie_info_dto.dart';
+import 'package:learn_dart_together/24_03_29/dto/production_company_dto.dart';
+import 'package:learn_dart_together/24_03_29/dto/production_country_dto.dart';
 import 'package:mockito/mockito.dart';
 
 class MockMovieInfoApiImpl extends Mock implements MovieInfoApi {
@@ -22,9 +28,63 @@ class MockMovieInfoApiImpl extends Mock implements MovieInfoApi {
     ),
   ];
 
+  final movieDetail = MovieDetailDto(
+    adult: false,
+    backdropPath: '/backdropPath',
+    belongsToCollection: BelongsToCollectionDto(
+      id: 1,
+      name: 'name',
+      posterPath: '/posterPath',
+      backdropPath: '/backdropPath',
+    ),
+    budget: 0,
+    genres: [
+      GenreDto(id: 1, name: '홍길동'),
+      GenreDto(id: 2, name: '임꺽정'),
+    ],
+    homepage: 'homepage',
+    id: 1,
+    imdbId: 'imdbId',
+    originalLanguage: 'en',
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1.0,
+    posterPath: '/posterPath',
+    productionCompanies: [
+      ProductionCompanyDto(
+        id: 1,
+        logoPath: '/logoPath',
+        name: 'name',
+        originCountry: 'KR',
+      )
+    ],
+    productionCountries: [
+      ProductionCountryDto(
+        iso31661: 'KR',
+        name: 'name',
+      )
+    ],
+    releaseDate: '2021-01-01',
+    revenue: 0,
+    runtime: 0,
+    spokenLanguages: null,
+    status: 'status',
+    tagline: 'tagline',
+    title: 'title',
+    video: false,
+    voteAverage: 1.0,
+    voteCount: 1,
+  );
+
   @override
   Future<List<MovieInfoDto>> getMovieInfoList() async =>
       super.noSuchMethod(Invocation.getter(#getMovieInfoList),
           returnValue: movieInfoDtoList,
           returnValueForMissingStub: movieInfoDtoList);
+
+  @override
+  Future<MovieDetailDto> getMovieDetail(int movieId) async =>
+      super.noSuchMethod(Invocation.method(#getMovieDetail, [movieId]),
+          returnValue: movieDetail,
+          returnValueForMissingStub: movieDetail);
 }
--- test/24_03_29/repository/movie_info_repository_impl_test.dart
+++ test/24_03_29/repository/movie_info_repository_impl_test.dart
@@ -1,5 +1,11 @@
 import 'package:collection/collection.dart';
+import 'package:learn_dart_together/24_03_29/dto/belongs_to_collection_dto.dart';
+import 'package:learn_dart_together/24_03_29/dto/genre_dto.dart';
+import 'package:learn_dart_together/24_03_29/dto/movie_detail_dto.dart';
 import 'package:learn_dart_together/24_03_29/dto/movie_info_dto.dart';
+import 'package:learn_dart_together/24_03_29/dto/production_company_dto.dart';
+import 'package:learn_dart_together/24_03_29/dto/production_country_dto.dart';
+import 'package:learn_dart_together/24_03_29/mapper/movie_detail_mapper.dart';
 import 'package:learn_dart_together/24_03_29/mapper/movie_info_mapper.dart';
 import 'package:learn_dart_together/24_03_29/repository/impl/movie_info_repository_impl.dart';
 import 'package:mockito/mockito.dart';
@@ -29,6 +35,53 @@ void main() {
         voteCount: 1,
       ),
     ];
+    final movieDetail = MovieDetailDto(
+      adult: false,
+      backdropPath: '/backdropPath',
+      belongsToCollection: BelongsToCollectionDto(
+        id: 1,
+        name: 'name',
+        posterPath: '/posterPath',
+        backdropPath: '/backdropPath',
+      ),
+      budget: 0,
+      genres: [
+        GenreDto(id: 1, name: '홍길동'),
+        GenreDto(id: 2, name: '임꺽정'),
+      ],
+      homepage: 'homepage',
+      id: 1,
+      imdbId: 'imdbId',
+      originalLanguage: 'en',
+      originalTitle: 'originalTitle',
+      overview: 'overview',
+      popularity: 1.0,
+      posterPath: '/posterPath',
+      productionCompanies: [
+        ProductionCompanyDto(
+          id: 1,
+          logoPath: '/logoPath',
+          name: 'name',
+          originCountry: 'KR',
+        )
+      ],
+      productionCountries: [
+        ProductionCountryDto(
+          iso31661: 'KR',
+          name: 'name',
+        )
+      ],
+      releaseDate: '2021-01-01',
+      revenue: 0,
+      runtime: 0,
+      spokenLanguages: null,
+      status: 'status',
+      tagline: 'tagline',
+      title: 'title',
+      video: false,
+      voteAverage: 1.0,
+      voteCount: 1,
+    );
     setUp(() {
       reset(movieInfoApi);
     });
@@ -60,6 +113,34 @@ void main() {
         //
         // // Then
         verify(movieInfoApi.getMovieInfoList()).called(1);
+      });
+    });
+
+    group('getMovieDetail 메소드는', () {
+      test(
+          'api.getMovieDetail를 호출하고 받은 MovieDetailDto를 MovieDetail로 변환해서 인스턴스를 반환한다.',
+          () async {
+        // Given
+        when(movieInfoApi.getMovieDetail(1))
+            .thenAnswer((_) async => movieDetail);
+
+        // When
+        final result = await movieInfoRepository.getMovieDetail(1);
+
+        // Then
+        expect(result, movieDetail.toMovieDetail());
+      });
+
+      test('api.getMovieDetail를 1번 호출한다.', () async {
+        // Given
+        when(movieInfoApi.getMovieDetail(1))
+            .thenAnswer((_) async => movieDetail);
+
+        // When
+        await movieInfoRepository.getMovieDetail(1);
+        //
+        // // Then
+        verify(movieInfoApi.getMovieDetail(1)).called(1);
       });
     });
   });
