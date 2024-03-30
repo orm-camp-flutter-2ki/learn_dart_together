f15b12b42f9b846f8111ef4f8599cf239bd181a1
itbebop <it.bebop@gmail.com>
2024-03-30 14:29:51 +0900

    feat: 동기/비동기 파일 다운로드 속도 비교

--- lib/240329/data_source/photo_api.dart
+++ lib/240329/data_source/photo_api.dart
@@ -23,9 +23,41 @@ class PhotoApi {
     return uint8list;
   }
 
+  Future<Uint8List> downloadImages(String url) async {
+    stopwatch.start();
+    print('다운로드 시작');
+    final response = await _client.get(Uri.parse(url)).onError((error, stackTrace) => throw Exception('Error:$error'));
+    print('다운로드 끝');
+    stopwatch.stop();
+    print('===========');
+    print('소요시간 :  ${stopwatch.elapsed}');
+    print('용량 : ${response.contentLength}bytes');
+    final uint8list = response.statusCode == 200 ? (response.bodyBytes) : throw Exception('statusCode:${response.statusCode}');
+
+    return uint8list;
+  }
+
   Future<File> saveFile(Uint8List bytes, String fileName) async {
     File file = await File('lib/240329/image/$fileName').create();
     file.writeAsBytes(bytes);
     return file;
+  }
+
+  Future<void> synchronousPicture() async {
+    PhotoApi photoApi2 = PhotoApi();
+    await photoApi2.downloadImages('https://alimipro.com/favicon.ico');
+    PhotoApi photoApi3 = PhotoApi();
+    await photoApi3.downloadImages('https://alimipro.com/favicon.ico');
+    PhotoApi photoApi4 = PhotoApi();
+    await photoApi4.downloadImages('https://alimipro.com/favicon.ico');
+  }
+
+  Future<void> asynchronousPicture() async {
+    PhotoApi photoApi5 = PhotoApi();
+    photoApi5.downloadImages('https://alimipro.com/favicon.ico');
+    PhotoApi photoApi6 = PhotoApi();
+    photoApi6.downloadImages('https://alimipro.com/favicon.ico');
+    PhotoApi photoApi7 = PhotoApi();
+    photoApi7.downloadImages('https://alimipro.com/favicon.ico');
   }
 }
--- test/240329/photo_test.dart
+++ test/240329/photo_test.dart
@@ -6,7 +6,7 @@ import 'package:learn_dart_together/240329/data_source/photo_api.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('', () async {
+  test('다운 대상 파일과 다운받은 파일 내용 비교', () async {
     final Uint8List uint8list = await PhotoApi().downloadImage('https://alimipro.com/favicon.ico');
     final file = PhotoApi().saveFile(uint8list, 'icon.ico');
 
@@ -14,15 +14,27 @@ void main() {
     final saved = file1.readAsBytesSync();
     print(saved.runtimeType);
     print(uint8list.runtimeType);
-    expect(saved, uint8list);
+    expect(saved, uint8list); // 통과 안됨..
   });
-  test('', () async {
+  test('별개의 파일과 다운받은 파일 내용 비교', () async {
     final Uint8List uint8list = await PhotoApi().downloadImage(
         'https://www.google.com/search?q=%EA%B3%A0%EC%96%91%EC%9D%B4&sca_esv=6dba56f94bcc106c&sca_upv=1&sxsrf=ACQVn0_FckNX5LiSaJNPzhSD34XBDXDPeg:1711712326615&tbm=isch&source=iu&ictx=1&vet=1&fir=dL5FLpR6VH3wlM%252CwSQ6Ue94ijOfPM%252C%252Fm%252F01yrx%253BsDiEjeRwQrSTcM%252CFIVAl-Pt0-9lEM%252C_%253B1Y6bwWMHGsc2jM%252CFIVAl-Pt0-9lEM%252C_%253BiwSSoMC6gttaxM%252CSLs1ywtInfLqyM%252C_%253B2oT0RojRphoBCM%252ClrHZrbfz37smVM%252C_%253BB7_0OUJBGuaqIM%252CsAq9zDI5yKL2XM%252C_&usg=AI4_-kQvZBUmiR_n4RAkMAzfLZ-Guy4nsw&sa=X&ved=2ahUKEwjfyr2psZmFAxUulq8BHaCsBDIQ_B16BAg_EAE#imgrc=dL5FLpR6VH3wlM');
     final file = PhotoApi().saveFile(uint8list, 'icon.ico');
 
     File file1 = File('lib/240329/image/icon.ico');
     final saved = file1.readAsBytesSync();
-    expect(saved != uint8list, isTrue);
+    expect(saved != uint8list, isTrue); // 통과 안됨..
+  });
+  Stopwatch stopwatch = Stopwatch();
+  test('비동기/동기 파일 다운로드 소요시간 비교', () async {
+    stopwatch.start();
+    final synch = PhotoApi().synchronousPicture();
+    stopwatch.stop();
+    print('synchronous 소요시간 :  ${stopwatch.elapsed}');
+
+    stopwatch.start();
+    final async = PhotoApi().asynchronousPicture();
+    stopwatch.stop();
+    print('asynchronous 소요시간 :  ${stopwatch.elapsed}');
   });
 }
