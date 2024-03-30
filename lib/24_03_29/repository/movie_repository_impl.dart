154c3b39deef684bff07519799bd62a37d53184c
sseung <sseutta@naver.com>
2024-03-30 20:09:42 +0900

    Feat: Movie 클래스, Repository, dto 작성

---
+++ lib/24_03_29/repository/movie_repository_impl.dart
@@ -0,0 +1,25 @@
+import 'package:learn_dart_together/24_03_29/repository/movie_repository.dart';
+
+import '../data_source/movie_api.dart';
+import '../model/movie.dart';
+
+
+
+class MovieRepositoryImpl implements MovieRepository{
+final MovieApi api;
+
+    MovieRepositoryImpl(this.api);
+Future<Movie> getMovieDetail(int id) async {
+         Movie movie = await api.getMovieDetail(id);
+         return movie;
+
+
+  }
+
+}
+
+void main()async{
+  MovieRepositoryImpl impl = MovieRepositoryImpl(MovieApi());
+  print(await impl.getMovieDetail(1011985));
+
+}
