d64c96c63f03d6becf07174fcd6a25bbc4cbe5d4
gyubit <gyubros@gmail.com>
2024-04-01 00:01:21 +0900

    UPDATE: PRACTICE 1, 2_ json형태로 출력하기 (실패)

--- lib/240329/main.dart
+++ lib/240329/main.dart
@@ -4,11 +4,16 @@ import 'package:learn_dart_together/240329/repository/movie_repository.dart';
 void main() async {
   MovieRepository repository = MovieRepositoryImpl();
 
-  // // PRACTICE1.
-  // final movieList = await repository.getMovieInfoList();
-  // print(movieList);
+  // PRACTICE1.
+  // 데이터 출력.
+  final movieList = await repository.getMovieInfoList();
+  // Json화. (실패)
+  var movieListDto = movieList.map((e) => e.MovieDto.toJson());
+
+  print(movieList);
 
   // PRACTICE2.
-  final movieDetail = await repository.getMovieDetail(77816);
-  print(movieDetail);
+  // 데이터 출력.
+  // final movieDetail = await repository.getMovieDetail(77816);
+  // print(movieDetail);
 }
