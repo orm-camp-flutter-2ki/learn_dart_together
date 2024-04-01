0c166077b6595f17c7a1c9c006ef057068fc3780
David Kim <mycodingtool9@gmail.com>
2024-04-02 00:05:33 +0900

    240401 과제 2 - 이미지 검색 기능 작성 feat : 보안이슈로 apikey 비공개 적용

--- lib/24_04_01/data_source/photo_api.dart
+++ lib/24_04_01/data_source/photo_api.dart
@@ -7,14 +7,14 @@ import 'package:learn_dart_together/24_04_01/repository/photo_repository.dart';
 class PhotoApi {
   final http.Client _client;
   final _baseUrl = 'https://pixabay.com/api/?key=';
-  final _apiKey = '43171373-5c54586eb6b3236716e54d34c';
+  final _apiKey = ''; // 보안을 위해 키를 비워두었습니다.
   final _type = '&image_type=photo&pretty=true';
 
   PhotoApi({http.Client? client}) : _client = client ?? http.Client();
 
   Future<PhotoDto> getPhotos(String query) async {
-    final http.Response response = await _client.get(
-        Uri.parse('$_baseUrl$_apiKey&q={$query}$_type'));
+    final http.Response response =
+        await _client.get(Uri.parse('$_baseUrl$_apiKey&q={$query}$_type'));
 
     if (response.statusCode != 200) {
       throw NetworkError.unknown;
