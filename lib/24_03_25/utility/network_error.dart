11a62381ccd42c5dcde14414336d955b27ee6d60
JEON-Sungsu <anagma@naver.com>
2024-04-01 17:57:01 +0900

    refactor: 네트워크 에러 이넘타입 수정

--- lib/24_03_25/utility/network_error.dart
+++ lib/24_03_25/utility/network_error.dart
@@ -3,20 +3,23 @@ enum NetworkingError {
   taskingError,
   badClientRequest,
   badServerResponse,
+  invalidString,
   corruptedData;
 
   String get message {
     switch (this) {
       case NetworkingError.unknown:
-        return "알 수 없는 에러입니다.";
+        return '알 수 없는 에러입니다.';
       case NetworkingError.taskingError:
-        return "DataTask 작업 중 에러가 발생했습니다.";
+        return 'DataTask 작업 중 에러가 발생했습니다.';
       case NetworkingError.badClientRequest:
-        return "클라이언트 측 에러입니다.";
+        return '클라이언트 측 에러입니다.';
       case NetworkingError.badServerResponse:
-        return "서버 측 에러입니다.";
+        return '서버 측 에러입니다.';
+      case NetworkingError.invalidString:
+        return '비속어를 사용할 수 없습니다.';
       case NetworkingError.corruptedData:
-        return "손상된 데이터입니다.";
+        return '손상된 데이터입니다.';
     }
   }
 }
