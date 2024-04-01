8b8f34253f57375e30667effec77f62a5cdd84ad
hoogom88 <ziziwhdgus@naver.com>
2024-04-01 16:01:27 +0900

    Create. PhotoDto 클래스 생성

---
+++ lib/24_04_01/dto/photo_dto.dart
@@ -0,0 +1,138 @@
+class PhotoDto {
+  PhotoDto({
+    this.total,
+    this.totalHits,
+    this.hits,
+  });
+
+  PhotoDto.fromJson(dynamic json) {
+    total = json['total'];
+    totalHits = json['totalHits'];
+    if (json['hits'] != null) {
+      hits = [];
+      json['hits'].forEach((v) {
+        hits?.add(HitDto.fromJson(v));
+      });
+    }
+  }
+
+  num? total;
+  num? totalHits;
+  List<HitDto>? hits;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['total'] = total;
+    map['totalHits'] = totalHits;
+    if (hits != null) {
+      map['hits'] = hits?.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+}
+
+class HitDto {
+  HitDto({
+    this.id,
+    this.pageURL,
+    this.type,
+    this.tags,
+    this.previewURL,
+    this.previewWidth,
+    this.previewHeight,
+    this.webformatURL,
+    this.webformatWidth,
+    this.webformatHeight,
+    this.largeImageURL,
+    this.fullHDURL,
+    this.imageURL,
+    this.imageWidth,
+    this.imageHeight,
+    this.imageSize,
+    this.views,
+    this.downloads,
+    this.likes,
+    this.comments,
+    this.userId,
+    this.user,
+    this.userImageURL,
+  });
+
+  HitDto.fromJson(dynamic json) {
+    id = json['id'];
+    pageURL = json['pageURL'];
+    type = json['type'];
+    tags = json['tags'];
+    previewURL = json['previewURL'];
+    previewWidth = json['previewWidth'];
+    previewHeight = json['previewHeight'];
+    webformatURL = json['webformatURL'];
+    webformatWidth = json['webformatWidth'];
+    webformatHeight = json['webformatHeight'];
+    largeImageURL = json['largeImageURL'];
+    fullHDURL = json['fullHDURL'];
+    imageURL = json['imageURL'];
+    imageWidth = json['imageWidth'];
+    imageHeight = json['imageHeight'];
+    imageSize = json['imageSize'];
+    views = json['views'];
+    downloads = json['downloads'];
+    likes = json['likes'];
+    comments = json['comments'];
+    userId = json['user_id'];
+    user = json['user'];
+    userImageURL = json['userImageURL'];
+  }
+
+  num? id;
+  String? pageURL;
+  String? type;
+  String? tags;
+  String? previewURL;
+  num? previewWidth;
+  num? previewHeight;
+  String? webformatURL;
+  num? webformatWidth;
+  num? webformatHeight;
+  String? largeImageURL;
+  String? fullHDURL;
+  String? imageURL;
+  num? imageWidth;
+  num? imageHeight;
+  num? imageSize;
+  num? views;
+  num? downloads;
+  num? likes;
+  num? comments;
+  num? userId;
+  String? user;
+  String? userImageURL;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['pageURL'] = pageURL;
+    map['type'] = type;
+    map['tags'] = tags;
+    map['previewURL'] = previewURL;
+    map['previewWidth'] = previewWidth;
+    map['previewHeight'] = previewHeight;
+    map['webformatURL'] = webformatURL;
+    map['webformatWidth'] = webformatWidth;
+    map['webformatHeight'] = webformatHeight;
+    map['largeImageURL'] = largeImageURL;
+    map['fullHDURL'] = fullHDURL;
+    map['imageURL'] = imageURL;
+    map['imageWidth'] = imageWidth;
+    map['imageHeight'] = imageHeight;
+    map['imageSize'] = imageSize;
+    map['views'] = views;
+    map['downloads'] = downloads;
+    map['likes'] = likes;
+    map['comments'] = comments;
+    map['user_id'] = userId;
+    map['user'] = user;
+    map['userImageURL'] = userImageURL;
+    return map;
+  }
+}
