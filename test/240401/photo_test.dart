0c0e530cd49e522b40a026f94325ab8a87c54353
algochemy <algochemy@gmail.com>
2024-04-01 17:46:40 +0900

    feat:Result 패턴 적용, 테스트코드 작성

--- test/240401/photo_test.dart
+++ test/240401/photo_test.dart
@@ -6,19 +6,18 @@ import 'package:learn_dart_together/240401/photo/data_source/photo_data_source.d
 
 void main() {
   test('Test PhotoRepositoryImpl getPhotos method', () async {
-    // Initialize dependencies
+    // Given
     final PhotoDataSource photoDataSource = PhotoDataSource();
     final PhotoRepositoryImpl photoRepository = PhotoRepositoryImpl(photoDataSource);
 
-    // Set up test query
     final String queryInput1 = '바보';
     final String queryInput2 = '천재';
 
-    // Call the method under test
+    // When
     final Result<List<Photo>> result1 = await photoRepository.getPhotos(queryInput1);
     final Result<List<Photo>> result2 = await photoRepository.getPhotos(queryInput2);
 
-    // Check the result
+    // Then
     expect(result1.maybeWhen(argumentError: (e) => e, orElse: () => null), isA<Exception>());
     expect(result2.maybeWhen(success: (data) => data, orElse: () => null), isA<List<Photo>>());
   });
