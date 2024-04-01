72ade10917f3059f8239af485ed92420b615f9bf
KimDonghyeok <qoophon@gmail.com>
2024-04-01 18:55:45 +0900

    fix: Result 수정 (연습문제 2)
    - Result -> PhotoResult 로 이름 변경

--- lib/240401/repository/photo_repository.dart
+++ lib/240401/repository/photo_repository.dart
@@ -1,6 +1,6 @@
 import 'package:dart_cli_practice/240401/model/photo.dart';
-import 'package:dart_cli_practice/240401/repository/result.dart';
+import 'package:dart_cli_practice/240401/repository/photo_result.dart';
 
 abstract interface class PhotoRepository {
-  Future<Result<List<Photo>>> findPhotosByKeyword(String keyword);
+  Future<PhotoResult<List<Photo>>> findPhotosByKeyword(String keyword);
 }
--- lib/240401/repository/photo_repository_impl.dart
+++ lib/240401/repository/photo_repository_impl.dart
@@ -3,7 +3,7 @@ import 'package:dart_cli_practice/240401/data_source/photo_api_impl.dart';
 import 'package:dart_cli_practice/240401/mapper/photo_mapper.dart';
 import 'package:dart_cli_practice/240401/model/photo.dart';
 import 'package:dart_cli_practice/240401/repository/photo_repository.dart';
-import 'package:dart_cli_practice/240401/repository/result.dart';
+import 'package:dart_cli_practice/240401/repository/photo_result.dart';
 
 class PhotoRepositoryImpl implements PhotoRepository {
   final PhotoApi _api;
@@ -11,19 +11,19 @@ class PhotoRepositoryImpl implements PhotoRepository {
   PhotoRepositoryImpl({PhotoApi? api}) : _api = api ?? PhotoApiImpl();
 
   @override
-  Future<Result<List<Photo>>> findPhotosByKeyword(String keyword) async {
+  Future<PhotoResult<List<Photo>>> findPhotosByKeyword(String keyword) async {
     final List<Photo> photos;
     try {
       if (keyword == '바보') {
-        return Result.error('비속어를 사용할 수 없습니다');
+        return PhotoResult.error('비속어를 사용할 수 없습니다');
       }
 
       final photoDtoList = await _api.getPhotos(keyword);
       photos = photoDtoList.map((e) => e.toPhoto()).toList();
 
-      return Result.success(photos);
+      return PhotoResult.success(photos);
     } catch (e) {
-      return Result.error('알 수 없는 네트워크 에러');
+      return PhotoResult.error('알 수 없는 네트워크 에러');
     }
   }
 }
