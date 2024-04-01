025f2b8890785c641fe169ae7c1bf3f4f02a6d19
JEON-Sungsu <anagma@naver.com>
2024-04-01 17:57:37 +0900

    feat: Result 타입 구현

---
+++ lib/24_04_01/result/result.dart
@@ -0,0 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'result.freezed.dart';
+
+@freezed
+sealed class Result<D, E> with _$Result<D, E> {
+  const factory Result.success(D data) = Success;
+  const factory Result.error(E networkError) = Error;
+}
