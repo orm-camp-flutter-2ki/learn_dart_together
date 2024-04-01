e275361b8b4275ff68d13038a27817f89adadf09
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-04-01 12:00:38 +0900

    feat: 네트워크 통신 과제 5 리뷰

--- lib/240401/exam_03/download.dart
+++ lib/240401/exam_03/download.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 
-Future<Uint8List> downloadImage(String url) async {
+Future<Uint8List> download(String url) async {
   final response = await http.get(Uri.parse(url));
   return response.bodyBytes;
 }
@@ -11,16 +11,58 @@ Future<File> saveFile(Uint8List bytes, String fileName) async {
   return await File(fileName).writeAsBytes(bytes);
 }
 
-void main() async {
-  final stopWatch = Stopwatch()..start();
+Future<File> downloadImage(String url, String fileName) async {
+  return await saveFile(await download(url), fileName);
+}
 
+Future<void> checkTime(Stopwatch stopWatch, Future<File> func) async {
+  stopWatch.start();
   print('다운로드 시작');
-  final Uint8List bytes =
-      await downloadImage('https://alimipro.com/favicon.ico');
-  final File file = await saveFile(bytes, 'icon.ico');
+  File file = await func;
   stopWatch.stop();
   print('다운로드 끝');
   print('=========');
   print('소요시간 : ${stopWatch.elapsed.toString()}');
   print('용량 : ${await file.length()}');
+  stopWatch.reset();
+}
+
+void main() async {
+  // final fileNames = [
+  //   'icon1.ico',
+  //   'icon2.ico',
+  //   'icon3.ico',
+  // ];
+  //
+  // final stopWatch = Stopwatch();
+  //
+  // for (final fileName in fileNames) {
+  //   await checkTime(
+  //     stopWatch,
+  //     downloadImage('https://alimipro.com/favicon.ico', fileName),
+  //   );
+  // }
+
+  final fileNames = [
+    'icon1.ico',
+    'icon2.ico',
+    'icon3.ico',
+  ];
+
+  final futures = [
+    downloadImage('https://alimipro.com/favicon.ico', 'icon1.ico'),
+    downloadImage('https://alimipro.com/favicon.ico', 'icon2.ico'),
+    downloadImage('https://alimipro.com/favicon.ico', 'icon3.ico'),
+  ];
+
+  final stopWatch = Stopwatch();
+
+  stopWatch.start();
+  print('다운로드 시작');
+  await Future.wait(futures);
+  stopWatch.stop();
+  print('다운로드 끝');
+  print('=========');
+  print('소요시간 : ${stopWatch.elapsed.toString()}');
+  stopWatch.reset();
 }
