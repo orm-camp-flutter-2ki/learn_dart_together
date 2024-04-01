2f952b761f4debccf42127266651d6b2fb0b1086
byjun8932 <byjun8932@gmail.com>
2024-04-02 08:49:09 +0900

    feat: task2 (Using pixabay Api)

---
+++ lib/24_04_01/repository/photo_repository.dart
@@ -0,0 +1,7 @@
+import 'package:learn_dart_together/24_04_01/repository/result.dart';
+
+import '../model/photo.dart';
+
+abstract interface class PhotoRepository {
+  Future<Result<List<Photo>, Exception>> getPhotos(String q);
+}
