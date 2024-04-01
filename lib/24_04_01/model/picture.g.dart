5ec0c4293b5a03dda8cca7480a449261be533d2f
JEON-Sungsu <anagma@naver.com>
2024-04-01 17:57:57 +0900

    feat: Picture 모델 생성

---
+++ lib/24_04_01/model/picture.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'picture.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$PictureImpl _$$PictureImplFromJson(Map<String, dynamic> json) =>
+    _$PictureImpl(
+      tags: json['tags'] as String,
+      imageUrl: json['imageUrl'] as String,
+    );
+
+Map<String, dynamic> _$$PictureImplToJson(_$PictureImpl instance) =>
+    <String, dynamic>{
+      'tags': instance.tags,
+      'imageUrl': instance.imageUrl,
+    };
