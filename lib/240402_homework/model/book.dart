c7eb640d44137fc6bc09adc88c1c5004c02293df
Yejin.Shin <mingmi_27@naver.com>
2024-04-02 22:39:46 +0900

    feat: book 모델 작성

---
+++ lib/240402_homework/model/book.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'book.freezed.dart';
+
+part 'book.g.dart';
+
+@freezed
+class Book with _$Book {
+  const factory Book({
+    required int id,
+    required String title,
+    required DateTime publishDate,
+    required bool isAbleLoan,
+    required DateTime loanDate,
+    required DateTime returnDate,
+  }) = _Book;
+  
+  factory Book.fromJson(Map<String, Object?> json) => _$BookFromJson(json); 
+}
